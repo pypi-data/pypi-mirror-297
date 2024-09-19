4229142465730e8cddc64f2bce941a3c999d012c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:03:12 +0000

    Add package interpretablefa/0.2.2/interpretablefa-0.2.2.tar.gz

---
+++ packages/interpretablefa/interpretablefa-0.2.2.tar.gz/interpretablefa-0.2.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from os import path
+working_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(working_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="interpretablefa",
+    version="0.2.2",
+    author="Justin Philip Tuazon, Gia Mizrane Abubo",
+    description="A package for interpretable factor analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "scipy",
+        "tensorflow_hub",
+        "nlopt",
+        "factor_analyzer",
+        "seaborn",
+        "matplotlib"
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering"
+    ]
+)
