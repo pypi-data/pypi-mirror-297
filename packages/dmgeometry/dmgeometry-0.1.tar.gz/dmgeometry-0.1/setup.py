98d61c811fee5ed0511fec59fa9ec340ec10b8e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 16:03:36 +0000

    Add package dmgeometry/0.1/dmgeometry-0.1.tar.gz

---
+++ packages/dmgeometry/dmgeometry-0.1.tar.gz/dmgeometry-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dmgeometry',
+    version='0.1',
+    description='A simple Python library for calculating the area of geometric shapes.',
+    author='murali',
+    packages=find_packages(),
+)
