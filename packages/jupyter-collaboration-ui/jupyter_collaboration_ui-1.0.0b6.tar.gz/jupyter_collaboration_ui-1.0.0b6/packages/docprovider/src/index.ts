81d79784d06b7c8f38302b8dcb9a948f7e1cf63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:06:17 +0000

    Add package jupyter-collaboration-ui/1.0.0b6/jupyter_collaboration_ui-1.0.0b6.tar.gz

---
+++ packages/jupyter-collaboration-ui/jupyter_collaboration_ui-1.0.0b6.tar.gz/jupyter_collaboration_ui-1.0.0b6/packages/docprovider/src/index.ts
@@ -0,0 +1,16 @@
+/* -----------------------------------------------------------------------------
+| Copyright (c) Jupyter Development Team.
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
+/**
+ * @packageDocumentation
+ * @module docprovider
+ */
+
+export * from './awareness';
+export * from './notebookCellExecutor';
+export * from './requests';
+export * from './ydrive';
+export * from './yprovider';
+export * from './tokens';
+export * from './TimelineSlider';
