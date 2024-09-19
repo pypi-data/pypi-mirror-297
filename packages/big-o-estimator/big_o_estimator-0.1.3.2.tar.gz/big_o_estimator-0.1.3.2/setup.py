23e36eaaa63c84e6a86283c37d5df9a19583bdd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:05:56 +0000

    Add package big-o-estimator/0.1.3.2/big_o_estimator-0.1.3.2.tar.gz

---
+++ packages/big-o-estimator/big_o_estimator-0.1.3.2.tar.gz/big_o_estimator-0.1.3.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="big_o_estimator",  # Name of your package
+    version="0.1.3.2",  # Initial release version
+    description="A package to estimate Big-O complexity of Python functions.",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",  # If your README is in Markdown
+    author="Arghya ",
+    author_email="arghyazzz@gmail.com",
+    url="https://github.com/amukherjee1991/big_o_estimator_package",  # GitHub repo or project homepage
+    packages=find_packages(),  # Automatically find and include the package
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',  # Python version requirement
+    include_package_data=True,  # Include additional files (like README.md)
+    license="MIT",  # License for your package
+)
