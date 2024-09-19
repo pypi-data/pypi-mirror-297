d68535bf7e8c50a9c951452149bf149fb5ec450f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 16:03:29 +0000

    Add package punchboot/2.3.0/punchboot-2.3.0-cp313-cp313-macosx_12_0_x86_64.whl

---
+++ packages/punchboot/punchboot-2.3.0-cp313-cp313-macosx_12_0_x86_64.whl/punchboot/__init__.py
@@ -0,0 +1,60 @@
+"""Punchboot."""
+
+from _punchboot import (  # type: ignore[import-not-found] # noqa: F401
+    ArgumentError,
+    AuthenticationError,
+    CommandError,
+    Error,
+    GenericError,
+    IOError,
+    KeyRevokedError,
+    MemError,
+    NoMemoryError,
+    NotAuthenticatedError,
+    NotFoundError,
+    NotSupportedError,
+    PartNotBootableError,
+    PartVerifyError,
+    SignatureError,
+    StreamNotInitializedError,
+    TimeoutError,
+    TransferError,
+)
+
+from .helpers import library_version, list_usb_devices, pb_id, wait_for_device
+from .partition import Partition, PartitionFlags
+from .session import Session
+from .slc import SLC
+
+_pb_exceptions = [
+    "Error",
+    "GenericError",
+    "AuthenticationError",
+    "NotAuthenticatedError",
+    "NotSupportedError",
+    "ArgumentError",
+    "CommandError",
+    "PartVerifyError",
+    "PartNotBootableError",
+    "NoMemoryError",
+    "TransferError",
+    "NotFoundError",
+    "StreamNotInitializedError",
+    "TimeoutError",
+    "KeyRevokedError",
+    "SignatureError",
+    "MemError",
+    "IOError",
+]
+
+__all__ = [
+    "Session",
+    "Partition",
+    "PartitionFlags",
+    "SLC",
+    "library_version",
+    "pb_id",
+    "wait_for_device",
+    "list_usb_devices",
+]
+__all__ += _pb_exceptions
