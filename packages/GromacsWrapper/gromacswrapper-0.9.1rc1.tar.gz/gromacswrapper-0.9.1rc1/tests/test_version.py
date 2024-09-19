b7c08dec913b5c983432b315c7fed543fff7e9ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:03:34 +0000

    Add package GromacsWrapper/0.9.1rc1/gromacswrapper-0.9.1rc1.tar.gz

---
+++ packages/GromacsWrapper/gromacswrapper-0.9.1rc1.tar.gz/gromacswrapper-0.9.1rc1/tests/test_version.py
@@ -0,0 +1,20 @@
+# GromacsWrapper: test_example.py
+# Copyright (c) 2009 Oliver Beckstein <orbeckst@gmail.com>
+# Released under the GNU Public License 3 (or higher, your choice)
+# See the file COPYING for details.
+
+import re
+import pytest
+
+import gromacs
+
+
+def test_version():
+    release = gromacs.__version__
+    assert isinstance(release, str)
+
+
+def test_version_pep440_like():
+    release = gromacs.__version__
+    match = re.match(r"\d+\.\d+\.\d+", release)
+    assert match, f"Version {release} does not look like MAJOR.MINOR.PATCH..."
