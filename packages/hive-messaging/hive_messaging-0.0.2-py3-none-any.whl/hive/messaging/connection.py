dffb0490af4e1709bcaf018774f442d6e181d23a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:03:32 +0000

    Add package hive-messaging/0.0.2/hive_messaging-0.0.2-py3-none-any.whl

---
+++ packages/hive-messaging/hive_messaging-0.0.2-py3-none-any.whl/hive/messaging/connection.py
@@ -0,0 +1,23 @@
+from .channel import Channel
+from .wrapper import WrappedPikaThing
+
+
+class Connection(WrappedPikaThing):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exc_info):
+        self._pika.close()
+
+    def channel(self, *args, **kwargs):
+        """Like :class:pika.channel.Channel` but with different defaults.
+
+        :param confirm_delivery: Whether to enable delivery confirmations.
+            Hive's default is True.  Use `confirm_delivery=False` for the
+            original Pika behaviour.
+        """
+        confirm_delivery = kwargs.pop("confirm_delivery", True)
+        channel = Channel(self._pika.channel(*args, **kwargs))
+        if confirm_delivery:
+            channel.confirm_delivery()  # Don't fail silently.
+        return channel
