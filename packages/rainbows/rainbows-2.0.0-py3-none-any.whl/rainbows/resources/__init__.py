c016dc1e4394942acc625d3198395296e7077eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:04:38 +0000

    Add package rainbows/2.0.0/rainbows-2.0.0-py3-none-any.whl

---
+++ packages/rainbows/rainbows-2.0.0-py3-none-any.whl/rainbows/resources/__init__.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .jobow import (
+    JobowResource,
+    AsyncJobowResource,
+    JobowResourceWithRawResponse,
+    AsyncJobowResourceWithRawResponse,
+    JobowResourceWithStreamingResponse,
+    AsyncJobowResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "JobowResource",
+    "AsyncJobowResource",
+    "JobowResourceWithRawResponse",
+    "AsyncJobowResourceWithRawResponse",
+    "JobowResourceWithStreamingResponse",
+    "AsyncJobowResourceWithStreamingResponse",
+]
