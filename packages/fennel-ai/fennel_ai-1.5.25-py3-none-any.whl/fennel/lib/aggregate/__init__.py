4dbf020e3eb1c41ce13a98a4d956d57c87f3d46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:03:51 +0000

    Add package fennel-ai/1.5.25/fennel_ai-1.5.25-py3-none-any.whl

---
+++ packages/fennel-ai/fennel_ai-1.5.25-py3-none-any.whl/fennel/lib/aggregate/__init__.py
@@ -0,0 +1,14 @@
+# Soon to be deprecated, keep just for backward compatibility
+from fennel.datasets.aggregate import (
+    Count,
+    Distinct,
+    Sum,
+    LastK,
+    Min,
+    Max,
+    Average,
+    AggregateType,
+    Stddev,
+    Quantile,
+    ExpDecaySum,
+)
