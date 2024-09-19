f680855accdd567f352244e95ba621fdcaa102cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:04:29 +0000

    Add package django-username-email/2.5.6/django_username_email-2.5.6.tar.gz

---
+++ packages/django-username-email/django_username_email-2.5.6.tar.gz/django_username_email-2.5.6/setup.py
@@ -0,0 +1,60 @@
+from os import path
+from setuptools import setup, find_namespace_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name='django-username-email',
+
+    version='2.5.6',
+
+    description='Custom Django User model that makes email the USERNAME_FIELD.',
+    long_description=long_description,
+
+    url='https://github.com/tmm/django-username-email/',
+
+    author='Adam Taylor',
+    author_email='ataylor32@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+        'Operating System :: OS Independent',
+    ],
+    keywords='user email username',
+
+    packages=find_namespace_packages(),
+    include_package_data=True,
+
+    install_requires=[
+        'django>=3.2',
+    ]
+)
