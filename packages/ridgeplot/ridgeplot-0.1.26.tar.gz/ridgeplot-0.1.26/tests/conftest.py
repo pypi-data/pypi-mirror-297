52efcbca98ab1e9035d5b8e28e53bb90e48024f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:03:31 +0000

    Add package ridgeplot/0.1.26/ridgeplot-0.1.26.tar.gz

---
+++ packages/ridgeplot/ridgeplot-0.1.26.tar.gz/ridgeplot-0.1.26/tests/conftest.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from typing import Generator
+
+import pytest
+
+from cicd.test_helpers import patch_plotly_show
+
+
+@pytest.fixture(autouse=True, scope="session")
+def _patch_plotly_show() -> Generator[None, None, None]:
+    with patch_plotly_show():
+        yield
