09c79909399e880d5de9a32fa8c265abc9411198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:04:55 +0000

    Add package iris-idealai/2.1/iris_idealai-2.1.tar.gz

---
+++ packages/iris-idealai/iris_idealai-2.1.tar.gz/iris_idealai-2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iris_idealai",
+    version="2.1",
+    packages=find_packages(),
+    install_requires=[
+        "google-generativeai",
+        "pillow", 
+        "requests", 
+    ],
+    description="A module to interact with Iris developed by Ideal AI",
+    author="Ideal AI",
+    author_email="your-email@example.com",
+    url="https://idealai.netlify.app",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
