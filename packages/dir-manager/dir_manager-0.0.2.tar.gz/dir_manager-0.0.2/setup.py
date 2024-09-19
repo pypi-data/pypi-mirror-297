afb4e068832bae811ae3a0e9f6b2888b5556a274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:03:35 +0000

    Add package dir-manager/0.0.2/dir_manager-0.0.2.tar.gz

---
+++ packages/dir-manager/dir_manager-0.0.2.tar.gz/dir_manager-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dir-manager",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[],
+    description="simple-dir-manager",
+    author="Song Seung Hwan",
+    author_email="shdth117@gmail.com",
+    url="https://github.com/alkaline2018/dir-manager",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
