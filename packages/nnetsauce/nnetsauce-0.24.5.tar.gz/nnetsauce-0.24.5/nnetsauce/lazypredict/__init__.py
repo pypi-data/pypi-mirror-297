8651399717d7d328428323a82f93917c72102f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:03:47 +0000

    Add package nnetsauce/0.24.5/nnetsauce-0.24.5.tar.gz

---
+++ packages/nnetsauce/nnetsauce-0.24.5.tar.gz/nnetsauce-0.24.5/nnetsauce/lazypredict/__init__.py
@@ -0,0 +1,13 @@
+from .lazyClassifier import LazyClassifier
+from .lazyRegressor import LazyRegressor
+from .lazydeepClassifier import LazyDeepClassifier
+from .lazydeepRegressor import LazyDeepRegressor
+from .lazydeepMTS import LazyDeepMTS
+
+__all__ = [
+    "LazyClassifier",
+    "LazyRegressor",
+    "LazyDeepClassifier",
+    "LazyDeepRegressor",
+    "LazyDeepMTS",
+]
