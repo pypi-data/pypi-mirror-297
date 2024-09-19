ff9617c9bdfac3e6a4e5e41ac7d9e5f4c42fee6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:03:14 +0000

    Add package kbrainsdk/0.17.3/kbrainsdk-0.17.3.tar.gz

---
+++ packages/kbrainsdk/kbrainsdk-0.17.3.tar.gz/kbrainsdk-0.17.3/kbrainsdk/ingest.py
@@ -0,0 +1,46 @@
+from uuid import uuid4 as uuid_generator
+from kbrainsdk.validation.ingest import validate_ingest_status, validate_ingest_focused_chat
+from kbrainsdk.apibase import APIBase
+
+class Ingest(APIBase):
+
+    def __init__(self, *args, **kwds):
+        return super().__init__(*args, **kwds)
+
+
+    def get_status(self, focused_chat_id):
+        payload = {
+            "focused_chat_id": focused_chat_id
+        }
+
+        validate_ingest_status(payload)
+
+        path = f"/ingest/status/v2"
+        response = self.apiobject.call_endpoint(path, payload, "post")
+        return response
+    
+    def ingest_focused_chat(
+            self, 
+            focused_chat_id:str,
+            assertion_token:str|None=None,         
+            client_secret:str|None=None,         
+            client_id:str|None=None, 
+            tenant_id:str|None=None, 
+        ) -> str:
+
+        operation_id = str(uuid_generator())
+
+        payload = {
+            "focused_chat_id": focused_chat_id,
+            "operation_id": operation_id,
+            "assertion_token": assertion_token,
+            "client_secret": client_secret,
+            "client_id": client_id,
+            "tenant_id": tenant_id
+        }
+
+        validate_ingest_focused_chat(payload)
+
+        path = f"/ingest/focused-chat/v1"
+        response = self.apiobject.call_endpoint(path, payload, "post")
+        return response, operation_id
