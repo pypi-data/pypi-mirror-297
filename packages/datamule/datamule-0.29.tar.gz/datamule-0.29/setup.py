4b5316732408fcd8facbca0edd9135a3e843df14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:05:41 +0000

    Add package datamule/0.29/datamule-0.29.tar.gz

---
+++ packages/datamule/datamule-0.29.tar.gz/datamule-0.29/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+long_description = Path("../readme.md").read_text()
+
+setup(
+    name="datamule",
+    author="John Friedman",
+    version="0.29",
+    description="Making it easier to use SEC filings.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    url="https://github.com/john-friedman/datamule-python",
+    install_requires=[
+        'aiohttp',
+        'aiolimiter',
+        'tqdm',
+        'requests',
+        'nest_asyncio'
+    ],
+    extras_require={
+        "filing_viewer": ["lxml"
+        ],
+    },
+       package_data={
+        "datamule": ["data/company_ticker.csv"],
+    },
+    include_package_data=True,
+)
