789fee1ae8850495bae15767f6477876f4e5ef63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:03:38 +0000

    Add package apropos-ai/0.3.15/apropos_ai-0.3.15-py3-none-any.whl

---
+++ packages/apropos-ai/apropos_ai-0.3.15-py3-none-any.whl/apropos/__init__.py
@@ -0,0 +1,5 @@
+from apropos.src.core.lms.helpers import LLM
+from apropos.src.core.programs.dag import LM_DAG
+from apropos.src.core.lms.cost import CostMonitor
+
+__all__ = ["LLM", "LM_DAG", "CostMonitor"]
