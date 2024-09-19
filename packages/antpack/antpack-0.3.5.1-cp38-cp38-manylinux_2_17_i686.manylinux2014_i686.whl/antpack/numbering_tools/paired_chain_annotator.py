01beea1b8f68133eb22fb4eca40ac79fb76718a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:03:48 +0000

    Add package antpack/0.3.5.1/antpack-0.3.5.1-cp38-cp38-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/antpack/antpack-0.3.5.1-cp38-cp38-manylinux_2_17_i686.manylinux2014_i686.whl/antpack/numbering_tools/paired_chain_annotator.py
@@ -0,0 +1,24 @@
+"""Contains the PairedChainAnnotator class, which extracts variable region
+heavy and light chains from an input sequence that is presumed to contain
+a single variable heavy and a single variable light chain."""
+import os
+from antpack.antpack_cpp_ext import PairedChainAnnotatorCpp
+
+
+
+class PairedChainAnnotator(PairedChainAnnotatorCpp):
+
+    def __init__(self, scheme = "imgt"):
+        """Class constructor.
+
+        Args:
+            scheme (str): The numbering scheme. Must be one of "imgt",
+                "martin", "kabat", "aho".
+
+        Raises:
+            RuntimeError: A RuntimeError is raised if unacceptable inputs are
+                supplied.
+        """
+        consensus_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                "consensus_data")
+        super().__init__(scheme, consensus_path)
