8844b2b61ae8e159c3fabc6ca24f216b2ceeb0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:03:39 +0000

    Add package pbatoolkit/0.0.7/pbatoolkit-0.0.7-cp311-cp311-macosx_14_0_arm64.whl

---
+++ packages/pbatoolkit/pbatoolkit-0.0.7-cp311-cp311-macosx_14_0_arm64.whl/pbatoolkit/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/runner/work/PhysicsBasedAnimationToolkit/PhysicsBasedAnimationToolkit/bindings
+# Build directory: /Users/runner/work/PhysicsBasedAnimationToolkit/PhysicsBasedAnimationToolkit/build/cp311-cp311-macosx_14_0_arm64/bindings
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("pypbat")
