4e6b2a256ce2c74965cdc7eba3a7fe7270dcfed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:03:26 +0000

    Add package deeporigin/2.1.3/deeporigin-2.1.3.tar.gz

---
+++ packages/deeporigin/deeporigin-2.1.3.tar.gz/deeporigin-2.1.3/src/config/__init__.py
@@ -0,0 +1,112 @@
+import functools
+import os
+import pathlib
+from typing import Any, Optional
+
+import confuse
+
+from ..exceptions import DeepOriginException
+
+CONFIG_DIR = pathlib.Path(__file__).parent
+DEFAULT_CONFIG_FILENAME = os.path.join(CONFIG_DIR, "default.yml")
+CONFIG_YML_LOCATION = os.path.expanduser(
+    os.path.join(
+        "~",
+        ".deeporigin",
+        "config.yml",
+    )
+)
+
+__all__ = ["get_value"]
+
+
+# validate configuration
+TEMPLATE = {
+    "organization_id": confuse.String(),
+    "bench_id": confuse.Optional(confuse.String()),
+    "env": confuse.String(),
+    "api_endpoint": confuse.Optional(confuse.String()),
+    "nucleus_api_route": confuse.String(),
+    "graphql_api_route": confuse.String(),
+    "auth_domain": confuse.String(),
+    "auth_device_code_endpoint": confuse.String(),
+    "auth_token_endpoint": confuse.String(),
+    "auth_audience": confuse.String(),
+    "auth_grant_type": confuse.String(),
+    "auth_client_id": confuse.String(),
+    "auth_client_secret": confuse.String(),
+    "list_workstation_variables_query_template": confuse.String(),
+    "api_tokens_filename": confuse.Filename(),
+    "variables_cache_filename": confuse.Filename(),
+    "auto_install_variables_filename": confuse.Filename(),
+    "feature_flags": confuse.Optional(
+        confuse.MappingTemplate(
+            {
+                "variables": confuse.TypeTemplate(bool),
+            }
+        )
+    ),
+}
+
+
+@functools.cache
+def get_value(
+    config_file_location: Optional[str] = None, override_values: tuple = None
+) -> confuse.templates.AttrDict:
+    """Get the configuration for the Deep Origin CLI
+
+    Args:
+        user_config_filename: path to the user's configuration file
+        override_values: values to use to override the default value
+
+    Returns:
+        :obj:`confuse.templates.AttrDict`: configuration for the Deep Origin CLI
+    """
+
+    # if we're running on AWS lambda, read config values
+    # from env and return those.
+    if os.environ.get("AWS_LAMBDA_FUNCTION_NAME"):
+        return confuse.AttrDict(
+            dict(
+                organization_id=os.environ.get("DEEP_ORIGIN_ORGANIZATION_ID"),
+                api_endpoint=os.environ.get("DEEP_ORIGIN_API_ENDPOINT"),
+                nucleus_api_route=os.environ.get("DEEP_ORIGIN_NUCLEUS_API_ROUTE"),
+            )
+        )
+
+    # read the default configuration
+    value = confuse.Configuration("deep_origin", __name__)
+
+    # read the default configuration
+    DEFAULT_CONFIG_FILENAME = os.path.join(CONFIG_DIR, "default.yml")
+    value.set_file(DEFAULT_CONFIG_FILENAME, base_for_paths=True)
+
+    # read configuration overrides from the user
+    if config_file_location is None:
+        config_file_location = CONFIG_YML_LOCATION
+    if os.path.isfile(config_file_location):
+        value.set_file(
+            config_file_location,
+            base_for_paths=True,
+        )
+
+    # read configuration from environment variables
+    value.set_env(sep="__")
+
+    # set overriding values
+    if override_values is not None:
+        for k, v in override_values:
+            value.set({k: v})
+
+    try:
+        validated_value = value.get(TEMPLATE)
+    except confuse.exceptions.ConfigTypeError as exception:
+        detail = str(exception).replace("\n", "\n  ")
+        key = detail.split(":")[0].strip()
+        raise DeepOriginException(
+            title="Invalid configuration",
+            message=f"The Deep Origin CLI and Python client requires a valid configuration. The {key} field is not valid:\n {detail}",
+            fix=f"To fix this issue, run `deeporigin config set {key} <value>`",
+        )
+
+    return validated_value
---
+++ packages/deeporigin/deeporigin-2.1.3.tar.gz/deeporigin-2.1.3/tests/test_cli_data.py
@@ -0,0 +1,136 @@
+import json
+from typing import Union
+
+import pytest
+from beartype import beartype
+from deeporigin.data_hub import api
+from mock_client import MockClient
+
+from tests.utils import _run_cli_command
+
+# this allows us to try every CLI command with both
+# multiple options using pytest.mark.parametrize
+JSON_OPTIONS = (
+    ["--json"],
+    [],
+)
+LIST_OPTIONS = (
+    [],
+    ["--rows"],
+    ["--databases"],
+    ["--files"],
+    ["--folders"],
+    ["--folders", "--databases"],
+    ["--folders", "--rows"],
+    ["--databases", "--rows"],
+)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def config(pytestconfig):
+    """this fixture performs some setup tasks
+    before all tests are run, and runs only once"""
+
+    data = dict()
+
+    # set up client
+    if pytestconfig.getoption("client") == "mock":
+        data["client"] = MockClient()
+
+        # unpack mock data from client
+        data["folders"] = data["client"].folders
+        data["databases"] = data["client"].databases
+        data["rows"] = data["client"].rows
+        data["file"] = data["client"].file
+    else:
+        client = api._get_default_client()
+        data["client"] = client
+
+        # if we're going to be making requests to a live
+        # instance, we need to make sensible requests
+        databases = api.list_rows(row_type="database")
+        data["databases"] = [db.hid for db in databases]
+        rows = api.list_rows(row_type="row")
+        data["rows"] = [row.id for row in rows]
+
+        # get a list of all files
+        files = api.list_files()
+        if len(files) > 0:
+            data["file"] = files[0].file
+
+    # tests run on yield
+    yield data
+
+    # teardown tasks, if any
+
+
+def test_data(config):
+    stdout = _run_cli_command(
+        ["data"],
+        config["client"],
+    )
+
+    assert "List data in the data hub on Deep Origin" in stdout, "Unexpected output"
+
+
+@pytest.mark.parametrize("json_option", JSON_OPTIONS)
+def test_describe_file(config, json_option):
+    file_id = config["file"].id
+
+    stdout = _run_cli_command(
+        ["data", "describe", file_id] + json_option,
+        config["client"],
+    )
+
+    if json_option == ["--json"]:
+        data = _check_json(stdout)
+        assert data["id"] == file_id, "Expected file ID to match"
+
+
+@pytest.mark.parametrize("option", JSON_OPTIONS)
+def test_describe_row(config, option):
+    row_id = config["rows"][0]
+
+    stdout = _run_cli_command(
+        ["data", "describe", row_id] + option,
+        config["client"],
+    )
+    assert row_id in stdout, "Expected to see row_id in output"
+
+    # check that we can parse into JSON
+    if option == ["--json"]:
+        data = _check_json(stdout)
+
+        assert data["id"] == row_id, "Expected row ID to match"
+
+
+@pytest.mark.parametrize("json_option", JSON_OPTIONS)
+@pytest.mark.parametrize("list_option", LIST_OPTIONS)
+def test_list(config, list_option, json_option):
+    stdout = _run_cli_command(
+        ["data", "list"] + list_option + json_option,
+        config["client"],
+    )
+
+    # check that we can parse into JSON
+    if json_option == ["--json"]:
+        _check_json(stdout)
+
+
+@pytest.mark.parametrize("json_option", JSON_OPTIONS)
+def test_show_db(config, json_option):
+    stdout = _run_cli_command(
+        ["data", "show", config["databases"][0]] + json_option,
+        config["client"],
+    )
+
+    if json_option == ["--json"]:
+        _check_json(stdout)
+
+
+@beartype
+def _check_json(txt: str) -> Union[dict, list[dict]]:
+    """helper function to check that a string is valid JSON"""
+    data = json.loads(txt)
+
+    return data
