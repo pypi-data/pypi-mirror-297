dd6ef459b22697f0057610cf1479c3312a5fce30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:04:16 +0000

    Add package davidshack4u/0.2.0/davidshack4u-0.2.0.tar.gz

---
+++ packages/davidshack4u/davidshack4u-0.2.0.tar.gz/davidshack4u-0.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# Leer el contenido del archivo README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+
+setup(
+	name="davidshack4u",
+	version="0.2.0",
+	packages=find_packages(),
+	install_requires=[],
+	author="David Lopez",
+	description="Biblioteca para consultar cursos de hack4u.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://hack4u.io",
+)
