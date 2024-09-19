a6ccb02743bf82f66c8d2f8ebd76fbf6d92c4e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:03:47 +0000

    Add package nnetsauce/0.24.5/nnetsauce-0.24.5-py3-none-any.whl

---
+++ packages/nnetsauce/nnetsauce-0.24.5-py3-none-any.whl/nnetsauce/__init__.py
@@ -0,0 +1,63 @@
+from .base.base import Base
+from .base.baseRegressor import BaseRegressor
+from .boosting.adaBoostClassifier import AdaBoostClassifier
+from .custom.customClassifier import CustomClassifier
+from .custom.customRegressor import CustomRegressor
+from .datasets import Downloader
+from .deep.deepClassifier import DeepClassifier
+from .deep.deepRegressor import DeepRegressor
+from .deep.deepMTS import DeepMTS
+from .glm.glmClassifier import GLMClassifier
+from .glm.glmRegressor import GLMRegressor
+from .lazypredict.lazyClassifier import LazyClassifier
+from .lazypredict.lazyRegressor import LazyRegressor
+from .lazypredict.lazydeepClassifier import LazyDeepClassifier
+from .lazypredict.lazydeepRegressor import LazyDeepRegressor
+from .lazypredict.lazydeepMTS import LazyDeepMTS
+from .mts.mts import MTS
+from .mts.classical import ClassicalMTS
+from .multitask.multitaskClassifier import MultitaskClassifier
+from .multitask.simplemultitaskClassifier import SimpleMultitaskClassifier
+from .optimizers.optimizer import Optimizer
+from .predictioninterval import PredictionInterval
+from .randombag.randomBagClassifier import RandomBagClassifier
+from .randombag.randomBagRegressor import RandomBagRegressor
+from .ridge2.ridge2Classifier import Ridge2Classifier
+from .ridge2.ridge2Regressor import Ridge2Regressor
+from .ridge2.ridge2MultitaskClassifier import Ridge2MultitaskClassifier
+from .rvfl.bayesianrvflRegressor import BayesianRVFLRegressor
+from .rvfl.bayesianrvfl2Regressor import BayesianRVFL2Regressor
+from .sampling import SubSampler
+
+__all__ = [
+    "AdaBoostClassifier",
+    "Base",
+    "BaseRegressor",
+    "BayesianRVFLRegressor",
+    "BayesianRVFL2Regressor",
+    "ClassicalMTS",
+    "CustomClassifier",
+    "CustomRegressor",
+    "DeepClassifier",
+    "DeepRegressor",
+    "DeepMTS",
+    "Downloader",
+    "GLMClassifier",
+    "GLMRegressor",
+    "LazyClassifier",
+    "LazyRegressor",
+    "LazyDeepClassifier",
+    "LazyDeepRegressor",
+    "LazyDeepMTS",
+    "MTS",
+    "MultitaskClassifier",
+    "PredictionInterval",
+    "SimpleMultitaskClassifier",
+    "Optimizer",
+    "RandomBagRegressor",
+    "RandomBagClassifier",
+    "Ridge2Regressor",
+    "Ridge2Classifier",
+    "Ridge2MultitaskClassifier",
+    "SubSampler",
+]
