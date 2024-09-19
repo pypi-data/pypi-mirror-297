9484f44d4fb5ead7c9fff40b0a8555ba05ad8360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:05:28 +0000

    Add package django-nice/0.3.76/django_nice-0.3.76.tar.gz

---
+++ packages/django-nice/django_nice-0.3.76.tar.gz/django_nice-0.3.76/django_nice/views.py
@@ -0,0 +1,32 @@
+from django.http import JsonResponse
+from django.views import View
+from django.apps import apps
+from .sse import sse_manager
+class ModelAPI(View):
+    def get(self, request, app_label, model_name, object_id, field_name):
+        # Fetch the model dynamically
+        model = apps.get_model(app_label, model_name)
+        try:
+            # Retrieve the instance based on the object_id
+            instance = model.objects.get(pk=object_id)
+            
+            # Dynamically get the value of the specified field
+            field_value = getattr(instance, field_name)
+            
+            data = {
+                field_name: field_value  # Respond with the dynamic field name and its value
+            }
+            return JsonResponse(data)
+        except model.DoesNotExist:
+            return JsonResponse({"error": "Object not found"}, status=404)
+
+    def post(self, request, app_label, model_name, object_id, field_name):
+        model = apps.get_model(app_label, model_name)
+        instance = model.objects.get(pk=object_id)
+        field_value = request.POST.get(field_name)  # Fetch value from POST
+
+        if field_name and hasattr(instance, field_name):
+            setattr(instance, field_name, field_value)
+            instance.save()
+            return JsonResponse({field_name: getattr(instance, field_name)})
+        return JsonResponse({'error': 'Field not found or invalid data'}, status=400)
