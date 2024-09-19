06c13e4a4ef95402c682d657d68f3b5e49d4beb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:04:39 +0000

    Add package imagecompression8/0.1.10/imagecompression8-0.1.10.tar.gz

---
+++ packages/imagecompression8/imagecompression8-0.1.10.tar.gz/imagecompression8-0.1.10/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='imagecompression8',
+    version='0.1.10',
+    packages=find_packages(),
+    description='It is an image compression Python package based on Singular Value Decomposition (SVD) technology. This tool offers an efficient block-based image compression method, reducing the storage requirements of images by dividing them into blocks and applying SVD, while retaining as much visual information as possible.',
+    install_requires=[
+        'numpy',
+        'pillow',
+        'scipy',
+    ],
+    author='Katie Wen-Ling Kuo',
+    author_email='katie20030705@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>=3.6',
+)
