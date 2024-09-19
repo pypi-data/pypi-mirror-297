53fe5b0e534c3f4d915ba0de1b00467749f4ffbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:04:53 +0000

    Add package plover-steno-engine-hooks-logger/0.3.3/plover_steno_engine_hooks_logger-0.3.3-py3-none-any.whl

---
+++ packages/plover-steno-engine-hooks-logger/plover_steno_engine_hooks_logger-0.3.3-py3-none-any.whl/plover_steno_engine_hooks_logger/steno_engine_hooks/__init__.py
@@ -0,0 +1,12 @@
+"""
+# Steno Engine Hooks
+
+A package dealing with logging out details of steno engine hooks to the
+`plover.log` file.
+"""
+
+from .logger import Logger
+
+__all__ = [
+    "Logger"
+]
