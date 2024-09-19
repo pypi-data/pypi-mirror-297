46c23fbb1d502d2aec7d07c782c9e1eb3043a915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:04:43 +0000

    Add package comeit/0.3.0/comeit-0.3.0-py3-none-any.whl

---
+++ packages/comeit/comeit-0.3.0-py3-none-any.whl/comeit/checks/body.py
@@ -0,0 +1,7 @@
+# Body tests
+class Body:
+    def check_body(self):
+        """A full test of the body."""
+
+    def summary_ended_with_newline():
+        """Summary needs to end with newline for body to make sense to run."""
