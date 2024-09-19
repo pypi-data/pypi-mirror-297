789703e6f5ab0d7eb576fa0103699dda29bd26e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 16:03:26 +0000

    Add package krutrim-cloud/0.4.1/krutrim_cloud-0.4.1-py3-none-any.whl

---
+++ packages/krutrim-cloud/krutrim_cloud-0.4.1-py3-none-any.whl/krutrim_cloud/resources/__init__.py
@@ -0,0 +1,78 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .chat import (
+    ChatResource,
+    AsyncChatResource,
+    ChatResourceWithRawResponse,
+    AsyncChatResourceWithRawResponse,
+    ChatResourceWithStreamingResponse,
+    AsyncChatResourceWithStreamingResponse,
+)
+from .audio import (
+    AudioResource,
+    AsyncAudioResource,
+    AudioResourceWithRawResponse,
+    AsyncAudioResourceWithRawResponse,
+    AudioResourceWithStreamingResponse,
+    AsyncAudioResourceWithStreamingResponse,
+)
+from .images import (
+    ImagesResource,
+    AsyncImagesResource,
+    ImagesResourceWithRawResponse,
+    AsyncImagesResourceWithRawResponse,
+    ImagesResourceWithStreamingResponse,
+    AsyncImagesResourceWithStreamingResponse,
+)
+from .multimodal import (
+    MultimodalResource,
+    AsyncMultimodalResource,
+    MultimodalResourceWithRawResponse,
+    AsyncMultimodalResourceWithRawResponse,
+    MultimodalResourceWithStreamingResponse,
+    AsyncMultimodalResourceWithStreamingResponse,
+)
+from .registry import RegistryResource
+
+from .deploy import (
+    DeployResource,
+    AsyncDeployResource,
+    DeployResourceWithRawResponse,
+    AsyncDeployResourceWithRawResponse,
+    DeployResourceWithStreamingResponse,
+    AsyncDeployResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ImagesResource",
+    "AsyncImagesResource",
+    "ImagesResourceWithRawResponse",
+    "AsyncImagesResourceWithRawResponse",
+    "ImagesResourceWithStreamingResponse",
+    "AsyncImagesResourceWithStreamingResponse",
+    "MultimodalResource",
+    "AsyncMultimodalResource",
+    "MultimodalResourceWithRawResponse",
+    "AsyncMultimodalResourceWithRawResponse",
+    "MultimodalResourceWithStreamingResponse",
+    "AsyncMultimodalResourceWithStreamingResponse",
+    "AudioResource",
+    "AsyncAudioResource",
+    "AudioResourceWithRawResponse",
+    "AsyncAudioResourceWithRawResponse",
+    "AudioResourceWithStreamingResponse",
+    "AsyncAudioResourceWithStreamingResponse",
+    "ChatResource",
+    "AsyncChatResource",
+    "ChatResourceWithRawResponse",
+    "AsyncChatResourceWithRawResponse",
+    "ChatResourceWithStreamingResponse",
+    "AsyncChatResourceWithStreamingResponse",
+    "RegistryResource",
+    "DeployResource",
+    "AsyncDeployResource",
+    "DeployResourceWithRawResponse",
+    "AsyncDeployResourceWithRawResponse",
+    "DeployResourceWithStreamingResponse",
+    "AsyncDeployResourceWithStreamingResponse",
+]
