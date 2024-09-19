cd2d6f0e9ead4e0a58dc6eac0198c11a2fa2c822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 16:03:35 +0000

    Add package rpa-test/0.1.2/rpa_test-0.1.2-py3-none-any.whl

---
+++ packages/rpa-test/rpa_test-0.1.2-py3-none-any.whl/rpa_test_zhang/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2024-present U.N. Owen <void@some.where>
+#
+# SPDX-License-Identifier: MIT
+
+import requests
+
+
+def main():
+    return 'rpa_test_zhang:' + '0.1.0--trusted-host mirrors.aliyun.com'
+    # api token
+    # pypi-AgEIcHlwaS5vcmcCJDI2MWNkY2NiLTUzOWQtNDFjZi04YTNhLTk2M2NiMTE3NWJiMAACEFsxLFsicnBhLXRlc3QiXV0AAixbMixbIjliODAxMTE5LTc2MzItNGRlMi1hYmUwLWZmMThkNWVjMmQ2ZSJdXQAABiAbwTB4vJ3AmlJ0ZignGpKaBGN9F0I8eT_CqAu_eKviYQ
