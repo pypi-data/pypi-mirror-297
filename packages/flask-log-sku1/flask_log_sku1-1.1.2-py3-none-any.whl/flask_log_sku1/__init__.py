fcbf4a1a337dd941342d456c56a2e31a1e1184e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 16:03:32 +0000

    Add package flask-log-sku1/1.1.2/flask_log_sku1-1.1.2-py3-none-any.whl

---
+++ packages/flask-log-sku1/flask_log_sku1-1.1.2-py3-none-any.whl/flask_log_sku1/__init__.py
@@ -0,0 +1,41 @@
+#Used to print out messages server side, and make them pretty
+import datetime
+from os import environ
+
+useColor = environ.get("FLASK_LOG_COLOR") == 'True' if environ.get("FLASK_LOG_COLOR") is not None else True
+dateFormat = environ.get("FLASK_LOG_DATE_FORMAT") if environ.get("FLASK_LOG_DATE_FORMAT") is not None else "%d/%b/%Y %H:%M:%S"
+
+class bcolors:
+    HEADER = '\033[35m'
+    OKBLUE = '\033[34m'
+    OKCYAN = '\033[36m'
+    OKGREEN = '\033[32m'
+    WARNING = '\033[33m'.
+    FAIL = '\033[31m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class log:
+    def __init__(self,*messages):
+        emptystring = ''
+        for msg in messages:
+            emptystring += str(msg)
+        self.string = emptystring
+        self.main = '[LOG] '
+        self.timestamp = str(get_current_time())
+        
+    def success(self):        
+        print(bcolors.OKGREEN,self.main,self.timestamp,'[**Success**]',self.string,bcolors.ENDC) if useColor else print(self.main,self.timestamp,'[**Success**]',self.string)
+        
+    def info(self):
+        print(bcolors.OKBLUE,self.main,self.timestamp,'[**Info**]',self.string,bcolors.ENDC) if useColor else print(self.main,self.timestamp,'[**Info**]',self.string)
+                
+    def warning(self):
+        print (bcolors.FAIL,self.main,self.timestamp,'[**Warning**]',self.string,bcolors.ENDC) if useColor else print(self.main,self.timestamp,'[**Warning**]',self.string)
+        
+    def error(self):
+        print (bcolors.WARNING,self.main,self.timestamp,'[**Error**]',self.string,bcolors.ENDC) if useColor else print(self.main,self.timestamp,'[**Error**]',self.string)
+
+def get_current_time():
+    return datetime.datetime.now().strftime("["+dateFormat+"]")
