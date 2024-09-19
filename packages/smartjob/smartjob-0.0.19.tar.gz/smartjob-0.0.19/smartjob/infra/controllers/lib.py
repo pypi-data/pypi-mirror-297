bcdbd62853af90ec441ef7c23bc85ccbeffad9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 16:03:28 +0000

    Add package smartjob/0.0.19/smartjob-0.0.19.tar.gz

---
+++ packages/smartjob/smartjob-0.0.19.tar.gz/smartjob-0.0.19/smartjob/infra/controllers/lib.py
@@ -0,0 +1,42 @@
+from smartjob.app.executor import ExecutorPort, ExecutorService
+from smartjob.infra.adapters.executor.cloudrun import CloudRunExecutorAdapter
+from smartjob.infra.adapters.executor.docker import DockerExecutorAdapter
+from smartjob.infra.adapters.executor.vertex import VertexExecutorAdapter
+
+# Default max workers for vertex executor and for file uploader (cloud run executor is not multi-threaded)
+DEFAULT_MAX_WORKERS = 10
+__cache: dict[str, ExecutorService] = {}
+
+
+def get_executor_service(
+    type: str = "cloudrun",
+    max_workers: int = DEFAULT_MAX_WORKERS,
+    use_cache: bool = True,
+) -> ExecutorService:
+    """Return a singleton instance of ExecutorService (initialized on first call with given arguments).
+
+    Args:
+        type: The type of executor to use ('cloudrun', 'vertex' or 'docker').
+        max_workers: Maximum number of workers for the vertex executor and for the file uploader.
+        use_cache: if set to True, the same instance will be returned for the same type.
+
+    Returns:
+        Instance of ExecutorService.
+
+    """
+    adapter: ExecutorPort
+    if type == "cloudrun":
+        adapter = CloudRunExecutorAdapter(max_workers=max_workers)
+    elif type == "vertex":
+        adapter = VertexExecutorAdapter(max_workers=max_workers)
+    elif type == "docker":
+        adapter = DockerExecutorAdapter(max_workers=max_workers)
+    else:
+        raise ValueError(
+            f"Invalid executor type: {type} => must be 'cloudrun', 'vertex' or 'docker'"
+        )
+    if not use_cache or type not in __cache:
+        __cache[type] = ExecutorService(
+            adapter=adapter,
+        )
+    return __cache[type]
