5200ead2531ed0d919df5bd4845a5d77dbbc74cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 16:03:46 +0000

    Add package mstrio-py/11.4.9.101/mstrio_py-11.4.9.101-py3-none-any.whl

---
+++ packages/mstrio-py/mstrio_py-11.4.9.101-py3-none-any.whl/mstrio/object_management/shortcut.py
@@ -0,0 +1,264 @@
+from enum import IntFlag
+from typing import Any, TypeVar
+
+from mstrio.api import browsing
+from mstrio.connection import Connection
+from mstrio.utils.entity import CopyMixin, Entity, MoveMixin, ObjectTypes
+from mstrio.utils.enum_helper import get_enum_val
+from mstrio.utils.helper import deprecation_warning, fetch_objects, get_valid_project_id
+from mstrio.utils.response_processors import objects as objects_processors
+from mstrio.utils.translation_mixin import TranslationMixin
+
+
+class ShortcutInfoFlags(IntFlag):
+    DssDossierShortcutInfoBookmark = 0b10
+    DssDossierShortcutInfoTOC = 0b01
+    DssDossierShortcutInfoDefault = 0b00
+    DssDashboardShortcutInfoBookmark = 0b10
+    DssDashboardShortcutInfoTOC = 0b01
+    DssDashboardShortcutInfoDefault = 0b00
+
+
+class Shortcut(Entity, CopyMixin, MoveMixin, TranslationMixin):
+    _OBJECT_TYPE = ObjectTypes.SHORTCUT_TYPE
+    _API_GETTERS = {
+        (
+            'name',
+            'id',
+            'project_id',
+            'owned_by_current_user',
+            'target',
+            'encode_html_content',
+            'current_page_key',
+            'shared_time',
+            'last_viewed_time',
+            'last_modified_time',
+            'stid',
+            'current_bookmark',
+            'prompted',
+            'datasets_cache_info_hash',
+            'shortcut_info_flag',
+            'dossier_version_hash',
+        ): browsing.get_shortcut,
+        (
+            'abbreviation',
+            'type',
+            'subtype',
+            'ext_type',
+            'date_created',
+            'date_modified',
+            'version',
+            'owner',
+            'icon_path',
+            'view_media',
+            'ancestors',
+            'certified_info',
+            'acg',
+            'acl',
+            'target_info',
+            'hidden',
+        ): objects_processors.get_info,
+    }
+    _FROM_DICT_MAP = {**Entity._FROM_DICT_MAP, 'shortcut_info_flag': ShortcutInfoFlags}
+    _API_PATCH: dict = {
+        ('name', 'description', 'abbreviation', 'hidden', 'folder_id'): (
+            objects_processors.update,
+            'partial_put',
+        )
+    }
+
+    def __init__(
+        self,
+        connection: Connection,
+        id: str,
+        project_id: str = None,
+        project_name: str = None,
+        shortcut_info_flag: (
+            ShortcutInfoFlags | int
+        ) = ShortcutInfoFlags.DssDossierShortcutInfoTOC,
+    ):
+        """Initialize the Shortcut object and populate it with I-Server data.
+
+        Specify either `project_id` or `project_name`.
+        When `project_id` is provided (not `None`), `project_name` is omitted.
+
+        Note:
+            When `project_id` is `None` and `project_name` is `None`, then
+            its value is overwritten by `project_id` from `connection` object.
+
+        Args:
+            connection: MicroStrategy connection object returned
+                by `connection.Connection()`.
+            id: Shortcut ID
+            project_id: ID of the project that the shortcut is in
+            project_name: Project name
+            shortcut_info_flag: flag indicating what information about shortcut
+                should be loaded
+        """
+        if id is None:
+            raise AttributeError("Please specify 'id' parameter in the constructor.")
+        else:
+            project_id = get_valid_project_id(
+                connection=connection,
+                project_id=project_id,
+                project_name=project_name,
+                with_fallback=not project_name,
+            )
+            super().__init__(
+                connection=connection,
+                object_id=id,
+                project_id=project_id,
+                shortcut_info_flag=get_enum_val(shortcut_info_flag, ShortcutInfoFlags),
+            )
+
+    def _init_variables(self, **kwargs) -> None:
+        super()._init_variables(**kwargs)
+        self._owned_by_current_user = kwargs.get('owned_by_current_user')
+        self._target = kwargs.get('target')
+        self._encode_html_content = kwargs.get('encode_html_content')
+        self._shortcut_info_flag = (
+            ShortcutInfoFlags(kwargs["shortcut_info_flag"])
+            if kwargs.get("shortcut_info_flag")
+            else None
+        )
+        self._current_page_key = kwargs.get('current_page_key')
+        self._shared_time = kwargs.get('shared_time')
+        self._last_viewed_time = kwargs.get('last_viewed_time')
+        self._last_modified_time = kwargs.get('last_modified_time')
+        self._stid = kwargs.get('stid')
+        self._current_bookmark = kwargs.get('current_bookmark')
+        self._prompted = kwargs.get('prompted')
+        self._datasets_cache_info_hash = kwargs.get('datasets_cache_info_hash')
+        self._dossier_version_hash = kwargs.get('dossier_version_hash')
+        self._dashboard_version_hash = kwargs.get('dossier_version_hash')
+
+    T = TypeVar('T')
+
+    @classmethod
+    def from_dict(cls: T, source: dict[str, Any], connection: Connection) -> T:
+        """Instantiate a Shortcut from a dict source."""
+        return super(Entity, cls).from_dict(source=source, connection=connection)
+
+    @property
+    def owned_by_current_user(self):
+        return self._owned_by_current_user
+
+    @property
+    def target(self):
+        return self._target
+
+    @property
+    def encode_html_content(self):
+        return self._encode_html_content
+
+    @property
+    def shortcut_info_flag(self):
+        return self._shortcut_info_flag
+
+    @property
+    def current_page_key(self):
+        return self._current_page_key
+
+    @property
+    def shared_time(self):
+        return self._shared_time
+
+    @property
+    def last_viewed_time(self):
+        return self._last_viewed_time
+
+    @property
+    def last_modified_time(self):
+        return self._last_modified_time
+
+    @property
+    def stid(self):
+        return self._stid
+
+    @property
+    def current_bookmark(self):
+        return self._current_bookmark
+
+    @property
+    def prompted(self):
+        return self._prompted
+
+    @property
+    def datasets_cache_info_hash(self):
+        return self._datasets_cache_info_hash
+
+    @property
+    def dossier_version_hash(self):
+        deprecation_warning(
+            'property `dossier_version_hash`',
+            'property `dashboard_version_hash`',
+            '11.5.03',
+            False,
+        )
+        return self._dossier_version_hash
+
+    @property
+    def dashboard_version_hash(self):
+        return self._dashboard_version_hash
+
+
+def get_shortcuts(
+    connection: Connection,
+    shortcut_ids: list[str],
+    project_id: str = None,
+    project_name: str = None,
+    shortcut_info_flag: (
+        ShortcutInfoFlags | int
+    ) = ShortcutInfoFlags.DssDossierShortcutInfoDefault,
+    to_dictionary: bool = False,
+    limit: int | None = None,
+    **filters,
+) -> list[dict] | list[Shortcut]:
+    """Retrieve information about specific published shortcuts
+     in specific project.
+
+     Specify either `project_id` or `project_name`.
+     When `project_id` is provided (not `None`), `project_name` is omitted.
+
+    Note:
+         When `project_id` is `None` and `project_name` is `None`,
+         then its value is overwritten by `project_id` from `connection`
+         object.
+
+     Args:
+         shortcut_ids: ids of target shortcuts
+         project_id: id of project that the shortcuts are in
+         project_name: Project name
+         shortcut_info_flag: a single ShortcutInfoFlags that describes what
+           exact info are to be fetched
+         to_dictionary: parameter describing output format
+         limit (int): limit the number of elements returned.
+             If `None` (default), all objects are returned.
+     Return:
+         list of dictionaries or Shortcut objects,
+           depending on `to_dictionary` parameter
+    """
+    project_id = get_valid_project_id(
+        connection=connection,
+        project_id=project_id,
+        project_name=project_name,
+        with_fallback=not project_name,
+    )
+
+    shortcuts = fetch_objects(
+        connection=connection,
+        api=browsing.get_shortcuts,
+        dict_unpack_value="shortcuts",
+        limit=limit,
+        filters=filters,
+        body=[{"projectId": project_id, "shortcutIds": shortcut_ids}],
+        shortcut_info_flag=get_enum_val(shortcut_info_flag, ShortcutInfoFlags),
+    )
+
+    if to_dictionary:
+        return shortcuts
+    else:
+        return [
+            Shortcut.from_dict(source=short, connection=connection)
+            for short in shortcuts
+        ]
---
+++ packages/mstrio-py/mstrio_py-11.4.9.101-py3-none-any.whl/mstrio/utils/response_processors/users.py
@@ -0,0 +1,292 @@
+from requests import JSONDecodeError
+
+from mstrio.api import usergroups as usergroups_api
+from mstrio.api import users as users_api
+from mstrio.connection import Connection
+from mstrio.helpers import IServerError
+from mstrio.utils.helper import deprecation_warning, fetch_objects, fetch_objects_async
+from mstrio.utils.version_helper import method_version_handler
+
+
+def get(connection: Connection, id: str):
+    """Get user by a specified ID.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+
+    Returns:
+        dict representing user object
+    """
+    return users_api.get_user_info(connection=connection, id=id).json()
+
+
+def get_addresses(connection: Connection, id: str):
+    """Get addresses for a specified user.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+
+    Returns:
+        dict representing user addresses
+    """
+    return users_api.get_addresses_v2(connection=connection, id=id).json()
+
+
+def get_security_roles(connection: Connection, id: str):
+    """Get security roles for a specified user.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+
+    Returns:
+        dict representing user security roles
+    """
+    return users_api.get_user_security_roles(connection=connection, id=id).json()
+
+
+def get_settings(connection: Connection, id: str):
+    """Get settings for a specified user.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+
+    Returns:
+        dict representing user security roles
+    """
+    return users_api.get_settings(connection=connection, id=id).json()
+
+
+def get_privileges(connection: Connection, id: str):
+    """Get privileges for a specified user.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+
+    Returns:
+        dict representing user privileges
+    """
+    return users_api.get_user_privileges(connection=connection, id=id).json()
+
+
+def update(connection: Connection, id: str, body: dict):
+    """Update info for a specified user.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+        body: body of the request
+
+    Returns:
+        dict representing user object
+    """
+    return users_api.update_user_info(connection=connection, id=id, body=body).json()
+
+
+def create(connection: Connection, body: dict, username: str):
+    """Create a user.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        body: body of the request
+        username: name of the user, for error purposes only
+
+    Returns:
+        dict representing user object
+    """
+    return users_api.create_user(
+        connection=connection, body=body, username=username
+    ).json()
+
+
+def get_all(
+    connection: Connection,
+    limit: int,
+    msg: str,
+    name_begins: str,
+    abbreviation_begins: str,
+    filters: dict,
+):
+    """Get list of users.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        limit: limit of users to list
+        msg: optional error message,
+        name_begins: optional filter for name beginning with
+        abbreviation_begins: optional filter for abbreviation beginning with
+        filters: filters
+
+    Returns:
+        list of dicts representing users
+    """
+    if filters.get('initials'):
+        deprecation_warning(
+            deprecated="possibility of providing 'initials' as a filter",
+            new="New options to filter on 'full_name' and 'enabled' fields instead",
+            version="11.3.13.101",
+            module=False,
+        )
+        return fetch_objects_async(
+            connection=connection,
+            api=users_api.get_users_info,
+            async_api=users_api.get_users_info_async,
+            limit=limit,
+            chunk_size=1000,
+            error_msg=msg,
+            name_begins=name_begins,
+            abbreviation_begins=abbreviation_begins,
+            filters=filters,
+        )
+    if filters.get('name') or name_begins:
+        name_begins_filter = ('starts', name_begins) if name_begins else None
+
+        filters['name'] = filters.get('name') or name_begins_filter
+
+    if filters.get('abbreviation') or abbreviation_begins:
+        abbreviation_begins_filter = (
+            ('starts', abbreviation_begins) if abbreviation_begins else None
+        )
+        filters['abbreviation'] = (
+            filters.get('abbreviation') or abbreviation_begins_filter
+        )
+    # Getting information from members of 'Everyone' user group
+    return fetch_objects(
+        connection=connection,
+        api=usergroups_api.get_members,
+        limit=limit,
+        error_msg=msg,
+        filters=filters,
+        id='C82C6B1011D2894CC0009D9F29718E4F',
+    )
+
+
+def create_address(connection: Connection, id: str, body: dict):
+    """Create an email user address.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+        body: body of the request
+
+    Returns:
+    created address dictionary
+    """
+    return users_api.create_address(connection=connection, id=id, body=body).json()
+
+
+def create_address_v2(connection: Connection, id: str, body: dict):
+    """Create a non-email user address.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+        body: body of the request
+
+    Returns:
+        dict representing user addresses
+    """
+    return users_api.create_address_v2(connection=connection, id=id, body=body).json()
+
+
+def update_address(connection: Connection, id: str, address_id: str, body: dict):
+    """Update a user address.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+        address_id: ID of the address
+        body: body of the request
+
+    Returns:
+        True if successfully updated, False otherwise
+    """
+    return users_api.update_address_v2(
+        connection=connection, id=id, address_id=address_id, body=body
+    ).ok
+
+
+def delete_address(connection: Connection, id: str, address_id: str):
+    """Delete a user address.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+        address_id: ID of the address
+
+    Returns:
+        True if successfully deleted, False otherwise
+    """
+    return users_api.delete_address(
+        connection=connection, id=id, address_id=address_id
+    ).ok
+
+
+def get_security_filters(connection: Connection, id: str, projects: str | list[str]):
+    """Get security filters for a user.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+        projects: IDs of the projects
+
+    Returns:
+        list of user security filters
+    """
+    return users_api.get_security_filters(
+        connection=connection, id=id, projects=projects
+    ).json()
+
+
+@method_version_handler('11.3.0700')
+def update_user_settings(connection: Connection, id: str, body: str):
+    """Update user settings.
+
+    Args:
+        connection: MicroStrategy REST API connection object
+        id: ID of the user
+
+    Returns:
+        True if successfully updated, False otherwise
+    """
+    response = users_api.update_user_settings(connection=connection, id=id, json=body)
+    if response.ok:
+        # Body is returned as endpoint returns 204 with empty response, which
+        # would fail when using response.json() .
+        # In order to alter user correctly, we return body to reflect changes
+        return body
+    return response
+
+
+def get_user_last_login(connection: Connection, id: str):
+    response = users_api.get_user_last_login(
+        connection=connection,
+        id=id,
+        whitelist=[(404, 404)],
+        throw_error=False,
+        verbose=False,
+    )
+    no_login_date_msg = 'No login found for the given user'
+    try:
+        res = response.json()
+    except JSONDecodeError:
+        if no_login_date_msg in response.text:
+            return None
+        raise response.raise_for_status()
+    error = res.get('errors', [None])[0]
+
+    if error and no_login_date_msg not in error.get('message'):
+        server_code = error.get('code')
+        ticket_id = error.get('ticketId')
+        server_msg = error.get('message')
+
+        raise IServerError(
+            message=f"{server_msg}; code: '{server_code}', ticket_id: '{ticket_id}'",
+            http_code=response.status_code,
+        )
+
+    return None if error else res
