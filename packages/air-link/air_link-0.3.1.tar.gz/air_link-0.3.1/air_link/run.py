f9c57e0fe1c0e28158455919ffc88a922bbc10b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:05:56 +0000

    Add package air-link/0.3.1/air_link-0.3.1.tar.gz

---
+++ packages/air-link/air_link-0.3.1.tar.gz/air_link-0.3.1/air_link/run.py
@@ -0,0 +1,20 @@
+import logging
+
+from nicegui import app, ui
+
+from .main_page import create_page
+from .package import read_env
+from .ssh import setup
+
+
+def run() -> None:
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger('watchfiles').setLevel(logging.WARNING)
+
+    on_air = app.storage.general.get('air_link_token', False)
+    if on_air:
+        app.on_startup(setup)
+
+    create_page()
+    app.on_startup(read_env)
+    ui.run(title='Air Link', favicon='⛑', reload=False, on_air=on_air)
