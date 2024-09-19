eb9682de1fe1cb42abb43d490bb569c751eae3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:04:52 +0000

    Add package sop-utils/1.0/sop_utils-1.0.tar.gz

---
+++ packages/sop-utils/sop_utils-1.0.tar.gz/sop_utils-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sop_utils",
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description="Utilities for the SOP Netbox Plugins.",
+    author="Leorevoir",
+    author_email="leo.quinzler@epitech.eu",
+)
