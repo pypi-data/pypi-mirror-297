72f85a1baa39b1578bca450da3c9981c5cc79b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:03:54 +0000

    Add package antpack/0.3.5.1/antpack-0.3.5.1-cp39-cp39-win32.whl

---
+++ packages/antpack/antpack-0.3.5.1-cp39-cp39-win32.whl/antpack/numbering_tools/single_chain_annotator.py
@@ -0,0 +1,36 @@
+"""Contains the SingleChainAnnotator class, which provides the tools needed
+to parse single chains (either heavy or light) or a sequence which likely contains
+a heavy or light chain."""
+import os
+from antpack.antpack_cpp_ext import SingleChainAnnotatorCpp
+
+
+
+
+class SingleChainAnnotator(SingleChainAnnotatorCpp):
+
+    def __init__(self, chains=["H", "K", "L"], scheme="imgt",
+            compress_init_gaps=False):
+        """Class constructor.
+
+        Args:
+            chains (list): A list of chains. Each must be one of "H", "K", "L".
+                If ["H", "K", "L"] (default), the annotator will automatically
+                determine the most appropriate chain type for each input
+                sequence.
+            scheme (str): The numbering scheme. Must be one of "imgt",
+                "martin", "kabat", "aho".
+            compress_init_gaps (bool): If True, rearrange gaps in the first 5
+                positions post-alignment so that gaps are at the beginning of
+                the sequence wherever possible. This is more consistent with
+                results from some other tools although it is debatable
+                if this is more correct. Defaults to False.
+
+        Raises:
+            ValueError: A ValueError is raised if unacceptable inputs are
+                supplied.
+        """
+        consensus_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                "consensus_data")
+        super().__init__(chains, scheme, compress_init_gaps,
+                consensus_path)
