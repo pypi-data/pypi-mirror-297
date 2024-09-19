b1506568aff4d0eb16417ac61880171693c6ffd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:03:44 +0000

    Add package discord-rpc/5.1/discord_rpc-5.1.tar.gz

---
+++ packages/discord-rpc/discord_rpc-5.1.tar.gz/discord_rpc-5.1/README.md
@@ -0,0 +1,79 @@
+[![Visitors](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2FSenophyx%2FDiscord-RPC&label=Visitors&countColor=%2337d67a&style=flat&labelStyle=none)](https://github.com/Senophyx/Discord-RPC)
+[![Discord](https://img.shields.io/discord/887650006977347594?label=EterNomm&logo=discord)](https://discord.gg/qpT2AeYZRN)
+[![Total Downloads](https://static.pepy.tech/badge/discord-rpc)](https://pepy.tech/project/discord-rpc)
+[![PyPI](https://img.shields.io/pypi/v/discord-rpc?label=PyPI%20Version&logo=pypi)](https://pypi.org/project/discord-rpc)
+[![PyPI - Status](https://img.shields.io/pypi/status/discord-rpc?label=Packages%20Status&logo=pypi)](https://pypi.org/project/discord-rpc)
+[![GitHub commit activity](https://img.shields.io/github/commit-activity/y/Senophyx/discord-rpc?label=Commit%20Activity&logo=github)](https://github.com/Senophyx/discord-rpc)
+
+# Discord RPC
+An Python wrapper for Discord RPC API. Allow you to make own custom RPC.
+
+[![Changelog](https://img.shields.io/badge/Discord--RPC-Changelog-informational?style=for-the-badge&logo=github)](https://gist.github.com/Senophyx/019b77be3cca743c4ada423ccf80b836)
+
+## Install
+- PyPI
+```
+pip install discord-rpc
+```
+
+## Quick example
+```py
+import discordrpc
+
+rpc = discordrpc.RPC(app_id=12345678910)
+
+rpc.set_activity(
+      state="A super simple rpc",
+      details="simple RPC"
+    )
+
+# Required if you only run Discord RPC on this file or current instance.
+rpc.run()
+```
+`rpc.run()` is only used if you are only running Discord RPC on the current file/instance. If there are other programs/tasks on the current instance, `rpc.run()` does not need to be used.
+
+See documentation [here](https://github.com/Senophyx/Discord-RPC/blob/main/DOCS.md).<br>
+More examples [here](https://github.com/Senophyx/discord-rpc/tree/main/examples).
+
+
+## Contributors
+Big thanks for contributors who help this project keep updated, and maintained.
+
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<table>
+  <tbody>
+    <tr>
+      <td align="center" valign="top" width="25%"><a href="https://pukima.site"><img src="https://avatars.githubusercontent.com/u/58347116?v=4?s=100" width="100px;" alt="Pukima"/><br /><sub><b>Pukima</b></sub></a><br /><a href="#bug-Pukimaa" title="Bug reports">🐛</a></td>
+      <td align="center" valign="top" width="25%"><a href="https://github.com/k9ur"><img src="https://avatars.githubusercontent.com/u/67886793?v=4?s=100" width="100px;" alt="k9er"/><br /><sub><b>k9er</b></sub></a><br /><a href="#doc-k9ur" title="Documentation">📖</a></td>
+      <td align="center" valign="top" width="25%"><a href="https://www.kaleko.dev/"><img src="https://avatars.githubusercontent.com/u/54416665?v=4?s=100" width="100px;" alt="Kale"/><br /><sub><b>Kale</b></sub></a><br /><a href="#bug-Kale-Ko" title="Bug reports">🐛</a></td>
+      <td align="center" valign="top" width="25%"><a href="https://github.com/TaIFeel"><img src="https://avatars.githubusercontent.com/u/94287800?v=4?s=100" width="100px;" alt="TaIFeel"/><br /><sub><b>TaIFeel</b></sub></a><br /><a href="#code-TaIFeel" title="Code">💻</a></td>
+    </tr>
+    <tr>
+      <td align="center" valign="top" width="25%"><a href="https://buymeacoffee.com/saves"><img src="https://avatars.githubusercontent.com/u/8108358?v=4?s=100" width="100px;" alt="Jesusaves"/><br /><sub><b>Jesusaves</b></sub></a><br /><a href="#bug-pazkero" title="Bug reports">🐛</a></td>
+      <td align="center" valign="top" width="25%"><a href="http://dipeshaggarwal.com"><img src="https://avatars.githubusercontent.com/u/1311129?v=4?s=100" width="100px;" alt="Dipesh Aggarwal"/><br /><sub><b>Dipesh Aggarwal</b></sub></a><br /><a href="#code-DipeshAggarwal" title="Code">💻</a></td>
+      <td align="center" valign="top" width="25%"><a href="https://github.com/maxsspeaker"><img src="https://avatars.githubusercontent.com/u/56259377?v=4?s=100" width="100px;" alt="Maxsspeaker"/><br /><sub><b>Maxsspeaker</b></sub></a><br /><a href="#code-maxsspeaker" title="Code">💻</a></td>
+      <td align="center" valign="top" width="25%"><a href="https://fixator10.ru"><img src="https://avatars.githubusercontent.com/u/11073934?v=4?s=100" width="100px;" alt="Fixator10"/><br /><sub><b>Fixator10</b></sub></a><br /><a href="#doc-fixator10" title="Documentation">📖</a></td>
+    </tr>
+  </tbody>
+</table>
+
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+## Links
+- [Github Repository](https://github.com/Senophyx/Discord-RPC)
+- [PyPI Project page](https://pypi.org/project/discord-rpc/)
+- [TestPyPI Project page](https://test.pypi.org/project/discord-rpc/)
+- [Discord Server](https://discord.gg/qpT2AeYZRN)
+
+## Licence & Copyright
+
+```
+This Project under MIT License
+Copyright (c) 2021-2024 Senophyx
+```
---
+++ packages/discord-rpc/discord_rpc-5.1.tar.gz/discord_rpc-5.1/discordrpc/presence.py
@@ -0,0 +1,291 @@
+import sys
+import os
+import socket
+import json
+import struct
+import uuid
+import re
+from .exceptions import *
+from .utils import remove_none
+import logging
+import time
+
+OP_HANDSHAKE = 0
+OP_FRAME = 1
+OP_CLOSE = 2
+
+TRY_RECONNECTING = True
+
+### Logger ###
+log = logging.getLogger("Discord RPC")
+log.setLevel(logging.INFO)
+logging.basicConfig(format="%(asctime)s :: [%(levelname)s @ %(filename)s.%(funcName)s:%(lineno)d] :: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
+
+class RPC:
+    def __init__(self, app_id:int, debug:bool=False, output:bool=True, exit_if_discord_close:bool=True):
+        self.app_id = str(app_id)
+        self.exit_if_discord_close = exit_if_discord_close
+        self.User={}
+
+        if debug == True:
+            log.setLevel(logging.DEBUG)
+        
+        if output == False:
+            log.disabled = True
+
+        self.is_running = False
+        self._setup()
+
+    def _setup(self):
+        if sys.platform == "win32":
+            self.ipc = WindowsPipe(self.app_id, self.exit_if_discord_close)
+            if not self.ipc.connected:
+                return
+
+            self.User=self.ipc.handshake()
+
+        else:
+            self.ipc = UnixPipe(self.app_id, self.exit_if_discord_close)
+            if not self.ipc.connected:
+                return
+
+            self.User=self.ipc.handshake()
+    
+    def set_activity(
+            self,
+            state: str=None, details:str=None, act_type:int=0,
+            ts_start:int=None, ts_end:int=None,
+            large_image:str=None, large_text:str=None,
+            small_image:str=None, small_text:str=None,
+            party_id:str=None, party_size:list=None,
+            join_secret:str=None, spectate_secret:str=None,
+            match_secret:str=None, buttons:list=None
+        ):
+
+        if type(party_id) == int:
+            party_id = str(party_id)
+
+        # https://github.com/Senophyx/Discord-RPC/issues/28#issuecomment-2301287350
+        invalidType = ["1", "4"]
+        if any(invtype in str(act_type) for invtype in invalidType):
+            raise InvalidActivityType()
+            
+        act = {
+            "state": state,
+            "details": details,
+            "type": act_type,
+            "timestamps": {
+                "start": ts_start,
+                "end": ts_end
+            },
+            "assets": {
+                "large_image": large_image,
+                "large_text": large_text,
+                "small_image": small_image,
+                "small_text": small_text
+            },
+            "party": {
+                "id": party_id,
+                "size": party_size
+            },
+            "secrets": {
+                "join": join_secret,
+                "spectate": spectate_secret,
+                "match": match_secret
+            },
+            "buttons": buttons
+        }
+
+        payload = {
+            'cmd': 'SET_ACTIVITY',
+            'args': {
+                'pid': os.getpid(),
+                'activity': remove_none(act)
+            },
+            'nonce': str(uuid.uuid4())
+        }
+
+        if not self.ipc.connected and TRY_RECONNECTING:
+            self._setup()
+
+        if not self.ipc.connected:
+            return
+
+        self.ipc._send(payload, OP_FRAME)
+        self.is_running = True
+        log.info('RPC set')
+
+    def disconnect(self):
+        if not self.ipc.connected:
+            return
+
+        self.ipc.disconnect()
+        self.is_running = False
+
+    def run(self, update_every:int=1):
+        try:
+            while True:
+                time.sleep(update_every)
+        except KeyboardInterrupt:
+            self.disconnect()
+
+class WindowsPipe:
+    def __init__(self, app_id, exit_if_discord_close):
+        self.app_id = app_id
+        self.exit_if_discord_close = exit_if_discord_close
+        self.connected = True
+
+        base_path = R'\\?\pipe\discord-ipc-{}'
+        
+        for i in range(10):
+            path = base_path.format(i)
+
+            try:
+                self.socket = open(path, "w+b")
+            except OSError as e:
+                if not self.exit_if_discord_close:
+                    raise Error("Failed to open {!r}: {}".format(path, e))
+            else:
+                break
+
+        else:
+            if not self.exit_if_discord_close:
+                raise DiscordNotOpened()
+            else:
+                log.debug("Discord seems to be close.")
+                self.connected = False
+
+        if self.connected:
+            log.debug(f"Connected to {path}")
+
+    def _recv(self):
+        enc_header = b''
+        header_size = 8
+
+        while header_size:
+            enc_header += self.socket.read(header_size)
+            header_size -= len(enc_header)
+
+        dec_header = struct.unpack("<ii", enc_header)
+        enc_data = b''
+        remain_packet_size = int(dec_header[1])
+
+        while remain_packet_size:
+            enc_data += self.socket.read(remain_packet_size)
+            remain_packet_size -= len(enc_data)
+        
+        output = json.loads(enc_data.decode('UTF-8'))
+
+        log.debug(output)
+        return output
+
+    def _send(self, payload, op=OP_FRAME):
+        log.debug(payload)
+
+        payload = json.dumps(payload).encode('UTF-8')
+        payload = struct.pack('<ii', op, len(payload)) + payload
+
+        self.socket.write(payload)
+        self.socket.flush()
+
+    def handshake(self):
+        self._send({'v': 1, 'client_id': self.app_id}, op=OP_HANDSHAKE)
+        data = self._recv()
+
+        try:
+            if data['cmd'] == 'DISPATCH' and data['evt'] == 'READY':
+                log.info(f"Connected to {data['data']['user']['username']} ({data['data']['user']['id']})")
+                return data['data']['user']
+            
+            else:
+                raise RPCException()
+
+        except KeyError:
+            if data['code'] == 4000:
+                raise InvalidID
+
+    def disconnect(self):
+        self._send({}, OP_CLOSE)
+        
+        self.socket.close()
+        self.socket = None
+
+        log.warning("Closing RPC")
+        sys.exit()
+
+class UnixPipe:
+    def __init__(self, app_id, exit_if_discord_close):
+        self.app_id = app_id
+        self.exit_if_discord_close = exit_if_discord_close
+        self.connected = True
+
+        self.socket = socket.socket(socket.AF_UNIX)
+
+        base_path = path = os.environ.get('XDG_RUNTIME_DIR') or os.environ.get('TMPDIR') or os.environ.get('TMP') or os.environ.get('TEMP') or '/tmp'
+        base_path = re.sub(r'\/$', '', path) + '/discord-ipc-{0}'
+
+        for i in range(10):
+            path = base_path.format(i)
+
+            try:
+                self.socket.connect(path)
+                break
+            except FileNotFoundError:
+                pass
+
+        else:
+            if not self.exit_if_discord_close:
+                raise DiscordNotOpened()
+            else:
+                log.debug("Discord seems to be close.")
+                self.connected = False
+
+        if self.connected:
+            log.debug(f"Connected to {path}")
+
+
+    def _recv(self):
+        recv_data = self.socket.recv(1024)
+        enc_header = recv_data[:8]
+        dec_header = struct.unpack("<ii", enc_header)
+        enc_data = recv_data[8:]
+
+        output = json.loads(enc_data.decode('UTF-8'))
+        
+        log.debug(output)
+        return output
+    
+    def _send(self, payload, op=OP_FRAME):
+        log.debug(payload)
+
+        payload = json.dumps(payload).encode('UTF-8')
+        payload = struct.pack('<ii', op, len(payload)) + payload
+
+        self.socket.send(payload)
+    
+    def handshake(self):
+        self._send({'v': 1, 'client_id': self.app_id}, op=OP_HANDSHAKE)
+        data = self._recv()
+
+        try:
+            if data['cmd'] == 'DISPATCH' and data['evt'] == 'READY':
+                log.info(f"Connected to {data['data']['user']['username']} ({data['data']['user']['id']})")
+                return data['data']['user']
+            
+            else:
+                raise RPCException()
+
+        except KeyError:
+            if data['code'] == 4000:
+                raise InvalidID
+    
+    def disconnect(self):
+        self._send({}, OP_CLOSE)
+
+        self.socket.shutdown(socket.SHUT_RDWR)
+        self.socket.close()
+        self.socket = None
+
+        log.warning("Closing RPC")
+        sys.exit()
