2a7e0c7609d37d168ced3a24b6508113af1f2383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:04:47 +0000

    Add package sop-voice/1.0/sop_voice-1.0.tar.gz

---
+++ packages/sop-voice/sop_voice-1.0.tar.gz/sop_voice-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sop_voice",
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description="Manage voice informations of each sites.",
+    author="Leorevoir",
+    author_email="leoquinzler@epitech.eu",
+)
