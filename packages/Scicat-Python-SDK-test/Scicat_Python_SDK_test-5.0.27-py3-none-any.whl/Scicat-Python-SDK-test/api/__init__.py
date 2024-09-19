367ada747ac2b5c2d53469fe5cb34726d3ddb507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 16:03:29 +0000

    Add package Scicat-Python-SDK-test/5.0.27/Scicat_Python_SDK_test-5.0.27-py3-none-any.whl

---
+++ packages/Scicat-Python-SDK-test/Scicat_Python_SDK_test-5.0.27-py3-none-any.whl/Scicat-Python-SDK-test/api/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# import apis into api package
+from Scicat-Python-SDK-test.api.admin_api import AdminApi
+from Scicat-Python-SDK-test.api.auth_api import AuthApi
+from Scicat-Python-SDK-test.api.datasets_api import DatasetsApi
+from Scicat-Python-SDK-test.api.default_api import DefaultApi
+from Scicat-Python-SDK-test.api.elastic_search_api import ElasticSearchApi
+from Scicat-Python-SDK-test.api.instruments_api import InstrumentsApi
+from Scicat-Python-SDK-test.api.jobs_api import JobsApi
+from Scicat-Python-SDK-test.api.logbooks_api import LogbooksApi
+from Scicat-Python-SDK-test.api.origdatablocks_api import OrigdatablocksApi
+from Scicat-Python-SDK-test.api.policies_api import PoliciesApi
+from Scicat-Python-SDK-test.api.proposals_api import ProposalsApi
+from Scicat-Python-SDK-test.api.published_data_api import PublishedDataApi
+from Scicat-Python-SDK-test.api.samples_api import SamplesApi
+from Scicat-Python-SDK-test.api.user_identities_api import UserIdentitiesApi
+from Scicat-Python-SDK-test.api.users_api import UsersApi
+
