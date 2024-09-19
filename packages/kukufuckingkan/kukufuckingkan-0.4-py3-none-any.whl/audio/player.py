353b465c94d3d18ad72c7d70ad356d369c931fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:03:50 +0000

    Add package kukufuckingkan/0.4/kukufuckingkan-0.4-py3-none-any.whl

---
+++ packages/kukufuckingkan/kukufuckingkan-0.4-py3-none-any.whl/audio/player.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 17:47:16 2024
+
+@author: kukukan
+"""
+
+
+from IPython.core.display import HTML
+from IPython.display import HTML
+
+
+def player(filePath: str, env: str) -> str:
+    path = getPath(filePath, env)
+    #print(path)
+    # HTML for the audio player without newlines
+    audio_html = '''<audio controls style="width:150px">
+                      <source src="{}" type="audio/mpeg">
+                      Your browser does not support the audio element.
+                    </audio>'''.format(path)
+    
+    return audio_html
+
+
+def getPath(path:str,env: str) -> str:
+    PATH_RESOURCE = 'resource/'
+    
+    if PATH_RESOURCE in path:
+        if 'DEV' == env:
+            return path.replace(PATH_RESOURCE,'')
+        return path
+    return ''
+
+
+#resource/asset/audio/1_kan.mp3
+# if __name__ == "__main__":
+#     x = "../resource/asset/audio/1_kan.mp3"
+#     y =x.replace("resource/",'')
+#     print(y)
+#     wavPlayer('kan.mp3')
+    
