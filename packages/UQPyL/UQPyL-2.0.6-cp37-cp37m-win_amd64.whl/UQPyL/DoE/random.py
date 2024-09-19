87015a4cea9aa68b531b8fa94a286f6baac3ab78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:03:30 +0000

    Add package UQPyL/2.0.6/UQPyL-2.0.6-cp37-cp37m-win_amd64.whl

---
+++ packages/UQPyL/UQPyL-2.0.6-cp37-cp37m-win_amd64.whl/UQPyL/DoE/random.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from .samplerABC import Sampler
+
+class Random(Sampler):
+    '''
+    Random Design
+    
+    Method:
+    __call__ or sample: Generate a random design
+    
+    Examples:
+        >>> random=RANDOM()
+        >>> random(10,10) or random.sample(10,10)
+    '''
+    def _generate(self,nt: int, nx: int) -> np.ndarray:
+        
+        H=np.random.random((nt,nx))
+        
+        return H
+    
+    def sample(self, nt: int, nx: int) -> np.ndarray:
+        '''
+        Generate a sample with random values between zero and one 
+        '''
+        
+        return self._generate(nt, nx)
