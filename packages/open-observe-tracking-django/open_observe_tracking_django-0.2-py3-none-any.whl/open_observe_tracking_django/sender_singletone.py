5590b2149762d9c0f8624a0dcc41e3bcb5ffb15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:04:28 +0000

    Add package open-observe-tracking-django/0.2/open_observe_tracking_django-0.2-py3-none-any.whl

---
+++ packages/open-observe-tracking-django/open_observe_tracking_django-0.2-py3-none-any.whl/open_observe_tracking_django/sender_singletone.py
@@ -0,0 +1,21 @@
+
+# The sender has to be setted up with setup_open_observe_sender function
+from .open_observe_config import OpenObserveConfig
+from .open_observe_sender import OpenObserveSender
+
+from django.conf import settings
+
+sender = None
+def setup_open_observe_sender()->OpenObserveSender:
+  config_from_settings = settings.OPENOBSERVE_CONFIG
+  config = OpenObserveConfig(
+    username=config_from_settings["username"],
+    password=config_from_settings["password"],
+    open_observe_host=config_from_settings["open_observe_host"],
+    open_observe_organization_name=config_from_settings["open_observe_organization_name"],
+    open_observe_stream_name=config_from_settings["open_observe_stream_name"],
+  )
+  new_sender = OpenObserveSender(config=config)
+  global sender
+  sender = new_sender
+  return new_sender
