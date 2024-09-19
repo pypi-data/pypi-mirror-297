4f98200fdd65421da2ae6ea4bf1a459b09530a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:03:13 +0000

    Add package eth-rpc-py/0.1.8.post7/eth_rpc_py-0.1.8.post7.tar.gz

---
+++ packages/eth-rpc-py/eth_rpc_py-0.1.8.post7.tar.gz/eth_rpc_py-0.1.8.post7/src/eth_rpc/rpc/method/__init__.py
@@ -0,0 +1,22 @@
+from typing import Awaitable, Callable, Generic, ParamSpec, TypeVar
+
+from eth_rpc.types import HexInt, NoArgs
+from eth_typing import HexStr
+from pydantic import BaseModel
+
+from .base import RPCMethodBase
+
+Params = TypeVar(
+    "Params", bound=BaseModel | HexInt | HexStr | list[HexInt | HexStr] | NoArgs
+)
+Response = TypeVar("Response")
+P = ParamSpec("P")
+
+
+class RPCMethod(RPCMethodBase[Params, Response], Generic[Params, Response]):
+    def __call__(self, *params: Params) -> Awaitable[Response]:
+        return self.call_async(*params)
+
+    @property
+    def sync(self) -> Callable[..., Response]:
+        return self.call_sync
