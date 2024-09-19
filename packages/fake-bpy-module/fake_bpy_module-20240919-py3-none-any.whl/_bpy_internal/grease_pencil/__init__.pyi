0940cd20a56f547e471b5ee7b144dceae07090b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:04:54 +0000

    Add package fake-bpy-module/20240919/fake_bpy_module-20240919-py3-none-any.whl

---
+++ packages/fake-bpy-module/fake_bpy_module-20240919-py3-none-any.whl/_bpy_internal/grease_pencil/__init__.pyi
@@ -0,0 +1,7 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import stroke as stroke
+
+_GenericType1 = typing.TypeVar("_GenericType1")
+_GenericType2 = typing.TypeVar("_GenericType2")
