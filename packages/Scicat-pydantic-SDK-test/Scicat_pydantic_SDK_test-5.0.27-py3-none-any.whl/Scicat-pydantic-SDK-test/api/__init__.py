a98069020e941ccc780e2b58c5c6c5c57e67b6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:04:42 +0000

    Add package Scicat-pydantic-SDK-test/5.0.27/Scicat_pydantic_SDK_test-5.0.27-py3-none-any.whl

---
+++ packages/Scicat-pydantic-SDK-test/Scicat_pydantic_SDK_test-5.0.27-py3-none-any.whl/Scicat-pydantic-SDK-test/api/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# import apis into api package
+from Scicat-pydantic-SDK-test.api.admin_api import AdminApi
+from Scicat-pydantic-SDK-test.api.auth_api import AuthApi
+from Scicat-pydantic-SDK-test.api.datasets_api import DatasetsApi
+from Scicat-pydantic-SDK-test.api.default_api import DefaultApi
+from Scicat-pydantic-SDK-test.api.elastic_search_api import ElasticSearchApi
+from Scicat-pydantic-SDK-test.api.instruments_api import InstrumentsApi
+from Scicat-pydantic-SDK-test.api.jobs_api import JobsApi
+from Scicat-pydantic-SDK-test.api.logbooks_api import LogbooksApi
+from Scicat-pydantic-SDK-test.api.origdatablocks_api import OrigdatablocksApi
+from Scicat-pydantic-SDK-test.api.policies_api import PoliciesApi
+from Scicat-pydantic-SDK-test.api.proposals_api import ProposalsApi
+from Scicat-pydantic-SDK-test.api.published_data_api import PublishedDataApi
+from Scicat-pydantic-SDK-test.api.samples_api import SamplesApi
+from Scicat-pydantic-SDK-test.api.user_identities_api import UserIdentitiesApi
+from Scicat-pydantic-SDK-test.api.users_api import UsersApi
+
