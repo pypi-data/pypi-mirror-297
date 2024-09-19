f89ea651749841ea9c803ec0bddd833d6399a14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:05:24 +0000

    Add package langchain-teddynote/0.0.32/langchain_teddynote-0.0.32-py3-none-any.whl

---
+++ packages/langchain-teddynote/langchain_teddynote-0.0.32-py3-none-any.whl/langchain_teddynote/evaluator.py
@@ -0,0 +1,94 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+# 데이터 모델
+class GradeRetrievalQuestion(BaseModel):
+    """A binary score to determine the relevance of the retrieved documents to the question."""
+
+    score: str = Field(
+        description="Whether the retrieved context is relevant to the question, 'yes' or 'no'"
+    )
+
+
+# 데이터 모델
+class GradeRetrievalAnswer(BaseModel):
+    """A binary score to determine the relevance of the retrieved documents to the answer."""
+
+    score: str = Field(
+        description="Whether the retrieved context is relevant to the answer, 'yes' or 'no'"
+    )
+
+
+class OpenAIRelevanceGrader:
+    """
+    OpenAI 기반의 관련성 평가기 클래스입니다.
+
+    이 클래스는 검색된 문서가 주어진 질문이나 답변과 얼마나 관련이 있는지 평가합니다.
+    'retrieval-question' 또는 'retrieval-answer' 두 가지 모드로 작동할 수 있습니다.
+
+    Attributes:
+        llm: 사용할 언어 모델 인스턴스
+        structured_llm_grader: 구조화된 출력을 생성하는 LLM 인스턴스
+        grader_prompt: 평가에 사용될 프롬프트 템플릿
+
+    Args:
+        llm: 사용할 언어 모델 인스턴스
+        target (str): 평가 대상 ('retrieval-question' 또는 'retrieval-answer')
+    """
+
+    def __init__(self, llm, target="retrieval-question"):
+        """
+        OpenAIRelevanceGrader 클래스의 초기화 메서드입니다.
+
+        Args:
+            llm: 사용할 언어 모델 인스턴스
+            target (str): 평가 대상 ('retrieval-question' 또는 'retrieval-answer')
+
+        Raises:
+            ValueError: 유효하지 않은 target 값이 제공될 경우 발생
+        """
+        self.llm = llm
+
+        if target == "retrieval-question":
+            self.structured_llm_grader = llm.with_structured_output(
+                GradeRetrievalQuestion
+            )
+        elif target == "retrieval-answer":
+            self.structured_llm_grader = llm.with_structured_output(
+                GradeRetrievalAnswer
+            )
+        else:
+            raise ValueError(f"Invalid target: {target}")
+
+        # 프롬프트
+        target_variable = (
+            "user question" if target == "retrieval-question" else "answer"
+        )
+        system = f"""You are a grader assessing relevance of a retrieved document to a {target_variable}. \n 
+            It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+            If the document contains keyword(s) or semantic meaning related to the {target_variable}, grade it as relevant. \n
+            Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to {target_variable}."""
+
+        grade_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system),
+                (
+                    "human",
+                    f"Retrieved document: \n\n {{context}} \n\n {target_variable}: {{input}}",
+                ),
+            ]
+        )
+        self.grader_prompt = grade_prompt
+
+    def create(self):
+        """
+        관련성 평가기를 생성하고 반환합니다.
+
+        Returns:
+            관련성 평가를 수행할 수 있는 체인 객체
+        """
+
+        retrieval_grader_oai = self.grader_prompt | self.structured_llm_grader
+        return retrieval_grader_oai
