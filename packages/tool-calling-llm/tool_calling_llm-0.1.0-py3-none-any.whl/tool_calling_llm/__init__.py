0648ff8259a0452f0973d9581efa15fcdb8b96f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:04:08 +0000

    Add package tool-calling-llm/0.1.0/tool_calling_llm-0.1.0-py3-none-any.whl

---
+++ packages/tool-calling-llm/tool_calling_llm-0.1.0-py3-none-any.whl/tool_calling_llm/__init__.py
@@ -0,0 +1,9 @@
+from .tool_calling_llm import ToolCallingLLM
+from .tool_calling_llm import DEFAULT_SYSTEM_TEMPLATE
+from .tool_calling_llm import DEFAULT_RESPONSE_FUNCTION
+
+__all__ = [
+    "ToolCallingLLM",
+    "DEFAULT_SYSTEM_TEMPLATE",
+    "DEFAULT_RESPONSE_FUNCTION"
+]
