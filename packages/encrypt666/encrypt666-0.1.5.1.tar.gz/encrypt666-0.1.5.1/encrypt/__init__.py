72a8fc4d508f2220478a7b1d84e9804567d97a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:03:59 +0000

    Add package encrypt666/0.1.5.1/encrypt666-0.1.5.1.tar.gz

---
+++ packages/encrypt666/encrypt666-0.1.5.1.tar.gz/encrypt666-0.1.5.1/encrypt/__init__.py
@@ -0,0 +1,7 @@
+# encrypt666/__init__.py
+
+"""
+Encrypt666 Package
+"""
+
+# Optionally, you can include any package-level imports here.
---
+++ packages/encrypt666/encrypt666-0.1.5.1.tar.gz/encrypt666-0.1.5.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='encrypt666',
+    version='0.1.5.1',
+    packages=find_packages(),  # This finds the 'encrypt666' package
+    install_requires=[],
+    description='A custom encryption package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/encrypt666',
+    entry_points={
+        'console_scripts': [
+            'encrypt666=encrypt666.encrypt666:main',  # Make sure this path is correct
+        ],
+    },
+)
