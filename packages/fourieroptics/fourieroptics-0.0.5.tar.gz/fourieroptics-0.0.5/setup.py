59882356b6da1fb215b96a15de264ab086c77ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 14:04:37 +0000

    Add package fourieroptics/0.0.5/fourieroptics-0.0.5.tar.gz

---
+++ packages/fourieroptics/fourieroptics-0.0.5.tar.gz/fourieroptics-0.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.5"
+DESCRIPTION = "Library for Fourier Optics Helper Functions"
+
+# Setting up
+setup(
+    name="fourieroptics",
+    version=VERSION,
+    author="Bretton Scarbrough",
+    author_email="<bretton.scarbrough@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["numpy>=2.0.0", "scipy"],
+    keywords=[
+        "python",
+        "fourier",
+        "optics",
+        "fourier optics",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
