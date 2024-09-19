3c832dcaecd7fab6ad935b9b090148b747c78027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 04:04:07 +0000

    Add package scarfer/0.5.3/scarfer-0.5.3.tar.gz

---
+++ packages/scarfer/scarfer-0.5.3.tar.gz/scarfer-0.5.3/scarfer/format/interface.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2023 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+class Settings:
+    def __init__(self, copyrights=False, licenses=False, matches=False, cumulative=False, license_summary=False, copyright_summary=False):
+        self.settings_map = {}
+        self.settings_map['copyrights'] = copyrights
+        self.settings_map['licenses'] = licenses
+        self.settings_map['matches'] = matches
+        self.settings_map['cumulative'] = cumulative
+        self.settings_map['license_summary'] = license_summary
+        self.settings_map['copyright_summary'] = copyright_summary
+
+    def get(self, key):
+        # print(" get " + key + " from " + str(self.settings_map) + " =====> " + str(self.settings_map.get(key, False)))
+        return self.settings_map.get(key, False)
+
+class FormatInterface:
+
+    def __init__(self):
+        return
+
+    def format(self, report, settings={}):
+        return
+
+    def format_fixes(self, report, settings={}):
+        return
+
+    def format_license_summary(self, report, settings={}):
+        return
+
+    def format_copyright_summary(self, report, settings={}):
+        return
+
+    def format_cumulative(self, report, settings={}):
+        return
