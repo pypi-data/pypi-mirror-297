8ac586b672548af23c1b9f6177a11ea9f1e14169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 03:05:25 +0000

    Add package solanapy-toolkit/0.0.3/solanapy_toolkit-0.0.3.tar.gz

---
+++ packages/solanapy-toolkit/solanapy_toolkit-0.0.3.tar.gz/solanapy_toolkit-0.0.3/src/solanapy_toolkit/lib/Constants.py
@@ -0,0 +1,4 @@
+CONVERSION_FACTOR = 9
+LAMPORT_PER_SOL = 10**CONVERSION_FACTOR
+SOL_PER_LAMPORT = 1 / LAMPORT_PER_SOL
+VALID_COMMIT_MENTS = ["processed", "confirmed", "finalized", "recent", "single", "root", "max"]
