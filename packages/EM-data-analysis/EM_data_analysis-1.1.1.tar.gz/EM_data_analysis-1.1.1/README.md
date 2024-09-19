d79866c7b1ee08c1092ee4ed9b7f502ac1c37719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 16:03:26 +0000

    Add package EM-data-analysis/1.1.1/EM_data_analysis-1.1.1.tar.gz

---
+++ packages/EM-data-analysis/EM_data_analysis-1.1.1.tar.gz/EM_data_analysis-1.1.1/README.md
@@ -0,0 +1,39 @@
+# Description
+This project provides a useful tool for analyzing electron microscopy data. 
+More specifically, the `morph_analysis.py` module offers various functions that iteratively analyze several morphological properties of networks (such as porosity, tortuosity, fractal dimension, etc.) obtained from in-situ measurement movies. This module is highly valuable as it facilitates the investigation of how specific properties evolve over time, thereby reducing the time required for data analysis.
+
+# Features
+- Normalization for a better image contrast (optional)
+- Scalebar calibration 
+- ROI selection 
+- Plotting a specific property value as a function of time
+- Generating a .csv file for a subsequent analysis with other programs (such as curve fitting)
+- Provides images and corresponding tables for the segmentation measurements
+
+# Installation
+`EM_data_analysis` requires Python 3.6 or above. 
+
+### Installation using `pip` (recommended):
+```bash
+pip install EM_data_analysis
+```
+  
+# Usage
+Create an empty directory (will be used to store all the frames created from the movie). Then, simply input the path of the movie and the directory created to store the frames.
+
+```python
+from EM_data_analysis import morph_analysis as ma
+
+ma.Network_analysis("path-of-the-movie", "path-of-the-directory") 
+```
+
+
+The movie format must be explicit and could be in AVI or MP4 format.
+
+# Contributions
+`EM_data_analysis` is created by Mattia Lizzano, Giorgio Divitini and the Electron Microscopy and Nanoscopy group of Italian Institute of Technology (IIT)
+
+
+
+
+
---
+++ packages/EM-data-analysis/EM_data_analysis-1.1.1.tar.gz/EM_data_analysis-1.1.1/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+import subprocess
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+class PostInstallCommand(install):
+    def run(self):
+        try:
+            subprocess.check_call([os.sys.executable, "-m", "pip", "install", "--upgrade", "nbconvert"])
+            
+            installed_packages = subprocess.check_output([os.sys.executable, "-m", "pip", "freeze"]).decode("utf-8")
+            if "mistune==3.0.2" in installed_packages:
+                subprocess.check_call([os.sys.executable, "-m", "pip", "uninstall", "-y", "mistune"])
+            
+            subprocess.check_call([os.sys.executable, "-m", "pip", "install", "mistune==2.0.4"])
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred during post-install: {e}")
+            
+        install.run(self)
+
+setup(
+    name='EM_data_analysis',  
+    version='1.1.1',  
+    packages=find_packages(),  
+    install_requires=[
+        'opencv-python',       
+        'numpy',               
+        'porespy',             
+        'matplotlib',          
+        'openpnm',             
+        'scipy',              
+        'pandas',              
+        'moviepy',             
+        'IPython',             
+        'scikit-image',      
+        'dataframe-image',
+    ],
+    description='A Python package for EM data analysis',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Mattia Lizzano, Giorgio Divitini',
+    author_email='mattializzano@gmail.com',
+    url='https://github.com/Nomeutente1/EM_data_analysis',  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+)
+
