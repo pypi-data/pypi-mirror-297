b90c6b710daa38b336b85748b38f64cb6e64f48e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:05:21 +0000

    Add package EasyFlowQ/1.5.7/easyflowq-1.5.7-py3-none-any.whl

---
+++ packages/EasyFlowQ/easyflowq-1.5.7-py3-none-any.whl/EasyFlowQ/window_About.py
@@ -0,0 +1,14 @@
+from PyQt5 import uic
+from os import path, getcwd
+
+__location__ = path.realpath(path.join(getcwd(), path.dirname(__file__)))
+wUi, wBase = uic.loadUiType(path.join(__location__, 'uiDesigns/AboutWindow.ui')) # Load the .ui file
+
+class aboutWindow(wUi, wBase):
+    def __init__(self) -> None:
+
+        wBase.__init__(self)
+        self.setupUi(self)
+
+if __name__ == '__main__':
+    pass
