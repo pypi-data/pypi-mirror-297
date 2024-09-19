0016e67c770677acdc0deb753b822ea8cef0ee0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-19 15:03:17 +0000

    Add package bunny-sdk/0.0.15/bunny_sdk-0.0.15-py3-none-any.whl

---
+++ packages/bunny-sdk/bunny_sdk-0.0.15-py3-none-any.whl/bunny_sdk/edge_storage_api_client/item/with_storage_zone_name_item_request_builder.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+from kiota_abstractions.base_request_builder import BaseRequestBuilder
+from kiota_abstractions.base_request_configuration import RequestConfiguration
+from kiota_abstractions.default_query_parameters import QueryParameters
+from kiota_abstractions.get_path_parameters import get_path_parameters
+from kiota_abstractions.method import Method
+from kiota_abstractions.request_adapter import RequestAdapter
+from kiota_abstractions.request_information import RequestInformation
+from kiota_abstractions.request_option import RequestOption
+from kiota_abstractions.serialization import Parsable, ParsableFactory
+from typing import Any, Callable, Dict, List, Optional, TYPE_CHECKING, Union
+from warnings import warn
+
+if TYPE_CHECKING:
+    from .item.with_path_item_request_builder import WithPathItemRequestBuilder
+    from .with_storage_zone_name_post_request_body import WithStorageZoneNamePostRequestBody
+
+class WithStorageZoneNameItemRequestBuilder(BaseRequestBuilder):
+    """
+    Builds and executes requests for operations under /{storageZoneName}
+    """
+    def __init__(self,request_adapter: RequestAdapter, path_parameters: Union[str, Dict[str, Any]]) -> None:
+        """
+        Instantiates a new WithStorageZoneNameItemRequestBuilder and sets the default values.
+        param path_parameters: The raw url or the url-template parameters for the request.
+        param request_adapter: The request adapter to use to execute the requests.
+        Returns: None
+        """
+        super().__init__(request_adapter, "{+baseurl}/{storageZoneName}", path_parameters)
+    
+    def by_path(self,path: str) -> WithPathItemRequestBuilder:
+        """
+        Gets an item from the EdgeStorageApiClient.item.item collection
+        param path: The directory path that you want to list.
+        Returns: WithPathItemRequestBuilder
+        """
+        if path is None:
+            raise TypeError("path cannot be null.")
+        from .item.with_path_item_request_builder import WithPathItemRequestBuilder
+
+        url_tpl_params = get_path_parameters(self.path_parameters)
+        url_tpl_params["path"] = path
+        return WithPathItemRequestBuilder(self.request_adapter, url_tpl_params)
+    
+    async def post(self,body: WithStorageZoneNamePostRequestBody, request_configuration: Optional[RequestConfiguration[QueryParameters]] = None) -> bytes:
+        """
+        [DownloadZip API Docs](https://toshy.github.io/BunnyNet-PHP/edge-storage-api/#download-zip)
+        param body: The request body
+        param request_configuration: Configuration for the request such as headers, query parameters, and middleware options.
+        Returns: bytes
+        """
+        if body is None:
+            raise TypeError("body cannot be null.")
+        request_info = self.to_post_request_information(
+            body, request_configuration
+        )
+        if not self.request_adapter:
+            raise Exception("Http core is null") 
+        return await self.request_adapter.send_primitive_async(request_info, "bytes", None)
+    
+    def to_post_request_information(self,body: WithStorageZoneNamePostRequestBody, request_configuration: Optional[RequestConfiguration[QueryParameters]] = None) -> RequestInformation:
+        """
+        [DownloadZip API Docs](https://toshy.github.io/BunnyNet-PHP/edge-storage-api/#download-zip)
+        param body: The request body
+        param request_configuration: Configuration for the request such as headers, query parameters, and middleware options.
+        Returns: RequestInformation
+        """
+        if body is None:
+            raise TypeError("body cannot be null.")
+        request_info = RequestInformation(Method.POST, self.url_template, self.path_parameters)
+        request_info.configure(request_configuration)
+        request_info.headers.try_add("Accept", "application/octet-stream")
+        request_info.set_content_from_parsable(self.request_adapter, "application/json", body)
+        return request_info
+    
+    def with_url(self,raw_url: str) -> WithStorageZoneNameItemRequestBuilder:
+        """
+        Returns a request builder with the provided arbitrary URL. Using this method means any other path or query parameters are ignored.
+        param raw_url: The raw URL to use for the request builder.
+        Returns: WithStorageZoneNameItemRequestBuilder
+        """
+        if raw_url is None:
+            raise TypeError("raw_url cannot be null.")
+        return WithStorageZoneNameItemRequestBuilder(self.request_adapter, raw_url)
+    
+    @dataclass
+    class WithStorageZoneNameItemRequestBuilderPostRequestConfiguration(RequestConfiguration[QueryParameters]):
+        """
+        Configuration for the request such as headers, query parameters, and middleware options.
+        """
+        warn("This class is deprecated. Please use the generic RequestConfiguration class generated by the generator.", DeprecationWarning)
+    
+
