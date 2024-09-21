5e10ac6cdd0da0c6baaf2151736a8ca108f5dc93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:07:47 +0000

    Add package bazpy/0.0.10/bazpy-0.0.10.tar.gz

---
+++ packages/bazpy/bazpy-0.0.10.tar.gz/bazpy-0.0.10/tests/testbase_.py
@@ -0,0 +1,55 @@
+from src.bazpy.testing.testbase import TestBase
+from pathlib import Path
+
+
+class TestBase_(TestBase):
+    def test_has_basic_stuff(self):
+        self.assertIsInstance(self.few, int)
+        self.assertIsInstance(self.some, int)
+        self.assertIsInstance(self.many, int)
+
+    assertLen_cases = [
+        (False, [], -1),
+        (True, [], 0),
+        (False, [], 1),
+    ]
+
+    def test_assertLen(self):
+        for should, input, expect in self.assertLen_cases:
+            with self.subTest():
+                if should:
+                    self.assertLen(input, expect)
+                else:
+                    self.assertFails(self.assertLen, input, expect)
+
+    assertBetween_cases = [
+        (False, 0.3, 1, 2.5),
+        (True, 1.3, 1, 2.5),
+        (False, 2.51, 1, 2.5),
+    ]
+
+    def test_assertBetween(self):
+        for should, value, lower, upper in self.assertBetween_cases:
+            with self.subTest():
+                if should:
+                    self.assertBetween(value, lower, upper)
+                else:
+                    self.assertFails(self.assertBetween, value, lower, upper)
+
+    def test_assertIsFile(self):
+        path = "./tests/scratch/baz.txt"
+        self.assertIsNotFile(path)
+        handle = Path(path)
+        handle.touch()
+        self.assertIsFile(path)
+        handle.unlink()
+        self.assertIsNotFile(path)
+
+    def test_assertIsFile(self):
+        path = "./tests/scratch/bazdir"
+        self.assertIsNotDir(path)
+        handle = Path(path)
+        handle.mkdir(parents=True, exist_ok=True)
+        self.assertIsDir(path)
+        handle.rmdir()
+        self.assertIsNotDir(path)
