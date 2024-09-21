9d53a93b86a7d60a397df2fa5d8b320c713a21be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 09:04:23 +0000

    Add package RTPSender/3.9.3/rtpsender-3.9.3.tar.gz

---
+++ packages/RTPSender/rtpsender-3.9.3.tar.gz/rtpsender-3.9.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RTPSender",
+    version="3.9.3",
+    author="liushihai02",
+    author_email="liushihai02@58.com",
+    packages=find_packages(),
+    install_requires=[
+        'scapy',
+        'opencv-python',
+        'pydub',
+        'loguru'
+        # 'av'
+        # Note: Do not include 'av' here as it will be handled separately
+    ],
+    description="A SDK for sending RTP streams",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3.9',
+)
