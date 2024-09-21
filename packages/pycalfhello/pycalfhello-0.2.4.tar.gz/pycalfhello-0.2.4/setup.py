2cbff1b51a8fc5f965eb9af388877c795d216f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:04:12 +0000

    Add package pycalfhello/0.2.4/pycalfhello-0.2.4.tar.gz

---
+++ packages/pycalfhello/pycalfhello-0.2.4.tar.gz/pycalfhello-0.2.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+class PostInstallCommand(install):
+    def run(self):
+        with open('post_install_log.txt', 'w') as f:
+            f.write("Post-installation script executed successfully!")
+        install.run(self)
+
+setup(
+    name='pycalfhello',
+    version='0.2.4',
+    packages=find_packages(),
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+)
