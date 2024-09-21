937d26ba5a2ba550d9f446d8eb68b57c54d6c2f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:03:59 +0000

    Add package langchain-graphrag/0.0.5/langchain_graphrag-0.0.5.tar.gz

---
+++ packages/langchain-graphrag/langchain_graphrag-0.0.5.tar.gz/langchain_graphrag-0.0.5/src/langchain_graphrag/indexing/graph_generation/generator.py
@@ -0,0 +1,24 @@
+import networkx as nx
+import pandas as pd
+
+from .entity_relationship_extraction import EntityRelationshipExtractor
+from .entity_relationship_summarization import EntityRelationshipDescriptionSummarizer
+from .graphs_merger import GraphsMerger
+
+
+class GraphGenerator:
+    def __init__(
+        self,
+        er_extractor: EntityRelationshipExtractor,
+        graphs_merger: GraphsMerger,
+        er_description_summarizer: EntityRelationshipDescriptionSummarizer,
+    ):
+        self._er_extractor = er_extractor
+        self._graphs_merger = graphs_merger
+        self._er_description_summarizer = er_description_summarizer
+
+    def run(self, text_units: pd.DataFrame) -> tuple[nx.Graph, nx.Graph]:
+        er_graphs = self._er_extractor.invoke(text_units)
+        er_merged_graph = self._graphs_merger(er_graphs)
+        er_summarized_graph = self._er_description_summarizer.invoke(er_merged_graph)
+        return er_merged_graph, er_summarized_graph
