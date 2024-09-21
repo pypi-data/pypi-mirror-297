ca88c76cb5887f82100e7d246a9f4d32109e6e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:06:09 +0000

    Add package telliot-feeds/0.1.28/telliot_feeds-0.1.28.tar.gz

---
+++ packages/telliot-feeds/telliot_feeds-0.1.28.tar.gz/telliot_feeds-0.1.28/src/telliot_feeds/feeds/btc_usd_feed.py
@@ -0,0 +1,22 @@
+from telliot_feeds.datafeed import DataFeed
+from telliot_feeds.queries.price.spot_price import SpotPrice
+from telliot_feeds.sources.price.spot.binance import BinanceSpotPriceSource
+from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
+from telliot_feeds.sources.price.spot.gemini import GeminiSpotPriceSource
+from telliot_feeds.sources.price.spot.kraken import KrakenSpotPriceSource
+from telliot_feeds.sources.price_aggregator import PriceAggregator
+
+btc_usd_median_feed = DataFeed(
+    query=SpotPrice(asset="BTC", currency="USD"),
+    source=PriceAggregator(
+        asset="btc",
+        currency="usd",
+        algorithm="median",
+        sources=[
+            CoinGeckoSpotPriceSource(asset="btc", currency="usd"),
+            BinanceSpotPriceSource(asset="btc", currency="usdt"),
+            GeminiSpotPriceSource(asset="btc", currency="usd"),
+            KrakenSpotPriceSource(asset="xbt", currency="usd"),
+        ],
+    ),
+)
