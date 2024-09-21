f28b0dc62170e0c4c285d39669a6e2c29446e8ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:08:45 +0000

    Add package colortimelog/0.0.4/colortimelog-0.0.4-py3-none-any.whl

---
+++ packages/colortimelog/colortimelog-0.0.4-py3-none-any.whl/colortimelog/__init__.py
@@ -0,0 +1,9 @@
+"""__init__ file."""
+
+from . import colortimelog
+
+BColors = colortimelog.BColors
+TimeLog = colortimelog.TimeLog
+timeblock = colortimelog.timeblock
+timefunc = colortimelog.timefunc
+Logger = colortimelog.Logger
