338e0ae007d8d98adc7052172d54d9ef26248f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 09:04:26 +0000

    Add package amz-extractor/1.0.5/amz_extractor-1.0.5.tar.gz

---
+++ packages/amz-extractor/amz_extractor-1.0.5.tar.gz/amz_extractor-1.0.5/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(name='amz_extractor',
+      version='1.0.5',
+      description='提取亚马逊详情页和评论信息',
+      author='lonely',
+      packages=['amz_extractor'],
+      package_dir={'amz_extractor': 'amz_extractor'},
+      install_requires=['dateparser>=1.1.4', 'pyquery>=1.4.3']
+      )
+
+"""
+# 更新版本命令
+
+python setup.py sdist bdist_wheel
+
+twine upload dist/*
+
+pypi-AgEIcHlwaS5vcmcCJDVhM2M0Mzg5LTRhMTktNGQ5Ni1hZDVkLTU3MjdlYjExMGJmYQACKlszLCJiYWRlYjNjMS1lNDE2LTQ2MzEtYjYyMy1lNGIwZTFiNzYyNTYiXQAABiB1iKdrpSyPn-PngiJ_nqwKIVSDpQtRPOJu4XY-4doy-A
+
+"""
