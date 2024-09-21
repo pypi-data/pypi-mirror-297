d71df1df3ecb719ebb5b5532a3267b459ca90dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 11:03:43 +0000

    Add package simple-math-engine/0.1/simple-math-engine-0.1.tar.gz

---
+++ packages/simple-math-engine/simple-math-engine-0.1.tar.gz/simple-math-engine-0.1/README.md
@@ -0,0 +1,7 @@
+# Simple Math Engine
+A simple Python library for basic and advanced mathematical operations.
+
+## Installation
+
+```bash
+pip install simple-math-engine
---
+++ packages/simple-math-engine/simple-math-engine-0.1.tar.gz/simple-math-engine-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple-math-engine',
+    version='0.1',
+    description='A simple math utility library',
+    author='Kirafin',
+    author_email='kirafin.jacques@bahwancybertek.com',
+    packages=find_packages(),
+    install_requires=[],  # Add external dependencies if needed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
