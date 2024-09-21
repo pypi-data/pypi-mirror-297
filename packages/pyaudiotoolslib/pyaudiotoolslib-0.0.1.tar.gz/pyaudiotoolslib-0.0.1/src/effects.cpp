a942de8b798fc8ac9a60d02ebe0a8d40dc7e644e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:03:55 +0000

    Add package pyaudiotoolslib/0.0.1/pyaudiotoolslib-0.0.1.tar.gz

---
+++ packages/pyaudiotoolslib/pyaudiotoolslib-0.0.1.tar.gz/pyaudiotoolslib-0.0.1/src/effects.cpp
@@ -0,0 +1,36 @@
+#include "effects.h"
+
+#include "AudioFile.h"
+
+void remove_clicks_from_file(
+        const std::filesystem::path &input_file,
+        const std::filesystem::path &output_file,
+        int mThresholdLevel,
+        int mClickWidth
+) {
+    AudioFile(input_file)
+            .remove_clicks(mThresholdLevel, mClickWidth)
+            .save(output_file);
+}
+
+
+void normalize_from_file(
+        const std::filesystem::path &input_file,
+        const std::filesystem::path &output_file,
+        double mPeakLevel,
+        bool mRemoveDC,
+        bool mStereoIndependent
+) {
+    AudioFile(input_file)
+            .normalize(mPeakLevel, mRemoveDC, mStereoIndependent)
+            .save(output_file);
+}
+
+
+float measure_rms_linear_from_file(const std::filesystem::path &input_file) {
+    return AudioFile(input_file).rms_linear();
+}
+
+float measure_rms_db_from_file(const std::filesystem::path &input_file) {
+    return AudioFile(input_file).rms_db();
+}
