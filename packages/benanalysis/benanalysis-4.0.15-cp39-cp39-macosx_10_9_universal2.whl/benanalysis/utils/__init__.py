745b543bebc555a296a2602f14b6c571da39bb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:03:38 +0000

    Add package benanalysis/4.0.15/benanalysis-4.0.15-cp39-cp39-macosx_10_9_universal2.whl

---
+++ packages/benanalysis/benanalysis-4.0.15-cp39-cp39-macosx_10_9_universal2.whl/benanalysis/utils/__init__.py
@@ -0,0 +1,9 @@
+#
+# @file __init__.py
+# @author Markus Führer
+# @date 5 May 2023
+# @copyright Copyright © 2023 Bentham Instruments Ltd. All Rights Reserved.
+#
+
+# import benanalysis._benpy_core.colorimetry as colorimetry
+from benanalysis._benpy_core.utils import *
