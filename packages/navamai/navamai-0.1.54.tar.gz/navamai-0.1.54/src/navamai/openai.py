21a196cec663dcfc511f94c307e6e29a944ab1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 08:03:51 +0000

    Add package navamai/0.1.54/navamai-0.1.54.tar.gz

---
+++ packages/navamai/navamai-0.1.54.tar.gz/navamai-0.1.54/src/navamai/openai.py
@@ -0,0 +1,65 @@
+import base64
+from typing import Generator
+
+from openai import OpenAI
+
+import navamai.configure as configure
+from navamai.provider import Provider
+
+
+class Openai(Provider):
+    def __init__(self):
+        super().__init__()
+        self.client = OpenAI()
+        self.full_config = configure.load_config()
+
+    def create_request_data(self, prompt: str) -> dict:
+        config = self.model_config
+        model = self.resolve_model(config["model"])
+        return {
+            "model": model,
+            "max_tokens": config["max-tokens"],
+            "temperature": config["temperature"],
+            "messages": [
+                {"role": "system", "content": config["system"]},
+                {"role": "user", "content": prompt},
+            ],
+        }
+
+    def stream_response(self, prompt: str) -> Generator[str, None, None]:
+        request_data = self.create_request_data(prompt)
+        for chunk in self.client.chat.completions.create(**request_data, stream=True):
+            if chunk.choices[0].delta.content is not None:
+                yield chunk.choices[0].delta.content
+
+    def create_vision_request_data(self, image_data: bytes, prompt: str) -> dict:
+        config = self.model_config
+        model = self.resolve_model(config["model"])
+        return {
+            "model": model,
+            "max_tokens": config["max-tokens"],
+            "temperature": config["temperature"],
+            "messages": [
+                {"role": "system", "content": config["system"]},
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64.b64encode(image_data).decode('utf-8')}"
+                            },
+                        },
+                        {"type": "text", "text": prompt},
+                    ],
+                },
+            ],
+        }
+
+    def stream_vision_response(
+        self, image_data: bytes, prompt: str, media_type: str
+    ) -> Generator[str, None, None]:
+        request_data = self.create_vision_request_data(image_data, prompt)
+        for chunk in self.client.chat.completions.create(**request_data, stream=True):
+            if chunk.choices[0].delta.content is not None:
+                yield chunk.choices[0].delta.content
