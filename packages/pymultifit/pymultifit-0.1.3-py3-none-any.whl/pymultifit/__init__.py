41d6fb0dbfa7897ae37acca908b4cb7abdebec53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 11:04:34 +0000

    Add package pymultifit/0.1.3/pymultifit-0.1.3-py3-none-any.whl

---
+++ packages/pymultifit/pymultifit-0.1.3-py3-none-any.whl/pymultifit/__init__.py
@@ -0,0 +1,20 @@
+"""Created on Jul 18 00:15:42 2024"""
+
+import numpy as np
+
+EPSILON = np.finfo(float).eps
+
+GAUSSIAN = 'gaussian'
+NORMAL = GAUSSIAN
+
+LOG_NORMAL = 'log_normal'
+SKEW_NORMAL = 'skew_normal'
+LAPLACE = 'laplace'
+GAMMA = 'gamma'
+BETA = 'beta'
+ARCSINE = 'arcSine'
+
+LINE = 'line'
+LINEAR = LINE
+QUADRATIC = 'quad'
+CUBIC = 'cubic'
