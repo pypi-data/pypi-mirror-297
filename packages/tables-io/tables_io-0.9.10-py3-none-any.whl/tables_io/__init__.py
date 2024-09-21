dcd3d205d85e07f5c3681848cecdd3ec685f5bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 03:05:42 +0000

    Add package tables-io/0.9.10/tables_io-0.9.10-py3-none-any.whl

---
+++ packages/tables-io/tables_io-0.9.10-py3-none-any.whl/tables_io/__init__.py
@@ -0,0 +1,47 @@
+"""tables_io is a library of functions for input, output and conversion of tabular data formats"""
+
+try:
+    from ._version import version
+except:  # pylint: disable=bare-except   #pragma: no cover
+    version = "unknown"
+
+from .lazy_modules import *
+
+from .tableDict import TableDict
+
+from . import convUtils as conv
+
+from . import ioUtils as io
+
+from . import concatUtils
+
+from . import sliceUtils
+
+
+convertObj = conv.convertObj
+
+convert = conv.convert
+
+writeNative = io.writeNative
+
+write = io.write
+
+readNative = io.readNative
+
+read = io.read
+
+io_open = io.io_open
+
+iteratorNative = io.iteratorNative
+
+iterator = io.iterator
+
+concatObjs = concatUtils.concatObjs
+
+concat = concatUtils.concat
+
+sliceObj = sliceUtils.sliceObj
+
+sliceObjs = sliceUtils.sliceObjs
+
+check_columns = io.check_columns
