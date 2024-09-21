dafa5896bcacca842b7ea48e9ede9ce99f1e83a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:57 +0000

    Add package tatc/3.2.2/tatc-3.2.2.tar.gz

---
+++ packages/tatc/tatc-3.2.2.tar.gz/tatc-3.2.2/src/tatc/schemas/__init__.py
@@ -0,0 +1,15 @@
+"""
+Defines object schemas.
+"""
+
+from .architecture import Architecture
+from .instrument import Instrument
+from .orbit import TwoLineElements, CircularOrbit, SunSynchronousOrbit, KeplerianOrbit
+from .point import Point, GroundStation
+from .satellite import (
+    Satellite,
+    TrainConstellation,
+    WalkerConfiguration,
+    WalkerConstellation,
+    MOGConstellation,
+)
