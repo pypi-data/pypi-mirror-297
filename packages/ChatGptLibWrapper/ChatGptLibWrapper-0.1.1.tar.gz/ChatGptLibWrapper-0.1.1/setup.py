9d5f5b779ea4b9f4c4794b89fd92970492865948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:08:40 +0000

    Add package ChatGptLibWrapper/0.1.1/ChatGptLibWrapper-0.1.1.tar.gz

---
+++ packages/ChatGptLibWrapper/ChatGptLibWrapper-0.1.1.tar.gz/ChatGptLibWrapper-0.1.1/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+setup(
+    name="ChatGptLibWrapper",
+    version="0.1.1",  # Update to a new version
+    packages=find_packages(),
+    install_requires=["openai"],
+    author="Shai",
+    description="A simple ChatGPT wrapper",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
