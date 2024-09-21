0ab9125a20d567eb99ef21c45118de3cdded5da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 04:04:11 +0000

    Add package nonebot-plugin-yareminder/0.1.5/nonebot_plugin_yareminder-0.1.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-yareminder/nonebot_plugin_yareminder-0.1.5-py3-none-any.whl/nonebot_plugin_yareminder/__init__.py
@@ -0,0 +1,30 @@
+from nonebot import require
+from nonebot.plugin import PluginMetadata
+
+from .command import rmd_app
+
+require('nonebot_plugin_saa')
+from nonebot_plugin_saa import __plugin_meta__ as __saa_plugin_meta__
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot-plugin-yareminder",
+    description="Yet another reminder plugin supporting per-task configuration and check-in.",
+    usage=":rmd COMMAND [ARGS] [OPTIONS]",
+    type="application",
+    homepage="https://github.com/yao-yun/nonebot-plugin-yareminder",
+    supported_adapters=__saa_plugin_meta__.supported_adapters
+)
+
+from nonebot_plugin_saa import enable_auto_select_bot
+enable_auto_select_bot()
+
+require("nonebot_plugin_localstore")
+from nonebot_plugin_localstore import get_data_dir
+
+require("nonebot_plugin_apscheduler")
+from nonebot_plugin_apscheduler import scheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+scheduler.add_jobstore(
+    SQLAlchemyJobStore(url="sqlite:///" + str(get_data_dir(__plugin_meta__.name) / "apscheduler.sqlite3")),
+    alias='nonebot-plugin-yareminder-jobstore'
+)
