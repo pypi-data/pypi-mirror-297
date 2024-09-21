0cf3e31c7f12b222023b38cb8aca0d09da5fad31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:03:58 +0000

    Add package BSTPythonSDK/3.21.0.dev8/bstpythonsdk-3.21.0.dev8.tar.gz

---
+++ packages/BSTPythonSDK/bstpythonsdk-3.21.0.dev8.tar.gz/bstpythonsdk-3.21.0.dev8/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="BSTPythonSDK",
+    version="3.21.0.dev8",
+    author="Black Swift Technologies",
+    author_email="ben.busby@blackswifttech.com",
+    description="BST Flight Management SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bstaero/sdk",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'scipy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ]
+)
