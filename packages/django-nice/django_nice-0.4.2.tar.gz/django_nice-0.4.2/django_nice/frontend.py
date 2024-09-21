ce54f0a70f48ec9e4550ad3dc788448fbcafe453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:03:44 +0000

    Add package django-nice/0.4.2/django_nice-0.4.2.tar.gz

---
+++ packages/django-nice/django_nice-0.4.2.tar.gz/django_nice-0.4.2/django_nice/frontend.py
@@ -0,0 +1,65 @@
+from nicegui import ui
+import requests
+from .config import Config
+
+def bind_element_to_model(element, app_label, model_name, object_id, field_name, element_id, property_name='value'):
+    host = Config.get_host()
+    api_endpoint = Config.get_api_endpoint()
+
+    def fetch_initial_data():
+        url = f'{host}{api_endpoint}/{app_label}/{model_name}/{object_id}/{field_name}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json().get(field_name, '')
+        return ''
+
+    def update_data(value):
+        if value is None or value == '':
+            pass
+        else:
+            url = f'{host}{api_endpoint}/{app_label}/{model_name}/{object_id}/{field_name}/'
+            response = requests.post(url, json={field_name: value})
+
+    setattr(element, property_name, fetch_initial_data())
+
+    if isinstance(element, ui.input):
+        listener_event = 'update:model-value'
+    elif isinstance(element, ui.checkbox):
+        listener_event = 'update:model-checked'
+    elif isinstance(element, ui.slider):
+        listener_event = 'update:model-value'
+    elif isinstance(element, ui.button):
+        listener_event = 'click'
+    else:
+        listener_event = f'update:model-{property_name}'
+
+    def on_frontend_change(e):
+        new_value = ''.join(e.args)
+        update_data(new_value) 
+
+    element.on(listener_event, on_frontend_change)
+
+    element.props(f'id={element_id}')
+    sse_url = f'{host}{api_endpoint}/sse/{app_label}/{model_name}/{object_id}/{field_name}/'
+    ui.add_body_html(f"""
+        <script>
+            let eventSource = new EventSource("{sse_url}");
+
+            eventSource.onmessage = function(event) {{
+                const newValue = event.data;
+
+                // Update the NiceGUI element dynamically via the framework using set_value
+                const element = nicegui.elements['{element_id}'];
+                if (element) {{
+                    element.set_value(newValue);  // Use NiceGUI's set_value method
+                }}
+            }};
+
+            // Cleanup: Close the EventSource connection when the page is closed or navigated away from
+            window.addEventListener('beforeunload', function() {{
+                if (eventSource) {{
+                    eventSource.close();
+                }}
+            }});
+        </script>
+    """)
