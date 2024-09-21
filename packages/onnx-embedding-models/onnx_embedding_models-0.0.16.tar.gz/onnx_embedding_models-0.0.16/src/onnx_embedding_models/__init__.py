58d9663d723258d72dec0ed807c378fa112b5422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 08:04:05 +0000

    Add package onnx-embedding-models/0.0.16/onnx_embedding_models-0.0.16.tar.gz

---
+++ packages/onnx-embedding-models/onnx_embedding_models-0.0.16.tar.gz/onnx_embedding_models-0.0.16/src/onnx_embedding_models/__init__.py
@@ -0,0 +1,11 @@
+from .embedding import (
+    EmbeddingModel,
+    SpladeModel
+)
+from .crossencoder import CrossEncoder
+
+__all__ = [
+    "EmbeddingModel",
+    "SpladeModel",
+    "CrossEncoder"
+]
