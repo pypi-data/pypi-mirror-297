c560793ea1d6dfe9753eb07fd3776d259ef547ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 06:03:55 +0000

    Add package django-nice/0.4.29/django_nice-0.4.29.tar.gz

---
+++ packages/django-nice/django_nice-0.4.29.tar.gz/django_nice-0.4.29/django_nice/sse.py
@@ -0,0 +1,70 @@
+from django.http import StreamingHttpResponse
+
+class SSEManager:
+    _listeners = {}
+
+    @classmethod
+    def register_listener(cls, model_name, object_id, field_name):
+        if model_name not in cls._listeners:
+            cls._listeners[model_name] = {}
+        if object_id not in cls._listeners[model_name]:
+            cls._listeners[model_name][object_id] = {}
+        if field_name not in cls._listeners[model_name][object_id]:
+            cls._listeners[model_name][object_id][field_name] = []
+        return cls._listeners[model_name][object_id][field_name]
+
+    @classmethod
+    def notify_listeners(cls, model_name, object_id, field_name, new_value):
+        listeners = cls._listeners.get(model_name, {}).get(object_id, {}).get(field_name, [])
+        print(f"Notifying {len(listeners)} listeners for {model_name} {object_id} {field_name} with new value {new_value}")
+        for listener in listeners:
+            listener(new_value)  # Directly call each listener
+
+    @classmethod
+    def stream_updates(cls, request, app_label, model_name, object_id, field_name):
+        # Create an event stream to yield events to the client
+        def event_stream():
+            listeners = cls.register_listener(model_name, object_id, field_name)
+
+            from django.apps import apps
+            model = apps.get_model(app_label, model_name)
+            try:
+                instance = model.objects.get(pk=object_id)
+                last_value = getattr(instance, field_name)
+            except model.DoesNotExist:
+                last_value = None
+
+            # Function to send updates to the client
+            def send_update(new_value):
+                nonlocal last_value
+                if new_value != last_value:
+                    last_value = new_value
+                    print(f"Sending SSE update: {new_value}")
+                    yield f"data: {new_value}\n\n"  # Yield the update to the client
+
+            # Listener that sends updates via the stream
+            def listener_callback(new_value):
+                for update in send_update(new_value):
+                    yield update
+
+            print(f"Appending listener for {model_name} {object_id} {field_name}")
+            listeners.append(listener_callback)
+
+            # Send the initial value if present
+            if last_value is not None:
+                yield from send_update(last_value)
+
+            # Keep the connection open for further updates
+            try:
+                while True:
+                    # This loop ensures the SSE connection remains open until the client disconnects
+                    if request.META.get('HTTP_CONNECTION', '').lower() == 'close':
+                        break
+            except GeneratorExit:
+                # Client disconnected, remove the listener
+                print(f"Removing listener for {model_name} {object_id} {field_name}")
+                listeners.remove(listener_callback)
+                raise
+
+        # Create and return a StreamingHttpResponse
+        return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
