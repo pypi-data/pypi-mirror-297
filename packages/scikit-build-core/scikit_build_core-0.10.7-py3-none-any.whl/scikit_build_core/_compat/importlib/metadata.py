dd4ad60c0a3d17515020d3283ee729effd22e058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:14 +0000

    Add package scikit-build-core/0.10.7/scikit_build_core-0.10.7-py3-none-any.whl

---
+++ packages/scikit-build-core/scikit_build_core-0.10.7-py3-none-any.whl/scikit_build_core/_compat/importlib/metadata.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import sys
+import typing
+
+if sys.version_info < (3, 8):
+    import importlib_metadata as _metadata
+    from importlib_metadata import PathDistribution, version
+else:
+    import importlib.metadata as _metadata
+    from importlib.metadata import PathDistribution, version
+
+
+if typing.TYPE_CHECKING:
+    if sys.version_info < (3, 8):
+        from importlib_metadata import EntryPoints
+    elif sys.version_info < (3, 10):
+        from importlib.metadata import EntryPoint
+
+        EntryPoints = typing.List[EntryPoint]
+    else:
+        from importlib.metadata import EntryPoints
+
+__all__ = ["entry_points", "PathDistribution", "version"]
+
+
+def entry_points(*, group: str) -> EntryPoints:
+    if sys.version_info >= (3, 10):
+        return _metadata.entry_points(group=group)
+
+    epg = _metadata.entry_points()
+
+    if sys.version_info < (3, 8) and hasattr(epg, "select"):
+        return epg.select(group=group)  # type: ignore[no-any-return, no-untyped-call]
+
+    # pylint: disable-next=no-member
+    return epg.get(group, [])
+
+
+def __dir__() -> list[str]:
+    return __all__
---
+++ packages/scikit-build-core/scikit_build_core-0.10.7-py3-none-any.whl/scikit_build_core/build/metadata.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import copy
+from typing import TYPE_CHECKING, Any
+
+from packaging.version import Version
+
+from .._vendor.pyproject_metadata import StandardMetadata
+from ..settings._load_provider import load_dynamic_metadata
+
+if TYPE_CHECKING:
+    from collections.abc import Mapping
+
+    from ..settings.skbuild_model import ScikitBuildSettings
+
+__all__ = ["get_standard_metadata"]
+
+
+def __dir__() -> list[str]:
+    return __all__
+
+
+# If pyproject-metadata eventually supports updates, this can be simplified
+def get_standard_metadata(
+    pyproject_dict: Mapping[str, Any],
+    settings: ScikitBuildSettings,
+) -> StandardMetadata:
+    new_pyproject_dict = copy.deepcopy(pyproject_dict)
+    # Handle any dynamic metadata
+    for field, provider, config in load_dynamic_metadata(settings.metadata):
+        if provider is None:
+            msg = f"{field} is missing provider"
+            raise KeyError(msg)
+        if field not in pyproject_dict.get("project", {}).get("dynamic", []):
+            msg = f"{field} is not in project.dynamic"
+            raise KeyError(msg)
+        new_pyproject_dict["project"][field] = provider.dynamic_metadata(field, config)
+        new_pyproject_dict["project"]["dynamic"].remove(field)
+
+    metadata = StandardMetadata.from_pyproject(new_pyproject_dict)
+
+    # For scikit-build-core < 0.5, we keep the normalized name for back-compat
+    if settings.minimum_version is not None and settings.minimum_version < Version(
+        "0.5"
+    ):
+        metadata.name = metadata.canonical_name
+
+    # The description field is required to be one line. Instead of merging it
+    # or cutting off subsequent lines (setuptools), we throw a nice error.
+    # But we didn't validate before 0.9.
+    if (
+        settings.minimum_version is None or settings.minimum_version >= Version("0.9")
+    ) and "\n" in (metadata.description or ""):
+        msg = "Multiple lines in project.description are not supported; this is supposed to be a one line summary"
+        raise ValueError(msg)
+
+    return metadata
