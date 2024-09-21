6bbc295647a9de913d0b1177943715d3b2ba385b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:07:29 +0000

    Add package dagster-looker/0.24.8/dagster_looker-0.24.8-py3-none-any.whl

---
+++ packages/dagster-looker/dagster_looker-0.24.8-py3-none-any.whl/dagster_looker/__init__.py
@@ -0,0 +1,11 @@
+from dagster._core.libraries import DagsterLibraryRegistry
+
+from dagster_looker.asset_specs import build_looker_asset_specs as build_looker_asset_specs
+from dagster_looker.dagster_looker_translator import (
+    DagsterLookerTranslator as DagsterLookerTranslator,
+    LookMLStructureType as LookMLStructureType,
+)
+from dagster_looker.resource import LookerResource as LookerResource
+from dagster_looker.version import __version__ as __version__
+
+DagsterLibraryRegistry.register("dagster-looker", __version__)
