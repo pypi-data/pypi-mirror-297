b0cf85d0933c1e2c6570490126370a7bc913382b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 03:04:43 +0000

    Add package adata/2.6.6/adata-2.6.6.tar.gz

---
+++ packages/adata/adata-2.6.6.tar.gz/adata-2.6.6/adata/stock/info/concept/stock_concept_template.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@desc: 
+@author: 1nchaos
+@time:2023/8/3
+@log: 
+"""
+
+
+class StockConceptTemplate(object):
+    """
+    股票概念模板
+    """
+    _CONCEPT_CONSTITUENT_COLUMNS = ['stock_code', 'short_name']
+    _CONCEPT_CODE_COLUMNS = ['concept_code', 'index_code', 'name', 'source']
+    _CONCEPT_INFO_COLUMNS = ['stock_code', 'concept_code', 'name', 'source', 'reason']
+
+    def __init__(self) -> None:
+        super().__init__()
