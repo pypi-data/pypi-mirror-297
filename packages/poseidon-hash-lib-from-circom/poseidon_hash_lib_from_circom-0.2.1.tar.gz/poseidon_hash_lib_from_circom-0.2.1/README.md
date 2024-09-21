a98e2899e0b2543ac6f3b436678b9dd877f8f40a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:06:08 +0000

    Add package poseidon-hash-lib-from-circom/0.2.1/poseidon_hash_lib_from_circom-0.2.1.tar.gz

---
+++ packages/poseidon-hash-lib-from-circom/poseidon_hash_lib_from_circom-0.2.1.tar.gz/poseidon_hash_lib_from_circom-0.2.1/README.md
@@ -0,0 +1,4 @@
+Python3 Poseidon lib implementation
+
+Cloned from iden3 circomlib by https://github.com/Sdoba16/Circom-DL-course.
+
---
+++ packages/poseidon-hash-lib-from-circom/poseidon_hash_lib_from_circom-0.2.1.tar.gz/poseidon_hash_lib_from_circom-0.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+print(find_packages())
+setup(
+    name="poseidon-hash-lib-from-circom",
+    version="0.2.1",
+    author="erdognishe",
+    author_email="khotyanmisha@gmail.com",
+    description="Poseidon Hash Library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/erdoganishe/python3-poseidon-hash-lib",  # Add your repo link
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+    ],
+)
