106582568fc3335f2bb0d69abc6580ed0091f043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:09:09 +0000

    Add package ob-metaflow-stubs/6.0.3.103rc0/ob_metaflow_stubs-6.0.3.103rc0-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.103rc0-py2.py3-none-any.whl/metaflow-stubs/mflog/mflog.pyi
@@ -0,0 +1,22 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.22.1+obcheckpoint(0.0.10);ob(v1)                              #
+# Generated on 2024-09-20T18:44:03.688125                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import datetime
+
+VERSION: bytes
+
+RE: bytes
+
+ISOFORMAT: str
+
+MISSING_TIMESTAMP: datetime.datetime
+
+MISSING_TIMESTAMP_STR: str
+
