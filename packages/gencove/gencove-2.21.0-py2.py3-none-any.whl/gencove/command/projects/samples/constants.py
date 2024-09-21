fce89557c4f30aaa87557f81d2ebbd58b3188a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:04:06 +0000

    Add package gencove/2.21.0/gencove-2.21.0-py2.py3-none-any.whl

---
+++ packages/gencove/gencove-2.21.0-py2.py3-none-any.whl/gencove/command/projects/samples/constants.py
@@ -0,0 +1,14 @@
+"""Describe constants in samples subcommand."""
+from typing import Optional
+
+from gencove.constants import Optionals
+
+
+# pylint: disable=too-few-public-methods
+class SamplesOptions(Optionals):
+    """SamplesOptions model"""
+
+    status: Optional[str]
+    archive_status: Optional[str]
+    search: Optional[str]
+    include_run: Optional[bool]
