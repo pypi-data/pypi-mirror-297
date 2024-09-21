2b6e2b04d08860e41cd61d3622845673b16e11eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:14 +0000

    Add package pandora/1.6.2/pandora-1.6.2.tar.gz

---
+++ packages/pandora/pandora-1.6.2.tar.gz/pandora-1.6.2/pandora/cost_volume_confidence/ambiguity.py
@@ -0,0 +1,353 @@
+# Copyright (c) 2024 Centre National d'Etudes Spatiales (CNES).
+#
+# This file is part of PANDORA
+#
+#     https://github.com/CNES/Pandora_pandora
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""
+This module contains functions for estimating confidence from ambiguity.
+"""
+import logging
+import warnings
+import os
+from typing import Dict, Tuple, Union
+from ast import literal_eval
+import numpy as np
+from json_checker import Checker, And
+from numba import njit, prange
+import xarray as xr
+
+from . import cost_volume_confidence
+
+
+@cost_volume_confidence.AbstractCostVolumeConfidence.register_subclass("ambiguity")
+class Ambiguity(cost_volume_confidence.AbstractCostVolumeConfidence):
+    """
+    Ambiguity class allows to estimate a confidence from the cost volume
+    """
+
+    # Default configuration, do not change this value
+    _ETA_MIN = 0.0
+    _ETA_MAX = 0.7
+    _ETA_STEP = 0.01
+    # Percentile value to normalize ambiguity
+    _PERCENTILE = 1.0
+    # Ambiguity normalization
+    _NORMALIZATION = True
+    # Method name
+    _method = "ambiguity"
+
+    def __init__(self, **cfg: str) -> None:
+        """
+        :param cfg: optional configuration, {'confidence_method': 'ambiguity', 'eta_min': float, 'eta_max': float,
+        'eta_step': float}
+        :type cfg: dict
+        :return: None
+        """
+        self.cfg = self.check_conf(**cfg)
+        self._eta_min = self._ETA_MIN
+        self._percentile = self._PERCENTILE
+        self._normalization = self.cfg["normalization"]
+        self._eta_max = float(self.cfg["eta_max"])
+        self._eta_step = float(self.cfg["eta_step"])
+        self._indicator = self._method + str(self.cfg["indicator"])
+        self._etas = np.arange(self._eta_min, self._eta_max, self._eta_step)
+        self._nbr_etas = self._etas.shape[0]
+
+    def check_conf(self, **cfg: Union[str, float]) -> Dict[str, Union[str, float]]:
+        """
+        Add default values to the dictionary if there are missing elements and check if the dictionary is correct
+
+        :param cfg: ambiguity configuration
+        :type cfg: dict
+        :return cfg: ambiguity configuration updated
+        :rtype: dict
+        """
+        if "eta_max" not in cfg:
+            cfg["eta_max"] = self._ETA_MAX
+        if "eta_step" not in cfg:
+            cfg["eta_step"] = self._ETA_STEP
+        if "indicator" not in cfg:
+            cfg["indicator"] = self._indicator
+        if "normalization" not in cfg:
+            cfg["normalization"] = self._NORMALIZATION
+
+        schema = {
+            "confidence_method": And(str, lambda input: "ambiguity"),
+            "eta_max": And(float, lambda input: 0 < input < 1),
+            "eta_step": And(float, lambda input: 0 < input < 1),
+            "normalization": bool,
+            "indicator": str,
+        }
+
+        checker = Checker(schema)
+        checker.validate(cfg)
+        return cfg
+
+    def desc(self) -> None:
+        """
+        Describes the confidence method
+        :return: None
+        """
+        print("Ambiguity confidence method")
+
+    def confidence_prediction(
+        self,
+        disp: xr.Dataset,
+        img_left: xr.Dataset = None,
+        img_right: xr.Dataset = None,
+        cv: xr.Dataset = None,
+    ) -> Tuple[xr.Dataset, xr.Dataset]:
+        """
+        Computes a confidence measure that evaluates the matching cost function at each point
+
+        :param disp: the disparity map dataset
+        :type disp: xarray.Dataset
+        :param img_left: left Dataset image
+        :tye img_left: xarray.Dataset
+        :param img_right: right Dataset image
+        :type img_right: xarray.Dataset
+        :param cv: cost volume dataset
+        :type cv: xarray.Dataset
+        :return: the disparity map and the cost volume with a new indicator 'ambiguity_confidence' in the DataArray
+                 confidence_measure
+        :rtype: Tuple(xarray.Dataset, xarray.Dataset) with the data variables:
+
+                - confidence_measure 3D xarray.DataArray (row, col, indicator)
+        """
+
+        grids = np.array(
+            [img_left["disparity"].sel(band_disp="min"), img_left["disparity"].sel(band_disp="max")], dtype=np.int64
+        )
+        # Get disparity intervals parameters
+        disparity_range = cv["disp"].data.astype(np.float32)
+
+        type_measure_min = cv.attrs["type_measure"] == "min"
+
+        # This silences numba's TBB threading layer warning
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore")
+            # Computes ambiguity using numba in parallel for memory and computation time optimization
+            ambiguity = self.compute_ambiguity(
+                cv["cost_volume"].data, self._etas, self._nbr_etas, grids, disparity_range, type_measure_min
+            )
+
+        # If activated, ambiguity normalization with percentile
+        if self._normalization:
+            if "global_disparity" in img_left.attrs:
+                ambiguity = self.normalize_with_extremum(ambiguity, img_left, nbr_etas=self._nbr_etas)
+                logging.info(
+                    "You are not using ambiguity normalization by percentile; \n"
+                    "you are in a specific case with the instantiation of global_disparity."
+                )
+            # in case of cross correlation
+            elif "global_disparity" in img_right.attrs:
+                ambiguity = self.normalize_with_extremum(ambiguity, img_right, nbr_etas=self._nbr_etas)
+            else:
+                ambiguity = self.normalize_with_percentile(ambiguity)
+
+        # Conversion of ambiguity into a confidence measure
+        ambiguity = 1 - ambiguity
+
+        disp, cv = self.allocate_confidence_map(self._indicator, ambiguity, disp, cv)
+
+        return disp, cv
+
+    def normalize_with_percentile(self, ambiguity: np.ndarray) -> np.ndarray:
+        """
+        Normalize ambiguity with percentile
+
+        :param ambiguity: ambiguity
+        :type ambiguity: 2D np.ndarray (row, col) dtype = float32
+        :return: the normalized ambiguity
+        :rtype: 2D np.ndarray (row, col) dtype = float32
+        """
+
+        norm_amb = np.copy(ambiguity)
+        perc_min = np.percentile(norm_amb, self._percentile)
+        perc_max = np.percentile(norm_amb, 100 - self._percentile)
+        np.clip(norm_amb, perc_min, perc_max, out=norm_amb)
+
+        return (norm_amb - np.min(norm_amb)) / (np.max(norm_amb) - np.min(norm_amb))
+
+    @staticmethod
+    @njit(
+        "f4[:, :](f4[:, :, :], f8[:], i8, i8[:, :, :],f4[:], bool_)",
+        parallel=literal_eval(os.environ.get("PANDORA_NUMBA_PARALLEL", "False")),
+        cache=literal_eval(os.environ.get("PANDORA_NUMBA_CACHE", "True")),
+    )
+    def compute_ambiguity(
+        cv: np.ndarray,
+        etas: np.ndarray,
+        nbr_etas: int,
+        grids: np.ndarray,
+        disparity_range: np.ndarray,
+        type_measure_min: bool,
+    ) -> np.ndarray:
+        """
+        Computes ambiguity.
+
+        :param cv: cost volume
+        :type cv: 3D np.ndarray (row, col, disp)
+        :param etas: range between eta_min and eta_max with step eta_step
+        :type etas: np.ndarray
+        :param nbr_etas: number of etas
+        :type nbr_etas: int
+        :param grids: array containing min and max disparity grids
+        :type grids: 2D np.ndarray (min, max)
+        :param disparity_range: array containing disparity range
+        :type disparity_range: np.ndarray
+        :param type_measure_min: True for min and False for max
+        :type type_measure_min: bool
+        :return: the normalized ambiguity
+        :rtype: 2D np.ndarray (row, col) dtype = float32
+        """
+
+        # Minimum and maximum of all costs, useful to normalize the cost volume
+        max_cost = np.nanmax(cv)
+        min_cost = np.nanmin(cv)
+
+        n_row, n_col, nb_disps = cv.shape
+
+        if type_measure_min:
+            extremum_cost = min_cost
+        else:
+            extremum_cost = max_cost
+
+        # Numba does not support the np.tile operation
+        two_dim_etas = np.repeat(etas, nb_disps).reshape((-1, nb_disps)).T.flatten()
+
+        # integral of ambiguity
+        ambiguity = np.zeros((n_row, n_col), dtype=np.float32)
+
+        diff_cost = max_cost - min_cost
+
+        for row in prange(n_row):  # pylint: disable=not-an-iterable
+            for col in prange(n_col):  # pylint: disable=not-an-iterable
+                # Normalized minimum cost for one point
+
+                if type_measure_min:
+                    normalized_min_cost = (np.nanmin(cv[row, col, :]) - extremum_cost) / diff_cost
+                else:
+                    normalized_min_cost = (np.nanmax(cv[row, col, :]) - extremum_cost) / diff_cost
+
+                # If all costs are at nan, set the maximum value of the ambiguity for this point
+                if np.isnan(normalized_min_cost):
+                    ambiguity[row, col] = nbr_etas * nb_disps
+                else:
+                    idx_disp_min = np.searchsorted(disparity_range, grids[0][row, col])
+                    idx_disp_max = np.searchsorted(disparity_range, grids[1][row, col]) + 1
+
+                    normalized_min_cost = np.repeat(normalized_min_cost, nb_disps * nbr_etas)
+                    # Normalized cost volume for one point
+                    normalized_cv = (cv[row, col, :] - extremum_cost) / diff_cost
+
+                    # Mask nan to -inf to increase the value of the ambiguity if a point contains nan costs
+                    normalized_cv[idx_disp_min:idx_disp_max][
+                        np.isnan(normalized_cv[idx_disp_min:idx_disp_max])
+                    ] = -np.inf
+
+                    normalized_cv[:idx_disp_min][np.isnan(normalized_cv[:idx_disp_min])] = np.inf
+                    normalized_cv[idx_disp_max:][np.isnan(normalized_cv[idx_disp_max:])] = np.inf
+
+                    normalized_cv = np.repeat(normalized_cv, nbr_etas)
+
+                    if type_measure_min:
+                        ambiguity[row, col] += np.nansum(normalized_cv <= (normalized_min_cost + two_dim_etas))
+                    else:
+                        ambiguity[row, col] += np.nansum(normalized_cv >= (normalized_min_cost - two_dim_etas))
+
+        return ambiguity
+
+    @staticmethod
+    @njit(
+        "Tuple((f4[:, :],f4[:, :, :]))(f4[:, :, :], f8[:], i8, i8[:, :, :], f4[:])",
+        parallel=literal_eval(os.environ.get("PANDORA_NUMBA_PARALLEL", "False")),
+        cache=literal_eval(os.environ.get("PANDORA_NUMBA_CACHE", "True")),
+    )
+    def compute_ambiguity_and_sampled_ambiguity(
+        cv: np.ndarray,
+        etas: np.ndarray,
+        nbr_etas: int,
+        grids: np.ndarray,
+        disparity_range: np.ndarray,
+    ):
+        """
+        Return the ambiguity and sampled ambiguity, useful for evaluating ambiguity in notebooks
+
+        :param cv: cost volume
+        :type cv: 3D np.ndarray (row, col, disp)
+        :param etas: range between eta_min and eta_max with step eta_step
+        :type etas: np.ndarray
+        :param nbr_etas: nuber of etas
+        :type nbr_etas: int
+        :param grids: array containing min and max disparity grids
+        :type grids: 2D np.ndarray (min, max)
+        :param disparity_range: array containing disparity range
+        :type disparity_range: np.ndarray
+        :return: the normalized ambiguity and sampled ambiguity
+        :rtype: Tuple(2D np.ndarray (row, col) dtype = float32, 3D np.ndarray (row, col) dtype = float32)
+        """
+        # Minimum and maximum of all costs, useful to normalize the cost volume
+        min_cost = np.nanmin(cv)
+        max_cost = np.nanmax(cv)
+
+        n_row, n_col, nb_disps = cv.shape
+
+        # Numba does not support the np.tile operation
+        two_dim_etas = np.repeat(etas, nb_disps).reshape((-1, nb_disps)).T.flatten()
+
+        # integral of ambiguity
+        ambiguity = np.zeros((n_row, n_col), dtype=np.float32)
+        sampled_ambiguity = np.zeros((n_row, n_col, nbr_etas), dtype=np.float32)
+
+        diff_cost = max_cost - min_cost
+
+        for row in prange(n_row):  # pylint: disable=not-an-iterable
+            for col in prange(n_col):  # pylint: disable=not-an-iterable
+                # Normalized minimum cost for one point
+                normalized_min_cost = (np.nanmin(cv[row, col, :]) - min_cost) / diff_cost
+
+                # If all costs are at nan, set the maximum value of the ambiguity for this point
+                if np.isnan(normalized_min_cost):
+                    ambiguity[row, col] = nbr_etas * nb_disps
+                    sampled_ambiguity[row, col, :] = nb_disps
+                else:
+                    normalized_min_cost = np.repeat(normalized_min_cost, nb_disps * nbr_etas)
+
+                    # Normalized cost volume for one point
+                    normalized_cv = (cv[row, col, :] - min_cost) / (max_cost - min_cost)
+
+                    idx_disp_min = np.searchsorted(disparity_range, grids[0][row, col])
+                    idx_disp_max = np.searchsorted(disparity_range, grids[1][row, col]) + 1
+
+                    # Mask nan to -inf to increase the value of the ambiguity if a point contains nan costs
+                    normalized_cv[idx_disp_min:idx_disp_max][
+                        np.isnan(normalized_cv[idx_disp_min:idx_disp_max])
+                    ] = -np.inf
+                    normalized_cv[:idx_disp_min][np.isnan(normalized_cv[:idx_disp_min])] = np.inf
+                    normalized_cv[idx_disp_max:][np.isnan(normalized_cv[idx_disp_max:])] = np.inf
+
+                    normalized_cv = np.repeat(normalized_cv, nbr_etas)
+
+                    # fill integral ambiguity
+                    ambiguity[row, col] += np.nansum(normalized_cv <= (normalized_min_cost + two_dim_etas))
+
+                    # fill sampled ambiguity
+                    costs_comparison = normalized_cv <= (normalized_min_cost + two_dim_etas)
+                    costs_comparison = costs_comparison.reshape((nb_disps, nbr_etas))
+                    sampled_ambiguity[row, col, :] = np.sum(costs_comparison, axis=0)
+
+        return ambiguity, sampled_ambiguity
---
+++ packages/pandora/pandora-1.6.2.tar.gz/pandora-1.6.2/pandora/matching_cost/census.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+# coding: utf8
+#
+# Copyright (c) 2024 Centre National d'Etudes Spatiales (CNES).
+#
+# This file is part of PANDORA
+#
+#     https://github.com/CNES/Pandora
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""
+This module contains functions associated to census method used in the cost volume measure step.
+"""
+
+from typing import Dict, Union, Tuple, List
+
+import numpy as np
+import xarray as xr
+from json_checker import Checker, And
+
+from pandora.img_tools import shift_right_img, census_transform
+from pandora.matching_cost import matching_cost
+
+
+@matching_cost.AbstractMatchingCost.register_subclass("census")
+class Census(matching_cost.AbstractMatchingCost):
+    """
+    Census class allows to compute the cost volume
+    """
+
+    def __init__(self, **cfg: Dict[str, Union[str, int]]) -> None:
+        """
+        :param cfg: optional configuration,  {'window_size': value, 'subpix': value}
+        :type cfg: dict
+        :return: None
+        """
+
+        super().instantiate_class(**cfg)  # type: ignore
+
+    def check_conf(self, **cfg: Dict[str, Union[str, int]]) -> Dict[str, Union[str, int]]:
+        """
+        Add default values to the dictionary if there are missing elements and check if the dictionary is correct
+
+        :param cfg: matching cost configuration
+        :type cfg: dict
+        :return cfg: matching cost configuration updated
+        :rtype: dict
+        """
+        cfg = super().check_conf(**cfg)
+
+        schema = self.schema
+        schema["matching_cost_method"] = And(str, lambda input: "census")
+        schema["window_size"] = And(int, lambda input: input in (3, 5))
+
+        checker = Checker(schema)
+        checker.validate(cfg)
+        return cfg
+
+    def compute_cost_volume(
+        self,
+        img_left: xr.Dataset,
+        img_right: xr.Dataset,
+        cost_volume: xr.Dataset,
+    ) -> xr.Dataset:
+        """
+        Computes the cost volume for a pair of images
+
+        :param img_left: left Dataset image containing :
+
+                - im: 2D (row, col) or 3D (band_im, row, col) xarray.DataArray float32
+                - disparity (optional): 3D (disp, row, col) xarray.DataArray float32
+                - msk (optional): 2D (row, col) xarray.DataArray int16
+                - classif (optional): 3D (band_classif, row, col) xarray.DataArray int16
+                - segm (optional): 2D (row, col) xarray.DataArray int16
+        :type img_left: xarray.Dataset
+        :param img_right: right Dataset image containing :
+
+                - im: 2D (row, col) or 3D (band_im, row, col) xarray.DataArray float32
+                - disparity (optional): 3D (disp, row, col) xarray.DataArray float32
+                - msk (optional): 2D (row, col) xarray.DataArray int16
+                - classif (optional): 3D (band_classif, row, col) xarray.DataArray int16
+                - segm (optional): 2D (row, col) xarray.DataArray int16
+        :type img_right: xarray.Dataset
+        :param cost_volume: an empty cost volume
+        :type cost_volume: xr.Dataset
+        :return: the cost volume dataset , with the data variables:
+
+                - cost_volume 3D xarray.DataArray (row, col, disp)
+        :rtype: xarray.Dataset
+        """
+        # check band parameter
+        self.check_band_input_mc(img_left, img_right)
+
+        # Contains the shifted right images
+        img_right_shift = shift_right_img(img_right, self._subpix, self._band, self._spline_order)
+
+        # Maximal cost of the cost volume with census measure
+        cmax = int(self._window_size**2)
+        cost_volume.attrs.update(
+            {
+                "type_measure": "min",
+                "cmax": cmax,
+            }
+        )
+
+        # Apply census transformation
+        left = census_transform(img_left, self._window_size, self._band)
+        for i, img in enumerate(img_right_shift):
+            img_right_shift[i] = census_transform(img, self._window_size, self._band)
+
+        disparity_range = cost_volume.coords["disp"].data
+        cv = self.allocate_numpy_cost_volume(img_left, disparity_range)
+        cv_crop = self.crop_cost_volume(cv, cost_volume.attrs["offset_row_col"])
+
+        # Giving the 2 images, the matching cost will be calculated as :
+        #                 1, 1, 1                2, 5, 6
+        #                 2, 1, 4                1, 1, 3
+        #                 1, 3, 1                6, 1, 2
+        #
+        # for disp = 0, the costvolume will be
+        #                        (1 - 2) (1 - 5) (1 - 6)
+        #                        (2 - 1) (1 - 1) (4 - 3)
+        #                        (1 - 6) (3 - 1) (1 - 2)
+        # , we make the difference between the two full images
+        #
+        # for disp = -1, compute the difference between
+        #                    1, 1                   2, 5
+        #                    1, 4                   1, 1
+        #                    3, 1                   6, 1
+        # , the costvolume will be
+        #                        nan  (1-2) (1-5)
+        #                        nan  (1-1) (4-1)
+        #                        nan  (3-6) (1-1)
+        # , nan correspond to the first column of the left image that cannot be calculated,
+        #  because there is no corresponding column for the disparity -1.
+        #
+        # In the following loop, the tuples point_p,point_q describe
+        # to which part of the image the difference will be applied,
+        # for disp = 0, we compute the difference on the whole images so point_p=(:,0:3) et point_q=(:,0:3),
+        # for disp = -1, we use only a part of the images so point_p=(:,1:3) et point_q=(:,0:2)
+
+        # Computes the matching cost
+        # In the loop, cv is of shape (disp, col, row) and images / masks of shape (row, col)
+        # np.swapaxes allow to interchange row and col in images and masks
+        for disp_index, disp in enumerate(disparity_range):
+            i_right = int((disp % 1) * self._subpix)
+            point_p, point_q = self.point_interval(left, img_right_shift[i_right], disp)
+
+            cv_crop[disp_index, point_p[0] : point_p[1], :] = np.swapaxes(
+                self.census_cost(point_p, point_q, left, img_right_shift[i_right]), 0, 1
+            )
+
+        # Create the xarray.DataSet that will contain the cv of dimensions (row, col, disp)
+        # Computations were optimized with a cost_volume of dimensions (disp, row, col)
+        # As we are expected to return a cost_volume of dimensions (row, col, disp),
+        # we swap axes.
+        cv = np.swapaxes(cv, 0, 2)
+        index_col = cost_volume.attrs["col_to_compute"]
+        index_col = index_col - img_left.coords["col"].data[0]  # If first col coordinate is not 0
+        cost_volume["cost_volume"].data = cv[:, index_col, :]
+
+        return cost_volume
+
+    def census_cost(
+        self,
+        point_p: Tuple[int, int],
+        point_q: Tuple[int, int],
+        img_left: xr.Dataset,
+        img_right: xr.Dataset,
+    ) -> List[int]:
+        """
+        Computes xor pixel-wise between pre-processed images by census transform
+
+        :param point_p: Point interval, in the left image, over which the squared difference will be applied
+        :type point_p: tuple
+        :param point_q: Point interval, in the right image, over which the squared difference will be applied
+        :type point_q: tuple
+        :param img_left: left Dataset image containing :
+
+                - im: 2D (row, col) or 3D (band_im, row, col) xarray.DataArray float32
+                - disparity (optional): 3D (disp, row, col) xarray.DataArray float32
+                - msk (optional): 2D (row, col) xarray.DataArray int16
+                - classif (optional): 3D (band_classif, row, col) xarray.DataArray int16
+                - segm (optional): 2D (row, col) xarray.DataArray int16
+        :type img_left: xarray.Dataset
+        :param img_right: right Dataset image containing :
+
+                - im: 2D (row, col) or 3D (band_im, row, col) xarray.DataArray float32
+                - disparity (optional): 3D (disp, row, col) xarray.DataArray float32
+                - msk (optional): 2D (row, col) xarray.DataArray int16
+                - classif (optional): 3D (band_classif, row, col) xarray.DataArray int16
+                - segm (optional): 2D (row, col) xarray.DataArray int16
+        :type img_right: xarray.Dataset
+        :return: the xor pixel-wise between elements in the interval
+        :rtype: numpy array
+        """
+        xor_ = img_left["im"].data[:, point_p[0] : point_p[1]].astype("uint32") ^ img_right["im"].data[
+            :, point_q[0] : point_q[1]
+        ].astype("uint32")
+        return list(map(self.popcount32b, xor_))
+
+    @staticmethod
+    def popcount32b(row: int) -> int:
+        """
+        Computes the Hamming weight for the input row,
+        Hamming weight is the number of symbols that are different from the zero
+
+        :param row: 32-bit integer
+        :type row: int
+        :return: the number of symbols that are different from the zero
+        :rtype: int
+        """
+        row -= (row >> 1) & 0x55555555
+        row = (row & 0x33333333) + ((row >> 2) & 0x33333333)
+        row = (row + (row >> 4)) & 0x0F0F0F0F
+        row += row >> 8
+        row += row >> 16
+        return row & 0x7F
