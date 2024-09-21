894a6ff6b45b55fea753b763c021d2d42e4e0ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 11:03:46 +0000

    Add package asp-test-2024-09-20/0.1.0/asp_test_2024_09_20-0.1.0.tar.gz

---
+++ packages/asp-test-2024-09-20/asp_test_2024_09_20-0.1.0.tar.gz/asp_test_2024_09_20-0.1.0/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="asp_test_2024_09_20",
+    version="0.1.0",
+    author="Your Name",
+    description="asp_test_2024_09_20 package",
+    packages=find_packages(),
+    install_requires=[
+        # Здесь перечислите зависимости вашего пакета
+    ],
+)
