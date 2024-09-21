e2882b3644c2658e37787c1a51838573c34a03a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:03:45 +0000

    Add package pygments-nitecode/0.0.2/pygments_nitecode-0.0.2-py3-none-any.whl

---
+++ packages/pygments-nitecode/pygments_nitecode-0.0.2-py3-none-any.whl/pygments_nitecode/NiteCodeLexer.py
@@ -0,0 +1,40 @@
+"""A Pygments lexer for NiteCode language."""
+from pygments.lexer import RegexLexer, words, bygroups
+from pygments.token import *
+
+__all__ = ("NiteCodeLexer",)
+
+
+class NiteCodeLexer(RegexLexer):
+    name = 'NiteCode'
+    aliases = ['nite', 'nitecode']
+    filenames = ['*.nite', '*.nitecode']
+    mimetypes = ['text/nitecode']
+
+    tokens = {
+        'root': [
+            (r'\\\\.*$', Comment.Single)
+            (r'0x[0-9a-fA-F]+([ui]((8)|(16)|(32)|(64)))?', Number.Hex),
+            (r'[0-9]+([ui]((8)|(16)|(32)|(64)))?', Number.Integer),
+            (r'[+-~=^|&<>!&?:/*]', Operator),
+            (r'[;{}()\[\],.]', Punctuation),
+            # (r'(L?)(")', bygroups(String.Affix, String), 'string'),
+            (words((
+                # Types
+                'u8', 'u16', 'u32', 'u64',
+                'i8', 'i16', 'i32', 'i64',
+                'f32', 'f64',
+                'void',
+                # Flow keywords
+                'for', 'do', 'while',
+                'if', 'else'
+                # Other
+                'use', 'type'
+            )), Keyword),
+            (words((
+                # Constants
+                'false', 'true', 'nil'
+            )), Keyword.Constant),
+            (r'[a-zA-Z_]\w*', Name)
+        ]
+    }
