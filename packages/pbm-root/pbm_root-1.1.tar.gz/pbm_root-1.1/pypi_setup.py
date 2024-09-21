7b6d788f8ee224c2c5271d44368daf110a2d3205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:04:13 +0000

    Add package pbm-root/1.1/pbm_root-1.1.tar.gz

---
+++ packages/pbm-root/pbm_root-1.1.tar.gz/pbm_root-1.1/pypi_setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as file:
+    long_description: str = file.read()
+
+setup(
+    name="pbm-root",
+    version="1.1",
+    author="elemenom",
+    author_email="pixilreal@gmail.com",
+    description="Version control at its fullest.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elemenom/pbm",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    license="GPLv3",
+    include_package_data=True,
+)
