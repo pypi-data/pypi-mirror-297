50660241229bc782898bed1edf4323f51ce34eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 04:04:19 +0000

    Add package zf-pd/0.2.15/zf_pd-0.2.15-py3-none-any.whl

---
+++ packages/zf-pd/zf_pd-0.2.15-py3-none-any.whl/pd/__model__/utils.py
@@ -0,0 +1,15 @@
+def parse_str_list_int(ctx, param, value) -> list[int]:
+    if not value:
+        return []
+    delim: str | None = None
+    if ',' in value:
+        delim = ','
+    elif 'x' in value:
+        delim = 'x'
+    elif ' ' in value:
+        delim = ' '
+
+    if not delim:
+        return [int(value)]
+
+    return [int(s) for s in value.split(delim)]
