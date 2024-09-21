13e3f7e050c6389007271ca8827f72505be6b986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:25 +0000

    Add package agentdesk/0.2.94/agentdesk-0.2.94-py3-none-any.whl

---
+++ packages/agentdesk/agentdesk-0.2.94-py3-none-any.whl/agentdesk/__init__.py
@@ -0,0 +1,11 @@
+from .device import ConnectConfig, Desktop, ProvisionConfig, SimpleDesktop
+from .server.models import V1Desktop, V1ProviderData
+
+__all__ = [
+    "Desktop",
+    "SimpleDesktop",
+    "ProvisionConfig",
+    "ConnectConfig",
+    "V1Desktop",
+    "V1ProviderData",
+]
