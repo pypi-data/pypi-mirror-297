ebb5b44bdf054a38bf04b6757b973e014aae16f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 03:05:35 +0000

    Add package bc-rabbitmq/0.0.1/bc_rabbitmq-0.0.1.tar.gz

---
+++ packages/bc-rabbitmq/bc_rabbitmq-0.0.1.tar.gz/bc_rabbitmq-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bc-rabbitmq",
+    version="0.0.1",
+    description="Empty package to prevent dependency confusion",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+    include_package_data=False,
+)
