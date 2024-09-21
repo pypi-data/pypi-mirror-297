6c6e1fd648c797cb4270caa8ca2da2badb9594f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 03:04:54 +0000

    Add package neuondb/0.1.0/neuondb-0.1.0.tar.gz

---
+++ packages/neuondb/neuondb-0.1.0.tar.gz/neuondb-0.1.0/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 18:52:15 2024
+
+@author: yangl
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+      name="neuondb",
+      version="0.1.0",
+      author="holmes",
+      author_email="tidusyuna88@gmail.com",
+      description="Wrapper for MongoDB using pymongo",
+      packages=find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      python_requires=">=3.6",
+      )
