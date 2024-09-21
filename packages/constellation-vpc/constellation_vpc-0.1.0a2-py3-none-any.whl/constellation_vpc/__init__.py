826da0f1456208b411a7530df73dd1130d128f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:04:01 +0000

    Add package constellation-vpc/0.1.0a2/constellation_vpc-0.1.0a2-py3-none-any.whl

---
+++ packages/constellation-vpc/constellation_vpc-0.1.0a2-py3-none-any.whl/constellation_vpc/__init__.py
@@ -0,0 +1,7 @@
+__name__ = "constellation_vpc"
+__version__ = "0.1.0a2"
+__author__ = "Coulter Stutz"
+__email__ = "coulterstutz@gmail.com"
+__license__ = "MIT"
+from subnet import Subnet
+__all__ = ["Subnet"]
---
+++ packages/constellation-vpc/constellation_vpc-0.1.0a2-py3-none-any.whl/constellation_vpc/vpc.py
@@ -0,0 +1,1 @@
+from _vpc import _vpc
