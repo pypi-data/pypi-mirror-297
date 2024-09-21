11acd149ef18cc4ed885e1c0c6c30dc97d74f14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:04:09 +0000

    Add package dify-plugin/0.0.1b8/dify_plugin-0.0.1b8.tar.gz

---
+++ packages/dify-plugin/dify_plugin-0.0.1b8.tar.gz/dify_plugin-0.0.1b8/dify_plugin/interfaces/endpoint/__init__.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from collections.abc import Mapping
+from werkzeug import Request, Response
+
+from ...core.runtime import Session
+
+
+
+class Endpoint(ABC):
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    ############################################################
+    #        Methods that can be implemented by plugin         #
+    ############################################################
+
+    @abstractmethod
+    def _invoke(self, r: Request, values: Mapping, settings: Mapping) -> Response:
+        """
+        Invokes the endpoint with the given request.
+
+        To be implemented by subclasses.
+        """
+
+    ############################################################
+    #                 For executor use only                    #
+    ############################################################
+
+    def invoke(self, r: Request, values: Mapping, settings: Mapping) -> Response:
+        """
+        Invokes the endpoint with the given request.
+        """
+        if type(self) is Endpoint:
+            raise RuntimeError("Subclasses cannot call this method.")
+
+        return self._invoke(r, values, settings)
