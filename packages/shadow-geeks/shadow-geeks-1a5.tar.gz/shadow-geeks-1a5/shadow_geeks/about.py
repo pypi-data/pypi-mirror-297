dba04c96f0fd1c4935490f7799456b021a3c8baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:05:15 +0000

    Add package shadow-geeks/1a5/shadow-geeks-1a5.tar.gz

---
+++ packages/shadow-geeks/shadow-geeks-1a5.tar.gz/shadow-geeks-1a5/shadow_geeks/about.py
@@ -0,0 +1,37 @@
+def welcome():
+    print("Hello, Shadow geeks")
+
+
+def team():
+    print("Rahul P \n Udhay Karthik C \n Pravin Kumar S")
+
+
+def rahul_links():
+    print("Mail: mailto:rockr3876@gmail.com")
+    print("Portfolio Website: https://www.rahul2903.epizy.com")
+    print("LinkedIn: www.linkedin.com/in/rahul-p-96399b203")
+    print("GitHub: www.github.com/Rahul1511E")
+    print("GFG: www.geeksforgeeks.org/user/rockr3876/")
+    print("Hackerrank: www.hackerrank.com/profile/rockr3876")
+    print("Freecodecamp: www.freecodecamp.org/rahul1511e")
+    print("Leetcode: www.leetcode.com/u/rockr3876/")
+    print("Kaggle: www.kaggle.com/rahul1511e")
+    print("Huggingface: www.huggingface.co/Rahul1511E")
+
+
+def udhay_links():
+    print("Portfolio Website: www.github.com/udhaykarthi")
+    print("LinkedIn: www.linkedin.com/in/udhay-karthik-chandrasekar-182440257/")
+    print("Leetcode: www.leetcode.com/u/udhaypriya1408/")
+    print("GitHub: www.github.com/udhaykarthi")
+    print("Hackkerrank: www.hackerrank.com/profile/udhaypriya1408")
+    print("GFG: www.geeksforgeeks.org/user/udhaykarthik_55")
+
+
+def pravin_links():
+    print("LinkedIn: www.linkedin.com/in/pravin-kumar-s-10a341267/")
+    print("GFG: www.geeksforgeeks.org/user/pravinkumar_s")
+
+
+def our_projects():
+    print(" Face Based Emotion Detection \n Text Based Emotion Detection \n Rover(Tengen Uzui Mark-I & Mark-II) \n Face Based Attendance System \n Smart plug self automation using IoT \n Traffic Control using Vehicle Count \n Accident Detection Using RNN \n Private Chat Application(Study Guys & Roogle) \n AI Driven Drone \n AI integrated Crawler \n Attendance Management for NEC")
---
+++ packages/shadow-geeks/shadow-geeks-1a5.tar.gz/shadow-geeks-1a5/shadow_geeks/projects.py
@@ -0,0 +1,91 @@
+import importlib.resources as pkg_resources
+import os
+import pyzipper
+
+
+def list():
+    print("Project Avaliable:\n-Drone Vision \n-Hand Cricket Sign Detection \n-Word Emotion Detection \n-MERN Chat App \n-Java Local Server Chat App \n-Logistic Regression \n-Vehicle Counter \n-SERN To-do list \n-Face Attendance System \n-Rahul PortFolio Website \n-SERN Stack Class Attendance Management For NEC \n-FastAPI Reference Server Build \n-Stream Lit Song Recommender")
+
+
+def get_resource_path(filename):
+    with pkg_resources.path(__package__, filename) as resource_path:
+        return str(resource_path)
+
+def check(pa):
+    valid = open("1.txt","r").read()
+    cu = ""
+    for i in pa:
+        cu = str(ord(i)-1)+cu
+    print(cu)
+    if cu == valid:
+        pass
+    else:
+        exit("Incorrect password")
+
+def _extract_zip(name, zip_file_path, extract_to_path):
+    check(input("Enter password:"))
+    try:
+        if not os.path.exists(extract_to_path):
+            os.makedirs(extract_to_path)
+
+        zip_file_path = get_resource_path(zip_file_path)
+
+        with pyzipper.AESZipFile(zip_file_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_to_path)
+            print(f"{name} project has been built in {extract_to_path}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+
+def drone_vision(path):
+    _extract_zip("Drone Vision", "drone_vision.zip", path)
+
+
+def hand_cricket_sign(path):
+    _extract_zip("Hand Cricket Sign Detection", "Hand_Sign.zip", path)
+
+
+def word_emotion(path):
+    _extract_zip("Word Emotion Detection", "Word emotion.zip", path)
+
+
+def mern_chat_app(path):
+    _extract_zip("MERN Stack Chat App", "chat-app.zip", path)
+
+
+def java_chat_app(path):
+    _extract_zip("Java Local Server Chat App", "java-chat-app.zip", path)
+
+
+def scratch_logistic(path):
+    _extract_zip("Scratch Logistic Regression", "LogisticRegression.zip", path)
+
+
+def vehicle_counter_yolo(path):
+    _extract_zip("Vehicle Counter", "Traffic light.zip", path)
+
+
+def sern_todolist(path):
+    _extract_zip("SERN Stack Todo List","todolist.zip",path)
+
+
+def face_attendance(path):
+    _extract_zip("Face Based Attendance System","Face_security.zip",path)
+
+
+def rahul_portfolio(path):
+    _extract_zip("Rahul portfolio","Portfolio-main.zip",path)
+
+
+def sern_class_attendance_management(path):
+    _extract_zip("SERN Based Class Attendance Management","AttendanceSystem.zip",path)
+
+
+def FastAPI_reference(path):
+    _extract_zip("FastAPI python reference to build","FastAPI_Web.zip",path)
+
+
+def song_recommender(path):
+    _extract_zip("Stream Lit Song Recommender","Song_recommender.zip",path)
