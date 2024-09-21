c89a23320142cd489b75b0dc877f560e471f99c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 09:04:09 +0000

    Add package auto-config/0.1.5/auto_config-0.1.5-py3-none-any.whl

---
+++ packages/auto-config/auto_config-0.1.5-py3-none-any.whl/auto_config/generator/dns_config.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import json
+from collections.abc import Sequence
+
+from ..device import Device
+from ..field import DefaultExtraField
+from .base import GeneratorBase
+
+
+class DNSConfigGenerator(GeneratorBase):
+    def __init__(
+        self,
+        devices: Sequence[Device[DefaultExtraField]],
+        *,
+        filter_groups: list[str] | None = None,
+    ):
+        super().__init__()
+        self.devices = devices
+        self.filter_groups = filter_groups
+
+    def generate(self):
+        record_list = []
+        ignored_record_list = []
+        for device in self.devices:
+            name = device.get_domain()
+            if self.filter_groups is not None and device.group not in self.filter_groups:
+                ignored_record_list.append(name)
+                continue
+            target = device.extra.dns.public if device.extra.dns is not None else "unknown"
+            if device.extra.ansible is not None and device.extra.ansible.server:
+                record_list.append((device.group, "{:}.bone6.top".format(name)))
+            record_list.append(("{:}".format(name), target))
+
+        self._generated_code = json.dumps(
+            {"domain": "bone6.top", "records": record_list, "ignore": ignored_record_list}, indent=2
+        )
