997791b4e023f07ff0edbc32dc5bac6738a4b8fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 06:03:40 +0000

    Add package django-nice/0.4.25/django_nice-0.4.25-py3-none-any.whl

---
+++ packages/django-nice/django_nice-0.4.25-py3-none-any.whl/django_nice/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.apps import apps
+from .sse import SSEManager
+
+@receiver(post_save)
+def model_update_signal(sender, instance, **kwargs):
+    for field in instance._meta.fields:
+        field_name = field.name
+        new_value = getattr(instance, field_name, None)
+
+        if new_value is not None:
+            print(f"Notifying listeners for {sender.__name__} {instance.pk} {field_name} = {new_value}")
+            SSEManager.notify_listeners(sender.__name__, instance.pk, field_name, new_value)
+
+from django.db.models.signals import post_save
+
+def setup_signals(app_label, model_name, signal_handler):
+    model = apps.get_model(app_label, model_name)
+    post_save.connect(signal_handler, sender=model)
