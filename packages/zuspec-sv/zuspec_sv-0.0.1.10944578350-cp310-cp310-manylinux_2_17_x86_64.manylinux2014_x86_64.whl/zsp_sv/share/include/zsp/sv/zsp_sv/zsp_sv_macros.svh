5b0d4c96cfcfd3996fc7db5e24fe56ea4518e976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:03:30 +0000

    Add package zuspec-sv/0.0.1.10944578350/zuspec_sv-0.0.1.10944578350-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/zuspec-sv/zuspec_sv-0.0.1.10944578350-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/zsp_sv/share/include/zsp/sv/zsp_sv/zsp_sv_macros.svh
@@ -0,0 +1,8 @@
+`ifndef INCLUDED_ZSP_SV_MACROS_SVH
+`define INCLUDED_ZSP_SV_MACROS_SVH
+
+`define zsp_reg_type_offset(regtype, regname) regtype``::``inst(null).``regname``.``offset
+
+
+
+`endif /* INCLUDED_ZSP_SV_MACROS_SVH */
