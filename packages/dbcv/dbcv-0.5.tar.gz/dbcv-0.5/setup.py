915cae4d05143359dbdaf3cb9701d9e3be25c48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:04:40 +0000

    Add package dbcv/0.5/dbcv-0.5.tar.gz

---
+++ packages/dbcv/dbcv-0.5.tar.gz/dbcv-0.5/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+from setuptools.command.install import install
+from pathlib import Path
+import subprocess
+import sys
+
+class CustomInstallCommand(install):
+    def run(self):
+        script_path = Path(__file__).parent / 'dbcv' / 'setup_script.py'
+        subprocess.call([sys.executable, str(script_path.absolute())])
+        super(self).run()
+
+setup(
+    name='dbcv',
+    version='0.5',
+    author='db.boy',
+    author_email='minkin.d.d@gmail.com',
+    description='This library allows you to compute the DBCV clustering metric quickly and with low memory consumption',
+    long_description=open("README.md", 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/boyfws/DBCV',
+    packages=find_packages(),
+    install_requires=[
+        'numba==0.60.0',
+        'numpy>=1.19.5',
+    ],
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+)
