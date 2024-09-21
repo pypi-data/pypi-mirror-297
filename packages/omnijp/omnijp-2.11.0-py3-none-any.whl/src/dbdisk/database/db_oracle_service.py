f3e1ee3f582e42a539726c63a88d8c03a628433a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:04:20 +0000

    Add package omnijp/2.11.0/omnijp-2.11.0-py3-none-any.whl

---
+++ packages/omnijp/omnijp-2.11.0-py3-none-any.whl/src/dbdisk/database/db_oracle_service.py
@@ -0,0 +1,10 @@
+from src.dbdisk.database.db_service import DbService
+
+
+class DbOracleService(DbService):
+    def __init__(self, connection_string: str):
+        self.connection_string = connection_string
+
+    def connect(self, query: str):
+        import cx_Oracle
+        return cx_Oracle.connect(self.connection_string)
