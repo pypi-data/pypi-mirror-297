e0496e591433323bf8fa2e617a9d607d07895620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:06:26 +0000

    Add package FlowCyPy/0.10.0.post0/flowcypy-0.10.0.post0.tar.gz

---
+++ packages/FlowCyPy/flowcypy-0.10.0.post0.tar.gz/flowcypy-0.10.0.post0/FlowCyPy/units.py
@@ -0,0 +1,31 @@
+# Initialize a unit registry
+import pint_pandas as pint
+
+ureg = pint.PintType.ureg
+
+ureg.setup_matplotlib()
+Quantity = ureg.Quantity
+
+
+_scaled_units_str_list = [
+    'watt', 'volt', 'meter', 'second', 'liter', 'hertz', 'ohm', 'ampere'
+]
+
+for _units_str in _scaled_units_str_list:
+    for scale in ['nano', 'micro', 'milli', '', 'kilo', 'mega']:
+        _unit = scale + _units_str
+        globals()[_unit] = getattr(ureg, _unit)
+
+
+joule = ureg.joule
+coulomb = ureg.coulomb
+power = ureg.watt.dimensionality
+kelvin = ureg.kelvin
+celsius = ureg.celsius
+particle = ureg.particle
+refractive_index_unit = ureg.refractive_index_unit
+degree = ureg.degree
+distance = ureg.meter.dimensionality
+time = ureg.second.dimensionality
+volume = ureg.liter.dimensionality
+frequency = ureg.hertz.dimensionality
