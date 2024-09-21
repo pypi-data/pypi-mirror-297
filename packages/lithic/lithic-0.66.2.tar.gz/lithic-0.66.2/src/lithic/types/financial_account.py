8f6ff2655907a18192efb48e44d53bdbadecbab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 04:04:13 +0000

    Add package lithic/0.66.2/lithic-0.66.2.tar.gz

---
+++ packages/lithic/lithic-0.66.2.tar.gz/lithic-0.66.2/src/lithic/types/financial_account.py
@@ -0,0 +1,38 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["FinancialAccount"]
+
+
+class FinancialAccount(BaseModel):
+    token: str
+    """Globally unique identifier for the financial account."""
+
+    created: datetime
+    """Date and time for when the financial account was first created."""
+
+    is_for_benefit_of: bool
+    """Whether the financial account holds funds for benefit of another party."""
+
+    type: Literal["ISSUING", "OPERATING", "RESERVE"]
+    """Type of financial account"""
+
+    updated: datetime
+    """Date and time for when the financial account was last updated."""
+
+    account_number: Optional[str] = None
+    """Account number for your Lithic-assigned bank account number, if applicable."""
+
+    account_token: Optional[str] = None
+    """Account token of the financial account if applicable."""
+
+    nickname: Optional[str] = None
+    """User-defined nickname for the financial account."""
+
+    routing_number: Optional[str] = None
+    """Routing number for your Lithic-assigned bank account number, if applicable."""
---
+++ packages/lithic/lithic-0.66.2.tar.gz/lithic-0.66.2/src/lithic/types/financial_accounts/financial_account_credit_config.py
@@ -0,0 +1,25 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+
+from ..._models import BaseModel
+
+__all__ = ["FinancialAccountCreditConfig"]
+
+
+class FinancialAccountCreditConfig(BaseModel):
+    account_token: str
+    """Globally unique identifier for the account"""
+
+    credit_limit: Optional[int] = None
+
+    credit_product_token: Optional[str] = None
+    """Globally unique identifier for the credit product"""
+
+    external_bank_account_token: Optional[str] = None
+
+    financial_account_state: Optional[str] = None
+    """State of the financial account"""
+
+    tier: Optional[str] = None
+    """Tier assigned to the financial account"""
