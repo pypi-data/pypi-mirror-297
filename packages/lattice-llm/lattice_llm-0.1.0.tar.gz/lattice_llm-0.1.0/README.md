bff1cc677f40dad1f1058329e64fb0928212f873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:04:00 +0000

    Add package lattice-llm/0.1.0/lattice_llm-0.1.0.tar.gz

---
+++ packages/lattice-llm/lattice_llm-0.1.0.tar.gz/lattice_llm-0.1.0/README.md
@@ -0,0 +1,3 @@
+# Lattice LLM
+
+Lattice LLM is a Python library for authoring LLM-powered agents as executable graphs.
---
+++ packages/lattice-llm/lattice_llm-0.1.0.tar.gz/lattice_llm-0.1.0/lattice_llm/graph/__init__.py
@@ -0,0 +1,2 @@
+from .graph import Graph, GraphExecutionResult, END, START
+from .execution import run_graph, run_chatbot_on_cli
