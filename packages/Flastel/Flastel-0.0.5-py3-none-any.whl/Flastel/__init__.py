0e8e6bb56f85e6abc80adaee43807834d346289e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:04:23 +0000

    Add package Flastel/0.0.5/Flastel-0.0.5-py3-none-any.whl

---
+++ packages/Flastel/Flastel-0.0.5-py3-none-any.whl/Flastel/__init__.py
@@ -0,0 +1,5 @@
+from Flastel.func.polling_func import TelegramPollingBot, polling_func
+from Flastel.func.webhook_func import webhook_func
+from Flastel.func.methods import TelegramBot
+
+all = ["polling_func", "webhook_func", "TelegramBot", "TelegramPollingBot"]
