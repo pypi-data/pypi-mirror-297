4cb2d5842cbf24f5d4defc62a1b41950843ae949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 09:04:10 +0000

    Add package affinidi_tdk_wallets_client/1.26.0/affinidi_tdk_wallets_client-1.26.0-py3-none-any.whl

---
+++ packages/affinidi_tdk_wallets_client/affinidi_tdk_wallets_client-1.26.0-py3-none-any.whl/affinidi_tdk_wallets_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from affinidi_tdk_wallets_client.api.revocation_api import RevocationApi
+from affinidi_tdk_wallets_client.api.default_api import DefaultApi
+from affinidi_tdk_wallets_client.api.wallet_api import WalletApi
+
