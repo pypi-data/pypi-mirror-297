fb1a8d6359bb8418626b32f6ec87128827cd8b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:08:03 +0000

    Add package great-tables/0.11.1/great_tables-0.11.1.tar.gz

---
+++ packages/great-tables/great_tables-0.11.1.tar.gz/great_tables-0.11.1/tests/test_tab_create_modify.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import pytest
+from great_tables import GT
+from great_tables._locations import LocBody
+from great_tables._styles import CellStyleFill
+from great_tables._tab_create_modify import tab_style
+
+
+@pytest.fixture
+def gt():
+    return GT(pd.DataFrame({"x": [1, 2], "y": [4, 5]}))
+
+
+def test_tab_style(gt: GT):
+    style = CellStyleFill(color="blue")
+    new_gt = tab_style(gt, style, LocBody(["x"], [0]))
+
+    assert len(gt._styles) == 0
+    assert len(new_gt._styles) == 1
+
+    assert len(new_gt._styles[0].styles) == 1
+    assert new_gt._styles[0].styles[0] is style
+
+
+def test_tab_style_multiple_columns(gt: GT):
+    style = CellStyleFill(color="blue")
+    new_gt = tab_style(gt, style, LocBody(["x", "y"], [0]))
+
+    assert len(new_gt._styles) == 2
+
+    assert len(new_gt._styles[0].styles) == 1
+    assert new_gt._styles[0].styles[0] is style
