6e723b74225c4b70fbeffa9cb6cb0fcd16365114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:05:55 +0000

    Add package xarg-python/1.6/xarg-python-1.6.tar.gz

---
+++ packages/xarg-python/xarg-python-1.6.tar.gz/xarg-python-1.6/xarg/attribute.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+
+from urllib.parse import urljoin
+
+__project__ = "xarg-python"
+__version__ = "1.6"
+__description__ = "Simple command-line tool based on argparse."
+__url_home__ = "https://github.com/bondbox/xarg-python/"
+__url_code__ = __url_home__
+__url_docs__ = __url_home__
+__url_bugs__ = urljoin(__url_home__, "issues")
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
+
+# project name
+__prog_name__ = "xarg"
+__prog_complete__ = "xargcomplete"
+__prog_project__ = "xargproject"
