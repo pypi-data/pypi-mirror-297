9c53e4d9f2d2b15060573e1a8bafc2f86691a4ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:07:33 +0000

    Add package BinhoPulsar/0.1.0/BinhoPulsar-0.1.0-py3-none-any.whl

---
+++ packages/BinhoPulsar/BinhoPulsar-0.1.0-py3-none-any.whl/BinhoPulsar/Pulsar.py
@@ -0,0 +1,6 @@
+from .SupernovaSDK.BinhoSupernova.Supernova import Supernova
+
+class Pulsar(Supernova):
+    """
+    Pulsar class which inherits all from Supernova. Even I3C. It must not be used for I3C
+    """
