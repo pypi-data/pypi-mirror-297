4e1149523bf36e7df47fdd5f04f0f8655529b25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:04:22 +0000

    Add package greeng3-python/0.1.6/greeng3_python-0.1.6.tar.gz

---
+++ packages/greeng3-python/greeng3_python-0.1.6.tar.gz/greeng3_python-0.1.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='greeng3_python',
+    version='0.1.6',
+    packages=find_packages(),
+    install_requires=[
+        'bs4',
+        'docker',
+        'ebooklib',
+        'nltk',
+        'pydantic',
+        'requests',
+    ],
+    author='Gordon Greene',
+    author_email='greeng3@obscure-reference.com',
+    description='A library of things I find useful.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/greeng3/greeng3_python',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
