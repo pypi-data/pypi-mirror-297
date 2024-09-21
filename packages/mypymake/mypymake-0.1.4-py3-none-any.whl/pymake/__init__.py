5fc212a712d074197f68354fb60f03ce4ef288b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:29 +0000

    Add package mypymake/0.1.4/mypymake-0.1.4-py3-none-any.whl

---
+++ packages/mypymake/mypymake-0.1.4-py3-none-any.whl/pymake/__init__.py
@@ -0,0 +1,6 @@
+'''
+PyMake is a simple, Python-based build system designed to simplify the process of compiling and running C++ projects. It provides an intuitive API for configuring your build process and executing compiled programs.
+'''
+
+import pymake.bare as bare
+import pymake.cmake as cmake
