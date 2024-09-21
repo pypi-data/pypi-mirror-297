46f3078f8b3dce006300865cc465e5bb99665174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:08:30 +0000

    Add package arcade/3.0.0.dev36/arcade-3.0.0.dev36-py3-none-any.whl

---
+++ packages/arcade/arcade-3.0.0.dev36-py3-none-any.whl/arcade/future/input/__init__.py
@@ -0,0 +1,20 @@
+# ruff: noqa: F401
+#  type: ignore
+
+from .inputs import ControllerAxes, ControllerButtons, Keys, MouseAxes, MouseButtons
+from .manager import ActionState, InputManager
+from .input_mapping import Action, ActionMapping, Axis, AxisMapping
+
+__all__ = [
+    "ControllerAxes",
+    "ControllerButtons",
+    "Keys",
+    "MouseAxes",
+    "MouseButtons",
+    "ActionState",
+    "InputManager",
+    "Action",
+    "ActionMapping",
+    "Axis",
+    "AxisMapping",
+]
