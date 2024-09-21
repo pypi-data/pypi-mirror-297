8dc051d307078b8ac40daf129fbc4e50d4f79481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 11:04:11 +0000

    Add package xmlcord/1.1.2/xmlcord-1.1.2.tar.gz

---
+++ packages/xmlcord/xmlcord-1.1.2.tar.gz/xmlcord-1.1.2/xmlcord/__init__.py
@@ -0,0 +1,5 @@
+from .interface import GeneralInterface
+from .renderer  import DiscordRenderer
+from .listener  import DiscordListener
+
+from .tags.base  import TagException, InvalidEntry, EmptyEntry
