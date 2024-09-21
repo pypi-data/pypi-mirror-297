0cc9c8edd7c7a5de1446bf0bffa6ec6a9f6650d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:56 +0000

    Add package flet-contribute/2024.9.20.2219/flet_contribute-2024.9.20.2219-py3-none-any.whl

---
+++ packages/flet-contribute/flet_contribute-2024.9.20.2219-py3-none-any.whl/flet/fastapi2/__init__.py
@@ -0,0 +1,50 @@
+"""FastAPI framework, high performance, easy to learn, fast to code, ready for production"""
+
+__version__ = "0.115.0"
+
+from starlette import status as status
+
+from .applications import FastAPI as FastAPI
+from .background import BackgroundTasks as BackgroundTasks
+from .datastructures import UploadFile as UploadFile
+from .exceptions import HTTPException as HTTPException
+from .exceptions import WebSocketException as WebSocketException
+from .param_functions import Body as Body
+from .param_functions import Cookie as Cookie
+from .param_functions import Depends as Depends
+from .param_functions import File as File
+from .param_functions import Form as Form
+from .param_functions import Header as Header
+from .param_functions import Path as Path
+from .param_functions import Query as Query
+from .param_functions import Security as Security
+from .requests import Request as Request
+from .responses import Response as Response
+from .routing import APIRouter as APIRouter
+from .websockets import WebSocket as WebSocket
+from .websockets import WebSocketDisconnect as WebSocketDisconnect
+
+
+# Definir qué se exporta cuando se usa `import fasapi2`
+__all__ = [
+    "FastAPI",
+    "BackgroundTasks",
+    "UploadFile",
+    "HTTPException",
+    "WebSocketException",
+    "Body",
+    "Cookie",
+    "Depends",
+    "File",
+    "Form",
+    "Header",
+    "Path",
+    "Query",
+    "Security",
+    "Request",
+    "Response",
+    "APIRouter",
+    "WebSocket",
+    "WebSocketDisconnect",
+    "status",
+]
