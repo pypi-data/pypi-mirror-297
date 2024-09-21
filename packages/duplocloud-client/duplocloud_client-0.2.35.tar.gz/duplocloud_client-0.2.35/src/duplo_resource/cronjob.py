abf37821dea59962281f4081a7c3d0cb8fcd974d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:33 +0000

    Add package duplocloud-client/0.2.35/duplocloud_client-0.2.35.tar.gz

---
+++ packages/duplocloud-client/duplocloud_client-0.2.35.tar.gz/duplocloud_client-0.2.35/src/duplo_resource/cronjob.py
@@ -0,0 +1,45 @@
+from duplocloud.client import DuploClient
+from duplocloud.resource import DuploTenantResourceV3
+from duplocloud.commander import Command, Resource
+import duplocloud.args as args
+
+@Resource("cronjob")
+class DuploCronJob(DuploTenantResourceV3):
+  """
+  Duplo CronJob are scheduled jobs that run containers in a Kubernetes cluster.
+  """
+  
+  def __init__(self, duplo: DuploClient):
+    super().__init__(duplo, "k8s/cronJob")
+
+  @Command()
+  def update_image(self, 
+                   name: args.NAME, 
+                   image: args.IMAGE):
+    """Update the image of a cronjob.
+    
+    Args:
+      name (str): The name of the cronjob to update.
+      image (str): The new image to use for the cronjob.
+    """
+    cronjob = self.find(name)
+    cronjob["spec"]["jobTemplate"]["spec"]["template"]["spec"]["containers"][0]["image"] = image
+    self.update(cronjob)
+    return {"message": f"Successfully updated image for cronjob '{name}'"}
+  
+  @Command()
+  def update_schedule(self, 
+                      name: args.NAME, 
+                      cronschedule: args.CRONSCHEDULE) -> dict:
+    """Update the schedule of a cronjob.
+    
+    Args:
+      name: The name of the cronjob to update.
+      cronschedule: The new schedule to use for the cronjob.
+    Returns:
+      message: A success message.
+    """
+    cronjob = self.find(name)
+    cronjob["spec"]["schedule"] = cronschedule
+    self.update(cronjob)
+    return {"message": f"Successfully updated cron-schedule for cronjob '{name}'"}
