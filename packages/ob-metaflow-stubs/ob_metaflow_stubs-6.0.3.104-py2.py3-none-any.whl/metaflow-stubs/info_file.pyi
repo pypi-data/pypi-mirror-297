5b33dfaa2d1238341421e54a57e80309cab15ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:47 +0000

    Add package ob-metaflow-stubs/6.0.3.104/ob_metaflow_stubs-6.0.3.104-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.104-py2.py3-none-any.whl/metaflow-stubs/info_file.pyi
@@ -0,0 +1,16 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.22.1+obcheckpoint(0.0.11);ob(v1)                              #
+# Generated on 2024-09-20T21:46:07.328193                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+CURRENT_DIRECTORY: str
+
+INFO_FILE: str
+
+def read_info_file():
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.104-py2.py3-none-any.whl/metaflow-stubs/tuple_util.pyi
@@ -0,0 +1,14 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.22.1+obcheckpoint(0.0.11);ob(v1)                              #
+# Generated on 2024-09-20T21:46:07.338003                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def namedtuple_with_defaults(typename, field_descr, defaults = ()):
+    ...
+
+foreach_frame_field_list: list
+
