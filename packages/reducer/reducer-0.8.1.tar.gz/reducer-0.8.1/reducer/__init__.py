26491c98a70cd801f9de8a2af6eb11bc9e8c3c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:05:48 +0000

    Add package reducer/0.8.1/reducer-0.8.1.tar.gz

---
+++ packages/reducer/reducer-0.8.1.tar.gz/reducer-0.8.1/reducer/__init__.py
@@ -0,0 +1,6 @@
+# keep this at the top -- name is needed for imports to succeed
+NOTEBOOK_TEMPLATE_NAME = 'reducer-template.ipynb'
+
+from .core import *
+
+from ._version import __version__, __version_tuple__  # noqa: F401
