d81cbc3700e87ac5705f775d17a13fd108a9dd92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:08:05 +0000

    Add package massprince/1.0/massprince-1.0-py3-none-any.whl

---
+++ packages/massprince/massprince-1.0-py3-none-any.whl/massprince/__init__.py
@@ -0,0 +1,7 @@
+from massprince.vish1 import add
+from massprince.vish1 import sub
+from massprince.vish1 import mul
+from massprince.vish2 import div
+from massprince.vish2 import floor
+from massprince.vish2 import mod
+from massprince.vish3 import mass_prince
---
+++ packages/massprince/massprince-1.0-py3-none-any.whl/massprince/vish2.py
@@ -0,0 +1,6 @@
+def floor(a,b):
+    return a // b
+def mod(a,b):
+    return a % b
+def div(a,b):
+    return a / b
