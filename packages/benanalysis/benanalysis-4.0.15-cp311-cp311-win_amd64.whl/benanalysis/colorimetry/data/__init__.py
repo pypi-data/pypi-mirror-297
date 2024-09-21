5f78240512f40b40f2a640a24855ab5d73464b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:03:38 +0000

    Add package benanalysis/4.0.15/benanalysis-4.0.15-cp311-cp311-win_amd64.whl

---
+++ packages/benanalysis/benanalysis-4.0.15-cp311-cp311-win_amd64.whl/benanalysis/colorimetry/data/__init__.py
@@ -0,0 +1,8 @@
+#
+# @file __init__.py
+# @author Markus Führer
+# @date 7 Aug 2023
+# @copyright Copyright © 2023 Bentham Instruments Ltd. All Rights Reserved.
+#
+
+from benanalysis._benpy_core.colorimetry.data import *
