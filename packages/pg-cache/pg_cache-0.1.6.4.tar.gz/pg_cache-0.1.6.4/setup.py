9bb35892791066e17528058c06c80a073926786e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 03:07:38 +0000

    Add package pg-cache/0.1.6.4/pg_cache-0.1.6.4.tar.gz

---
+++ packages/pg-cache/pg_cache-0.1.6.4.tar.gz/pg_cache-0.1.6.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pg-cache',
+    version='0.1.6.4',
+    description='A PostgreSQL wrapper similar to diskcache',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/li-xiu-qi/pg-cache',
+    author='li-xiu-qi',
+    author_email='lixiuqixiaoke@qq.com',
+    packages=find_packages(),
+    install_requires=[
+        'sqlalchemy>=1.4',
+        'asyncpg>=0.21'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
