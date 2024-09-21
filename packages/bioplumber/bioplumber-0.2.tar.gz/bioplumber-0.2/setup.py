300eb41d0daada3e3bfe33cec18b042a635acda3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:09 +0000

    Add package bioplumber/0.2/bioplumber-0.2.tar.gz

---
+++ packages/bioplumber/bioplumber-0.2.tar.gz/bioplumber-0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bioplumber']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'bioplumber',
+    'version': '0.2',
+    'description': '',
+    'long_description': None,
+    'author': 'ParsaGhadermazi',
+    'author_email': '54489047+ParsaGhadermazi@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
