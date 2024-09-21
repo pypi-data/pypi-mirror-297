4ac1c70d3b3befb48be4d9202908d4d2b94193aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 09:04:15 +0000

    Add package structure-clustering/1.1.0/structure_clustering-1.1.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/structure-clustering/structure_clustering-1.1.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/structure_clustering/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from ._core import __doc__, __version__, Machine, Structure, Atom
+
+from structure_clustering.import_multi_xyz import (
+    import_multi_xyz,
+    element_to_atomic_number,
+)
