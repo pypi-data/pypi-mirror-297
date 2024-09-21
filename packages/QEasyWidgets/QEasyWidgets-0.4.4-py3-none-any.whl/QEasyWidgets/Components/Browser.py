b5f3c03d3b980e21016a87bb53c1dcec29d31bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:04:21 +0000

    Add package QEasyWidgets/0.4.4/QEasyWidgets-0.4.4-py3-none-any.whl

---
+++ packages/QEasyWidgets/QEasyWidgets-0.4.4-py3-none-any.whl/QEasyWidgets/Components/Browser.py
@@ -0,0 +1,29 @@
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+
+from ..Common.StyleSheet import *
+from ..Common.QFunctions import *
+from ..Resources.Sources import *
+
+##############################################################################################################################
+
+class TextBrowserBase(QTextBrowser):
+    '''
+    '''
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent)
+
+        StyleSheetBase.Browser.Apply(self)
+
+    def ClearDefaultStyleSheet(self) -> None:
+        StyleSheetBase.Browser.Deregistrate(self)
+
+
+class EmbeddedTextBrowser(TextBrowserBase):
+    '''
+    '''
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent)
+
+##############################################################################################################################
