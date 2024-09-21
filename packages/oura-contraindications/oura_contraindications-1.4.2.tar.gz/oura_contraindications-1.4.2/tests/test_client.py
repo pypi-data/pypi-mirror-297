527e37a2fa0af1cd038dad40f6705ea74a3d6fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:03:37 +0000

    Add package oura-contraindications/1.4.2/oura_contraindications-1.4.2.tar.gz

---
+++ packages/oura-contraindications/oura_contraindications-1.4.2.tar.gz/oura_contraindications-1.4.2/tests/test_client.py
@@ -0,0 +1,59 @@
+import json
+from urllib.parse import parse_qs, urlparse
+
+import requests_mock
+
+from oura_contraindications import OuraClient
+
+adapter = requests_mock.Adapter()
+
+
+def test_summary_url():
+    client = OuraClient("test_id")
+    url = client._build_summary_url(start="start-date", end=None, summary_type="sleep")
+    parsed_url = urlparse(url)
+    params = parse_qs(parsed_url.query)
+    assert "end" not in params.keys()
+
+    url2 = client._build_summary_url(
+        start="start-date", end="end_date", summary_type="sleep"
+    )
+    parsed_url = urlparse(url2)
+    params = parse_qs(parsed_url.query)
+    assert "end" in params.keys()
+
+
+def test_token_refresh():
+    update_called = []
+
+    # hacky way to test side effect
+    def token_updater(token):
+        update_called.append(1)
+
+    client = OuraClient(
+        client_id="test_id", access_token="token", refresh_callback=token_updater
+    )
+    adapter.register_uri(
+        requests_mock.POST,
+        requests_mock.ANY,
+        status_code=401,
+        text=json.dumps(
+            {
+                "access_token": "fake_return_access_token",
+                "refresh_token": "fake_return_refresh_token",
+            }
+        ),
+    )
+    adapter.register_uri(
+        requests_mock.GET,
+        requests_mock.ANY,
+        status_code=401,
+        text=json.dumps({"a": "b"}),
+    )
+
+    client._auth_handler._session.mount(client.API_ENDPOINT, adapter)
+    try:
+        client.user_info()
+    except Exception:
+        pass
+    assert len(update_called) == 1
