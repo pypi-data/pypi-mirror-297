ef92a69f17f9958c8b224e614d2184fc475d65b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:09:07 +0000

    Add package senseppi/0.7.3/senseppi-0.7.3.tar.gz

---
+++ packages/senseppi/senseppi-0.7.3.tar.gz/senseppi-0.7.3/senseppi/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.7.3"
+__author__ = "Konstantin Volzhenin"
+
+from . import model, commands, esm2_model, dataset, utils, network_utils
+
+__all__ = [
+    "model",
+    "commands",
+    "esm2_model",
+    "dataset",
+    "utils",
+    "network_utils"
+]
