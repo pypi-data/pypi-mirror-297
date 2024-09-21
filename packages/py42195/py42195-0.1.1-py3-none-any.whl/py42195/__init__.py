4bd5f2487420b14e2eb50e78b0bdcfaed11fb17c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 11:04:24 +0000

    Add package py42195/0.1.1/py42195-0.1.1-py3-none-any.whl

---
+++ packages/py42195/py42195-0.1.1-py3-none-any.whl/py42195/__init__.py
@@ -0,0 +1,15 @@
+from py42195.config import IMPERIAL, METRIC, get_unit_system, set_unit_system
+from py42195.types import Distance, Duration, Pace, distance, duration, pace
+
+__all__ = [
+    "Distance",
+    "Duration",
+    "Pace",
+    "distance",
+    "duration",
+    "pace",
+    "set_unit_system",
+    "get_unit_system",
+    "METRIC",
+    "IMPERIAL",
+]
