69cd160e598a495f058cd0c9fd8afa408a3b154c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 11:04:11 +0000

    Add package physeng/0.4.1/physeng-0.4.1.tar.gz

---
+++ packages/physeng/physeng-0.4.1.tar.gz/physeng-0.4.1/physeng/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Andreas Mussgiller
+"""
+
+from .singleton import Singleton
+
+from .utilities import *
+from .matplotlib import *
+
+from .units import *
+
+from .materialdb import MaterialDB, MaterialProperty, Material
+from .iso286 import ISO286Hole, ISO286Shaft
+from .humidity import Humidity
