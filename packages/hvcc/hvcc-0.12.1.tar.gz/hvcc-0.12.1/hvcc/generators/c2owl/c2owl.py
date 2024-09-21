3f9dd23677b495fc9d766d51ed5d7a0ac87c715e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:08:48 +0000

    Add package hvcc/0.12.1/hvcc-0.12.1.tar.gz

---
+++ packages/hvcc/hvcc-0.12.1.tar.gz/hvcc-0.12.1/hvcc/generators/c2owl/c2owl.py
@@ -0,0 +1,157 @@
+# import datetime
+import os
+import shutil
+import time
+import jinja2
+import json
+from typing import Dict, List, Optional
+
+import hvcc.core.hv2ir.HeavyLangObject as HeavyLangObject
+from ..copyright import copyright_manager
+
+
+heavy_hash = HeavyLangObject.HeavyLangObject.get_hash
+OWL_BUTTONS = ['Push', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8']
+
+
+class c2owl:
+    """ Generates a OWL wrapper for a given patch.
+    """
+
+    @classmethod
+    def make_jdata(cls, patch_ir: str) -> List:
+        jdata = list()
+
+        with open(patch_ir, mode="r") as f:
+            ir = json.load(f)
+
+            for name, v in ir['control']['receivers'].items():
+                # skip __hv_init and similar
+                if name.startswith("__"):
+                    continue
+
+                # If a name has been specified
+                if v['attributes'].get('raw'):
+                    key = v['attributes']['raw']
+                    jdata.append((key, name, 'RECV', f"0x{heavy_hash(name):X}",
+                                  v['attributes']['min'],
+                                  v['attributes']['max'],
+                                  v['attributes']['default'],
+                                  key in OWL_BUTTONS))
+
+                elif name.startswith('Channel-'):
+                    key = name.split('Channel-', 1)[1]
+                    jdata.append((key, name, 'RECV', f"0x{heavy_hash(name):X}",
+                                  0, 1, None, key in OWL_BUTTONS))
+
+            for k, v in ir['objects'].items():
+                try:
+                    if v['type'] == '__send':
+                        name = v['args']['name']
+                        if v['args']['attributes'].get('raw'):
+                            key = v['args']['attributes']['raw']
+                            jdata.append((key, f'{name}>', 'SEND', f"0x{heavy_hash(name):X}",
+                                          v['args']['attributes']['min'],
+                                          v['args']['attributes']['max'],
+                                          v['args']['attributes']['default'],
+                                          key in OWL_BUTTONS))
+                        elif name.startswith('Channel-'):
+                            key = name.split('Channel-', 1)[1]
+                            jdata.append((key, f'{name}>', 'SEND', f"0x{heavy_hash(name):X}",
+                                          0, 1, None, key in OWL_BUTTONS))
+                except Exception:
+                    pass
+
+            return jdata
+
+    @classmethod
+    def compile(
+        cls,
+        c_src_dir: str,
+        out_dir: str,
+        externs: Dict,
+        patch_name: Optional[str] = None,
+        patch_meta: Optional[Dict] = None,
+        num_input_channels: int = 0,
+        num_output_channels: int = 0,
+        copyright: Optional[str] = None,
+        verbose: Optional[bool] = False
+    ) -> Dict:
+
+        tick = time.time()
+
+        out_dir = os.path.join(out_dir, "Source")
+        patch_name = patch_name or "heavy"
+        copyright_c = copyright_manager.get_copyright_for_c(copyright)
+
+        try:
+            # ensure that the output directory does not exist
+            out_dir = os.path.abspath(out_dir)
+            if os.path.exists(out_dir):
+                shutil.rmtree(out_dir)
+
+            # copy over generated C source files
+            shutil.copytree(c_src_dir, out_dir)
+
+            # copy over deps
+            shutil.copytree(os.path.join(os.path.dirname(__file__), "deps"), out_dir, dirs_exist_ok=True)
+
+            # initialize the jinja template environment
+            env = jinja2.Environment()
+
+            env.loader = jinja2.FileSystemLoader(
+                os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates"))
+
+            # construct jdata from ir
+            ir_dir = os.path.join(c_src_dir, "../ir")
+            patch_ir = os.path.join(ir_dir, f"{patch_name}.heavy.ir.json")
+            jdata = cls.make_jdata(patch_ir)
+
+            # generate OWL wrapper from template
+            owl_hpp_path = os.path.join(out_dir, f"HeavyOWL_{patch_name}.hpp")
+            with open(owl_hpp_path, "w") as f:
+                f.write(env.get_template("HeavyOwl.hpp").render(
+                    jdata=jdata,
+                    name=patch_name,
+                    copyright=copyright_c))
+            owl_h_path = os.path.join(out_dir, "HeavyOwlConstants.h")
+            with open(owl_h_path, "w") as f:
+                f.write(env.get_template("HeavyOwlConstants.h").render(
+                    jdata=jdata,
+                    copyright=copyright_c))
+
+            # ======================================================================================
+
+            return {
+                "stage": "c2owl",
+                "notifs": {
+                    "has_error": False,
+                    "exception": None,
+                    "warnings": [],
+                    "errors": []
+                },
+                "in_dir": c_src_dir,
+                "in_file": "",
+                "out_dir": out_dir,
+                "out_file": os.path.basename(owl_h_path),
+                "compile_time": time.time() - tick
+            }
+
+        except Exception as e:
+            return {
+                "stage": "c2owl",
+                "notifs": {
+                    "has_error": True,
+                    "exception": e,
+                    "warnings": [],
+                    "errors": [{
+                        "enum": -1,
+                        "message": str(e)
+                    }]
+                },
+                "in_dir": c_src_dir,
+                "in_file": "",
+                "out_dir": out_dir,
+                "out_file": "",
+                "compile_time": time.time() - tick
+            }
---
+++ packages/hvcc/hvcc-0.12.1.tar.gz/hvcc-0.12.1/hvcc/generators/c2unity/c2unity.py
@@ -0,0 +1,134 @@
+# Copyright (C) 2014-2018 Enzien Audio, Ltd.
+# Copyright (C) 2021-2023 Wasted Audio
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import jinja2
+import os
+import shutil
+import time
+from typing import Dict, Optional
+
+from ..copyright import copyright_manager
+from ..filters import filter_string_cap, filter_templates, filter_xcode_build, filter_xcode_fileref
+
+
+class c2unity:
+    """Generates a Audio Native Plugin wrapper for Unity 5.
+    """
+
+    @classmethod
+    def compile(
+        cls,
+        c_src_dir: str,
+        out_dir: str,
+        externs: Dict,
+        patch_name: Optional[str] = None,
+        patch_meta: Optional[Dict] = None,
+        num_input_channels: int = 0,
+        num_output_channels: int = 0,
+        copyright: Optional[str] = None,
+        verbose: Optional[bool] = False
+    ) -> Dict:
+
+        tick = time.time()
+
+        parameter_list = externs["parameters"]["in"]
+        event_list = externs["events"]["in"]
+        table_list = externs["tables"]
+
+        out_dir = os.path.join(out_dir, "unity")
+        patch_name = patch_name or "heavy"
+
+        copyright = copyright_manager.get_copyright_for_c(copyright)
+
+        # initialise the jinja template environment
+        env = jinja2.Environment()
+        env.filters["xcode_build"] = filter_xcode_build
+        env.filters["xcode_fileref"] = filter_xcode_fileref
+        env.filters["cap"] = filter_string_cap
+        env.loader = jinja2.FileSystemLoader(
+            encoding="utf-8-sig",
+            searchpath=[os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")])
+
+        static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
+        src_out_dir = os.path.join(out_dir, "source")
+
+        try:
+            # ensure that the output directory does not exist
+            out_dir = os.path.abspath(out_dir)
+            if os.path.exists(out_dir):
+                shutil.rmtree(out_dir)
+
+            # copy over static files
+            shutil.copytree(static_dir, out_dir)
+
+            # copy over generated C source files
+            src_out_dir = os.path.join(out_dir, "source", "heavy")
+            shutil.copytree(c_src_dir, src_out_dir)
+
+            # generate files from templates
+            for f in env.list_templates(filter_func=filter_templates):
+                file_path = os.path.join(out_dir, f)
+                file_path = file_path.replace("{{name}}", patch_name)
+
+                if not os.path.exists(os.path.dirname(file_path)):
+                    os.makedirs(os.path.dirname(file_path))
+
+                with open(file_path, "w") as g:
+                    g.write(env.get_template(f).render(
+                        patch_name=patch_name,
+                        files=os.listdir(src_out_dir),
+                        num_input_channels=num_input_channels,
+                        num_output_channels=num_output_channels,
+                        parameters=parameter_list,
+                        events=event_list,
+                        tables=table_list,
+                        pool_sizes_kb=externs["memoryPoolSizesKb"],
+                        compile_files=os.listdir(src_out_dir),
+                        copyright=copyright))
+
+            return {
+                "stage": "c2unity",
+                "notifs": {
+                    "has_error": False,
+                    "exception": None,
+                    "warnings": [],
+                    "errors": []
+                },
+                "in_dir": c_src_dir,
+                "in_file": "",
+                "out_dir": out_dir,
+                "out_file": "",
+                "compile_time": time.time() - tick
+            }
+
+        except Exception as e:
+            return {
+                "stage": "c2unity",
+                "notifs": {
+                    "has_error": True,
+                    "exception": e,
+                    "warnings": [],
+                    "errors": [{
+                        "enum": -1,
+                        "message": str(e)
+                    }]
+                },
+                "in_dir": c_src_dir,
+                "in_file": "",
+                "out_dir": out_dir,
+                "out_file": "",
+                "compile_time": time.time() - tick
+            }
