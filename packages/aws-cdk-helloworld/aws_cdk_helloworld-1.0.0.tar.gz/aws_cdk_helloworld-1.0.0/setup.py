cf54b74acc8651434de133cb238888d1e693d917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 04:04:19 +0000

    Add package aws-cdk-helloworld/1.0.0/aws_cdk_helloworld-1.0.0.tar.gz

---
+++ packages/aws-cdk-helloworld/aws_cdk_helloworld-1.0.0.tar.gz/aws_cdk_helloworld-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='aws_cdk_helloworld',
+    version='1.0.0',
+    packages=['aws_cdk_helloworld'],
+    install_requires=['aws-cdk'],
+    author='Neo',
+    author_email='neo@acme.com'
+)
