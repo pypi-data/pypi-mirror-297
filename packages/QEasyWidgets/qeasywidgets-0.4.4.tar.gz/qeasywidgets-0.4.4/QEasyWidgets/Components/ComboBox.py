163c28c442c33351a4c86e6bc4fdc538a17ba46a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:04:21 +0000

    Add package QEasyWidgets/0.4.4/qeasywidgets-0.4.4.tar.gz

---
+++ packages/QEasyWidgets/qeasywidgets-0.4.4.tar.gz/qeasywidgets-0.4.4/QEasyWidgets/Components/ComboBox.py
@@ -0,0 +1,33 @@
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+
+from ..Common.StyleSheet import *
+from ..Common.QFunctions import *
+
+##############################################################################################################################
+
+class ComboBoxBase(QComboBox):
+    '''
+    '''
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent)
+
+        self.setFocusPolicy(Qt.StrongFocus)
+
+        StyleSheetBase.ComboBox.Apply(self)
+
+    def wheelEvent(self, event: QWheelEvent) -> None:
+        event.ignore()
+
+    def ClearDefaultStyleSheet(self) -> None:
+        StyleSheetBase.ComboBox.Deregistrate(self)
+
+
+class EmbeddedComboBox(ComboBoxBase):
+    '''
+    '''
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent)
+
+##############################################################################################################################
---
+++ packages/QEasyWidgets/qeasywidgets-0.4.4.tar.gz/qeasywidgets-0.4.4/QEasyWidgets/Components/__init__.py
@@ -0,0 +1,14 @@
+from .Widget import WidgetBase
+from .Label import LabelBase
+from .Button import ButtonBase, MenuButton, HollowButton, EmbeddedButton
+from .ScrollArea import ScrollAreaBase
+from .Tree import TreeWidgetBase
+from .List import ListBase
+from .ToolBox import ToolBoxBase
+from .GroupBox import GroupBoxBase
+from .SpinBox import SpinBoxBase, DoubleSpinBoxBase, EmbeddedDoubleSpinBox
+from .ComboBox import ComboBoxBase, EmbeddedComboBox
+from .Edit import LineEditBase, TextEditBase, EmbeddedLineEdit, EmbeddedTextEdit
+from .Browser import TextBrowserBase, EmbeddedTextBrowser
+from .Player import MediaPlayerBase, EmbeddedMediaPlayer
+from .Table import TableBase
