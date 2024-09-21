12e87120c07397df1c4820565b90920401d6107d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:03:53 +0000

    Add package powermake/1.11.1/powermake-1.11.1.tar.gz

---
+++ packages/powermake/powermake-1.11.1.tar.gz/powermake-1.11.1/powermake/display.py
@@ -0,0 +1,30 @@
+# Copyright 2024 Macéo Tuloup
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import colorama
+
+
+def init_colors():
+    colorama.init()
+
+
+def print_info(string: str, verbosity: int):
+    if verbosity >= 1:
+        print(colorama.Fore.LIGHTMAGENTA_EX + str(string) + colorama.Style.RESET_ALL)
+
+
+def print_debug_info(string: str, verbosity: int):
+    if verbosity >= 2:
+        print(colorama.Fore.LIGHTBLACK_EX + str(string) + colorama.Style.RESET_ALL)
