0d7b160842314379ca33fc1f6e415e992662b58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:03:42 +0000

    Add package pymodaq_plugins_mockexamples/1.0.1/pymodaq_plugins_mockexamples-1.0.1.tar.gz

---
+++ packages/pymodaq_plugins_mockexamples/pymodaq_plugins_mockexamples-1.0.1.tar.gz/pymodaq_plugins_mockexamples-1.0.1/src/pymodaq_plugins_mockexamples/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+from pymodaq.utils.logger import set_logger  # to be imported by other modules.
+from _version import __version__, __version_tuple__, version_tuple, version
+
+from .utils import Config
+config = Config()
+
