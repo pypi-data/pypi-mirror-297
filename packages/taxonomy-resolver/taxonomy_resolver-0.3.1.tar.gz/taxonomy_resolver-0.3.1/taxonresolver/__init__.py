959addbb5d2af77ff049a6fe07330ecb4207a0ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:06:05 +0000

    Add package taxonomy-resolver/0.3.1/taxonomy_resolver-0.3.1.tar.gz

---
+++ packages/taxonomy-resolver/taxonomy_resolver-0.3.1.tar.gz/taxonomy_resolver-0.3.1/taxonresolver/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+"""
+Taxonomy Resolver
+
+:copyright: (c) 2020-2024.
+:license: Apache 2.0, see LICENSE for more details.
+"""
+
+__author__ = "Fábio Madeira"
+__email__ = "fmadeira@ebi.ac.uk"
+__version__ = "0.3.1"
+__contributors__ = ["Fábio Madeira"]
+
+from taxonresolver.tree import TaxonResolver
