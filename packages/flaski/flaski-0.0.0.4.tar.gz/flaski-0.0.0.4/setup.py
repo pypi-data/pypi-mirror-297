2af3cb0da77ce4ca5e2d9e4fe26e27455bcb65ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:06:07 +0000

    Add package flaski/0.0.0.4/flaski-0.0.0.4.tar.gz

---
+++ packages/flaski/flaski-0.0.0.4.tar.gz/flaski-0.0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flaski',
+    version='0.0.0.4',
+    packages=['flask_crud'],
+    # packages=find_packages(),
+    include_package_data=True,  # Incluye archivos de templates y estáticos
+    description='Administra vistas con un enfoque diferente en flask.',
+    long_description=open('README.md', encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    # long_description=open('README.md').read(),
+    author='Jodriz Dev',
+    author_email='jrodriguez7603@utm.edu.ec',
+    url='https://github.com/jrodre/flask_crud.git',
+    install_requires=[
+    "Flask"
+    ],
+)
