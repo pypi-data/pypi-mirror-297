d506841c3c8b1429979e9c45f39eef6d629a7424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:07:40 +0000

    Add package victron-ble2mqtt/0.3.0rc1/victron_ble2mqtt-0.3.0rc1.tar.gz

---
+++ packages/victron-ble2mqtt/victron_ble2mqtt-0.3.0rc1.tar.gz/victron_ble2mqtt-0.3.0rc1/victron_ble2mqtt/user_settings.py
@@ -0,0 +1,44 @@
+import dataclasses
+import sys
+
+from cli_base.systemd.data_classes import BaseSystemdServiceInfo, BaseSystemdServiceTemplateContext
+from ha_services.mqtt4homeassistant.data_classes import MqttSettings
+
+
+@dataclasses.dataclass
+class SystemdServiceTemplateContext(BaseSystemdServiceTemplateContext):
+    """
+    Context values for the systemd service file content.
+    """
+
+    verbose_service_name: str = 'victron-ble2mqtt'
+    exec_start: str = f'{sys.argv[0]} publish-loop'
+
+
+@dataclasses.dataclass
+class SystemdServiceInfo(BaseSystemdServiceInfo):
+    """
+    Information for systemd helper functions.
+    """
+
+    template_context: SystemdServiceTemplateContext = dataclasses.field(default_factory=SystemdServiceTemplateContext)
+
+
+@dataclasses.dataclass
+class UserSettings:
+    """
+    Victron-BLE -> MQTT - settings
+
+    Note: Insert at least device address + key and your MQTT settings.
+
+    See README for more information.
+    """
+
+    device_name: str = 'Victron'
+    device_address: str = '<device MAC address>'
+    device_key: str = '<insert your device key here>'
+
+    # Information about the MQTT server:
+    mqtt: dataclasses = dataclasses.field(default_factory=MqttSettings)
+
+    systemd: dataclasses = dataclasses.field(default_factory=SystemdServiceInfo)
