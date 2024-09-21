cc3f6f61d012ef42e9f4d2084ed49525e384faa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:04:25 +0000

    Add package chalkpy/2.50.73/chalkpy-2.50.73.tar.gz

---
+++ packages/chalkpy/chalkpy-2.50.73.tar.gz/chalkpy-2.50.73/chalk/utils/datasource_tags.py
@@ -0,0 +1,11 @@
+import typing
+
+
+def tag_matches(datasource_tag: str, context_tags: typing.Set[str]) -> bool:
+    for t in context_tags:
+        if t == datasource_tag:
+            return True
+        split = t.split(":")
+        if len(split) == 2 and split[0] == datasource_tag:
+            return True
+    return False
