6ffb7e2b35d4f686362efb6f466d878181488045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 11:04:07 +0000

    Add package xmlcord/1.1.1/xmlcord-1.1.1.tar.gz

---
+++ packages/xmlcord/xmlcord-1.1.1.tar.gz/xmlcord-1.1.1/setup.py
@@ -0,0 +1,27 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xmlcord",
+    version="1.1.1",
+    author="Fliqqr",
+    author_email="author@example.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Fliqqr/xmlcord",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'aiohttp',
+        'discord.py>=2.4.0'
+    ],
+    python_requires='>=3.10',
+)
