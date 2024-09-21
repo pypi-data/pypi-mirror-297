4442b068aa42df42db88952509f8898e4f2ebdb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:04:18 +0000

    Add package pymembercli/0.3.3/pymembercli-0.3.3.tar.gz

---
+++ packages/pymembercli/pymembercli-0.3.3.tar.gz/pymembercli-0.3.3/pymembercli/task_item.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from .util import colorize
+
+
+@dataclass
+class TaskItem:
+    """Represents a todo item."""
+    id: int
+    name: str
+    desc: str
+    status: str
+    start_date: str
+
+    def __repr__(self):
+        newname = self.name
+        newstatus = self.status
+        newdesc = colorize(self.desc, 'dark_gray')
+        newdate = colorize(self.start_date, 'purple')
+        if self.status == 'todo':
+            newname = colorize(newname, 'red')
+            newstatus = colorize(newstatus, 'red')
+        elif self.status == 'doing':
+            newname = colorize(newname, 'yellow')
+            newstatus = colorize(newstatus, 'yellow')
+        elif self.status == 'done':
+            newname = colorize(newname, 'green')
+            newstatus = colorize(newstatus, 'green')
+        reprstr = str(self.id) + ". " + newname + " |  " + newdesc + \
+            "\n" + newstatus + "  added: " + newdate
+        return reprstr
