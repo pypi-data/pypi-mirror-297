921234ba5d8da65ee1232d55361ecfbc6cb53b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 13:08:16 +0000

    Add package DarkFream/1.0/DarkFream-1.0.tar.gz

---
+++ packages/DarkFream/DarkFream-1.0.tar.gz/DarkFream-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='DarkFream',
+    version='1.0',
+    author='vsp210',
+    author_email='vsp210@gmail.com',
+    description='My Python library',
+    packages=['DarkFream'],
+    install_requires=['winotify>=1.1.0', 'Jinja2>=3.1.4', 'MarkupSafe>=2.1.5'],
+)
