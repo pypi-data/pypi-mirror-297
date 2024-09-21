22c68ad1cead374fc47b12e2c8d775cddce81660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:04:53 +0000

    Add package dbcv/0.8/dbcv-0.8.tar.gz

---
+++ packages/dbcv/dbcv-0.8.tar.gz/dbcv-0.8/dbcv/__init__.py
@@ -0,0 +1,3 @@
+from .main import DBCV, set_number_of_threads
+
+__all__ = ["DBCV", "set_number_of_threads"]
---
+++ packages/dbcv/dbcv-0.8.tar.gz/dbcv-0.8/dbcv/setup_script.py
@@ -0,0 +1,22 @@
+import os
+from .main import set_number_of_threads
+
+
+def select_optimal_number_of_threads():
+    max_cpus = os.cpu_count()
+    if 3 <= max_cpus <= 5:
+        set_number_of_threads(max_cpus - 1, cahce_warm=True)
+    elif 6 <= max_cpus <= 10:
+        set_number_of_threads(max_cpus - 2, cahce_warm=True)
+    elif 11 <= max_cpus <= 15:
+        set_number_of_threads(max_cpus - 3, cahce_warm=True)
+    else:
+        set_number_of_threads(max_cpus - 4, cahce_warm=True)
+
+
+def main():
+    select_optimal_number_of_threads()
+
+
+if __name__ == "main":
+    main()
