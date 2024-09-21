49a63eaa01fc4bffc2aec693a55069abe516464b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:20 +0000

    Add package cs-rankings/0.1.1/cs_rankings-0.1.1-py3-none-any.whl

---
+++ packages/cs-rankings/cs_rankings-0.1.1-py3-none-any.whl/cs_rankings/__init__.py
@@ -0,0 +1,3 @@
+from .client import HLTVRankings, ESLRankings, ValveLiveRankings, ValveRankings, ValveInvitationRankings
+
+__all__ = ["HLTVRankings", "ESLRankings", "ValveRankings", "ValveLiveRankings", "ValveInvitationRankings"]
