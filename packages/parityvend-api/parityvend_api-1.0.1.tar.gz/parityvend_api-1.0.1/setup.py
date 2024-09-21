c12159c458fb540b59fc971e54f8a127cbee9bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:06:22 +0000

    Add package parityvend-api/1.0.1/parityvend_api-1.0.1.tar.gz

---
+++ packages/parityvend-api/parityvend_api-1.0.1.tar.gz/parityvend_api-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+long_description = """The official Python client library for the ParityVend API. Add location-based pricing to your Python applications and take your business to a global level."""
+
+setup(
+    name="parityvend_api",
+    version='1.0.1',
+    description="Official Python library for ParityVend API",
+    long_description=long_description,
+    url="https://github.com/parityvend/api_python",
+    author="ParityVend",
+    author_email="help@ambeteco.com",
+    license="Apache License 2.0",
+    packages=["parityvend_api", "parityvend_api.cache"],
+    install_requires=["requests>=2.31.0", "cachetools>=5.3.3", "aiohttp>=3.9.3"],
+    include_package_data=True,
+    zip_safe=False,
+)
