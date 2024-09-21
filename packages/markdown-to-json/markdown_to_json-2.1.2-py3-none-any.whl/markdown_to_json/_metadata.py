4ed55813b5f22266dac84bb25416e75acab07331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:25 +0000

    Add package markdown-to-json/2.1.2/markdown_to_json-2.1.2-py3-none-any.whl

---
+++ packages/markdown-to-json/markdown_to_json-2.1.2-py3-none-any.whl/markdown_to_json/_metadata.py
@@ -0,0 +1,15 @@
+"""
+Part of the markdown_to_json package
+Written by Nate Vack <njvack@freshforever.net>
+Copyright 2023 Board of Regents of the University of Wisconsin System
+"""
+
+# TODO, switch to dunder meta.
+# pylint: disable=invalid-name
+version = "2.1.2"
+author = "Nathan Vack"
+author_email = "njvack@wisc.edu"
+# pylint: disable=redefined-builtin
+license = "MIT"
+copyright = "Copyright 2024 Boards of Regent of the University of Wisconsin System"
+url = "https://github.com/njvack/markdown-to-json"
---
+++ packages/markdown-to-json/markdown_to_json-2.1.2-py3-none-any.whl/markdown_to_json/vendor/CommonMark/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+__all__ = ["HTMLRenderer", "DocParser", "dumpAST", "ASTtoJSON"]
+from .CommonMark import ASTtoJSON, DocParser, HTMLRenderer, dumpAST
