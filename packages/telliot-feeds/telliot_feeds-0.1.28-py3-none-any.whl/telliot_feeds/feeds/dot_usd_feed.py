1cacdedeb4d405536aa793b5175e9c7ccd5575c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:06:09 +0000

    Add package telliot-feeds/0.1.28/telliot_feeds-0.1.28-py3-none-any.whl

---
+++ packages/telliot-feeds/telliot_feeds-0.1.28-py3-none-any.whl/telliot_feeds/feeds/dot_usd_feed.py
@@ -0,0 +1,18 @@
+from telliot_feeds.datafeed import DataFeed
+from telliot_feeds.queries.price.spot_price import SpotPrice
+from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
+from telliot_feeds.sources.price.spot.gemini import GeminiSpotPriceSource
+from telliot_feeds.sources.price_aggregator import PriceAggregator
+
+dot_usd_median_feed = DataFeed(
+    query=SpotPrice(asset="DOT", currency="USD"),
+    source=PriceAggregator(
+        asset="dot",
+        currency="usd",
+        algorithm="median",
+        sources=[
+            CoinGeckoSpotPriceSource(asset="dot", currency="usd"),
+            GeminiSpotPriceSource(asset="dot", currency="usd"),
+        ],
+    ),
+)
---
+++ packages/telliot-feeds/telliot_feeds-0.1.28-py3-none-any.whl/telliot_feeds/feeds/shib_usd_feed.py
@@ -0,0 +1,18 @@
+from telliot_feeds.datafeed import DataFeed
+from telliot_feeds.queries.price.spot_price import SpotPrice
+from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
+from telliot_feeds.sources.price.spot.gemini import GeminiSpotPriceSource
+from telliot_feeds.sources.price_aggregator import PriceAggregator
+
+shib_usd_median_feed = DataFeed(
+    query=SpotPrice(asset="SHIB", currency="USD"),
+    source=PriceAggregator(
+        asset="shib",
+        currency="usd",
+        algorithm="median",
+        sources=[
+            CoinGeckoSpotPriceSource(asset="shib", currency="usd"),
+            GeminiSpotPriceSource(asset="shib", currency="usd"),
+        ],
+    ),
+)
