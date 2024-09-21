28f6e0be12e6cf86b838eb8686b35e41a24025b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:03:54 +0000

    Add package constallation-mmr/1.0.4/constallation_mmr-1.0.4-py3-none-any.whl

---
+++ packages/constallation-mmr/constallation_mmr-1.0.4-py3-none-any.whl/constallation_mmr/__init__.py
@@ -0,0 +1,6 @@
+__name__ = "constallation_mmr"
+__version__ = "1.0.4"
+__author__ = "Coulter Stutz"
+__email__ = "coulterstutz@constallation.wiki"
+from .rig import *
+__all__ = ["Rig", "fetch_rigs", "fetch_rig"]
