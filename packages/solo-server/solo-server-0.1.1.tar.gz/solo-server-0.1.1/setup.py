5864a4cff338763958516bf930b753b65925bb7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 07:04:27 +0000

    Add package solo-server/0.1.1/solo-server-0.1.1.tar.gz

---
+++ packages/solo-server/solo-server-0.1.1.tar.gz/solo-server-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="solo-server",
+    version="0.1.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "typer",
+        "GPUtil",
+        "psutil",
+        "tqdm",
+        "requests",
+        "argparse"  # Added this as it's used in cli.py
+    ],
+    entry_points={
+        "console_scripts": [
+            "solo-server = solo_server.cli:main",  # Updated this line
+        ],
+    },
+    author="Dhruv Diddi",
+    author_email="dhruv.diddi@gmail.com",
+    description="Simple server to manage compound AI.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AIEngineersDev/solo-server",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
