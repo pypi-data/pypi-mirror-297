a939e03afbd6e78e63a9569db27021b5c7972f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 14:04:47 +0000

    Add package ob-metaflow-stubs/6.0.3.104/ob-metaflow-stubs-6.0.3.104.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.104.tar.gz/ob-metaflow-stubs-6.0.3.104/metaflow-stubs/plugins/gcp/__init__.pyi
@@ -0,0 +1,12 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.22.1+obcheckpoint(0.0.11);ob(v1)                              #
+# Generated on 2024-09-20T21:46:07.370508                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def get_credentials(scopes, *args, **kwargs):
+    ...
+
