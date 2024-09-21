b9c834496a82abf83d3aa88c7776107188f8220b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 03:05:37 +0000

    Add package bursty-dynamics/0.1.1/bursty_dynamics-0.1.1.tar.gz

---
+++ packages/bursty-dynamics/bursty_dynamics-0.1.1.tar.gz/bursty_dynamics-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+
+try:
+    with open("README.rst", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "Long description of the bursty_dynamics package. README.rst not found."
+
+
+DESCRIPTION = 'bursty_dynamics is a Python package designed to facilitate the analysis of temporal patterns in longitudinal data. It provides functions to calculate the burstiness parameter (BP) and memory coefficient (MC), detect event trains, and visualise results. '
+
+    
+setup(
+    name= 'bursty_dynamics',
+    version = '0.1.1', 
+    description = DESCRIPTION,
+    packages = find_packages(),
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ai-multiply/bursty_dynamics",
+    author = "Alisha Angdmebe",
+    author_email = "alisha.angdembe1@gmail.com",
+    licence = "MIT",
+    classifiers = ["Programming Language :: Python :: 3"],
+    python_requires='>=3.6',
+    install_requires = ['numpy','pandas', 'seaborn','matplotlib','scipy' ]
+)
+
+
+    
+    
+    
+    
+    
