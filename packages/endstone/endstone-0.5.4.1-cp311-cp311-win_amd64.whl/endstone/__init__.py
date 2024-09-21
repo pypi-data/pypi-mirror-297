78f92fe2d72b84a5fab785c5c7a7ea033d0fe127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 05:04:06 +0000

    Add package endstone/0.5.4.1/endstone-0.5.4.1-cp311-cp311-win_amd64.whl

---
+++ packages/endstone/endstone-0.5.4.1-cp311-cp311-win_amd64.whl/endstone/__init__.py
@@ -0,0 +1,17 @@
+from endstone._internal.endstone_python import ColorFormat, GameMode, Logger, Player, Server, Skin, Translatable
+
+from endstone._internal.version import __version__
+
+__minecraft_version__ = "1.21.30"
+
+__all__ = [
+    "__version__",
+    "__minecraft_version__",
+    "ColorFormat",
+    "GameMode",
+    "Logger",
+    "Player",
+    "Server",
+    "Skin",
+    "Translatable",
+]
