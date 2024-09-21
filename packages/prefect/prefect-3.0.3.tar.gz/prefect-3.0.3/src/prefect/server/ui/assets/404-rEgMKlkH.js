531be3b9e1c0a5f0332fd5531dc349bb5505122e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 12:06:21 +0000

    Add package prefect/3.0.3/prefect-3.0.3.tar.gz

---
+++ packages/prefect/prefect-3.0.3.tar.gz/prefect-3.0.3/src/prefect/server/ui/assets/404-rEgMKlkH.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,m as a,o as t,de as r}from"./index-CfSL_DzU.js";import{u as s}from"./usePageTitle-TG-RZAHq.js";const _=e({__name:"404",setup(n){return s("Page Not Found"),(c,m)=>(t(),o(a(r)))}});export{_ as default};
+//# sourceMappingURL=404-rEgMKlkH.js.map
