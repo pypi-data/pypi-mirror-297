0d99f4431a61b9dd5163361c44ba0d2ee5244c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 04:04:32 +0000

    Add package pbm-r/1.6/pbm_r-1.6.tar.gz

---
+++ packages/pbm-r/pbm_r-1.6.tar.gz/pbm_r-1.6/pypi_setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as fh:
+    long_description: str = fh.read()
+
+setup(
+    name="pbm_r",
+    version="1.6",
+    author="elemenom",
+    author_email="pixilreal@gmail.com",
+    description="Version control at its fullest.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elemenom/pbm",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',
+    license="GPLv3",
+    include_package_data=True,
+)
