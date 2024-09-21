49cb37d0d58ab4dd792ce7fbfeaaa7d55881bba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 04:03:58 +0000

    Add package vgslify/0.12.0/vgslify-0.12.0-py3-none-any.whl

---
+++ packages/vgslify/vgslify-0.12.0-py3-none-any.whl/vgslify/core/config.py
@@ -0,0 +1,53 @@
+# Imports
+
+# > Standard library
+from dataclasses import dataclass
+
+
+@dataclass
+class Conv2DConfig:
+    activation: str
+    kernel_size: tuple
+    strides: tuple
+    filters: int
+
+
+@dataclass
+class Pooling2DConfig:
+    pool_size: tuple
+    strides: tuple
+
+
+@dataclass
+class DenseConfig:
+    activation: str
+    units: int
+
+
+@dataclass
+class RNNConfig:
+    units: int
+    return_sequences: bool
+    go_backwards: bool
+    dropout: float
+    recurrent_dropout: float
+    rnn_type: str = None
+
+
+@dataclass
+class DropoutConfig:
+    rate: float
+
+
+@dataclass
+class ReshapeConfig:
+    target_shape: tuple
+
+
+@dataclass
+class InputConfig:
+    batch_size: int
+    depth: int
+    height: int
+    width: int
+    channels: int
