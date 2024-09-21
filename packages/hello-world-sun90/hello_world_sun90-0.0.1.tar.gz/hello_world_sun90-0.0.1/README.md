90ff217d24acd13968ebb31a72713192c24c3da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-21 11:03:44 +0000

    Add package hello-world-sun90/0.0.1/hello_world_sun90-0.0.1.tar.gz

---
+++ packages/hello-world-sun90/hello_world_sun90-0.0.1.tar.gz/hello_world_sun90-0.0.1/README.md
@@ -0,0 +1,1 @@
+This is a Python package that prints "Hello, World!"
---
+++ packages/hello-world-sun90/hello_world_sun90-0.0.1.tar.gz/hello_world_sun90-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_python_package',  # Replace with your package name
+    version='0.1.0',  # Version of your package
+    author='SD',
+    description='A custom Python module for demonstrating publishing to PyPI',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Sun9990/my_python_package', 
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
