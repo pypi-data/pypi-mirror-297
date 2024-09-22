60df6431db2cb8ad85da9bf008d187782d198382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:35 +0000

    Add package apiverve-unitconverter/1.1.5/apiverve_unitconverter-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-unitconverter/apiverve_unitconverter-1.1.5-py3-none-any.whl/apiverve_unitconverter/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_unitconverter',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Unit Converter is a simple tool for converting units. It returns the converted value.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
