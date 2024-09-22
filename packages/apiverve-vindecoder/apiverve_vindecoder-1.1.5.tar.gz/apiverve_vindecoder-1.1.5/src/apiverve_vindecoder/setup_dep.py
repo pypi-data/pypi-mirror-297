fbbd6226475a4d3028481ab3c982b6b31df773bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:38 +0000

    Add package apiverve-vindecoder/1.1.5/apiverve_vindecoder-1.1.5.tar.gz

---
+++ packages/apiverve-vindecoder/apiverve_vindecoder-1.1.5.tar.gz/apiverve_vindecoder-1.1.5/src/apiverve_vindecoder/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_vindecoder',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='VIN Decoder is a simple tool for decoding vehicle identification numbers. It returns the make, model, and more of the vehicle.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
