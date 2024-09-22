06815e6e232c9fe17339a8877489810bfc74953d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:21 +0000

    Add package apiverve-loremipsumgenerator/1.1.5/apiverve_loremipsumgenerator-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-loremipsumgenerator/apiverve_loremipsumgenerator-1.1.5-py3-none-any.whl/apiverve_loremipsumgenerator/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_loremipsumgenerator',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Lorem Ipsum Generator is a simple tool for generating lorem ipsum text. It returns the generated text.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
