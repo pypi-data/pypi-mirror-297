4634bb630703528feda3548adc1fc658547e3389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:10 +0000

    Add package memz-test-package/0.1.1/memz_test_package-0.1.1-py3-none-any.whl

---
+++ packages/memz-test-package/memz_test_package-0.1.1-py3-none-any.whl/memz_test_package/__main__.py
@@ -0,0 +1,10 @@
+# memz_test_package/__main__.py
+
+
+def main():
+    print("Hello from memz-test-package!")
+    # Add your package's main functionality here
+
+
+if __name__ == "__main__":
+    main()
