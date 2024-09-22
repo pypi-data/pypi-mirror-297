ad427ef9d82520690c2090831305f7059d77e006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:31 +0000

    Add package apiverve-seodatascraper/1.1.5/apiverve_seodatascraper-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-seodatascraper/apiverve_seodatascraper-1.1.5-py3-none-any.whl/apiverve_seodatascraper/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_seodatascraper',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='SEO Scraper is a simple tool for scraping SEO data. It returns the meta title, meta description, and more.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
