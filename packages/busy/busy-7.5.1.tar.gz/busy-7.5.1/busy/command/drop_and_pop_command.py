25795077dd079d4c263dc34fd1ec5de548e7b0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:04:45 +0000

    Add package busy/7.5.1/busy-7.5.1.tar.gz

---
+++ packages/busy/busy-7.5.1.tar.gz/busy-7.5.1/busy/command/drop_and_pop_command.py
@@ -0,0 +1,57 @@
+from random import choice
+
+from busy.command import QueueCommand
+
+
+class DropCommand(QueueCommand):
+    """Move items to the end of the todo collection of a queue"""
+
+    name = 'drop'
+    default_filter = [1]
+
+    def handle_vals(self):
+        super().handle_vals()
+        if self.selection:
+            self.app.ui.send(self.selected_items_list)
+
+    @QueueCommand.wrap
+    def execute(self):
+        collection = self.app.storage.get_collection(self.queue)
+        lolist, hilist = self.collection.split(self.selection)
+        self.collection.data = hilist + lolist
+        self.status = f"Popped {self.summarize(lolist)}"
+
+
+class PopCommand(QueueCommand):
+    """Move items to the beginning of the collection"""
+
+    name = 'pop'
+    default_filter = ['-']
+
+    def handle_vals(self):
+        super().handle_vals()
+        if len(self.selection) > 1:
+            self.app.ui.send(self.selected_items_list)
+
+    @QueueCommand.wrap
+    def execute(self):
+        collection = self.app.storage.get_collection(self.queue)
+        hilist, lolist = self.collection.split(self.selection)
+        self.collection.data = hilist + lolist
+        self.status = f"Popped {self.summarize(hilist)}"
+
+
+class PickCommand(QueueCommand):
+    """Move a random item to the beginning of the collection"""
+
+    name = 'pick'
+    default_filter = ['-']
+
+    @QueueCommand.wrap
+    def execute(self):
+        index = choice(self.selection)
+        item = self.collection[index]
+        hilist, lolist = self.collection.split([index])
+        self.collection.data = hilist + lolist
+        return self.collection[0].simple if self.collection else None
+        # self.status = f"Picked {self.summarize([item])}"
