5401d63422282f0a55b1fb9c9a4bdf653da87185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:37 +0000

    Add package apiverve-useragentparser/1.1.5/apiverve_useragentparser-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-useragentparser/apiverve_useragentparser-1.1.5-py3-none-any.whl/apiverve_useragentparser/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_useragentparser',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='User Agent Parser is a simple tool for parsing user agent strings. It returns the browser, operating system, and more from the user agent string.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
