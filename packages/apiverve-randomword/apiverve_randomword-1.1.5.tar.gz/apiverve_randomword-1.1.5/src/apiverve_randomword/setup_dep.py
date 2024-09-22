e8745aca89fe0e23341a8a90d5a9ce95c3a49ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:30 +0000

    Add package apiverve-randomword/1.1.5/apiverve_randomword-1.1.5.tar.gz

---
+++ packages/apiverve-randomword/apiverve_randomword-1.1.5.tar.gz/apiverve_randomword-1.1.5/src/apiverve_randomword/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_randomword',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Random Word is a simple tool for getting random words. It returns a random word.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
