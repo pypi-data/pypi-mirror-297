7b03983bd651bbb14e694c80fa6288c7b486fc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:05:09 +0000

    Add package PyFairnessAI/0.0.2/PyFairnessAI-0.0.2-py3-none-any.whl

---
+++ packages/PyFairnessAI/PyFairnessAI-0.0.2-py3-none-any.whl/PyFairnessAI/__init__.py
@@ -0,0 +1,4 @@
+from .data import privileged_groups_sens, unprivileged_groups_sens, binary_data_simulation
+from .metrics import (statistical_parity_difference, abs_statistical_parity_difference, 
+                      equal_opportunity_difference, abs_equal_opportunity_difference,
+                      disparate_impact_ratio, average_odds_error, ) 
