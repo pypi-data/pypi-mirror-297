b3d22df795f46f4b147c3674cda4568a40b25d43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 05:04:14 +0000

    Add package abraia/0.15.3/abraia-0.15.3.tar.gz

---
+++ packages/abraia/abraia-0.15.3.tar.gz/abraia-0.15.3/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+import pkg_resources
+
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [str(requirement) for requirement
+        in pkg_resources.parse_requirements(requirements_txt)]
+
+setup(
+    name='abraia',
+    version='0.15.3',
+    description='Abraia Python SDK',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/abraia/abraia-multiple',
+    author='Jorge Rodriguez Araujo',
+    author_email='jorge@abraiasoftware.com',
+    license='MIT',
+    zip_safe=False,
+    packages=['abraia'],
+    tests_require=['pytest'],
+    setup_requires=['setuptools>=38.6.0', 'pytest-runner'],
+    scripts=['scripts/abraia', 'scripts/abraia.bat'],
+    install_requires=install_requires
+)
