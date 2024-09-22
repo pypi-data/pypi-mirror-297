3a4966b02fb7b0b3ce35f63a2fca1ba4abd03c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:21 +0000

    Add package apiverve-meteoritelandings/1.1.5/apiverve_meteoritelandings-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-meteoritelandings/apiverve_meteoritelandings-1.1.5-py3-none-any.whl/apiverve_meteoritelandings/exceptions.py
@@ -0,0 +1,3 @@
+class MeteoritesMeteoriteLandingsAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-meteoritelandings/apiverve_meteoritelandings-1.1.5-py3-none-any.whl/apiverve_meteoritelandings/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_meteoritelandings',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Meteorites is a simple tool for getting meteorite data. It returns a list of meteorites that have fallen to Earth.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
