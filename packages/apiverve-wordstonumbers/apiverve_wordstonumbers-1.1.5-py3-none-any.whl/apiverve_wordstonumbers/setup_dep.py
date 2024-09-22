f322535b34c753f6dc49da5eb89a7bfcf808a6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:11 +0000

    Add package apiverve-wordstonumbers/1.1.5/apiverve_wordstonumbers-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-wordstonumbers/apiverve_wordstonumbers-1.1.5-py3-none-any.whl/apiverve_wordstonumbers/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_wordstonumbers',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Words to Numbers is a simple tool for converting words to numbers. It returns the words in numbers.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
