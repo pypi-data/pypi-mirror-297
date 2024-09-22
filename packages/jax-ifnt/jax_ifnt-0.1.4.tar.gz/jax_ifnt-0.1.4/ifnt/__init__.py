7b70e76d1fb52d74b2f529d0561c32c7c0ddf4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 03:12:49 +0000

    Add package jax-ifnt/0.1.4/jax_ifnt-0.1.4.tar.gz

---
+++ packages/jax-ifnt/jax_ifnt-0.1.4.tar.gz/jax_ifnt-0.1.4/ifnt/__init__.py
@@ -0,0 +1,13 @@
+from .util import disable, index_guard, is_traced, raise_if_traced, skip_if_traced
+from . import random, testing
+
+
+__all__ = [
+    "disable",
+    "index_guard",
+    "is_traced",
+    "raise_if_traced",
+    "random",
+    "skip_if_traced",
+    "testing",
+]
