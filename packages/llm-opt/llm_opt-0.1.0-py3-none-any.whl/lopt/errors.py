751963ff622e9a862034cf23326715dc92043c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:40 +0000

    Add package llm-opt/0.1.0/llm_opt-0.1.0-py3-none-any.whl

---
+++ packages/llm-opt/llm_opt-0.1.0-py3-none-any.whl/lopt/errors.py
@@ -0,0 +1,42 @@
+from lopt.models import Language, ModelT
+
+
+class LoptError(Exception):
+    def __init__(self, msg: str) -> None:
+        super().__init__(msg)
+        self.msg = msg
+
+    def __str__(self) -> str:
+        return self.msg
+
+
+class NoCodeFoundError(LoptError):
+    def __init__(
+        self,
+        msg: str = "No code found",
+        *,
+        lang: Language,
+        data: str,
+    ) -> None:
+        super().__init__(msg)
+        self.lang = lang
+        self.data = data
+
+    def __str__(self) -> str:
+        return f"{self.msg}, lang: {self.lang}, data: {self.data}"
+
+
+class ParseModelError(LoptError):
+    def __init__(
+        self,
+        msg: str = "Failed to parse model",
+        *,
+        model: type[ModelT],
+        data: str,
+    ) -> None:
+        super().__init__(msg)
+        self.model = model
+        self.data = data
+
+    def __str__(self) -> str:
+        return f"{self.msg}, model: {self.model.__name__}, data: {self.data}"
