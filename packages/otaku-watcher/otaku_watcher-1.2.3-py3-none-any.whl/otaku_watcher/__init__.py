c373d122d70c88687cde2cd484283d8e101c8cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:05:14 +0000

    Add package otaku-watcher/1.2.3/otaku_watcher-1.2.3-py3-none-any.whl

---
+++ packages/otaku-watcher/otaku_watcher-1.2.3-py3-none-any.whl/otaku_watcher/__init__.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from mov_cli.plugins import PluginHookData
+
+from .anitaku import AnitakuScraper
+from .tokyo_insider import TokyoInsider
+
+plugin: PluginHookData = {
+    "version": 1,
+    "package_name": "otaku-watcher",  # Required for the plugin update checker.
+    "scrapers": {
+        "DEFAULT": AnitakuScraper,
+        "anitaku": AnitakuScraper,
+        "tokyoinsider": TokyoInsider
+    }
+}
+
+__version__ = "1.2.3"
