1f58f3315254537f9e1a4756ca7ed9e7377c3f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 14:04:40 +0000

    Add package calibrator/0.0.8/calibrator-0.0.8.tar.gz

---
+++ packages/calibrator/calibrator-0.0.8.tar.gz/calibrator-0.0.8/tests/test_local_calibrator.py
@@ -0,0 +1,18 @@
+def test_local_calibrator():
+    print("---Test LocalCalibrator---")
+
+
+    from calibrator import LocalCalibrator
+    import torch
+
+    val_logits, val_labels = torch.load("tests/test_logits/resnet50_cifar10_cross_entropy_val_0.1_vanilla.pt", weights_only=False)
+    test_logits, test_labels = torch.load("tests/test_logits/resnet50_cifar10_cross_entropy_test_0.9_vanilla.pt", weights_only=False)
+
+    calibrator = LocalCalibrator()
+    eps_opt = calibrator.fit(val_logits, val_labels)
+    calibrated_probability = calibrator.calibrate(test_logits)
+
+    print("!!! Pass LocalCalibrator Test !!!")
+
+if __name__ == "__main__":
+    test_local_calibrator()
