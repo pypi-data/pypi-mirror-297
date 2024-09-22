64c6b741a28cc6f5a02c2ed3730b87c8a953ce1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:32 +0000

    Add package apiverve-spellchecker/1.1.5/apiverve_spellchecker-1.1.5.tar.gz

---
+++ packages/apiverve-spellchecker/apiverve_spellchecker-1.1.5.tar.gz/apiverve_spellchecker-1.1.5/src/apiverve_spellchecker/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_spellchecker',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Spell Checker is a simple tool for checking spelling in a given body of text. It returns correction suggestions',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
