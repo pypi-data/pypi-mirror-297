a612319c614851206e058b3ab18c886a9c7fdb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:35 +0000

    Add package apiverve-triviagenerator/1.1.5/apiverve_triviagenerator-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-triviagenerator/apiverve_triviagenerator-1.1.5-py3-none-any.whl/apiverve_triviagenerator/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_triviagenerator',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Trivia is a simple tool for getting trivia questions. It returns a random trivia question based on your selected category.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
