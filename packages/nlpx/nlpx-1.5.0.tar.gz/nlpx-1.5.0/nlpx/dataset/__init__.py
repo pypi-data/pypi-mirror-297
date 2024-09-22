675d01f0305381f07e8624431d0a711f3f66cc25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:15 +0000

    Add package nlpx/1.5.0/nlpx-1.5.0.tar.gz

---
+++ packages/nlpx/nlpx-1.5.0.tar.gz/nlpx-1.5.0/nlpx/dataset/__init__.py
@@ -0,0 +1,12 @@
+from ._dataset import TokenDataset, SameLengthTokenDataset, TextDataset, TextDFDataset, TextVecCollator, \
+	TokenizeCollator, PaddingTokenCollator
+
+__all__ = [
+	"TokenDataset",
+	"SameLengthTokenDataset",
+	"TextDataset",
+	"TextDFDataset",
+	"TextVecCollator",
+	"TokenizeCollator",
+	"PaddingTokenCollator"
+]
---
+++ packages/nlpx/nlpx-1.5.0.tar.gz/nlpx-1.5.0/nlpx/text_token/__init__.py
@@ -0,0 +1,10 @@
+from ._tokenizer import BaseTokenizer, SimpleTokenizer, PaddingTokenizer, Tokenizer, TokenEmbedding, AutoTokenizer
+
+__all__ = [
+	"BaseTokenizer",
+	"SimpleTokenizer",
+	"PaddingTokenizer",
+	"Tokenizer",
+	"TokenEmbedding",
+	"AutoTokenizer",
+]
