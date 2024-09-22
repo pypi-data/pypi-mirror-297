db166121ad7dfdd551633528211a8bc801674578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 03:12:40 +0000

    Add package Det-Calc/0.1/det_calc-0.1.tar.gz

---
+++ packages/Det-Calc/det_calc-0.1.tar.gz/det_calc-0.1/code/__init__.py
@@ -0,0 +1,11 @@
+def dec_sum(a,b):
+    return a + b
+
+def dec_min(a,b):
+    return a - b
+
+def dec_mul(a,b):
+    return a * b
+
+def dec_div(a,b):
+    return a / b
---
+++ packages/Det-Calc/det_calc-0.1.tar.gz/det_calc-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name='Det_Calc',
+    version='0.1',
+    author='Detective Thaghra',
+    description='Detective Calculator',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
