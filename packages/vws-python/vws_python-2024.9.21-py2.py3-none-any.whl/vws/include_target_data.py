af80b0106b4b3469d247ff920acd4e53df73677d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:36 +0000

    Add package vws-python/2024.9.21/vws_python-2024.9.21-py2.py3-none-any.whl

---
+++ packages/vws-python/vws_python-2024.9.21-py2.py3-none-any.whl/vws/include_target_data.py
@@ -0,0 +1,19 @@
+"""
+Tools for managing ``CloudRecoService.query``'s ``include_target_data``.
+"""
+
+from enum import StrEnum, auto
+
+from beartype import beartype
+
+
+@beartype
+class CloudRecoIncludeTargetData(StrEnum):
+    """
+    Options for the ``include_target_data`` parameter of
+    ``CloudRecoService.query``.
+    """
+
+    TOP = auto()
+    NONE = auto()
+    ALL = auto()
