427a77e05b987ca83a27a24dd13ddd55f0a461b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:15 +0000

    Add package function-save/0.1.1/function_save-0.1.1.tar.gz

---
+++ packages/function-save/function_save-0.1.1.tar.gz/function_save-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+VERSION = '0.1.1'
+DESCRIPTION = 'This function automatically saves your function calls and result and prints it in a systematic format for better debugging and analysis.'
+LONG_DESCRIPTION = ''
+
+setup(
+    name = 'function_save',
+    version = VERSION,
+    author = 'Dev',
+    author_email = 'amazingdev.dev@gmail.com',
+    description = DESCRIPTION,
+    packages = find_packages(),
+    install_requires = [],
+    keywords = ['python','function']
+)
