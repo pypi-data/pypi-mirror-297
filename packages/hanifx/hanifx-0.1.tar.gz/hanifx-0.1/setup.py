1bbc38a7359eaff36884bfc4ba2424786b71aed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:47 +0000

    Add package hanifx/0.1/hanifx-0.1.tar.gz

---
+++ packages/hanifx/hanifx-0.1.tar.gz/hanifx-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hanifx',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple greeting package',
+    author='Hanif',
+    author_email='sajim4653@gmail.com',
+    url='https://hanif-storess.weebly.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
