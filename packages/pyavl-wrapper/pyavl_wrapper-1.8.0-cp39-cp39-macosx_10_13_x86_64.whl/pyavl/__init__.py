63b977dff8b1368594cfef24f18cf54fe5017ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 05:04:29 +0000

    Add package pyavl-wrapper/1.8.0/pyavl_wrapper-1.8.0-cp39-cp39-macosx_10_13_x86_64.whl

---
+++ packages/pyavl-wrapper/pyavl_wrapper-1.8.0-cp39-cp39-macosx_10_13_x86_64.whl/pyavl/__init__.py
@@ -0,0 +1,15 @@
+import importlib.metadata
+
+try:
+    # __package__ allows for the case where __name__ is "__main__"
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+from .pyAVL import AVLSolver
+
+try:
+    from .om_wrapper import AVLGroup, AVLMeshReader
+except ImportError:
+    # if openmdao is not installed, then we can't use the wrapper
+    pass
