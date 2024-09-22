0cb3dbab9571da129652b5fa23632b565b5f3997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:08 +0000

    Add package balboniprint/0.1.2/balboniprint-0.1.2.tar.gz

---
+++ packages/balboniprint/balboniprint-0.1.2.tar.gz/balboniprint-0.1.2/README.md
@@ -0,0 +1,10 @@
+# formatprint
+An easy-to-use package for custom formatted printing, allowing you to pretty-print complex data structures with color and style options.
+
+## Installation
+
+```bash
+pip install balboniprint
+```
+
+
---
+++ packages/balboniprint/balboniprint-0.1.2.tar.gz/balboniprint-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='balboniprint',
+    version='0.1.2',
+    author='sud0luke',
+    author_email='sud0luke@proton.me',
+    description='An easy-to-use package for custom formatted printing.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/lukesudom/balboniprint',
+    packages=find_packages(),
+    install_requires=[
+        'termcolor',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
