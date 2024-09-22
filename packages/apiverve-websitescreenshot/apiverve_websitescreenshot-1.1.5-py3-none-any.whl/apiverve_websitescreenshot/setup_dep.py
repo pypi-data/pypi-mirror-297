c4a4cd2155dc61dca39e3f1db59cf777ce40e27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:38 +0000

    Add package apiverve-websitescreenshot/1.1.5/apiverve_websitescreenshot-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-websitescreenshot/apiverve_websitescreenshot-1.1.5-py3-none-any.whl/apiverve_websitescreenshot/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_websitescreenshot',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Web Screenshots is a simple tool for capturing screenshots of web pages. It returns an image screenshot of the web page provided.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
