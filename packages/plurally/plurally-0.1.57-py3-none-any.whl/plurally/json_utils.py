0dbc21a65ce8afa6ab249aa51d8953d873a531ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:37 +0000

    Add package plurally/0.1.57/plurally-0.1.57-py3-none-any.whl

---
+++ packages/plurally/plurally-0.1.57-py3-none-any.whl/plurally/json_utils.py
@@ -0,0 +1,30 @@
+import json
+from datetime import datetime
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super().default(obj)
+
+
+# Custom hook to decode datetime strings
+def datetime_decoder(dct):
+    for key, value in dct.items():
+        try:
+            # Try to parse the value as ISO 8601 date format
+            dct[key] = datetime.fromisoformat(value)
+        except (ValueError, TypeError):
+            # If parsing fails, keep the value as is
+            pass
+    return dct
+
+
+# super hacky
+def dump_to_json_dict(data):
+    return json.loads(json.dumps(data, cls=DateTimeEncoder))
+
+
+def load_from_json_dict(data):
+    return json.loads(json.dumps(data), object_hook=datetime_decoder)
---
+++ packages/plurally/plurally-0.1.57-py3-none-any.whl/plurally/models/triggers.py
@@ -0,0 +1,5 @@
+from .meta import InstagramNewDm  # noqa: F401
+from .source.email_imap import NewEmail  # noqa: F401
+from .source.schedule import Schedule  # noqa: F401
+
+__all__ = ["NewEmail", "Schedule", "InstagramNewDm"]
