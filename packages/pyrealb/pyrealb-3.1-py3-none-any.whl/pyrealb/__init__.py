d39ce99b33ed636ef89cf5d63d230e5e037edc64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:04:47 +0000

    Add package pyrealb/3.1/pyrealb-3.1-py3-none-any.whl

---
+++ packages/pyrealb/pyrealb-3.1-py3-none-any.whl/pyrealb/__init__.py
@@ -0,0 +1,16 @@
+from .Lexicon import *
+from .utils import *
+from .Constituent import Constituent
+from .Terminal import Terminal
+from .Phrase import Phrase
+from .Dependent import Dependent
+
+
+__all__ = ['Constituent',
+     'fromJSON', 'oneOf', 'choice', 'mix', 'pyrealb_version', 'pyrealb_datecreated',              # from utils
+     'A', 'Adv', 'C', 'D', 'DT', 'N', 'NO', 'P', 'Pro', 'Q', 'V', 'Terminal',
+     'AP',  'AdvP',  'CP', 'NP', 'PP',  'VP', 'S', 'SP', 'Phrase',
+     'root', 'subj', 'det', 'mod', 'comp', 'coord', 'Dependent',
+     'currentLanguage', 'addToLexicon', 'updateLexicon', 'getLexicon', 'getLemma',      # from Lexicon
+     'loadEn', 'loadFr', 'load',
+ ]
