b8ccb4b76248e68d6ec00dbdbcabe9a639e49dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:34 +0000

    Add package apiverve-tenseconverter/1.1.5/apiverve_tenseconverter-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-tenseconverter/apiverve_tenseconverter-1.1.5-py3-none-any.whl/apiverve_tenseconverter/exceptions.py
@@ -0,0 +1,3 @@
+class TenseconverterTenseConverterAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-tenseconverter/apiverve_tenseconverter-1.1.5-py3-none-any.whl/apiverve_tenseconverter/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_tenseconverter',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Tense Converter is a simple tool for converting text to past or future tense. It returns the text converted to the specified tense.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
