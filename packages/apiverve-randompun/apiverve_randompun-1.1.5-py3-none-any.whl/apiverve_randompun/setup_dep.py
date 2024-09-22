d1f37c0244551c65735a9182aa380c4435bcd1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:24 +0000

    Add package apiverve-randompun/1.1.5/apiverve_randompun-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-randompun/apiverve_randompun-1.1.5-py3-none-any.whl/apiverve_randompun/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_randompun',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Random Pun is a simple tool for getting random puns. It returns a random pun from a collection of puns.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
