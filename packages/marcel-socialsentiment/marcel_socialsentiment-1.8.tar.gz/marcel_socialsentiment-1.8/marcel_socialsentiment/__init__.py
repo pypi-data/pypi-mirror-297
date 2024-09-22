b5fe09fd4e032a60dd1053a5a4dd763d27382990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:27 +0000

    Add package marcel-socialsentiment/1.8/marcel_socialsentiment-1.8.tar.gz

---
+++ packages/marcel-socialsentiment/marcel_socialsentiment-1.8.tar.gz/marcel_socialsentiment-1.8/marcel_socialsentiment/__init__.py
@@ -0,0 +1,9 @@
+from .marcel_socialsentiment import (
+    load_model,
+    prep_custom_data,
+    prep_seed_words,
+    random_walk,
+    label_propagate_continuous,
+    densify_polarity_scores,
+    viewscores,
+)
