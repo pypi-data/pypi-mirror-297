141e86dcd253384dde596a0663ebc976ec88fdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 03:12:15 +0000

    Add package asyncwhois/1.1.5/asyncwhois-1.1.5.tar.gz

---
+++ packages/asyncwhois/asyncwhois-1.1.5.tar.gz/asyncwhois-1.1.5/tests/test_not_found.py
@@ -0,0 +1,20 @@
+import sys
+
+import asyncwhois
+from asyncwhois.errors import NotFoundError
+
+if sys.version_info >= (3, 8):
+    from unittest import IsolatedAsyncioTestCase
+
+    class TestLookupNotFound(IsolatedAsyncioTestCase):
+        async def test_not_found_aio(self):
+            domain = "some-non-existent-domain123.com"
+            with self.assertRaises(NotFoundError):
+                await asyncwhois.aio_whois_domain(domain)
+
+        def test_not_found(self):
+            domain = "some-non-existent-domain123.com"
+            with self.assertRaises(NotFoundError):
+                asyncwhois.whois_domain(domain)
+
+            asyncwhois.whois_domain(domain, ignore_not_found=True)
