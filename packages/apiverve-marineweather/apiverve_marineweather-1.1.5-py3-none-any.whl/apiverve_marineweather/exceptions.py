d02fc1961cab9521289fc1fd4903e6da104edde0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:21 +0000

    Add package apiverve-marineweather/1.1.5/apiverve_marineweather-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-marineweather/apiverve_marineweather-1.1.5-py3-none-any.whl/apiverve_marineweather/exceptions.py
@@ -0,0 +1,3 @@
+class MarineweatherMarineWeatherAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-marineweather/apiverve_marineweather-1.1.5-py3-none-any.whl/apiverve_marineweather/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_marineweather',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Marine Weather is a simple tool for getting marine weather data. It returns the current weather conditions in coordinates within the ocean and sea.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
