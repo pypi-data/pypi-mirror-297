deff9c2e7d062bd3886b53c02b1d9acf6ebbaa28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 05:04:25 +0000

    Add package HintlyDataAnalisys/0.2.1/HintlyDataAnalisys-0.2.1-py3-none-any.whl

---
+++ packages/HintlyDataAnalisys/HintlyDataAnalisys-0.2.1-py3-none-any.whl/HintlyDataAnalisys/__init__.py
@@ -0,0 +1,9 @@
+# your_library/__init__.py
+
+from .DataAnalisys import Math, Finance, Text, FilterType, NormalizeType
+
+__all__ = ['Math', 'Finance', 'Text', 'FilterType', 'NormalizeType']
+
+__version__ = '0.2.1'
+__author__ = 'Franciszek Chmielewski'
+__email__ = 'ferko2610@gmail.com'
