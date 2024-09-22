3b7ab67676c38472984935c06426304322981ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 03:12:32 +0000

    Add package nshtrainer/0.33.0/nshtrainer-0.33.0.tar.gz

---
+++ packages/nshtrainer/nshtrainer-0.33.0.tar.gz/nshtrainer-0.33.0/src/nshtrainer/util/config/__init__.py
@@ -0,0 +1,4 @@
+from . import duration as duration
+from .duration import DurationConfig as DurationConfig
+from .duration import EpochsConfig as EpochsConfig
+from .duration import StepsConfig as StepsConfig
