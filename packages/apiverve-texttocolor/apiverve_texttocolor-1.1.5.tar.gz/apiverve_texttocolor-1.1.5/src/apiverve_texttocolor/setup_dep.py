3063516a0fda7eb1372508e937f26e26281ed7d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:34 +0000

    Add package apiverve-texttocolor/1.1.5/apiverve_texttocolor-1.1.5.tar.gz

---
+++ packages/apiverve-texttocolor/apiverve_texttocolor-1.1.5.tar.gz/apiverve_texttocolor-1.1.5/src/apiverve_texttocolor/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_texttocolor',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Text to Color is a simple tool for converting color from natural language. It returns the color code based on the text provided.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
