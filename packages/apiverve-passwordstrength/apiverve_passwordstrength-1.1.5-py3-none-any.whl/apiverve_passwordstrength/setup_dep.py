a2d6eba4c13f56c3165bd67e6e6ae44762d62d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:23 +0000

    Add package apiverve-passwordstrength/1.1.5/apiverve_passwordstrength-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-passwordstrength/apiverve_passwordstrength-1.1.5-py3-none-any.whl/apiverve_passwordstrength/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_passwordstrength',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Password Strength is a simple tool for checking the strength of a password. It returns the strength of the password based on various factors.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
