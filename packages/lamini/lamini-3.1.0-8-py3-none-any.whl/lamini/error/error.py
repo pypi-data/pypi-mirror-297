54732d1df976646b3ff940f30681688cb0c8ea62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 03:11:33 +0000

    Add package lamini/3.1.0/lamini-3.1.0-8-py3-none-any.whl

---
+++ packages/lamini/lamini-3.1.0-8-py3-none-any.whl/lamini/error/error.py
@@ -0,0 +1,46 @@
+class LlamaError(Exception):
+    def __init__(
+        self,
+        message=None,
+    ):
+        super(LlamaError, self).__init__(message)
+
+
+class ModelNotFound(LlamaError):
+    """The model name is invalid. Make sure it's a valid model in Huggingface or a finetuned model"""
+
+
+class APIError(LlamaError):
+    """There is an internal error in the Lamini API"""
+
+
+class AuthenticationError(LlamaError):
+    """The Lamini API key is invalid"""
+
+
+class RateLimitError(LlamaError):
+    """The QPS of requests to the API is too high"""
+
+
+class UserError(LlamaError):
+    """The user has made an invalid request"""
+
+
+class APIUnprocessableContentError(LlamaError):
+    """Invalid request format. Consider upgrading lamini library version"""
+
+
+class UnavailableResourceError(LlamaError):
+    """Model is still downloading"""
+
+
+class ServerTimeoutError(LlamaError):
+    """Model is still downloading"""
+
+
+class DownloadingModelError(LlamaError):
+    """Downloading model"""
+
+
+class RequestTimeoutError(LlamaError):
+    """Request Timeout. Please try again."""
---
+++ packages/lamini/lamini-3.1.0-8-py3-none-any.whl/lamini/generation/process_generation_batch.py
@@ -0,0 +1,112 @@
+import asyncio
+import logging
+
+from lamini.api.rest_requests import make_async_web_request
+from lamini.api.utils.batch import Batch
+
+logger = logging.getLogger(__name__)
+
+
+async def process_generation_batch(args: dict):
+    client = args["client"]
+    key = args["key"]
+    batch = args["batch"]
+    reservation_api = args["reservation_api"]
+
+    url = get_url_from_args(args)
+    # this will block until there is space in capacity
+    await reservation_api.async_pause_for_reservation_start()
+
+    def can_submit_query():
+        if reservation_api.current_reservation is None:
+            return True
+        if reservation_api.capacity_remaining < len(batch["prompt"]):
+            reservation_api.poll_for_reservation.set()
+            return False
+        # Now we can consume credits and send batch
+        reservation_api.update_capacity_use(len(batch["prompt"]))
+        logger.debug(
+            f"reservation_api.capacity_remaining {reservation_api.capacity_remaining}"
+        )
+        return True
+
+    if not can_submit_query():
+        async with reservation_api.condition:
+            await reservation_api.condition.wait_for(can_submit_query)
+    # Separate thread updates existing reservations
+    try:
+        reservation_id = None
+        if reservation_api.current_reservation is not None:
+            reservation_id = reservation_api.current_reservation["reservation_id"]
+        json = get_body_from_args(batch, reservation_id)
+        logger.info(f"Sending batch with {len(batch['prompt'])}")
+        result = await query_api(client, key, url, json, batch["type"])
+    except Exception as e:
+        logger.debug(
+            f"Error in process_generation_batch, type: {type(e)}, message: {e}"
+        )
+        for prompt_obj in batch["prompt"]:
+            prompt_obj.error.append(e)
+        raise e
+    finally:
+        if (
+            reservation_api.capacity_needed > 0
+            and reservation_api.capacity_remaining < len(batch["prompt"])
+            and not reservation_api.is_polling
+        ):
+            logger.debug(
+                f"capacity remaining after query: {reservation_api.capacity_remaining}"
+            )
+            reservation_api.poll_for_reservation.set()
+    for i, prompt_obj in enumerate(batch["prompt"]):
+        prompt_obj.response = result["outputs"][i]
+        prompt_obj.finish_reason = result["finish_reason"][i]
+
+
+async def query_api(client, key, url, json, type):
+    if type == "embedding":
+        # TODO: Replace make_async_web_request() with Completion.generate()
+        result = await make_async_web_request(client, key, url, "post", json)
+        result = result["embedding"]
+    else:
+        batch_api = Batch()
+        submit_response = batch_api.submit(
+            prompt=json["prompt"],
+            model_name=json["model_name"],
+            output_type=json["output_type"],
+            max_new_tokens=json["max_new_tokens"],
+        )  # TODO: Don't resubmit work if an error is thrown in the while loop
+        while True:
+            await asyncio.sleep(5)
+            result = batch_api.check_result(submit_response["id"])
+            if result:
+                break
+    return result
+
+
+def get_url_from_args(args: dict) -> str:
+    api_prefix = args["api_prefix"]
+    batch = args["batch"]
+    if batch["type"] == "embedding":
+        url = api_prefix + "inference/embedding"
+    else:
+        url = api_prefix + "completions"
+    return url
+
+
+def get_body_from_args(batch: dict, reservation_id: str) -> dict:
+    if batch["type"] == "embedding":
+        json = {
+            "model_name": batch["model_name"],
+            "prompt": [p.get_prompt() for p in batch["prompt"]],
+        }
+    else:
+        json = {
+            "reservation_id": reservation_id,
+            "model_name": batch["model_name"],
+            "prompt": [p.get_prompt() for p in batch["prompt"]],
+            "output_type": batch["output_type"],
+            "max_tokens": batch["max_tokens"],
+            "max_new_tokens": batch.get("max_new_tokens", None),
+        }
+    return json
