8ec1e1069ee5408578b1804c39987266f8879ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:38 +0000

    Add package apiverve-websitereadability/1.1.5/apiverve_websitereadability-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-websitereadability/apiverve_websitereadability-1.1.5-py3-none-any.whl/apiverve_websitereadability/exceptions.py
@@ -0,0 +1,3 @@
+class WebsitereadabilityWebsiteReadabilityAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-websitereadability/apiverve_websitereadability-1.1.5-py3-none-any.whl/apiverve_websitereadability/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_websitereadability',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Website Readability is a simple tool for analyzing the readability of a website. It returns the readability score of the website provided.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
