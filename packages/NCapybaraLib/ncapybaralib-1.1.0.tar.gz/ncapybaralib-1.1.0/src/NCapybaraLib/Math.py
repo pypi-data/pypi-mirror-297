dd267c8ac84fa0c181bd0524ac5cf29d68bb9edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:35 +0000

    Add package NCapybaraLib/1.1.0/ncapybaralib-1.1.0.tar.gz

---
+++ packages/NCapybaraLib/ncapybaralib-1.1.0.tar.gz/ncapybaralib-1.1.0/src/NCapybaraLib/Math.py
@@ -0,0 +1,12 @@
+"""
+NCapybaraLib; By NorbCodes.
+
+Submodule for math stuffs
+
+https://pypi.org/project/NCapybaraLib/
+"""
+
+from ._internal.Math import get_xy_in_2d_array
+from ._internal.Math import get_index_in_2d_array
+from ._internal.Math import tetration
+from ._internal.Math import clamp
