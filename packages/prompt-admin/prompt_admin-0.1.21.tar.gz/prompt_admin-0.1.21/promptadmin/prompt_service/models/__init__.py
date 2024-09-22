44daf09bdac6efcd31f5bbf2a484b06ce327e7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:44 +0000

    Add package prompt-admin/0.1.21/prompt_admin-0.1.21.tar.gz

---
+++ packages/prompt-admin/prompt_admin-0.1.21.tar.gz/prompt_admin-0.1.21/promptadmin/prompt_service/models/__init__.py
@@ -0,0 +1,19 @@
+from .anthropic import AnthropicModelService
+from .anthropic_bedrok import AnthropicBedrockModelService
+from .base_model_service import BaseModelService
+from ...types import ModelServiceInfo
+
+
+def build_model(model_service_info: ModelServiceInfo) -> BaseModelService:
+    if model_service_info.service == 'anthropic':
+        return AnthropicModelService.from_info(model_service_info)
+    elif model_service_info.service == 'anthropic_bedrock':
+        return AnthropicBedrockModelService.from_info(model_service_info)
+
+    raise ValueError()
+
+
+__all__ = [
+    AnthropicModelService,
+    build_model
+]
