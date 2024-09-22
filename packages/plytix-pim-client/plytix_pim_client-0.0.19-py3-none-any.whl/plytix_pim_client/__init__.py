663d9885855a66a31a5602f92f6cdb8cca93bbdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 05:04:29 +0000

    Add package plytix-pim-client/0.0.19/plytix_pim_client-0.0.19-py3-none-any.whl

---
+++ packages/plytix-pim-client/plytix_pim_client-0.0.19-py3-none-any.whl/plytix_pim_client/__init__.py
@@ -0,0 +1,16 @@
+from .client import PlytixPimClientSync, PlytixPimClientAsync
+from .dtos.filters import SearchFilter, OperatorEnum, ProductsRelationshipFilter, RelationshipSearchFilter
+from .dtos.product import Product
+
+__all__ = [
+    "PlytixPimClientSync",
+    "PlytixPimClientAsync",
+    "Product",
+    "SearchFilter",
+    "OperatorEnum",
+    "ProductsRelationshipFilter",
+    "RelationshipSearchFilter",
+    "__version__",
+]
+
+__version__ = "0.0.19"
