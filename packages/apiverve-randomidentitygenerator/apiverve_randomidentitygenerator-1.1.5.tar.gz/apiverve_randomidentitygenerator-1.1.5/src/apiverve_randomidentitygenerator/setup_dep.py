8dda3acf8a3fc824683d6d6f9e1b37f5d5def890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:24 +0000

    Add package apiverve-randomidentitygenerator/1.1.5/apiverve_randomidentitygenerator-1.1.5.tar.gz

---
+++ packages/apiverve-randomidentitygenerator/apiverve_randomidentitygenerator-1.1.5.tar.gz/apiverve_randomidentitygenerator-1.1.5/src/apiverve_randomidentitygenerator/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_randomidentitygenerator',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Random Identity is a simple tool for generating random identities. It returns a random name, address, and more.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
