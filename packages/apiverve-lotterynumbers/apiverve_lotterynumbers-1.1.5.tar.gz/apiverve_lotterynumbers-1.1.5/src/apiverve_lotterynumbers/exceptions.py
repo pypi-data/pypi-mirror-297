d7f3803c6d68157f69e57e64aed54748f552eb2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:21 +0000

    Add package apiverve-lotterynumbers/1.1.5/apiverve_lotterynumbers-1.1.5.tar.gz

---
+++ packages/apiverve-lotterynumbers/apiverve_lotterynumbers-1.1.5.tar.gz/apiverve_lotterynumbers-1.1.5/src/apiverve_lotterynumbers/exceptions.py
@@ -0,0 +1,3 @@
+class LotteryLotteryNumbersAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-lotterynumbers/apiverve_lotterynumbers-1.1.5.tar.gz/apiverve_lotterynumbers-1.1.5/src/apiverve_lotterynumbers/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_lotterynumbers',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Lottery Numbers is a simple tool for looking up winning lottery numbers. It returns the winning lottery numbers based on the lottery name provided.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
