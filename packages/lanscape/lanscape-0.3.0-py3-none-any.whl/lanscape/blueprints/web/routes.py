6bf48559034babe4e27a889c5a9133ed0a645b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:04:45 +0000

    Add package lanscape/0.3.0/lanscape-0.3.0-py3-none-any.whl

---
+++ packages/lanscape/lanscape-0.3.0-py3-none-any.whl/lanscape/blueprints/web/routes.py
@@ -0,0 +1,35 @@
+from flask import render_template, request
+from . import web_bp
+from ...libraries.subnet_scan import SubnetScanner
+from ...libraries.net_tools import get_primary_network_subnet
+
+# Template Renderer
+############################################
+@web_bp.route('/', methods=['GET'])
+def index():
+    subnet = get_primary_network_subnet()
+    port_list = 'medium'
+    parallelism = 0.7
+    if request.args.get('scan_id'):
+        scan = SubnetScanner.get_scan(request.args.get('scan_id'))
+        subnet = scan['subnet']
+        port_list = scan['port_list']
+        parallelism = scan['parallelism']
+    return render_template(
+        'main.html',
+        subnet=subnet, 
+        port_list=port_list, 
+        parallelism=parallelism
+    )
+
+@web_bp.route('/scan/<scan_id>', methods=['GET'])
+@web_bp.route('/scan/<scan_id>/<section>', methods=['GET'])
+def render_scan(scan_id, section='all'):
+    data = SubnetScanner.get_scan(scan_id)
+    filter = request.args.get('filter')
+    return render_template('scan.html', data=data, section=section, filter=filter)
+
+@web_bp.route('/errors/<scan_id>')
+def view_errors(scan_id):
+    data = SubnetScanner.get_scan(scan_id)
+    return render_template('error.html',data=data)
