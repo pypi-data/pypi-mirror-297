04709ccdc083871a1e1905266d7855a74aecc108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:08 +0000

    Add package fake-bpy-module/20240922/fake_bpy_module-20240922.tar.gz

---
+++ packages/fake-bpy-module/fake_bpy_module-20240922.tar.gz/fake_bpy_module-20240922/bl_rna_utils/__init__.pyi
@@ -0,0 +1,7 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import data_path as data_path
+
+_GenericType1 = typing.TypeVar("_GenericType1")
+_GenericType2 = typing.TypeVar("_GenericType2")
