a437b0102f940b1edbd81298ec81b0a5495273f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 03:12:27 +0000

    Add package qablet-basic/0.4.5/qablet_basic-0.4.5-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/qablet-basic/qablet_basic-0.4.5-cp310-cp310-macosx_11_0_arm64.whl/qablet/base/mc.py
@@ -0,0 +1,36 @@
+# Generic MC model
+
+from abc import ABC
+
+from .._qablet import mc_price
+
+
+class MCPricer(ABC):
+    """MC Pricer that uses a Py Model."""
+
+    def __init__(self, state_class):
+        self.state_class = state_class
+
+    def price(self, timetable, dataset):
+        """Calculate price of contract.
+
+        Parameters:
+            timetable (dict): timetable for the contract.
+            dataset (dict): dataset for the model.
+
+        Returns:
+            price (float): price of contract
+            stats (dict): stats such as standard error
+
+        """
+
+        model_state = self.state_class(dataset)
+        model_state.reset()
+        price = mc_price(
+            timetable["events"],
+            model_state,
+            dataset,
+            timetable.get("expressions", {}),
+        )
+
+        return price, model_state.stats
