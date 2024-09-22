6b0898842c8059f69da7f0b7e3d287e0614c5b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 05:04:14 +0000

    Add package qda-my-pkg/1.0.1/qda_my_pkg-1.0.1.tar.gz

---
+++ packages/qda-my-pkg/qda_my_pkg-1.0.1.tar.gz/qda_my_pkg-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qda_my_pkg', 
+    version='1.0.1',
+    author="Hardik Kanak",
+    author_email="hardik.kanak@softqubes.com",
+    packages=find_packages(),
+    description='A Python package with obfuscated code'
+)
