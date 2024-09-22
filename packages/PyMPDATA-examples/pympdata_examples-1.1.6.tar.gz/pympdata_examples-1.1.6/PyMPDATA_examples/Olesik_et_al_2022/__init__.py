cd32961ba0c33003ee993eb9b9e2c62228dc2216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 05:04:21 +0000

    Add package PyMPDATA-examples/1.1.6/pympdata_examples-1.1.6.tar.gz

---
+++ packages/PyMPDATA-examples/pympdata_examples-1.1.6.tar.gz/pympdata_examples-1.1.6/PyMPDATA_examples/Olesik_et_al_2022/__init__.py
@@ -0,0 +1,5 @@
+"""
+This is a PyMPDATA implementation of 1D particle population condensational growth
+problem with coordinate transformations, as presented in
+[Olesik et al. 2022](https://doi.org/10.5194/gmd-15-3879-2022).
+"""
