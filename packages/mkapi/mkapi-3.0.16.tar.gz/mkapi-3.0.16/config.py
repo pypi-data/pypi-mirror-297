e39bfc00f76cd343a492148b99936ce6ce8f0bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:04:49 +0000

    Add package mkapi/3.0.16/mkapi-3.0.16.tar.gz

---
+++ packages/mkapi/mkapi-3.0.16.tar.gz/mkapi-3.0.16/config.py
@@ -0,0 +1,38 @@
+"""Config functions."""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from mkdocs.config.defaults import MkDocsConfig
+
+    from mkapi.plugin import MkApiPlugin
+
+
+def before_on_config(config: MkDocsConfig, plugin: MkApiPlugin) -> None:  # noqa: ARG001
+    """Called before `on_config` event of MkAPI plugin."""
+    if "." not in sys.path:
+        sys.path.insert(0, ".")
+
+
+def after_on_config(config: MkDocsConfig, plugin: MkApiPlugin) -> None:  # noqa: ARG001
+    """Called after `on_config` event of MkAPI plugin."""
+
+
+def page_title(name: str, depth: int) -> str:  # noqa: ARG001
+    """Return a page title."""
+    return name
+    # return ".".join(name.split(".")[depth:])
+
+
+def section_title(name: str, depth: int) -> str:  # noqa: ARG001
+    """Return a section title."""
+    return name
+    # return ".".join(name.split(".")[depth:])
+
+
+def toc_title(name: str, depth: int) -> str:  # noqa: ARG001
+    """Return a toc title."""
+    return name.split(".")[-1]  # Remove prefix.
