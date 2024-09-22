d75027f0b8109c13eb299cf89c788e9af1d2f295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 14:04:55 +0000

    Add package imaginAIry/15.0.0/imaginAIry-15.0.0.tar.gz

---
+++ packages/imaginAIry/imaginAIry-15.0.0.tar.gz/imaginAIry-15.0.0/tests/test_config.py
@@ -0,0 +1,6 @@
+# from imaginairy import config
+# from imaginairy.samplers import SOLVER_TYPE_OPTIONS
+#
+#
+# def test_sampler_options():
+#     assert set(config.SOLVER_TYPE_NAMES) == set(SOLVER_TYPE_OPTIONS)
