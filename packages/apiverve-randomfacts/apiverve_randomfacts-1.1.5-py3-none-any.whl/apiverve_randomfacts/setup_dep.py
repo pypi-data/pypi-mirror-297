ae369f2f03ebfdf4286cf6550200b154afbf2a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:24 +0000

    Add package apiverve-randomfacts/1.1.5/apiverve_randomfacts-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-randomfacts/apiverve_randomfacts-1.1.5-py3-none-any.whl/apiverve_randomfacts/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_randomfacts',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Random Facts is a simple tool for getting random facts. It returns a random fact.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
