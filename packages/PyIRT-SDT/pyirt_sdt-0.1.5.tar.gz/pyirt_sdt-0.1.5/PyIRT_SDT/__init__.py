1f2ac5f1b3993dd2f87643a985f64d0b0b2fcf29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 03:11:02 +0000

    Add package PyIRT-SDT/0.1.5/pyirt_sdt-0.1.5.tar.gz

---
+++ packages/PyIRT-SDT/pyirt_sdt-0.1.5.tar.gz/pyirt_sdt-0.1.5/PyIRT_SDT/__init__.py
@@ -0,0 +1,7 @@
+from .PyIRT_SDT import *
+
+# If you want to specify which functions/classes to export, you can do:
+# from .pyIRT_SDT import returnTable, solve_IRT_for_matrix, plot_item_with_model, ...
+
+# Optionally, define a version number
+__version__ = "0.1.5"
---
+++ packages/PyIRT-SDT/pyirt_sdt-0.1.5.tar.gz/pyirt_sdt-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyIRT_SDT',
+    version='0.1.5',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    url='https://github.com/omarclaflin/pyIRT_SDT',
+    author='Omar Claflin',
+    author_email='your.email@example.com',
+    description='Python IRT calculator with SDT outputs, too, handling continuous and sparse data.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
