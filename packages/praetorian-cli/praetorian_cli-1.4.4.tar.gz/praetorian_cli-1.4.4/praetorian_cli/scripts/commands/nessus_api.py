cc4ed8c01fa6395177ca35b7672b3902dd23e1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 14:04:49 +0000

    Add package praetorian-cli/1.4.4/praetorian_cli-1.4.4.tar.gz

---
+++ packages/praetorian-cli/praetorian_cli-1.4.4.tar.gz/praetorian_cli-1.4.4/praetorian_cli/scripts/commands/nessus_api.py
@@ -0,0 +1,84 @@
+"""
+This script pulls data from the Nessus API server and creates assets and
+risks in the Chariot platform.
+
+Example usage:
+    praetorian chariot script nessus --url https://localhost:8834 --api-key <API_KEY> --secret-key <SECRET_KEY>
+"""
+from concurrent.futures import ThreadPoolExecutor
+
+import click
+import requests
+from requests.packages.urllib3 import disable_warnings, exceptions
+
+from praetorian_cli.sdk.chariot import Chariot
+
+
+def create_nessus_client(api_url, api_key, secret_key):
+    disable_warnings(exceptions.InsecureRequestWarning)
+
+    def nessus_api_req(api: str):
+        headers = {
+            'X-ApiKeys': f'accessKey={api_key}; secretKey={secret_key}'
+        }
+        response = requests.get(
+            f'{api_url}/{api}', headers=headers, verify=False)
+        if not response.ok:
+            click.echo(
+                f'[{response.status_code}] Request failed' + (f'\nError: {response.text}' if response.text else ''),
+                err=True)
+            exit(1)
+        return response.json()
+
+    return nessus_api_req
+
+
+def report_vulns(controller: Chariot, url: str, api_key: str, secret_key: str):
+    """ Ingest scan results from a Nessus server """
+    nessus_api_req = create_nessus_client(url, api_key, secret_key)
+
+    url = f'/scans'
+    response = nessus_api_req(url)
+    for scan in response['scans']:
+        scan_id = scan['id']
+        url = f'/scans/{scan_id}'
+        scan_details = nessus_api_req(url)
+
+        def get_host_scan(scan_id, host):
+            url = f"/scans/{scan_id}/hosts/{host['host_id']}"
+            host_details = nessus_api_req(url)
+            name = host_details['info']['host-ip']
+            dns = host_details['info']['host-ip']
+            if 'host-fqdn' in host_details['info']:
+                dns = host_details['info']['host-fqdn']
+
+            asset_key = ''
+            for vuln in host_details['vulnerabilities']:
+                if vuln['severity'] == 0:
+                    continue
+
+                if asset_key == '':  # only added assets with vulns
+                    asset = controller.add('asset', dict(
+                        dns=dns, name=name, status='F'))
+                    asset_key = asset[0]['key']
+
+                url = f"/scans/{scan_id}/hosts/{host['host_id']}/plugins/{vuln['plugin_id']}"
+                plugin_details = nessus_api_req(url)
+                proof_of_exploit = ''
+                for output in plugin_details['outputs']:
+                    proof_of_exploit += output['plugin_output']
+
+                risk = plugin_details['info']['plugindescription']['pluginattributes']['risk_information'][
+                    'risk_factor']
+                comment = plugin_details['info']['plugindescription']['pluginattributes']['description']
+                vuln = (''.join({vuln['plugin_name']})
+                        ).replace(' ', '-').lower()
+                status = 'T' + risk[0].upper()
+                controller.add('risk', dict(
+                    key=asset_key, name=vuln, source='nessus', status=status, comment=comment))
+                if proof_of_exploit != '':
+                    controller._upload(f'proofs/{dns}/{vuln}', proof_of_exploit)
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for host in scan_details['hosts']:
+            executor.submit(get_host_scan, scan_id, host)
---
+++ packages/praetorian-cli/praetorian_cli-1.4.4.tar.gz/praetorian_cli-1.4.4/praetorian_cli/scripts/commands/nessus_xml.py
@@ -0,0 +1,57 @@
+"""
+This script pulls data from a Nessus DB export and creates the assets
+and risks in the Chariot platform.
+
+Example usage:
+    praetorian chariot script nessus --file <PATH_TO_SCAN.nessus>
+"""
+import xml.etree.ElementTree as ET
+from concurrent.futures import ThreadPoolExecutor
+
+import click
+
+from praetorian_cli.sdk.chariot import Chariot
+
+
+def report_vulns(controller: Chariot, file: str):
+    """ Ingest scan results from a Nessus XML export file """
+
+    def parse_host_scan(reportHost):
+        name = reportHost.find('HostProperties/tag[@name="host-ip"]').text
+        dns = reportHost.find('HostProperties/tag[@name="host-fqdn"]')
+        if dns is not None:
+            dns = dns.text
+        else:
+            dns = name
+
+        asset_key = ''
+        for item in reportHost.iter('ReportItem'):
+            if item.get('severity') == '0':
+                continue
+
+            if asset_key == '':
+                asset = controller.add('asset', dict(
+                    dns=dns, name=name, status='F'))
+                asset_key = asset[0]['key']
+
+            vuln = item.get('pluginName').replace(' ', '-').lower()
+            status = 'T' + item.find('risk_factor').text[0].upper()
+            description = item.find('description').text
+
+            proof_of_exploit = item.find('plugin_output')
+            print(f'Adding {status} risk: "{vuln}" for "{dns}"')
+            controller.add('risk', dict(
+                key=asset_key, name=vuln, source='nessus', status=status, comment=description))
+            if proof_of_exploit is not None:
+                controller._upload(f'proofs/{dns}/{vuln}', proof_of_exploit.text)
+
+    try:
+        main = ET.parse(file)
+    except Exception as e:
+        click.echo(f'Unable to import file {file}. Error: {e}.', err=True)
+        exit(1)
+
+    root = main.getroot()
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for reportHost in root.iter('ReportHost'):
+            executor.submit(parse_host_scan, reportHost)
