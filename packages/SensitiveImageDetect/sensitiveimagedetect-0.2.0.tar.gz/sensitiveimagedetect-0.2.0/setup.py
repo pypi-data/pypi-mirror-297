cfef9463557dc48b1bc23123b7a9d5763fda9760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:50 +0000

    Add package SensitiveImageDetect/0.2.0/sensitiveimagedetect-0.2.0.tar.gz

---
+++ packages/SensitiveImageDetect/sensitiveimagedetect-0.2.0.tar.gz/sensitiveimagedetect-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SensitiveImageDetect',
+    version='0.2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'torch',
+        'torchvision',
+        'pillow',
+    ],
+    author='W1412X',
+    author_email='w108418@126.com',
+    description='A package used to detect sensitive image',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/W1412X/sensitive-image-detect-model',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
