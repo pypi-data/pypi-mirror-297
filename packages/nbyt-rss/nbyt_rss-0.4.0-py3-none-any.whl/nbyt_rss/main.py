781f4285c738f0689119024da3b57cc961542bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 14:04:35 +0000

    Add package nbyt-rss/0.4.0/nbyt_rss-0.4.0-py3-none-any.whl

---
+++ packages/nbyt-rss/nbyt_rss-0.4.0-py3-none-any.whl/nbyt_rss/main.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+from typing import List, Optional
+
+import typer
+from typing_extensions import Annotated
+
+from .__init__ import __version__
+from .getting import channel_name, getting_link
+
+app = typer.Typer()
+
+
+def version_callback(value: bool):
+    if value:
+        print(f"Awesome CLI Version: {__version__}")
+        raise typer.Exit()
+
+
+@app.command()
+def main(
+    urls: List[str],
+    version: Annotated[
+        Optional[bool],
+        typer.Option(
+            "--version",
+            "-v",
+            callback=version_callback,
+            is_eager=True,
+        ),
+    ] = None,
+):
+    """
+    Pass in a YouTube videos url and it will add the correct RSS feed to
+    newsboat.
+    """
+
+    for url in urls:
+
+        link = getting_link(url)
+
+        newsboat = Path("/Users/evanbaird/.newsboat/urls")
+
+        with newsboat.open(mode="a", encoding="utf-8") as wr:
+            wr.write(f'\n{link} "~{channel_name(url_name=url)}"')
