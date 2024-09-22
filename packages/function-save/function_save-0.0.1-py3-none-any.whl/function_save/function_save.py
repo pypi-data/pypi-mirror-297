011cb18117a3d26fb644def597eb2f55c04f0482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 14:05:00 +0000

    Add package function-save/0.0.1/function_save-0.0.1-py3-none-any.whl

---
+++ packages/function-save/function_save-0.0.1-py3-none-any.whl/function_save/function_save.py
@@ -0,0 +1,46 @@
+class FunctionSaver:
+    def __init__(self) -> None:
+        self.__save_output = {} # function : return value
+        self.__f_counter = {} # function : number of output
+        pass
+    
+    def add(self, func, return_value, *args):
+        if self.__save_output.get(func):
+            self.__save_output[func].append([return_value, *args])
+        else:
+            self.__save_output[func] = [[return_value, *args]]
+        if self.__f_counter.get(func):
+            self.__f_counter[func] += 1
+        else:
+            self.__f_counter[func] = 1
+        pass
+    
+    def counter(self):
+        print('Function Counts: ')
+        for key, value in self.__f_counter.items():
+            print(f'{key} : {value}')
+        print()
+    
+    def __str__(self):
+        data = self.__save_output
+        for key, value in data.items():
+            print(key)
+            for ans in value:
+                print(' - ',end='')
+                print(f'{ans[0]} Parameter {ans[1:]}')
+            print()
+        return ''
+
+function_results = FunctionSaver()
+
+def save(func):
+    recursive_depth = 0
+    def wrapper(*args, **kwargs):
+        nonlocal recursive_depth
+        recursive_depth += 1
+        result = func(*args, **kwargs)
+        recursive_depth -= 1
+        if recursive_depth == 0:
+            function_results.add(func.__name__, result, *args)
+        return result
+    return wrapper
