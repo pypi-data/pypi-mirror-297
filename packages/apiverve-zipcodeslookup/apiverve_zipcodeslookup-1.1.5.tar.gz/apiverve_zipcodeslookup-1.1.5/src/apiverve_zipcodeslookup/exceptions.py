c42774898e2c7240ac71cdf90e92086b6ae2dcbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:12 +0000

    Add package apiverve-zipcodeslookup/1.1.5/apiverve_zipcodeslookup-1.1.5.tar.gz

---
+++ packages/apiverve-zipcodeslookup/apiverve_zipcodeslookup-1.1.5.tar.gz/apiverve_zipcodeslookup-1.1.5/src/apiverve_zipcodeslookup/exceptions.py
@@ -0,0 +1,3 @@
+class ZipcodesZipCodesLookupAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-zipcodeslookup/apiverve_zipcodeslookup-1.1.5.tar.gz/apiverve_zipcodeslookup-1.1.5/src/apiverve_zipcodeslookup/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_zipcodeslookup',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Zip Codes Lookup is a simple tool for looking up the zip codes of a city. It returns the zip codes of the city.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
