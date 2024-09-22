5251c96a3573f66d3d589968e60916c124130433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:07 +0000

    Add package fake-bpy-module-latest/20240922/fake_bpy_module_latest-20240922-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20240922-py3-none-any.whl/_bpy_internal/__init__.pyi
@@ -0,0 +1,11 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import addons as addons
+from . import extensions as extensions
+from . import freedesktop as freedesktop
+from . import grease_pencil as grease_pencil
+from . import system_info as system_info
+
+_GenericType1 = typing.TypeVar("_GenericType1")
+_GenericType2 = typing.TypeVar("_GenericType2")
---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20240922-py3-none-any.whl/bpy_types/__init__.pyi
@@ -0,0 +1,32 @@
+import typing
+import collections.abc
+import typing_extensions
+
+_GenericType1 = typing.TypeVar("_GenericType1")
+_GenericType2 = typing.TypeVar("_GenericType2")
+
+class _GenericUI:
+    @classmethod
+    def append(cls, draw_func):
+        """Append a draw function to this menu,
+        takes the same arguments as the menus draw function
+
+                :param draw_func:
+        """
+
+    @classmethod
+    def is_extended(cls): ...
+    @classmethod
+    def prepend(cls, draw_func):
+        """Prepend a draw function to this menu, takes the same arguments as
+        the menus draw function
+
+                :param draw_func:
+        """
+
+    @classmethod
+    def remove(cls, draw_func):
+        """Remove a draw function that has been added to this menu
+
+        :param draw_func:
+        """
