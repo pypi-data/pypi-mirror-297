3b10dee10eb7d7b7dd4e2a58b3cbe98315275f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:04:42 +0000

    Add package expanse/0.4.2/expanse-0.4.2-py3-none-any.whl

---
+++ packages/expanse/expanse-0.4.2-py3-none-any.whl/expanse/core/console/commands/serve.py
@@ -0,0 +1,54 @@
+from typing import Any
+from typing import ClassVar
+
+from cleo.helpers import option
+from cleo.io.inputs.option import Option
+
+from expanse.console.commands.command import Command
+from expanse.core.application import Application
+
+
+class ServeCommand(Command):
+    name: str = "serve"
+
+    options: ClassVar[list[Option]] = [
+        option("--port", flag=False, default=8000),
+        option("watch"),
+    ]
+
+    def handle(self, app: Application) -> int:
+        import uvicorn
+
+        log_level: str = "info"
+        if self._io.is_verbose():
+            log_level = "debug"
+        elif self._io.output.is_quiet():
+            log_level = "error"
+
+        parameters: dict[str, Any] = {
+            "port": int(self.option("port")),
+            "interface": "wsgi",
+            "log_level": log_level,
+            "reload": self.option("watch"),
+            "use_colors": self._io.is_decorated(),
+        }
+
+        if self.option("watch"):
+            parameters["reload_dirs"] = [
+                app.base_path,
+                app.config_path,
+                app.base_path.joinpath("routes"),
+            ]
+
+        config = uvicorn.Config("app.app:app", **parameters)
+        server = uvicorn.Server(config)
+
+        if self.option("watch"):
+            from uvicorn.supervisors import ChangeReload
+
+            sock = config.bind_socket()
+            ChangeReload(config, target=server.run, sockets=[sock]).run()
+        else:
+            server.run()
+
+        return 0
