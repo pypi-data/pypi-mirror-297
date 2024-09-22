566a13bbcd01a1f6743afc80b5bb3f902e84e340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:04:51 +0000

    Add package jdlib/1.0.5/jdlib-1.0.5.tar.gz

---
+++ packages/jdlib/jdlib-1.0.5.tar.gz/jdlib-1.0.5/jdlib/django/models.py
@@ -0,0 +1,66 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from jdlib.django.fields import AutoCreatedField, AutoUpdatedField, UUIDField
+
+
+class TimeStampedMixin(models.Model):
+    created_at = AutoCreatedField()
+    updated_at = AutoUpdatedField()
+
+    class Meta:
+        abstract = True
+
+
+class UUIDMixin(models.Model):
+    uuid = UUIDField(_('UUID'))
+
+    class Meta:
+        abstract = True
+
+
+class UUIDModel(TimeStampedMixin, UUIDMixin):
+    class Meta:
+        abstract = True
+
+
+class User(AbstractUser, UUIDMixin):
+    class Meta:
+        abstract = True
+
+
+class EmailUserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if email is None:
+            raise ValueError('Users must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_user(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+
+
+class EmailUser(User):
+    email = models.EmailField(_('email address'), unique=True)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = EmailUserManager()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.email
