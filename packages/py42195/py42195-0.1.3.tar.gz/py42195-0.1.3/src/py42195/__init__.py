8fbf798b0a47c744a430bb1b9e6202f5387cd59c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:05:07 +0000

    Add package py42195/0.1.3/py42195-0.1.3.tar.gz

---
+++ packages/py42195/py42195-0.1.3.tar.gz/py42195-0.1.3/src/py42195/__init__.py
@@ -0,0 +1,26 @@
+from py42195.config import IMPERIAL, METRIC, get_unit_system, set_unit_system
+from py42195.types import (
+    Distance,
+    Duration,
+    Pace,
+    distance,
+    duration,
+    pace,
+    Speed,
+    speed,
+)
+
+__all__ = [
+    "Distance",
+    "Duration",
+    "Pace",
+    "Speed",
+    "distance",
+    "duration",
+    "pace",
+    "speed",
+    "set_unit_system",
+    "get_unit_system",
+    "METRIC",
+    "IMPERIAL",
+]
