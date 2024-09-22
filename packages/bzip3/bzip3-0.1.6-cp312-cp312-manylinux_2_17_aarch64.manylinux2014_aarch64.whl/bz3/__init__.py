53d0302f63a62f20a4091d1fa0e6fe87171c6cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:53 +0000

    Add package bzip3/0.1.6/bzip3-0.1.6-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/bzip3/bzip3-0.1.6-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/bz3/__init__.py
@@ -0,0 +1,16 @@
+"""
+Copyright (c) 2008-2023 synodriver <diguohuangjiajinweijun@gmail.com>
+"""
+__version__ = "0.1.6"
+
+from bz3.backends import (
+    bound,
+    compress_file,
+    compress_into,
+    decompress_file,
+    decompress_into,
+    libversion,
+    recover_file,
+    test_file,
+)
+from bz3.bz3 import BZ3File, compress, decompress, open
