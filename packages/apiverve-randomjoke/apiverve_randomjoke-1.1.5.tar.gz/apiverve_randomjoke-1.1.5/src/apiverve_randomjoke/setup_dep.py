11c1e65ab81e64735ad572dd6361ca311506b07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:24 +0000

    Add package apiverve-randomjoke/1.1.5/apiverve_randomjoke-1.1.5.tar.gz

---
+++ packages/apiverve-randomjoke/apiverve_randomjoke-1.1.5.tar.gz/apiverve_randomjoke-1.1.5/src/apiverve_randomjoke/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_randomjoke',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Random Joke is a simple tool for getting random jokes. It returns a random joke from a collection of jokes.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
