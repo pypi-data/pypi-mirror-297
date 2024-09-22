0a5b4992ecd2dc3e683e6a20eaef5b30cd2ff7d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:22 +0000

    Add package apiverve-mortgagecalculator/1.1.5/apiverve_mortgagecalculator-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-mortgagecalculator/apiverve_mortgagecalculator-1.1.5-py3-none-any.whl/apiverve_mortgagecalculator/exceptions.py
@@ -0,0 +1,3 @@
+class MortgagecalculatorMortgageCalculatorAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-mortgagecalculator/apiverve_mortgagecalculator-1.1.5-py3-none-any.whl/apiverve_mortgagecalculator/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_mortgagecalculator',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Mortgage Calculator is a simple tool for calculating mortgage payments. It returns the monthly payment, total interest, and more.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
