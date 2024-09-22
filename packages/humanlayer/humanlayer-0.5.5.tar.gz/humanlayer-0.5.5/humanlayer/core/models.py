7c9f8571991f2ece866f5103e2a08133216d545e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 14:05:01 +0000

    Add package humanlayer/0.5.5/humanlayer-0.5.5.tar.gz

---
+++ packages/humanlayer/humanlayer-0.5.5.tar.gz/humanlayer-0.5.5/humanlayer/core/models.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class FunctionCallStatus(BaseModel):
+    requested_at: datetime
+    responded_at: datetime | None = None
+    approved: bool | None = None
+    comment: str | None = None
+
+
+class SlackContactChannel(BaseModel):
+    """
+    Route for contacting a user or channel via slack
+    """
+
+    channel_or_user_id: str
+
+    # target context for the LLM, e.g. target_context="the channel with the director of engineering"
+    # will update the tool description to
+    # "contact a human in slack in the channel with the director of engineering"
+    #
+    # other examples e.g. "a dm with the user you're assisting"
+    context_about_channel_or_user: str | None = None
+
+    # a bot token to override the default contact channel
+    # if you use a custom bot token, ensure your app's
+    # slack webhook destination is set appropriately so your humanlayer server can receive events
+    bot_token: str | None = None
+    #
+    # bot_token_ref: str | None
+
+    # a list of responders to allow to respond to this message
+    # other messages will be ignored
+    # allowed_responder_ids: list[str] | None
+    experimental_slack_blocks: bool | None = None
+
+
+class ContactChannel(BaseModel):
+    slack: SlackContactChannel | None = None
+
+
+class FunctionCallSpec(BaseModel):
+    fn: str
+    kwargs: dict
+    channel: ContactChannel | None = None
+
+
+class FunctionCall(BaseModel):
+    run_id: str
+    call_id: str
+    spec: FunctionCallSpec
+    status: FunctionCallStatus | None = None
+
+
+class HumanContactSpec(BaseModel):
+    msg: str
+    channel: ContactChannel | None = None
+
+
+class HumanContactStatus(BaseModel):
+    response: str
+
+
+class HumanContact(BaseModel):
+    run_id: str
+    call_id: str
+    spec: HumanContactSpec
+    status: HumanContactStatus | None = None
