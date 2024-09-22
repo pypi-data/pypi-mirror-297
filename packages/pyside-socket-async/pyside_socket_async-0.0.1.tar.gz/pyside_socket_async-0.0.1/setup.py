b34dae26b1b8bb80b0a4c1c1aa29c44a72b6773a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:04:47 +0000

    Add package pyside-socket-async/0.0.1/pyside_socket_async-0.0.1.tar.gz

---
+++ packages/pyside-socket-async/pyside_socket_async-0.0.1.tar.gz/pyside_socket_async-0.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyside_socket_async",
+    version="0.0.1",
+    author="chakcy",
+    author_email="947105045@qq.com",
+    description="PySide6 Socket Async",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/cai-xinpenge/pyside_socket_async",
+    include_package_data=True,
+    packages=(
+        find_packages(where=".")
+    ),
+    package_dir={
+        "": ".",
+        "pyside_socket_async":"./pyside_socket_async"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12.4',
+    install_requires=[   
+        "pydantic>=2.9.1",
+        "PySide6>=6.7.2",
+        "requests>=2.32.3",
+        "colorlog>=6.8.2"
+    ]
+)
