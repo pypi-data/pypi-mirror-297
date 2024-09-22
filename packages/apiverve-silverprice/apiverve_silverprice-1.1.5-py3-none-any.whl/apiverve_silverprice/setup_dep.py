cd4a37dfadbdfe36c8e7b7d6baf0155e252cb5af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:31 +0000

    Add package apiverve-silverprice/1.1.5/apiverve_silverprice-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-silverprice/apiverve_silverprice-1.1.5-py3-none-any.whl/apiverve_silverprice/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_silverprice',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Silver Price is a simple tool for getting the current price of silver. It returns the current price of silver in USD and in various units.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
