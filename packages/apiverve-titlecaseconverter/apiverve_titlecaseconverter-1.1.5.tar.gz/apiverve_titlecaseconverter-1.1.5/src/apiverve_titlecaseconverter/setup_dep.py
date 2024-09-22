0a9b3e7cc28dc92ad460757bacc2adcd6fd3f561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:34 +0000

    Add package apiverve-titlecaseconverter/1.1.5/apiverve_titlecaseconverter-1.1.5.tar.gz

---
+++ packages/apiverve-titlecaseconverter/apiverve_titlecaseconverter-1.1.5.tar.gz/apiverve_titlecaseconverter-1.1.5/src/apiverve_titlecaseconverter/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_titlecaseconverter',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Title Capitalization is a simple tool for converting title text to various cases. It returns the text converted to your desired case.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
