fed8c4ad5c2da0544196aa5ccfbd12d6f10bf187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:23 +0000

    Add package apiverve-periodictable/1.1.5/apiverve_periodictable-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-periodictable/apiverve_periodictable-1.1.5-py3-none-any.whl/apiverve_periodictable/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_periodictable',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Periodic Table is a simple tool for getting information about chemical elements. It returns information such as the atomic number, symbol, and more based on the element provided.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
