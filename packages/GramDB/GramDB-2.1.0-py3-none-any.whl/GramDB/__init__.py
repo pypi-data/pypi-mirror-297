70ee94b45ddeb92dd86bc51700db8fd6f4627e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:45 +0000

    Add package GramDB/2.1.0/GramDB-2.1.0-py3-none-any.whl

---
+++ packages/GramDB/GramDB-2.1.0-py3-none-any.whl/GramDB/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "2.1.0"
+
+from GramDB.main import GramDB
+from GramDB.asyncio import GramDBAsync
+
+__all__ = [
+    "GramDB",
+    "GramDBAsync"
+]
---
+++ packages/GramDB/GramDB-2.1.0-py3-none-any.whl/GramDB/asyncio.py
@@ -0,0 +1,43 @@
+import threading
+import asyncio
+from queue import Queue
+from concurrent.futures import Future
+
+class GramDBAsync:
+    def __init__(self):
+        self.background_tasks = []
+        self.loop = asyncio.new_event_loop()
+        self.thread = threading.Thread(target=self.run_event_loop, name="GramDBAsync")
+        self.thread.start()
+        self.running = True
+        
+    def run_event_loop(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
+
+    def close_func(self):
+        self.loop.call_soon_threadsafe(self.loop.stop)
+
+    def close(self):
+        """Close the event loop and stop the thread gracefully."""
+        self.close_func()
+        self.thread.join()
+
+    def run_async(self, coroutine):
+        """
+        Schedule a coroutine to run on the GramDBAsync thread.
+        """
+        return asyncio.run_coroutine_threadsafe(coroutine, self.loop).result()
+
+    def create_task(self, coroutine):
+        """
+        Create a task to run the given coroutine.
+        
+        This method is similar to asyncio.create_task but works within the context of this class.
+        
+        :param coroutine: The coroutine to be executed.
+        :return: The created task.
+        """
+        task = self.loop.create_task(coroutine)
+        self.background_tasks.append(task)
+        return task
