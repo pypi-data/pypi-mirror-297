8ec49021fa5e4ec7ee3a86251a8d1b2001ae1b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 14:04:40 +0000

    Add package vital-ai-haley-kg/0.1.16/vital_ai_haley_kg-0.1.16.tar.gz

---
+++ packages/vital-ai-haley-kg/vital_ai_haley_kg-0.1.16.tar.gz/vital_ai_haley_kg-0.1.16/ai_haley_kg_domain/model/Edge_hasKGIndexDocumentCategory.pyi
@@ -0,0 +1,12 @@
+
+import datetime
+from vital_ai_vitalsigns_core.model.VITAL_PeerEdge import VITAL_PeerEdge
+
+
+class Edge_hasKGIndexDocumentCategory(VITAL_PeerEdge):
+        kGIdentifier: str
+        kGJSON: str
+
+tr
+        kGJSON: str
+
