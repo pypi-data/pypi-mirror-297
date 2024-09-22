d340b3e94b80a0d5eb0cade42b141efd55057971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:34 +0000

    Add package apiverve-textlanguage/1.1.5/apiverve_textlanguage-1.1.5.tar.gz

---
+++ packages/apiverve-textlanguage/apiverve_textlanguage-1.1.5.tar.gz/apiverve_textlanguage-1.1.5/src/apiverve_textlanguage/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_textlanguage',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Text Language is a simple tool for detecting the language of a text. It returns the language code.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
