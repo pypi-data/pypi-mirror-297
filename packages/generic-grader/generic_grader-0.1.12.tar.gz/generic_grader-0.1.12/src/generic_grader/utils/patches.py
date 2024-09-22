0002507bd3ac5ef82a6fc5113a5f2e631d57f12b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 03:11:33 +0000

    Add package generic-grader/0.1.12/generic_grader-0.1.12.tar.gz

---
+++ packages/generic-grader/generic_grader-0.1.12.tar.gz/generic_grader-0.1.12/src/generic_grader/utils/patches.py
@@ -0,0 +1,30 @@
+from generic_grader.utils.exceptions import TurtleDoneError, TurtleWriteError
+from generic_grader.utils.mocks import make_mock_function_raise_error
+
+
+def make_turtle_done_patches(modules):
+    """
+    Patch extra calls to done()/mainloop().
+
+    This prevents hangs when the student mistakenly calls one of them.  The `modules`
+    parameter should be a list or tuple of the assignment's module names.  E.g.
+    `modules = ["vowels", "random_vowels"]`.
+    """
+    return [
+        {"args": make_mock_function_raise_error(f"{module}.{func}", TurtleDoneError)}
+        for func in ["done", "mainloop"]
+        for module in ["turtle", *modules]
+    ]
+
+
+def make_turtle_write_patches(modules):
+    """
+    Make patches to block access to `turtle.write`.
+
+    The `modules` parameter should be a list or tuple of the assignment's module names.
+    E.g. `modules = ["vowels", "random_vowels"]`.
+    """
+    return [
+        {"args": make_mock_function_raise_error(f"{module}.write", TurtleWriteError)}
+        for module in ["turtle", *modules]
+    ]
