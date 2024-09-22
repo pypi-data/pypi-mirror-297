9ee06a90054d56f7b9f54d4bf347d4044eae5f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:32 +0000

    Add package testpulse-uploader/0.0.29/testpulse_uploader-0.0.29-py3-none-any.whl

---
+++ packages/testpulse-uploader/testpulse_uploader-0.0.29-py3-none-any.whl/uploader/utils/urls.py
@@ -0,0 +1,6 @@
+TESTPULSE_API = "https://api.testpulse.io"
+TESTPULSE_API_STAGING = "https://api-staging.testpulse.io"
+TESTPULSE_RECEIVER = "https://receiver.testpulse.io"
+TESTPULSE_RECEIVER_STAGING = "https://receiver-staging.testpulse.io"
+ENDPOINT_TOKENS_CHECK = '/internal/tokens/check'
+ENDPOINT_UPLOAD_RESULTS = '/upload/test_results'
