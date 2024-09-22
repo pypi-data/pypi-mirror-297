adbb52e67084c94b6ea9650bc5bd9529afc7e09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:35 +0000

    Add package langchain-opensearch/0.0.1/langchain_opensearch-0.0.1-py3-none-any.whl

---
+++ packages/langchain-opensearch/langchain_opensearch-0.0.1-py3-none-any.whl/langchain_opensearch/client.py
@@ -0,0 +1,35 @@
+from typing import Any, Dict, Optional
+
+from opensearchpy import OpenSearch
+
+def create_opensearch_client(
+    url: Optional[str] = None,
+    cloud_id: Optional[str] = None,
+    api_key: Optional[int] = None,
+    username: Optional[str] = None,
+    password: Optional[str] = None,
+    params: Optional[Dict[str, Any]] = None,
+) -> OpenSearch:
+    """Create an OpenSearch client."""
+    if url and cloud_id:
+        raise ValueError("Both url and cloud_id are defined. Please provide only one")
+    if not url and not cloud_id:
+        raise ValueError("Must provide either url or cloud_id")
+
+    connection_params: Dict[str, Any] = {}
+    if url:
+        connection_params["url"] = [url]
+    if cloud_id:
+        connection_params["cloud_id"] = cloud_id
+
+    if api_key:
+        connection_params["api_key"] = api_key
+    elif username and password:
+        connection_params["basic_auth"] = (username, password)
+
+    if params is not None:
+        connection_params.update(params)
+    os_client = OpenSearch(**connection_params)
+    os_client.info()
+    return os_client
+
