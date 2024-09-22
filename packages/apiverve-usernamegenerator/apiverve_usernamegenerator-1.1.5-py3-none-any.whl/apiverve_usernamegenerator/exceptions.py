255e7aeaa82bb871b873f4f73f50f5a2ea48b8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:38 +0000

    Add package apiverve-usernamegenerator/1.1.5/apiverve_usernamegenerator-1.1.5-py3-none-any.whl

---
+++ packages/apiverve-usernamegenerator/apiverve_usernamegenerator-1.1.5-py3-none-any.whl/apiverve_usernamegenerator/exceptions.py
@@ -0,0 +1,3 @@
+class UsernamegeneratorUsernameGeneratorAPIError(Exception):
+    """A custom exception for API errors"""
+    pass
---
+++ packages/apiverve-usernamegenerator/apiverve_usernamegenerator-1.1.5-py3-none-any.whl/apiverve_usernamegenerator/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_usernamegenerator',
+    version='1.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Username Generator is a simple tool for generating usernames. It returns a list of usernames based on the specified criteria.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
