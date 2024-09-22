6b689905633eb2f866e630c3f6f8ffcdd0e550ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:07 +0000

    Add package fake-bpy-module-latest/20240922/fake_bpy_module_latest-20240922.tar.gz

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20240922.tar.gz/fake_bpy_module_latest-20240922/bl_previews_utils/__init__.pyi
@@ -0,0 +1,7 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import bl_previews_render as bl_previews_render
+
+_GenericType1 = typing.TypeVar("_GenericType1")
+_GenericType2 = typing.TypeVar("_GenericType2")
---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20240922.tar.gz/fake_bpy_module_latest-20240922/blend_render_info/__init__.pyi
@@ -0,0 +1,12 @@
+import typing
+import collections.abc
+import typing_extensions
+
+_GenericType1 = typing.TypeVar("_GenericType1")
+_GenericType2 = typing.TypeVar("_GenericType2")
+
+class RawBlendFileReader:
+    """Return a file handle to the raw blend file data (abstracting compressed formats)."""
+
+def main(): ...
+def read_blend_rend_chunk(filepath): ...
