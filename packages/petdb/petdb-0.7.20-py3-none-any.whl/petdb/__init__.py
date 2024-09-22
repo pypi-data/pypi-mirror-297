4953bca175381f722aa96868e8cf3db1ebb014d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 05:04:09 +0000

    Add package petdb/0.7.20/petdb-0.7.20-py3-none-any.whl

---
+++ packages/petdb/petdb-0.7.20-py3-none-any.whl/petdb/__init__.py
@@ -0,0 +1,7 @@
+
+from petdb.pdb import PetDB
+from petdb.pcollection import PetCollection, PetMutable, PetArray
+from petdb.putils import NonExistent, NON_EXISTENT
+from petdb.pexceptions import QueryException
+
+__all__ = ["PetDB", "PetCollection", "PetArray", "NonExistent", "QueryException", "PetMutable", "NON_EXISTENT"]
