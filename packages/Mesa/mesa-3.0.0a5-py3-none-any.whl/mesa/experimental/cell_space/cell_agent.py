ae37e01e81f2f5901755a866536d3a4ae689e7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:25 +0000

    Add package Mesa/3.0.0a5/mesa-3.0.0a5-py3-none-any.whl

---
+++ packages/Mesa/mesa-3.0.0a5-py3-none-any.whl/mesa/experimental/cell_space/cell_agent.py
@@ -0,0 +1,42 @@
+"""An agent with movement methods for cell spaces."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from mesa import Agent, Model
+
+if TYPE_CHECKING:
+    from mesa.experimental.cell_space.cell import Cell
+
+
+class CellAgent(Agent):
+    """Cell Agent is an extension of the Agent class and adds behavior for moving in discrete spaces.
+
+    Attributes:
+        unique_id (int): A unique identifier for this agent.
+        model (Model): The model instance to which the agent belongs
+        pos: (Position | None): The position of the agent in the space
+        cell: (Cell | None): the cell which the agent occupies
+    """
+
+    def __init__(self, model: Model) -> None:
+        """Create a new agent.
+
+        Args:
+            model (Model): The model instance in which the agent exists.
+        """
+        super().__init__(model)
+        self.cell: Cell | None = None
+
+    def move_to(self, cell) -> None:
+        """Move agent to cell.
+
+        Args:
+            cell: cell to which agent is to move
+
+        """
+        if self.cell is not None:
+            self.cell.remove_agent(self)
+        self.cell = cell
+        cell.add_agent(self)
