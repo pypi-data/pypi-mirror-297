d0dc2477f13e344cfe936e3f21275533dbdbbb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 03:12:26 +0000

    Add package uncert/0.1.1/uncert-0.1.1.tar.gz

---
+++ packages/uncert/uncert-0.1.1.tar.gz/uncert-0.1.1/uncert/__init__.py
@@ -0,0 +1,10 @@
+"""Simple arithmetic and displaying of measurements and uncertainties.
+
+Written for the Physics 2CL course at UC San Diego."""
+
+import doctest
+
+from .measurement import Measurement
+from .uncertainty import Uncertainty
+
+__all__ = ["Measurement", "Uncertainty"]
