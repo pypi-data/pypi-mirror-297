d231b351d71f287a36476575497f6888e6ed559a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:37 +0000

    Add package openctp-ctp-channels/0.1.6/openctp_ctp_channels-0.1.6-py3-none-any.whl

---
+++ packages/openctp-ctp-channels/openctp_ctp_channels-0.1.6-py3-none-any.whl/openctp_ctp_channels/cli.py
@@ -0,0 +1,52 @@
+import click
+from openctp_ctp_channels import channels, __about__
+
+
+@click.group()
+@click.version_option(version=__about__.__version__)
+def main():
+    pass
+
+
+@click.command(help="Show current channel.")
+def check():
+    channel = channels.CTPChannel()
+    print('Current channel:', channel.current_channel())
+
+
+@click.command(help="Switch channel.")
+@click.argument("channel")
+def switch(channel):
+    if 'tts' == channel:
+        channel = channels.TTSChannel()
+    elif 'tts-s' == channel:
+        channel = channels.TTSSimuChannel()
+    elif 'ctp' == channel:
+        channel = channels.CTPChannel()
+    elif 'qq' == channel:
+        channel = channels.QQChannel()
+    elif 'sina' == channel:
+        channel = channels.SinaChannel()
+    elif 'emt' == channel:
+        channel = channels.EMTChannel()
+    elif 'xtp' == channel:
+        channel = channels.XTPChannel()
+    elif 'tora' == channel:
+        channel = channels.ToraChannel()
+    else:
+        print("Unsupported channel!")
+        return
+
+    channel.switch()
+
+
+@click.command(help="Show all channels.")
+def show():
+    print("Support channels:")
+    for channel, name in channels.CHANNELS.items():
+        print(f"\t{channel} - {name}")
+
+
+main.add_command(check)
+main.add_command(switch)
+main.add_command(show)
