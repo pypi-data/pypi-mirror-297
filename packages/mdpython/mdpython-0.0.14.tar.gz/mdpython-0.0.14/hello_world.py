68e8ead1f768568e1c34324221e60ae7fbe49642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 03:12:28 +0000

    Add package mdpython/0.0.14/mdpython-0.0.14.tar.gz

---
+++ packages/mdpython/mdpython-0.0.14.tar.gz/mdpython-0.0.14/hello_world.py
@@ -0,0 +1,31 @@
+from mdpython.datautils import jsonutil
+
+json_data = {
+    "data": [
+        {
+            "name": "John",
+            "age": 30,
+            "car": {
+                "make": "Toyota",
+                "model": "Camry"
+            },
+            "colors": ["red", "blue", "green"]
+        }, {
+            "name": "Sheema",
+            "age": 25,
+            "car": {
+                "make": "Audi",
+                "model": "a4",
+                "dimension": [5000, 1850, 1433]
+            },
+            "colors": ["blue", "yellow"]
+        }, {
+            "name": "Bruce",
+            "car": {
+                "make": "Ford"
+            }
+        }
+    ]
+}
+
+print(jsonutil.search(json_data, "blue"))
