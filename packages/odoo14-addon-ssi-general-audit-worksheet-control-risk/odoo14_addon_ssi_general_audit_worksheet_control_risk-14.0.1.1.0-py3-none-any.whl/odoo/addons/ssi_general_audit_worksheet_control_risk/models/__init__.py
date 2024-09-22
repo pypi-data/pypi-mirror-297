b8316800784345bc563f2fd428238cc873126825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 14:04:36 +0000

    Add package odoo14-addon-ssi-general-audit-worksheet-control-risk/14.0.1.1.0/odoo14_addon_ssi_general_audit_worksheet_control_risk-14.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-general-audit-worksheet-control-risk/odoo14_addon_ssi_general_audit_worksheet_control_risk-14.0.1.1.0-py3-none-any.whl/odoo/addons/ssi_general_audit_worksheet_control_risk/models/__init__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from . import (
+    general_audit_general_control_category,
+    general_audit_general_control,
+    general_audit_general_control_indicator,
+    general_audit_general_control_set,
+    general_audit_it_control_category,
+    general_audit_it_control,
+    general_audit_it_control_indicator,
+    general_audit_it_control_set,
+    general_audit_ws_d3d2719,
+    general_audit_ws_d3d2719_detail,
+    general_audit_ws_d3d2719_indicator,
+    general_audit_ws_f63f569,
+    general_audit_ws_f63f569_detail,
+    general_audit_ws_f63f569_indicator,
+)
