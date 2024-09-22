a38121d50127e82fde6ec94c152ed14d1c10c078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 14:04:55 +0000

    Add package kshort/0.0.4/kshort-0.0.4-py3-none-any.whl

---
+++ packages/kshort/kshort-0.0.4-py3-none-any.whl/kshort/__init__.py
@@ -0,0 +1,5 @@
+from config import ProjectConfig
+from project import Project
+from projects import get_projects, save_projects, remove_item, remove_cache
+from selector import run, open_in_kitty
+from utils import Config
