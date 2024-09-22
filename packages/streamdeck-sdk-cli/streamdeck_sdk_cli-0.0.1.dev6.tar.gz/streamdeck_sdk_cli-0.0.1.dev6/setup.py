9f3fb90ba403fe275a86a3e382ce086966bd5ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:27 +0000

    Add package streamdeck-sdk-cli/0.0.1.dev6/streamdeck_sdk_cli-0.0.1.dev6.tar.gz

---
+++ packages/streamdeck-sdk-cli/streamdeck_sdk_cli-0.0.1.dev6.tar.gz/streamdeck_sdk_cli-0.0.1.dev6/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+VERSION = "0.0.1.dev6"
+PACKAGE_DIR = "."
+
+setuptools.setup(
+    name="streamdeck_sdk_cli",
+    version=VERSION,
+    author="Grigoriy Gusev",
+    author_email="thegrigus@gmail.com",
+    description="Command Line Interface for streamdeck-sdk.",
+    long_description="",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(where=PACKAGE_DIR),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    license="Apache Software License",
+    keywords=[
+        "streamdeck sdk cli",
+        "streamdeck test project",
+        "python",
+        "sdk",
+        "streamdeck",
+        "streamdeck-sdk",
+        "streamdeck_sdk",
+        "stream deck sdk",
+        "stream deck",
+        "elgato",
+        "elgato sdk",
+        "elgato stream deck",
+        "streamdeck-python-sdk",
+        "streamdeck_python_sdk",
+        "streamdeck python sdk",
+    ],
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'streamdeck_sdk=streamdeck_sdk_cli.executable.executable:main',
+        ],
+    },
+)
