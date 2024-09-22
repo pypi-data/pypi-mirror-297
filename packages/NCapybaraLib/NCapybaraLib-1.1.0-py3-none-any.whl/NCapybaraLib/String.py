30fb0e2dd1e9178a73edc12df9dcfa02a3b8da8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 15:05:35 +0000

    Add package NCapybaraLib/1.1.0/NCapybaraLib-1.1.0-py3-none-any.whl

---
+++ packages/NCapybaraLib/NCapybaraLib-1.1.0-py3-none-any.whl/NCapybaraLib/String.py
@@ -0,0 +1,10 @@
+"""
+NCapybaraLib; By NorbCodes.
+
+String submodule for string operations.
+
+https://pypi.org/project/NCapybaraLib/
+"""
+
+from ._internal.String import insert
+from ._internal.String import string_similarity
