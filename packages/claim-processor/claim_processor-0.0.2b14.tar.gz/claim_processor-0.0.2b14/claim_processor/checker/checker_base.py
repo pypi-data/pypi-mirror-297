40a7cb525edaf621b63b636475e6c69ec9df2edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:05:02 +0000

    Add package claim-processor/0.0.2b14/claim_processor-0.0.2b14.tar.gz

---
+++ packages/claim-processor/claim_processor-0.0.2b14.tar.gz/claim_processor-0.0.2b14/claim_processor/checker/checker_base.py
@@ -0,0 +1,180 @@
+from typing import List, Union
+from itertools import groupby
+
+from ..utils import split_text
+from ..base import RCClaim
+
+
+def merge_ret(ret):
+    """Merge results from multiple paragraphs"""
+    if "Entailment" in ret:
+        return "Entailment"
+    if "Contradiction" in ret:
+        return "Contradiction"
+    return "Neutral"
+
+
+def merge_multi_psg_ret(ret):
+    """Merge results from multiple passages
+    TODO: consider possible cases where the results are inconsistent.
+    """
+    if "Entailment" in ret:
+        return "Entailment"
+    if "Contradiction" in ret:
+        return "Contradiction"
+    return "Neutral"
+
+
+class CheckerBase:
+    def __init__(self) -> None:
+        """
+        Initializer for the CheckerBase class.
+
+        Initialize labels for 'Entailment', 'Neutral', and 'Contradiction'.
+        Also initializes a list of all labels.
+        """
+
+        self.label_entailment = 'Entailment'
+        self.label_neutral = 'Neutral'
+        self.label_contradiction = 'Contradiction'
+        self.labels = ["Entailment", "Neutral", "Contradiction"]
+
+    def check(
+        self, 
+        batch_claims: List[List[Union[str, List[str]]]],
+        batch_references: Union[List[str], List[List[str]]],
+        batch_responses: List[str] = None,
+        batch_questions: List[str] = None,
+        max_reference_segment_length: int = 200,
+        merge_psg: bool = True,
+        is_joint: bool = False,
+        joint_check_num: int = 5
+    ):
+        """
+        Check claims against references.
+
+        Parameters
+        ----------
+        batch_claims : List[List[Union[str, List[str]]]]
+            List consists of the claims extracted from each given example.
+        batch_references : Union[List[str], List[List[str]]]
+            List of reference passages for each given example.
+        batch_responses : List[str], optional
+            List of model response texts, defaults to None.
+        batch_questions : List[str], optional
+            List of questions for each given example, defaults to None.
+        max_reference_segment_length : int, optional
+            Maximum length of each reference segment, defaults to 200.
+        merge_psg : bool, optional
+            Whether to merge results from multiple passages, defaults to True.
+        is_joint: bool, optional
+            Whether perform joint checking for claims to accelerate the checking process.
+        joint_check_num: int, optional
+            Number of claims to check jointly in one prompt. Defaults to 5.
+        
+        Returns
+        -------
+        results : List[List[str]]
+            Grouped triplet checking results corresponding to each given example.
+
+        """
+        assert len(batch_claims) == len(batch_references)
+        
+        if is_joint:
+            # joint checking is for LLM-based checkers only, and it doesn't need merge_psg
+            labels = self._check(
+                claims=batch_claims, 
+                references=batch_references, 
+                responses=batch_responses,
+                questions=batch_questions,
+                is_joint=True,
+                joint_check_num=joint_check_num
+            )
+            return labels
+        else:
+            batch_example_nums = [len(claims) for claims in batch_claims]
+
+            if batch_responses is None:
+                batch_responses = [None] * len(batch_claims)
+            if batch_questions is None:
+                batch_questions = [None] * len(batch_claims)
+            
+            input_flattened = []
+            input_ids = []
+            for idx, (claims, references, resonses, questions) in enumerate(zip(batch_claims, batch_references, batch_responses, batch_questions)):
+                if isinstance(references, str):
+                    references = [references]
+                segments_all_psg = []
+                for psg in references:
+                    if max_reference_segment_length > 0:
+                        segments = split_text(psg, max_reference_segment_length)
+                    else:
+                        segments = [psg]
+                    segments_all_psg.append(segments)
+                for c_idx, claim in enumerate(claims):
+                    for idx_psg, seg_psg in enumerate(segments_all_psg):
+                        for seg in seg_psg:
+                            input_flattened.append([claim, seg, resonses, questions])
+                            input_ids.append([idx, c_idx, idx_psg])
+            ret = self._check(
+                    claims=[inp[0] for inp in input_flattened],
+                    references=[inp[1] for inp in input_flattened],
+                    responses=[inp[2] for inp in input_flattened],
+                    questions=[inp[3] for inp in input_flattened],
+                    is_joint=False
+                )
+
+            ret = [[x] + y for x, y in zip(ret, input_ids)]
+            ret_merge_seg = [[merge_ret([item[0] for item in group])] + key[:-1] for key, group in groupby(ret, key=lambda x: x[1:])]
+            if merge_psg:
+                ret_merge_psg = [
+                    [merge_multi_psg_ret([item[0] for item in group])] + key[:-1] 
+                    for key, group in groupby(ret_merge_seg, key=lambda x: x[1:])
+                ]
+            else:
+                ret_merge_psg = [
+                    [[item[0] for item in group]] + key[:-1]
+                    for key, group in groupby(ret_merge_seg, key=lambda x: x[1:])
+                ]
+            ret_group_triplet = [[item[0] for item in group] for key, group in groupby(ret_merge_psg, key=lambda x: x[1:])]
+
+            results = []
+            i = 0
+            for n in batch_example_nums:
+                if n > 0:
+                    results.append(ret_group_triplet[i])
+                    i += 1
+                else:
+                    results.append([])
+            return results
+
+    def _check(
+        self,
+        claims: List[RCClaim],
+        references: List[str],
+        responses: List[str],
+        questions: List[str]
+    ):
+        """
+        Internal method for checking claims against references.
+
+        This method should be implemented by subclasses.
+
+        Parameters
+        ----------
+        claims : List[RCClaim]
+            List of claims to be checked.
+        references : List[str]
+            List of reference passages.
+        responses : List[str]
+            List of model response texts.
+        questions : List[str]
+            List of questions.
+
+        Returns
+        -------
+        List[str]
+            List of checking results.
+        """
+
+        raise NotImplementedError
---
+++ packages/claim-processor/claim_processor-0.0.2b14.tar.gz/claim_processor-0.0.2b14/claim_processor/utils.py
@@ -0,0 +1,164 @@
+import time
+import json
+import os
+
+import spacy
+from openai.types import Completion as OpenAICompletion
+from openai import RateLimitError as OpenAIRateLimitError
+from openai import APIError as OpenAIAPIError
+from openai import Timeout as OpenAITimeout
+
+#from litellm import batch_completion
+#from litellm.types.utils import ModelResponse
+from claim_processor.proxy_call import batch_completion
+
+# Setup spaCy NLP
+nlp = None
+
+# Setup OpenAI API
+openai_client = None
+
+# Setup Claude 2 API
+bedrock = None
+anthropic_client = None
+
+
+def get_llm_full_name(llm_name):
+    if llm_name == 'claude3-sonnet':
+        return 'anthropic.claude-3-sonnet-20240229-v1:0' if os.environ.get('AWS_REGION_NAME') else 'claude-3-sonnet-20240229'
+    elif llm_name == 'claude3-haiku':
+        return 'anthropic.claude-3-haiku-20240307-v1:0' if os.environ.get('AWS_REGION_NAME') else 'claude-3-haiku-20240307'
+    return llm_name
+
+
+def sentencize(text):
+    """Split text into sentences"""
+    global nlp
+    if not nlp:
+        nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    return [sent for sent in doc.sents]
+
+
+def split_text(text, segment_len=200):
+    """Split text into segments according to sentence boundaries."""
+    segments, seg = [], []
+    sents = [[token.text for token in sent] for sent in sentencize(text)]
+    for sent in sents:
+        if len(seg) + len(sent) > segment_len:
+            segments.append(" ".join(seg))
+            seg = sent
+            # single sentence longer than segment_len
+            if len(seg) > segment_len:
+                # split into chunks of segment_len
+                seg = [
+                    " ".join(seg[i:i+segment_len])
+                    for i in range(0, len(seg), segment_len)
+                ]
+                segments.extend(seg)
+                seg = []
+        else:
+            seg.extend(sent)
+    if seg:
+        segments.append(" ".join(seg))
+    return segments
+
+
+def get_model_batch_response(
+        prompts,
+        model='bedrock/anthropic.claude-3-sonnet-20240229-v1:0',
+        temperature=0,
+        n_choices=1,
+        max_new_tokens=500,
+        api_base=None
+):
+    """
+    Get batch generation results with given prompts.
+
+    Parameters
+    ----------
+    prompts : List[str]
+        List of prompts for generation.
+    temperature : float, optional
+        The generation temperature, use greedy decoding when setting
+        temperature=0, defaults to 0.
+    model : str, optional
+        The model for generation, defaults to 'bedrock/anthropic.claude-3-sonnet-20240229-v1:0'.
+    n_choices : int, optional
+        How many samples to return for each prompt input, defaults to 1.
+    max_new_tokens : int, optional
+        Maximum number of newly generated tokens, defaults to 500.
+
+    Returns
+    -------
+    response_list : List[str]
+        List of generated text.
+    """
+    if not prompts or len(prompts) == 0:
+        raise ValueError("Invalid input.")
+
+    message_list = []
+    for prompt in prompts:
+        if len(prompt) == 0:
+            raise ValueError("Invalid prompt.")
+        if isinstance(prompt, str):
+            messages = [{
+                'role': 'user',
+                'content': prompt
+            }]
+        elif isinstance(prompt, list):
+            messages = prompt
+        else:
+            raise ValueError("Invalid prompt type.")
+        message_list.append(messages)
+    import litellm
+    litellm.suppress_debug_info = True
+    # litellm.drop_params=True
+    while True:
+        try:
+            responses = batch_completion(
+                model=model,
+                messages=message_list,
+                temperature=temperature,
+                n=n_choices,
+                max_tokens=max_new_tokens,
+                api_base=api_base,
+                feature_key=''
+            )
+            try:
+                response_list = [response for response in responses]
+                assert all([r is not None for r in response_list])
+                return response_list
+            except ValueError as e1:
+                raise ValueError(str(e1))
+            except Exception as e:
+                print(e)
+                return None
+                #assert all([isinstance(r, ModelResponse) for r in responses])
+                '''if n_choices == 1:
+                    response_list = [r.choices[0].message.content for r in responses]
+                else:
+                    response_list = [[res.message.content for res in r.choices] for r in responses]
+                
+                assert all([r is not None for r in response_list])
+                return response_list
+            except:
+                exception = None
+                for e in responses:
+                    if isinstance(e, ModelResponse):
+                        continue
+                    elif isinstance(e, OpenAIRateLimitError) or isinstance(e, OpenAIAPIError) or isinstance(e, OpenAITimeout):
+                        exception = e
+                        break
+                    else:
+                        print('Exit with the following error:')
+                        print(e)
+                        return None
+                
+                print(f"{exception} [sleep 10 seconds]")
+                time.sleep(10)
+                continue'''
+        except ValueError as e1:
+            raise ValueError(str(e1))
+        except Exception as e:
+            return None
