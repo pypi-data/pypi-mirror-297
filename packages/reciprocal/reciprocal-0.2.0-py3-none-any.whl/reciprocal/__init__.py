8f8e23e6f22fd184efeb4101469bedd21dd80c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 06:05:02 +0000

    Add package reciprocal/0.2.0/reciprocal-0.2.0-py3-none-any.whl

---
+++ packages/reciprocal/reciprocal-0.2.0-py3-none-any.whl/reciprocal/__init__.py
@@ -0,0 +1,5 @@
+from .menus import Action  # noqa
+from .menus import EXIT_CODE  # noqa
+from .menus import LiteralAction  # noqa
+from .menus import Menu  # noqa
+from .menus import WaitingAction  # noqa
