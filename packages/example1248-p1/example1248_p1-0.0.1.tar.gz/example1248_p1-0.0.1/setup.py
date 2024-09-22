580d1da97b184cf3ad8ec16a891b2c2533bbd2d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 14:04:40 +0000

    Add package example1248-p1/0.0.1/example1248_p1-0.0.1.tar.gz

---
+++ packages/example1248-p1/example1248_p1-0.0.1.tar.gz/example1248_p1-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+import setuptools
+
+PACKAGE_DIR = "."
+
+setup(
+    name='example1248-p1',
+    version='0.0.1',
+    description='',
+    long_description='',
+    author="Grigoriy Gusev",
+    author_email="thegrigus@gmail.com",
+    license='Apache Software License',
+    packages=setuptools.find_packages(where=PACKAGE_DIR),
+)
