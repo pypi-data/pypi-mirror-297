758924df24cd8881a22d454a2e4a76380391f224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:12 +0000

    Add package osi-dump/0.1.3.2.2/osi_dump-0.1.3.2.2.tar.gz

---
+++ packages/osi-dump/osi_dump-0.1.3.2.2.tar.gz/osi_dump-0.1.3.2.2/src/osi_dump/model/load_balancer.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, ValidationError
+
+
+class LoadBalancer(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    id: str
+
+    load_balancer_name: Optional[str]
+
+    provisioning_status: Optional[str]
+
+    operating_status: Optional[str]
+
+    amphoraes: list[dict]
+
+    project_id: Optional[str]
