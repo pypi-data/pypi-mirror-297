487c9b42cfa99dab7b13dfc4ff7125ad988f5828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 04:04:26 +0000

    Add package sciduck/0.1.0/sciduck-0.1.0-py2.py3-none-any.whl

---
+++ packages/sciduck/sciduck-0.1.0-py2.py3-none-any.whl/sciduck/__init__.py
@@ -0,0 +1,9 @@
+"""
+sciduck
+
+single cell data analyis unification and communication toolkit
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Nelson Johansen'
+__credits__ = 'Allen Institute for Brain Science'
