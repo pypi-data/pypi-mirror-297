3249ade229c89b7857a79675eb0e79999bb14ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 03:11:54 +0000

    Add package myskoda/0.5.1/myskoda-0.5.1-py3-none-any.whl

---
+++ packages/myskoda/myskoda-0.5.1-py3-none-any.whl/myskoda/models/common.py
@@ -0,0 +1,71 @@
+"""Common models used in multiple responses."""
+
+from enum import StrEnum
+
+from pydantic import BaseModel, Field
+
+
+class OnOffState(StrEnum):
+    ON = "ON"
+    OFF = "OFF"
+    INVALID = "INVALID"
+
+
+class EnabledState(StrEnum):
+    ENABLED = "ENABLED"
+    DISABLED = "DISABLED"
+
+
+class ActiveState(StrEnum):
+    ACTIVATED = "ACTIVATED"
+    DEACTIVATED = "DEACTIVATED"
+
+
+class OpenState(StrEnum):
+    OPEN = "OPEN"
+    CLOSED = "CLOSED"
+    UNSUPPORTED = "UNSUPPORTED"
+
+
+class DoorLockedState(StrEnum):
+    LOCKED = "YES"
+    UNLOCKED = "NO"
+
+
+class ChargerLockedState(StrEnum):
+    LOCKED = "LOCKED"
+    UNLOCKED = "UNLOCKED"
+
+
+class ConnectionState(StrEnum):
+    CONNECTED = "CONNECTED"
+    DISCONNECTED = "DISCONNECTED"
+
+
+class Side(StrEnum):
+    LEFT = "LEFT"
+    RIGHT = "RIGHT"
+
+
+class Coordinates(BaseModel):
+    latitude: float
+    longitude: float
+
+
+class Address(BaseModel):
+    city: str
+    country: str | None
+    country_code: str = Field(None, alias="countryCode")
+    house_number: str | None = Field(None, alias="houseNumber")
+    street: str
+    zip_code: str = Field(None, alias="zipCode")
+
+
+class Weekday(StrEnum):
+    MONDAY = "MONDAY"
+    TUESDAY = "TUESDAY"
+    WEDNESDAY = "WEDNESDAY"
+    THURSDAY = "THURSDAY"
+    FRIDAY = "FRIDAY"
+    SATURDAY = "SATURDAY"
+    SUNDAY = "SUNDAY"
---
+++ packages/myskoda/myskoda-0.5.1-py3-none-any.whl/myskoda/models/position.py
@@ -0,0 +1,33 @@
+"""Models for responses of api/v2/vehicle-status/{vin}/driving-range."""
+
+from enum import StrEnum
+
+from pydantic import BaseModel, Field
+
+from .common import Address, Coordinates
+
+
+class PositionType(StrEnum):
+    VEHICLE = "VEHICLE"
+
+
+class Position(BaseModel):
+    address: Address
+    gps_coordinates: Coordinates = Field(None, alias="gpsCoordinates")
+    type: PositionType
+
+
+class ErrorType(StrEnum):
+    VEHICLE_IN_MOTION = "VEHICLE_IN_MOTION"
+
+
+class Error(BaseModel):
+    type: ErrorType
+    description: str
+
+
+class Positions(BaseModel):
+    """Positional information (GPS) for the vehicle and other things."""
+
+    errors: list[Error]
+    positions: list[Position]
