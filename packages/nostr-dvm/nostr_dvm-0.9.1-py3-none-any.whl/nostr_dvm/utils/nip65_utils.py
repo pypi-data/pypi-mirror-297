1873202b09ed484e1dc12d1c8e7a87cba61286a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-22 05:04:10 +0000

    Add package nostr-dvm/0.9.1/nostr_dvm-0.9.1-py3-none-any.whl

---
+++ packages/nostr-dvm/nostr_dvm-0.9.1-py3-none-any.whl/nostr_dvm/utils/nip65_utils.py
@@ -0,0 +1,26 @@
+from nostr_sdk import Tag, Keys, EventBuilder
+
+from nostr_dvm.utils.definitions import EventDefinitions
+from nostr_dvm.utils.nostr_utils import send_event
+from nostr_dvm.utils.print import bcolors
+
+
+async def nip65_announce_relays(dvm_config, client):
+    tags = []
+
+    for relay in dvm_config.RELAY_LIST:
+        r_tag = Tag.parse(["r", relay])
+        tags.append(r_tag)
+
+    keys = Keys.parse(dvm_config.NIP89.PK)
+    content = ""
+
+    event = EventBuilder(EventDefinitions.KIND_RELAY_ANNOUNCEMENT, content, tags).to_event(keys)
+    eventid = await send_event(event, client=client, dvm_config=dvm_config)
+    if (eventid is not None):
+        print(
+            bcolors.BLUE + "[" + dvm_config.NIP89.NAME + "] Announced NIP 65 for " + dvm_config.NIP89.NAME + " (EventID: " + str(
+                eventid.id.to_hex()) + ")" + bcolors.ENDC)
+    else:
+        print(
+            bcolors.RED + "[" + dvm_config.NIP89.NAME + "] Could not announce NIP 65 for " + dvm_config.NIP89.NAME + bcolors.ENDC)
