01b5174cc8e3eb3000b8493bad71aebf08b924cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:53 +0000

    Add package jje-cli/0.1.3/jje_cli-0.1.3.tar.gz

---
+++ packages/jje-cli/jje_cli-0.1.3.tar.gz/jje_cli-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='jje_cli',
+    version='0.1.3',
+    author='John Gbaya-kokoya Jr',
+    author_email='gbayakokoyajohnjr@gmail.com',
+    description='A simple to use CLI tool for creating a Vuejs project scaffold with Django as backend',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/jje_cli",
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'GitPython'
+    ],
+    entry_points='''
+        [console_scripts]
+        jje=jje_cli.cli:cli
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
