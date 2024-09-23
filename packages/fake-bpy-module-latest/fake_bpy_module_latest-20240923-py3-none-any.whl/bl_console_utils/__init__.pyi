e79592fa2b12b473f8a7b1f3cc883e4bbc0178bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 15:06:02 +0000

    Add package fake-bpy-module-latest/20240923/fake_bpy_module_latest-20240923-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20240923-py3-none-any.whl/bl_console_utils/__init__.pyi
@@ -0,0 +1,7 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import autocomplete as autocomplete
+
+_GenericType1 = typing.TypeVar("_GenericType1")
+_GenericType2 = typing.TypeVar("_GenericType2")
