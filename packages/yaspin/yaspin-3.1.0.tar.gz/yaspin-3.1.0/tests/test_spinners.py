d01fe240ea6c3ef761c1f5ffd3245e9baf789540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 04:06:28 +0000

    Add package yaspin/3.1.0/yaspin-3.1.0.tar.gz

---
+++ packages/yaspin/yaspin-3.1.0.tar.gz/yaspin-3.1.0/tests/test_spinners.py
@@ -0,0 +1,28 @@
+"""
+tests.test_spinners
+~~~~~~~~~~~~~~~~~~~
+
+Tests for spinners collection.
+"""
+
+import json
+from collections import OrderedDict
+
+import pytest
+
+from yaspin.spinners import SPINNERS_DATA, Spinners
+
+spinners_dict = OrderedDict(json.loads(SPINNERS_DATA))
+test_cases = [(name, v["frames"], v["interval"]) for name, v in spinners_dict.items()]
+
+
+def test_len():
+    assert len(Spinners) == len(spinners_dict)
+
+
+# Entry example:
+# ('balloon', [' ', '.', 'o', 'O', '@', '*', ' '], 140)
+@pytest.mark.parametrize("name, frames, interval", test_cases)
+def test_spinners(name, frames, interval):
+    assert getattr(Spinners, name).frames == frames
+    assert getattr(Spinners, name).interval == interval
