70b9ef381fdaac6a4c7ed45df924047478df837f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:55 +0000

    Add package iop-python/0.1.4/iop_python-0.1.4.tar.gz

---
+++ packages/iop-python/iop_python-0.1.4.tar.gz/iop_python-0.1.4/test.py
@@ -0,0 +1,24 @@
+import iop_python as iop
+from api import hydra
+import requests
+import json
+
+
+mywallet = hydra.HydraWallet()
+
+
+
+phrase = "blind market ability shoot topple round inmate pass lunch symbol average alpha party notice switch sea pass toy alien fuel pull angle weather scan"
+wallet = iop.get_morpheus_vault(phrase,"pass")
+
+
+
+password="horse-staple-God"
+data = iop.get_hyd_vault(mainnet_phrase,"unlockPassword","mainnet")
+wallet = iop.get_wallet(data,0,"mainnet")
+print(wallet) # hVKiVHyKFzsm2MdKR2NxBCs3K8EoKGz3Dq
+response = iop.generate_transaction(data,"hbnPgtVbYkDQkcWA47nbdZNfbstYQUay9D",50000000,8,password,0,"mainnet") # send 0.5
+url = "http://explorer.hydraledger.tech:4703/api/v2/transactions"
+# res = requests.post(url, json=json.loads(response))
+# response = res.json()
+# print(response)
