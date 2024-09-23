3da98f564e9e89ec283ab2d942e2f154d023b741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 05:04:42 +0000

    Add package stam/0.9.1/stam-0.9.1.tar.gz

---
+++ packages/stam/stam-0.9.1.tar.gz/stam-0.9.1/src/config.rs
@@ -0,0 +1,80 @@
+use pyo3::types::*;
+
+use stam::*;
+
+pub fn get_config(kwargs: &PyDict) -> Config {
+    let mut config = Config::default();
+    for (key, value) in kwargs {
+        if let Some(key) = key.extract().unwrap() {
+            match key {
+                "use_include" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_use_include(value);
+                    }
+                }
+                "debug" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_debug(value);
+                    }
+                }
+                "workdir" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_workdir(value);
+                    }
+                }
+                "textrelationmap" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_textrelationmap(value);
+                    }
+                }
+                "resource_annotation_map" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_resource_annotation_map(value);
+                    }
+                }
+                "dataset_annotation_map" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_dataset_annotation_map(value);
+                    }
+                }
+                "annotation_annotation_map" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_annotation_annotation_map(value);
+                    }
+                }
+                "key_annotation_metamap" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_key_annotation_metamap(value);
+                    }
+                }
+                "data_annotation_metamap" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_data_annotation_metamap(value);
+                    }
+                }
+                "generate_ids" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_generate_ids(value);
+                    }
+                }
+                "shrink_to_fit" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_shrink_to_fit(value);
+                    }
+                }
+                "strip_temp_ids" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_strip_temp_ids(value);
+                    }
+                }
+                "milestone_interval" => {
+                    if let Ok(Some(value)) = value.extract() {
+                        config = config.with_milestone_interval(value);
+                    }
+                }
+                _ => eprintln!("Ignored unknown kwargs option {}", key),
+            }
+        }
+    }
+    config
+}
