99abfc4d4b213602191e9f6cc0ce897a7d2d7678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 15:06:02 +0000

    Add package fake-bpy-module/20240923/fake_bpy_module-20240923.tar.gz

---
+++ packages/fake-bpy-module/fake_bpy_module-20240923.tar.gz/fake_bpy_module-20240923/_bpy_internal/extensions/__init__.pyi
@@ -0,0 +1,9 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import junction_module as junction_module
+from . import stale_file_manager as stale_file_manager
+from . import wheel_manager as wheel_manager
+
+_GenericType1 = typing.TypeVar("_GenericType1")
+_GenericType2 = typing.TypeVar("_GenericType2")
---
+++ packages/fake-bpy-module/fake_bpy_module-20240923.tar.gz/fake_bpy_module-20240923/bl_keymap_utils/__init__.pyi
@@ -0,0 +1,11 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import io as io
+from . import keymap_from_toolbar as keymap_from_toolbar
+from . import keymap_hierarchy as keymap_hierarchy
+from . import platform_helpers as platform_helpers
+from . import versioning as versioning
+
+_GenericType1 = typing.TypeVar("_GenericType1")
+_GenericType2 = typing.TypeVar("_GenericType2")
