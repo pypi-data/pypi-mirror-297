ae0da297c0dea66771ce7dc954941e5d7f7e031e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 17:04:38 +0000

    Add package iiris-cache/0.0.3/iiris_cache-0.0.3.tar.gz

---
+++ packages/iiris-cache/iiris_cache-0.0.3.tar.gz/iiris_cache-0.0.3/iiris_cache/__init__.py
@@ -0,0 +1,2 @@
+# in_memory_cache/__init__.py
+from .in_memory_cache import Cache
---
+++ packages/iiris-cache/iiris_cache-0.0.3.tar.gz/iiris_cache-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+    
+setup(
+    name='iiris_cache',
+    version='0.0.3',
+    description='Generic package which can be used for caching purpose locally',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Roopendra',
+    author_email='roopendra.naik@informa.com',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6'
+)
