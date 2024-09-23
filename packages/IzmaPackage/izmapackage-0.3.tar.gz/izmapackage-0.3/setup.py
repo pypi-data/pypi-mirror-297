10071c9b83fff7dd40047329b9cc326091189f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:09:17 +0000

    Add package IzmaPackage/0.3/izmapackage-0.3.tar.gz

---
+++ packages/IzmaPackage/izmapackage-0.3.tar.gz/izmapackage-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='IzmaPackage',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here
+        # e.g. 'numpy>=1.11.1'
+    ],
+    entry_points={
+        "console_scripts": [
+            "izma-hello = izmaPackage:hello",
+        ],
+    },
+)
