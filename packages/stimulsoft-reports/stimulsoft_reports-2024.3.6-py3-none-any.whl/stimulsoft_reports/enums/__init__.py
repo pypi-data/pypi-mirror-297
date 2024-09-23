053c74a26db3b4c062559c2db66ea01b4fd02e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 15:05:50 +0000

    Add package stimulsoft-reports/2024.3.6/stimulsoft_reports-2024.3.6-py3-none-any.whl

---
+++ packages/stimulsoft-reports/stimulsoft_reports-2024.3.6-py3-none-any.whl/stimulsoft_reports/enums/__init__.py
@@ -0,0 +1,5 @@
+from stimulsoft_data_adapters.enums import StiDatabaseType, StiDataCommand, StiFrameworkType
+
+from .Encoding import Encoding
+from .StiEventType import StiEventType
+from .StiHtmlMode import StiHtmlMode
