c0462eac114f03c385d20eeb125736b450ba06cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 17:04:53 +0000

    Add package xsarsea/1.5/xsarsea-1.5.tar.gz

---
+++ packages/xsarsea/xsarsea-1.5.tar.gz/xsarsea-1.5/src/xsarsea/windspeed/__init__.py
@@ -0,0 +1,13 @@
+"""
+windspeed module, for retrieving wind speed from sigma0 and models.
+"""
+__all__ = ['invert_from_model', 'available_models', 'get_model', 'register_cmod7',
+           'register_sarwing_luts', 'register_nc_luts', 'register_luts', 'nesz_flattening', 'GmfModel']
+from .windspeed import invert_from_model
+from .models import available_models, get_model, register_nc_luts, register_luts
+from .sarwing_luts import register_sarwing_luts
+from .cmod7 import register_cmod7
+from .utils import nesz_flattening, get_dsig
+from .gmfs import GmfModel
+from . import gmfs
+from . import gmfs_impl
