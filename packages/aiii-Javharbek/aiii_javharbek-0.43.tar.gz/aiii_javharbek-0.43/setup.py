2381b30b8cb6f7bb147aa02be91b4a5937264b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 15:06:03 +0000

    Add package aiii-Javharbek/0.43/aiii_javharbek-0.43.tar.gz

---
+++ packages/aiii-Javharbek/aiii_javharbek-0.43.tar.gz/aiii_javharbek-0.43/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiii_Javharbek',
+    version='0.43',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python==4.8.0.76',
+        'numpy==1.25.2',
+        'scipy==1.11.4',
+        'matplotlib==3.7.1',
+        'tensorflow==2.15.0',
+        'imgaug==0.4.0',
+        'scikit-learn==1.2.2',
+        'shapely==2.0.5'
+    ],
+    author='Javharbek',
+    author_email='jakharbek@gmail.com',
+    description='AIII',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
