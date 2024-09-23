14414c873e9e1e8ee0742a9d261484853da6f951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 15:06:03 +0000

    Add package fake-bpy-module/20240923/fake_bpy_module-20240923-py3-none-any.whl

---
+++ packages/fake-bpy-module/fake_bpy_module-20240923-py3-none-any.whl/bl_text_utils/__init__.pyi
@@ -0,0 +1,7 @@
+import typing
+import collections.abc
+import typing_extensions
+from . import external_editor as external_editor
+
+_GenericType1 = typing.TypeVar("_GenericType1")
+_GenericType2 = typing.TypeVar("_GenericType2")
