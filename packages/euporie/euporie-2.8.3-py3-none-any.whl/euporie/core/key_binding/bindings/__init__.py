213242fa4d14ec9b964dda87eee03fa2cfe044c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 17:04:38 +0000

    Add package euporie/2.8.3/euporie-2.8.3-py3-none-any.whl

---
+++ packages/euporie/euporie-2.8.3-py3-none-any.whl/euporie/core/key_binding/bindings/__init__.py
@@ -0,0 +1,11 @@
+"""Define collections of generic key-bindings which do not belong to widgets."""
+
+from euporie.core.key_binding.bindings import (
+    basic,
+    completion,
+    micro,
+    mouse,
+    page_navigation,
+)
+
+__all__ = ["basic", "completion", "micro", "mouse", "page_navigation"]
