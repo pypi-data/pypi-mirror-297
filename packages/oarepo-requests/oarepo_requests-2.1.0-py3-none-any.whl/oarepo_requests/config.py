e91ad0a214be51ef9934c7a4cccbfcb1bc42d49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:51 +0000

    Add package oarepo-requests/2.1.0/oarepo_requests-2.1.0-py3-none-any.whl

---
+++ packages/oarepo-requests/oarepo_requests-2.1.0-py3-none-any.whl/oarepo_requests/config.py
@@ -0,0 +1,86 @@
+from oarepo_requests.actions.components import (
+    AutoAcceptComponent,
+    RequestIdentityComponent,
+)
+from oarepo_requests.types.events.topic_update import TopicUpdateEventType
+
+try:
+    import oarepo_workflows  # noqa
+
+    from oarepo_requests.actions.components import WorkflowTransitionComponent
+
+    workflow_action_components = [WorkflowTransitionComponent]
+except ImportError:
+    workflow_action_components = []
+from oarepo_requests.resolvers.ui import (
+    FallbackEntityReferenceUIResolver,
+    GroupEntityReferenceUIResolver,
+    UserEntityReferenceUIResolver,
+)
+from oarepo_requests.types import (
+    DeletePublishedRecordRequestType,
+    EditPublishedRecordRequestType,
+    PublishDraftRequestType,
+)
+from invenio_requests.customizations import CommentEventType, LogEventType
+from invenio_requests.services.permissions import (
+    PermissionPolicy as InvenioRequestsPermissionPolicy,
+)
+from oarepo_workflows.requests.events import WorkflowEvent
+
+
+REQUESTS_REGISTERED_TYPES = [
+    DeletePublishedRecordRequestType(),
+    EditPublishedRecordRequestType(),
+    PublishDraftRequestType(),
+]
+
+REQUESTS_REGISTERED_EVENT_TYPES = [
+    TopicUpdateEventType(),
+]
+
+REQUESTS_ALLOWED_RECEIVERS = ["user", "group", "auto_approve"]
+
+DEFAULT_WORKFLOW_EVENT_SUBMITTERS = {
+    CommentEventType.type_id: WorkflowEvent(
+        submitters=InvenioRequestsPermissionPolicy.can_create_comment
+    ),
+    LogEventType.type_id: WorkflowEvent(
+        submitters=InvenioRequestsPermissionPolicy.can_create_comment
+    ),
+    TopicUpdateEventType.type_id: WorkflowEvent(
+        submitters=InvenioRequestsPermissionPolicy.can_create_comment
+    ),
+}
+
+ENTITY_REFERENCE_UI_RESOLVERS = {
+    "user": UserEntityReferenceUIResolver("user"),
+    "fallback": FallbackEntityReferenceUIResolver("fallback"),
+    "group": GroupEntityReferenceUIResolver("group"),
+}
+
+REQUESTS_UI_SERIALIZATION_REFERENCED_FIELDS = ["created_by", "receiver", "topic"]
+
+REQUESTS_ACTION_COMPONENTS = {
+    "accepted": [
+        *workflow_action_components,
+        RequestIdentityComponent,
+    ],
+    "submitted": [
+        *workflow_action_components,
+        AutoAcceptComponent,
+        RequestIdentityComponent,
+    ],
+    "declined": [
+        *workflow_action_components,
+        RequestIdentityComponent,
+    ],
+    "cancelled": [
+        *workflow_action_components,
+        RequestIdentityComponent,
+    ],
+    "expired": [
+        *workflow_action_components,
+        RequestIdentityComponent,
+    ],
+}
