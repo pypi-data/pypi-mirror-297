986365987f6ef2db395002d31a4d4a23c447587d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 14:04:57 +0000

    Add package mtmai/0.3.915/mtmai-0.3.915-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.915-py3-none-any.whl/mtmai/models/dash.py
@@ -0,0 +1,41 @@
+"""
+为前端，以 openapi 的方式生成类型
+
+"""
+
+from pydantic import BaseModel
+
+
+class DashNavItem(BaseModel):
+    """
+    菜单导航
+    目前 以列表的方式表示，
+    以后升级为树
+    """
+
+    label: str | None = None
+    icon: str | None = None
+    url: str | None = None
+
+
+class DashConfig(BaseModel):
+    """管理面板的后台配置"""
+
+    logo: str | None = None
+    pathPrefix: str | None = "/"
+    navMenus: list[DashNavItem] | None = []
+    loginUrl: str = "/auth/login"
+    theme: str | None = "light"
+    layout: str | None = "default"
+
+
+class CmdkItem(BaseModel):
+    """
+    命令行菜单
+    """
+
+    label: str | None = None
+    icon: str | None = None
+    url: str | None = (
+        None  # 用url的方式表示命令名称及参数, 例如： mtmai://local.mtmai.cmd?file_path=/data/data.json
+    )
