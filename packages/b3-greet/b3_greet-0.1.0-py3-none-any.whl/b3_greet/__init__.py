8b91d2f05b18b9e5e95636020191b8086afc3687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 15:05:22 +0000

    Add package b3-greet/0.1.0/b3_greet-0.1.0-py3-none-any.whl

---
+++ packages/b3-greet/b3_greet-0.1.0-py3-none-any.whl/b3_greet/__init__.py
@@ -0,0 +1,11 @@
+"""Module for generating greeting messages."""
+
+
+def say_hello(name: str) -> str:
+    """
+    Returns a greeting message for the given name.
+
+    :param name: The name of the person to greet.
+    :return: A greeting string.
+    """
+    return f"Hello, {name}!"
