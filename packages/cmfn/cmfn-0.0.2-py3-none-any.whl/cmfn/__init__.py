5986dbae06b2fbff0b8bd636b865606e679297b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 05:04:48 +0000

    Add package cmfn/0.0.2/cmfn-0.0.2-py3-none-any.whl

---
+++ packages/cmfn/cmfn-0.0.2-py3-none-any.whl/cmfn/__init__.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2024-present Hao Wu <haowu@dataset.sh>
+#
+# SPDX-License-Identifier: MIT
+
+
+import os
+
+
+def listdir(folder):
+    return [os.path.join(folder, x) for x in os.listdir(folder)]
+
+def chunk_it(iterator, chunk_size = 50):
+    """
+    Yield successive chunks of a given size from an iterator.
+
+    :param iterator: The input iterator.
+    :param chunk_size: The number of items per chunk.
+    :yield: A list containing the chunk of items.
+    """
+    chunk = []
+    for item in iterator:
+        chunk.append(item)
+        if len(chunk) == chunk_size:
+            yield chunk
+            chunk = []
+    if chunk:
+        yield chunk  # Yield the last chunk if it's not empty
+
+def read_file(fn):
+    with open(fn) as fd:
+        return fd.read()
