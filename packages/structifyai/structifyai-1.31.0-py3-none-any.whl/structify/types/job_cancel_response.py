2c4326ce0505ff29a2ccd36b18e6bca8b90f42d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:49 +0000

    Add package structifyai/1.31.0/structifyai-1.31.0-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.31.0-py3-none-any.whl/structify/types/job_cancel_response.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["JobCancelResponse"]
+
+
+class JobCancelResponse(BaseModel):
+    id: str
+
+    creation_time: datetime
+
+    status: Literal["Queued", "Running", "Completed", "Failed"]
+
+    run_started_time: Optional[datetime] = None
+    """What time did the job start running?"""
