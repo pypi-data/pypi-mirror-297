fdd1140b5e3cc5a8f2cf3d41b0dc806d9bf8aa78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:35 +0000

    Add package HydrogenLib/0.0.1.21/hydrogenlib-0.0.1.21.tar.gz

---
+++ packages/HydrogenLib/hydrogenlib-0.0.1.21.tar.gz/hydrogenlib-0.0.1.21/src/HydrogenLib/BoardcastRoom/socket.py
@@ -0,0 +1,10 @@
+from ..Classes.MemberManager import Manager
+from ..SocketPlus import *
+
+
+class BoardcastServer:
+    def __init__(self, port):
+        self.port = port
+        self.sock = Socket()
+        self.member_manager = Manager()
+
