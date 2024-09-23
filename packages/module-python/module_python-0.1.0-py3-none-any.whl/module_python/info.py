b1af3e3390b0ccd78ee9fc9a7568d3d9216e8a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 04:06:17 +0000

    Add package module-python/0.1.0/module_python-0.1.0-py3-none-any.whl

---
+++ packages/module-python/module_python-0.1.0-py3-none-any.whl/module_python/info.py
@@ -0,0 +1,8 @@
+# my_python_package/info.py
+
+def display_info():
+    """
+    Prints the roll number and name of the student.
+    """
+    print("Roll Number: 123456")
+    print("Name: Himanshu Kumar Jha")
