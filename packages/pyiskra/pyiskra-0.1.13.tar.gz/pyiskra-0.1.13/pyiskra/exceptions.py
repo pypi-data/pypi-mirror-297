dac7e522e583552c67cdbd6d6f9262e3fc33b048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 17:04:53 +0000

    Add package pyiskra/0.1.13/pyiskra-0.1.13.tar.gz

---
+++ packages/pyiskra/pyiskra-0.1.13.tar.gz/pyiskra-0.1.13/pyiskra/exceptions.py
@@ -0,0 +1,25 @@
+# Description: Custom exceptions for pyiskra
+
+
+class NotAuthorised(Exception):
+    pass
+
+
+class DeviceNotSupported(Exception):
+    pass
+
+
+class ProtocolNotSupported(Exception):
+    pass
+
+
+class DeviceConnectionError(Exception):
+    pass
+
+
+class DeviceTimeoutError(Exception):
+    pass
+
+
+class InvalidResponseCode(Exception):
+    pass
