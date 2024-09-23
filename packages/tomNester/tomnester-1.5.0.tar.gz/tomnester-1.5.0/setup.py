8296b2bcb53236acfe93daa33eb89ef31d04408d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:51 +0000

    Add package tomNester/1.5.0/tomnester-1.5.0.tar.gz

---
+++ packages/tomNester/tomnester-1.5.0.tar.gz/tomnester-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tomNester',
+    version='1.5.0',
+    py_modules=['tomNester'],
+    author='tom',
+    author_email='m18091286648@163.com',
+    url='https://github.com/tom/nester123',
+    description='tomNester',
+)
