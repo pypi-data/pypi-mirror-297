dd2aa6bb31b9ca414e10d9855d2560d955644636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:34 +0000

    Add package dashai-translation-plugin/0.0.1/dashai_translation_plugin-0.0.1-py3-none-any.whl

---
+++ packages/dashai-translation-plugin/dashai_translation_plugin-0.0.1-py3-none-any.whl/dashai_translation_plugin/translation_task.py
@@ -0,0 +1,43 @@
+"""DashAI Translation Task."""
+
+from typing import List
+
+from datasets import DatasetDict, Sequence, Value
+
+from DashAI.back.tasks.base_task import BaseTask
+
+
+class TranslationTask(BaseTask):
+    """Base class for translation task."""
+
+    metadata: dict = {
+        "inputs_types": [Value, Sequence],
+        "outputs_types": [Value, Sequence],
+        "inputs_cardinality": 1,
+        "outputs_cardinality": 1,
+    }
+
+    DESCRIPTION: str = """
+    The translation task is natural language processing (NLP) task that involves
+    converting text or speech from one language into another language while
+    preserving the meaning and context.
+    """
+
+    def prepare_for_task(
+        self, datasetdict: DatasetDict, outputs_columns: List[str]
+    ) -> DatasetDict:
+        """Change the column types to suit the translation task.
+
+        A copy of the dataset is created.
+
+        Parameters
+        ----------
+        datasetdict : DatasetDict
+            Dataset to be changed
+
+        Returns
+        -------
+        DatasetDict
+            Dataset with the new types
+        """
+        return datasetdict
