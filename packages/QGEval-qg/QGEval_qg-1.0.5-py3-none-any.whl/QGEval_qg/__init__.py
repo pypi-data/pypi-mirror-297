2e6a599ec3d0d13719640f5f5c40e5b1f29975b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 04:06:28 +0000

    Add package QGEval-qg/1.0.5/QGEval_qg-1.0.5-py3-none-any.whl

---
+++ packages/QGEval-qg/QGEval_qg-1.0.5-py3-none-any.whl/QGEval_qg/__init__.py
@@ -0,0 +1,13 @@
+# QGEval/__init__.py  
+  
+__version__ = '1.0.0'  
+  
+__doc__ = """  
+QGEval 
+============  
+  
+This is a QGEval package. It includes two modules: qg and metrics.  
+  
+"""
+
+from QGEval_qg import *
