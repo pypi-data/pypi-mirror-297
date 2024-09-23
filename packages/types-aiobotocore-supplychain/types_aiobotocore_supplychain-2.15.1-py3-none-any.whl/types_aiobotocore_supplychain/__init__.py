1a64f09426fe155cfafbff5a41257eb6bd94015f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 16:05:10 +0000

    Add package types-aiobotocore-supplychain/2.15.1/types_aiobotocore_supplychain-2.15.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-supplychain/types_aiobotocore_supplychain-2.15.1-py3-none-any.whl/types_aiobotocore_supplychain/__init__.py
@@ -0,0 +1,26 @@
+"""
+Main interface for supplychain service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_supplychain import (
+        Client,
+        SupplyChainClient,
+    )
+
+    session = get_session()
+    async with session.create_client("supplychain") as client:
+        client: SupplyChainClient
+        ...
+
+    ```
+"""
+
+from .client import SupplyChainClient
+
+Client = SupplyChainClient
+
+
+__all__ = ("Client", "SupplyChainClient")
