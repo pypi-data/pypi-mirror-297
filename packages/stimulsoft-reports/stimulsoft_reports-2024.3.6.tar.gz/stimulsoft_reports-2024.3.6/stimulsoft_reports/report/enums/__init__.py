19119af06d540626d27bef52d5f5b12f05490fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 15:05:54 +0000

    Add package stimulsoft-reports/2024.3.6/stimulsoft_reports-2024.3.6.tar.gz

---
+++ packages/stimulsoft-reports/stimulsoft_reports-2024.3.6.tar.gz/stimulsoft_reports-2024.3.6/stimulsoft_reports/report/enums/__init__.py
@@ -0,0 +1,4 @@
+from ...export.enums.StiExportFormat import StiExportFormat
+from .StiEngineType import StiEngineType
+from .StiRangeType import StiRangeType
+from .StiVariableType import StiVariableType
