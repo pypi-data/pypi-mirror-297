c769e2d7822910b249152e9b61f474d8a34f1c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 15:05:39 +0000

    Add package coba-aansyawaluddinn/0.0.1/coba_aansyawaluddinn-0.0.1.tar.gz

---
+++ packages/coba-aansyawaluddinn/coba_aansyawaluddinn-0.0.1.tar.gz/coba_aansyawaluddinn-0.0.1/Km2/__init__.py
@@ -0,0 +1,8 @@
+def meters_to_kilometers(meters):
+    return meters / 1000
+
+def kilometers_to_meters(kilometers):
+    return kilometers * 1000
+
+def feet_to_meters(feet):
+    return feet * 0.3048
