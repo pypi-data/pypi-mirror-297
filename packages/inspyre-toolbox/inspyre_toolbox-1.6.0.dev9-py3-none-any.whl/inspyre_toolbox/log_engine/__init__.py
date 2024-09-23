a7d25036a48ac014b223537f13190e2672b09ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:09:17 +0000

    Add package inspyre-toolbox/1.6.0.dev9/inspyre_toolbox-1.6.0.dev9-py3-none-any.whl

---
+++ packages/inspyre-toolbox/inspyre_toolbox-1.6.0.dev9-py3-none-any.whl/inspyre_toolbox/log_engine/__init__.py
@@ -0,0 +1,7 @@
+from inspy_logger import InspyLogger
+
+from inspyre_toolbox.settings import log_level as INSPY_LOG_LEVEL
+
+ROOT_LOGGER = InspyLogger('InspyreToolbox', console_level=INSPY_LOG_LEVEL)
+
+ROOT_LOGGER.debug('Module loaded: log_engine')
