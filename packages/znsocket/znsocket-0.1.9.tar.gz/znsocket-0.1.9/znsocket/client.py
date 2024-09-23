c37af672a71436be44ebc144b8834fef0b1e0333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 04:05:47 +0000

    Add package znsocket/0.1.9/znsocket-0.1.9.tar.gz

---
+++ packages/znsocket/znsocket-0.1.9.tar.gz/znsocket-0.1.9/znsocket/client.py
@@ -0,0 +1,137 @@
+import dataclasses
+
+import socketio.exceptions
+import typing_extensions as tyex
+
+from znsocket import exceptions
+
+
+@dataclasses.dataclass(frozen=True)
+class Client:
+    address: str
+    decode_responses: bool = True
+    sio: socketio.SimpleClient = dataclasses.field(
+        default_factory=socketio.SimpleClient, repr=False, init=False
+    )
+
+    @classmethod
+    def from_url(cls, url, **kwargs) -> "Client":
+        """Connect to a znsocket server using a URL.
+
+        Parameters
+        ----------
+        url : str
+            The URL of the znsocket server. Should be in the format
+            "znsocket://127.0.0.1:5000".
+        """
+        return cls(address=url.replace("znsocket://", "http://"), **kwargs)
+
+    def __post_init__(self):
+        try:
+            self.sio.connect(self.address)
+        except socketio.exceptions.ConnectionError as err:
+            raise exceptions.ConnectionError(self.address) from err
+
+        if not self.decode_responses:
+            raise NotImplementedError("decode_responses=False is not supported yet")
+
+    def delete(self, name):
+        return self.sio.call("delete", {"name": name})
+
+    def hget(self, name, key):
+        return self.sio.call("hget", {"name": name, "key": key})
+
+    def hset(self, name, key=None, value=None, mapping=None):
+        if key is not None and value is None:
+            raise exceptions.DataError(f"Invalid input of type {type(value)}")
+        if (key is None or value is None) and mapping is None:
+            raise exceptions.DataError("'hset' with no key value pairs")
+        if mapping is None:
+            mapping = {key: value}
+        if len(mapping) == 0:
+            raise exceptions.DataError("Mapping must not be empty")
+        return self.sio.call("hset", {"name": name, "mapping": mapping})
+
+    def hmget(self, name, keys):
+        return self.sio.call("hmget", {"name": name, "keys": keys})
+
+    def hkeys(self, name):
+        return self.sio.call("hkeys", {"name": name})
+
+    def exists(self, name):
+        return self.sio.call("exists", {"name": name})
+
+    def llen(self, name):
+        return self.sio.call("llen", {"name": name})
+
+    def rpush(self, name, value):
+        return self.sio.call("rpush", {"name": name, "value": value})
+
+    def lpush(self, name, value):
+        return self.sio.call("lpush", {"name": name, "value": value})
+
+    def lindex(self, name, index):
+        return self.sio.call("lindex", {"name": name, "index": index})
+
+    def set(self, name, value):
+        return self.sio.call("set", {"name": name, "value": value})
+
+    def get(self, name):
+        return self.sio.call("get", {"name": name})
+
+    def hgetall(self, name):
+        return self.sio.call("hgetall", {"name": name})
+
+    def smembers(self, name):
+        response = self.sio.call("smembers", {"name": name})
+        # check if response should raise an exception
+        if isinstance(response, dict) and "error" in response:
+            raise exceptions.ResponseError(response["error"])
+        return set(response)
+
+    def lrange(self, name, start, end):
+        return self.sio.call("lrange", {"name": name, "start": start, "end": end})
+
+    def lset(self, name, index, value):
+        response = self.sio.call("lset", {"name": name, "index": index, "value": value})
+        if response is not None:
+            raise exceptions.ResponseError(response)
+
+    def lrem(self, name: str, count: int, value: str):
+        return self.sio.call("lrem", {"name": name, "count": count, "value": value})
+
+    def sadd(self, name, value):
+        return self.sio.call("sadd", {"name": name, "value": value})
+
+    def srem(self, name, value):
+        return self.sio.call("srem", {"name": name, "value": value})
+
+    def linsert(self, name, where, pivot, value):
+        return self.sio.call(
+            "linsert", {"name": name, "where": where, "pivot": pivot, "value": value}
+        )
+
+    def flushall(self):
+        return self.sio.call("flushall", {})
+
+    def hexists(self, name, key):
+        return self.sio.call("hexists", {"name": name, "key": key})
+
+    def hdel(self, name, key):
+        return self.sio.call("hdel", {"name": name, "key": key})
+
+    def hlen(self, name):
+        return self.sio.call("hlen", {"name": name})
+
+    def hvals(self, name):
+        return self.sio.call("hvals", {"name": name})
+
+    def lpop(self, name):
+        return self.sio.call("lpop", {"name": name})
+
+    def scard(self, name):
+        return self.sio.call("scard", {"name": name})
+
+    @tyex.deprecated("hmset() is deprecated. Use hset() instead.")
+    def hmset(self, name, mapping):
+        return self.hset(name, mapping=mapping)
---
+++ packages/znsocket/znsocket-0.1.9.tar.gz/znsocket-0.1.9/znsocket/server.py
@@ -0,0 +1,306 @@
+import dataclasses
+import typing as t
+
+import eventlet.wsgi
+import socketio
+
+storage = {}
+
+
+@dataclasses.dataclass
+class Server:
+    port: int = 5000
+    max_http_buffer_size: t.Optional[int] = None
+    async_mode: t.Optional[str] = None
+
+    @classmethod
+    def from_url(cls, url: str, **kwargs) -> "Server":
+        # server url looks like "znsocket://127.0.0.1:5000"
+        if not url.startswith("znsocket://"):
+            raise ValueError("Invalid URL")
+        port = int(url.split(":")[-1])
+        return cls(port=port, **kwargs)
+
+    def run(self) -> None:
+        """Run the server (blocking)."""
+        sio = get_sio(
+            max_http_buffer_size=self.max_http_buffer_size,
+            async_mode=self.async_mode,
+        )
+        server_app = socketio.WSGIApp(sio)
+        eventlet.wsgi.server(eventlet.listen(("localhost", self.port)), server_app)
+
+
+def get_sio(
+    max_http_buffer_size: t.Optional[int] = None,
+    async_mode: t.Optional[str] = None,
+) -> socketio.Server:
+    kwargs = {}
+    if max_http_buffer_size is not None:
+        kwargs["max_http_buffer_size"] = max_http_buffer_size
+    if async_mode is not None:
+        kwargs["async_mode"] = async_mode
+    sio = socketio.Server(**kwargs)
+
+    @sio.event
+    def hset(sid, data):
+        name = data.pop("name")
+        mapping = data.pop("mapping")
+        for key, value in mapping.items():
+            try:
+                storage[name][key] = value
+            except KeyError:
+                storage[name] = {key: value}
+
+    @sio.event
+    def hget(sid, data):
+        name = data.pop("name")
+        key = data.pop("key")
+        try:
+            return storage[name][key]
+        except KeyError:
+            return None
+
+    @sio.event
+    def hmget(sid, data):
+        name = data.pop("name")
+        keys = data.pop("keys")
+        response = []
+        for key in keys:
+            try:
+                response.append(storage[name][key])
+            except KeyError:
+                response.append(None)
+        return response
+
+    @sio.event
+    def hkeys(sid, data):
+        name = data.pop("name")
+        try:
+            return list(storage[name].keys())
+        except KeyError:
+            return []
+
+    @sio.event
+    def delete(sid, data):
+        name = data.pop("name")
+        try:
+            del storage[name]
+            return 1
+        except KeyError:
+            return 0
+
+    @sio.event
+    def exists(sid, data):
+        name = data.pop("name")
+        return 1 if name in storage else 0
+
+    @sio.event
+    def llen(sid, data):
+        name = data.pop("name")
+        try:
+            return len(storage[name])
+        except KeyError:
+            return 0
+
+    @sio.event
+    def rpush(sid, data) -> int:
+        name = data.pop("name")
+        value = data.pop("value")
+        try:
+            storage[name].append(value)
+        except KeyError:
+            storage[name] = [value]
+
+        return len(storage[name])
+
+    @sio.event
+    def lpush(sid, data):
+        name = data.pop("name")
+        value = data.pop("value")
+        try:
+            storage[name].insert(0, value)
+        except KeyError:
+            storage[name] = [value]
+
+    @sio.event
+    def lindex(sid, data):
+        name = data.pop("name")
+        index = data.pop("index")
+        try:
+            return storage[name][index]
+        except KeyError:
+            return None
+        except IndexError:
+            return None
+
+    @sio.on("set")
+    def set_(sid, data):
+        name = data.pop("name")
+        value = data.pop("value")
+        storage[name] = value
+
+    @sio.event
+    def get(sid, data):
+        name = data.pop("name")
+        return storage.get(name)
+
+    @sio.event
+    def hgetall(sid, data):
+        name = data.pop("name")
+        return storage.get(name, {})
+
+    @sio.event
+    def smembers(sid, data):
+        name = data.pop("name")
+        try:
+            response = storage[name]
+        except KeyError:
+            response = set()
+
+        if not isinstance(response, set):
+            return {
+                "error": "WRONGTYPE Operation against a key holding the wrong kind of value"
+            }
+        return list(response)
+
+    @sio.event
+    def lrange(sid, data):
+        name = data.pop("name")
+        start = data.pop("start")
+        end = data.pop("end")
+        if end == -1:
+            end = None
+        elif end >= 0:
+            end += 1
+        try:
+            return storage[name][start:end]
+        except KeyError:
+            return []
+
+    @sio.event
+    def lset(sid, data):
+        name = data.pop("name")
+        index = data.pop("index")
+        value = data.pop("value")
+        try:
+            storage[name][index] = value
+        except KeyError:
+            return "no such key"
+        except IndexError:
+            return "index out of range"
+
+    @sio.event
+    def lrem(sid, data):
+        name = data.pop("name")
+        count = data.pop("count")
+        value = data.pop("value")
+
+        if count == 0:
+            try:
+                storage[name] = [x for x in storage[name] if x != value]
+            except KeyError:
+                return 0
+        else:
+            removed = 0
+            while removed < count:
+                try:
+                    storage[name].remove(value)
+                    removed += 1
+                except KeyError:
+                    return 0
+
+    @sio.event
+    def sadd(sid, data):
+        name = data.pop("name")
+        value = data.pop("value")
+        try:
+            storage[name].add(value)
+        except KeyError:
+            storage[name] = {value}
+
+    @sio.event
+    def flushall(sid, data):
+        storage.clear()
+
+    @sio.event
+    def srem(sid, data):
+        name = data.pop("name")
+        value = data.pop("value")
+        try:
+            storage[name].remove(value)
+            return 1
+        except KeyError:
+            return 0
+
+    @sio.event
+    def linsert(sid, data):
+        name = data.pop("name")
+        where = data.pop("where")
+        pivot = data.pop("pivot")
+        value = data.pop("value")
+        try:
+            index = storage[name].index(pivot)
+            if where == "BEFORE":
+                storage[name].insert(index, value)
+            elif where == "AFTER":
+                storage[name].insert(index + 1, value)
+        except KeyError:
+            return 0
+        except ValueError:
+            return -1
+
+    @sio.event
+    def hexists(sid, data):
+        name = data.pop("name")
+        key = data.pop("key")
+        try:
+            return 1 if key in storage[name] else 0
+        except KeyError:
+            return 0
+
+    @sio.event
+    def hdel(sid, data):
+        name = data.pop("name")
+        key = data.pop("key")
+        try:
+            del storage[name][key]
+            return 1
+        except KeyError:
+            return 0
+
+    @sio.event
+    def hlen(sid, data):
+        name = data.pop("name")
+        try:
+            return len(storage[name])
+        except KeyError:
+            return 0
+
+    @sio.event
+    def hvals(sid, data):
+        name = data.pop("name")
+        try:
+            return list(storage[name].values())
+        except KeyError:
+            return []
+
+    @sio.event
+    def lpop(sid, data) -> t.Optional[t.Any]:
+        name = data.pop("name")
+        try:
+            return storage[name].pop(0)
+        except KeyError:
+            return None
+        except IndexError:
+            return None
+
+    @sio.event
+    def scard(sid, data) -> int:
+        name = data.pop("name")
+        try:
+            return len(storage[name])
+        except KeyError:
+            return 0
+
+    return sio
