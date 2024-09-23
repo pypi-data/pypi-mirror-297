9c57e20a71fe6209322cf74c7bf8e52c768723a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 15:05:21 +0000

    Add package egeometry/0.7.0/egeometry-0.7.0.tar.gz

---
+++ packages/egeometry/egeometry-0.7.0.tar.gz/egeometry-0.7.0/src/egeometry/__init__.py
@@ -0,0 +1,58 @@
+# generated from codegen/templates/__init__.py
+
+from __future__ import annotations
+
+__all__ = [
+    "separating_axis_theorem",
+    "DCircle",
+    "DCircleOverlappable",
+    "FCircle",
+    "FCircleOverlappable",
+    "ICircle",
+    "ICircleOverlappable",
+    "DBoundingBox2d",
+    "DBoundingBox2dOverlappable",
+    "FBoundingBox2d",
+    "FBoundingBox2dOverlappable",
+    "IBoundingBox2d",
+    "IBoundingBox2dOverlappable",
+    "DTriangle2d",
+    "DTriangle2dOverlappable",
+    "FTriangle2d",
+    "FTriangle2dOverlappable",
+    "ITriangle2d",
+    "ITriangle2dOverlappable",
+    "DRectangle",
+    "DRectangleOverlappable",
+    "FRectangle",
+    "FRectangleOverlappable",
+    "IRectangle",
+    "IRectangleOverlappable",
+]
+
+# egeometry
+from ._dboundingbox2d import DBoundingBox2d
+from ._dboundingbox2d import DBoundingBox2dOverlappable
+from ._dcircle import DCircle
+from ._dcircle import DCircleOverlappable
+from ._drectangle import DRectangle
+from ._drectangle import DRectangleOverlappable
+from ._dtriangle2d import DTriangle2d
+from ._dtriangle2d import DTriangle2dOverlappable
+from ._fboundingbox2d import FBoundingBox2d
+from ._fboundingbox2d import FBoundingBox2dOverlappable
+from ._fcircle import FCircle
+from ._fcircle import FCircleOverlappable
+from ._frectangle import FRectangle
+from ._frectangle import FRectangleOverlappable
+from ._ftriangle2d import FTriangle2d
+from ._ftriangle2d import FTriangle2dOverlappable
+from ._iboundingbox2d import IBoundingBox2d
+from ._iboundingbox2d import IBoundingBox2dOverlappable
+from ._icircle import ICircle
+from ._icircle import ICircleOverlappable
+from ._irectangle import IRectangle
+from ._irectangle import IRectangleOverlappable
+from ._itriangle2d import ITriangle2d
+from ._itriangle2d import ITriangle2dOverlappable
+from ._separating_axis_theorem import separating_axis_theorem
