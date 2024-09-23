ca195ec5db04240230a1213e0bf81a9b5b349f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 04:06:08 +0000

    Add package biliass/2.0.0a1/biliass-2.0.0a1-cp39-abi3-manylinux_2_5_i686.manylinux1_i686.whl

---
+++ packages/biliass/biliass-2.0.0a1-cp39-abi3-manylinux_2_5_i686.manylinux1_i686.whl/biliass/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from .biliass import (
+    Danmaku2ASS as Danmaku2ASS,
+    read_comments_bilibili_protobuf as read_comments_bilibili_protobuf,
+    read_comments_bilibili_xml as read_comments_bilibili_xml,
+)
