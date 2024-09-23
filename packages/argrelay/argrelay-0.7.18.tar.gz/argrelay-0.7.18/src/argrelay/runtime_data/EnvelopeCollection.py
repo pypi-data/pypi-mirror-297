acf440bb8c2cb396a08fb193c34e7cdc5d6bff46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:38 +0000

    Add package argrelay/0.7.18/argrelay-0.7.18.tar.gz

---
+++ packages/argrelay/argrelay-0.7.18.tar.gz/argrelay-0.7.18/src/argrelay/runtime_data/EnvelopeCollection.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+
+
+@dataclass(frozen = True)
+class EnvelopeCollection:
+    """
+    Allows distributing `data_envelope`-s across FS_56_43_05_79 different collections.
+
+    Specifies collection of `data_envelope`-s to load and associated `index_props` to create.
+
+    See also `EnvelopeCollectionSchema`.
+    """
+
+    index_props: list[str] = field()
+    """
+    Lists fields of `data_envelop` which are to be indexed by MongoDB for given collection.
+    """
+
+    data_envelopes: list = field()
