60a0a26f14e0cfc78a26d04c279ec9b89d21cfda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:34 +0000

    Add package tomtom-apis/0.0.1a0/tomtom_apis-0.0.1a0.tar.gz

---
+++ packages/tomtom-apis/tomtom_apis-0.0.1a0.tar.gz/tomtom_apis-0.0.1a0/src/tomtom_apis/places/ev_search.py
@@ -0,0 +1,61 @@
+"""EV Search API."""
+
+from ..api import BaseApi
+from .models import EvSearchByIdParams, EvSearchNearbyParams, SearchResponse
+
+
+class EVSearchApi(BaseApi):
+    """EV Search API.
+
+    EV Search is a REST API limited to and optimized for electric vehicle station POI category. It provides complete EV POI data including static
+    location information (lat/long), address, opening hours, access restrictions, technical specs of the charging station (connector type, voltage,
+    power, current, current type), etc. as well as dynamic availability status. Wide range of EV specific filters (including dynamic availability)
+    allows for narrowing the search results to match the personal preference (like charging power or access type) of the driver or the technical
+    specification of the electric vehicle (connector type).
+
+    For more information, see: https://developer.tomtom.com/ev-search-api/documentation/product-information/introduction
+    """
+
+    async def get_ev_search_nearby(
+        self,
+        *,
+        params: EvSearchNearbyParams | None = None,
+    ) -> SearchResponse:
+        """Get EV search nearby.
+
+        For more information, see: https://developer.tomtom.com/ev-search-api/documentation/ev-search-api/ev-search-nearby
+
+        Args:
+            params (EvSearchNearbyParams | None, optional): Additional parameters for the request. Defaults to None.
+
+        Returns:
+            SearchResponse: Response containing search results.
+        """
+        response = await self.get(
+            endpoint="/search/2/evsearch",
+            params=params,
+        )
+
+        return await response.deserialize(SearchResponse)
+
+    async def get_ev_search_by_id(
+        self,
+        *,
+        params: EvSearchByIdParams | None = None,
+    ) -> SearchResponse:
+        """Get EV search by id.
+
+        For more information, see: https://developer.tomtom.com/ev-search-api/documentation/ev-search-api/ev-search-by-id
+
+        Args:
+            params (EvSearchByIdParams | None, optional): Additional parameters for the request. Defaults to None.
+
+        Returns:
+            SearchResponse: Response containing search results.
+        """
+        response = await self.get(
+            endpoint="/search/2/evbyid",
+            params=params,
+        )
+
+        return await response.deserialize(SearchResponse)
---
+++ packages/tomtom-apis/tomtom_apis-0.0.1a0.tar.gz/tomtom_apis-0.0.1a0/src/tomtom_apis/traffic/od_analytics.py
@@ -0,0 +1,22 @@
+"""O/D Analysis API."""
+
+from ..api import BaseApi
+
+
+class ODAnalysisApi(BaseApi):
+    """O/D Analysis API.
+
+    O/D Analysis is a suite of web services designed for developers to create web applications which analyze historical traffic data. These web
+    services are RESTful APIs. The O/D Analysis API services are based on the collection of Floating Car Data (FCD), which is a proven and innovative
+    method of measuring what is happening on the road.
+
+    For more information, see: https://developer.tomtom.com/od-analysis/documentation/product-information/introduction
+    """
+
+    def __init__(self):  # pylint: disable=super-init-not-called
+        """Not implemented.
+
+        Raises:
+            NotImplementedError: Always.
+        """
+        raise NotImplementedError
