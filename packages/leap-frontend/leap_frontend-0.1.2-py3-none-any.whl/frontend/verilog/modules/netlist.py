87a38a8a3e45aa3e1121fc3afb4139aa61608dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 15:05:18 +0000

    Add package leap-frontend/0.1.2/leap_frontend-0.1.2-py3-none-any.whl

---
+++ packages/leap-frontend/leap_frontend-0.1.2-py3-none-any.whl/frontend/verilog/modules/netlist.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+"""
+Author: Hanyu Wang
+Created time: 2024-06-25 17:11:35
+Last Modified by: Hanyu Wang
+Last Modified time: 2024-06-25 22:59:35
+"""
+
+from .module import Module
+from .definitions import *
+
+
+class Netlist(Macros, TimeScale):
+    """
+    A netlist is a collection of modules
+
+    :param modules: a list of modules
+    :type modules: list
+    :param definitions: a dictionary of definitions
+    :type definitions: dict
+    """
+
+    def __init__(self):
+        Macros.__init__(self)
+        TimeScale.__init__(self)
+        self.modules = {}
+
+    def addModule(self, module: Module):
+        self.modules[module.getName()] = module
+
+    def __repr__(self):
+        return "Netlist({})".format(self.modules)
+
+    def getModule(self, name: str) -> Module:
+        return self.modules.get(name, None)
+
+    def getModules(self) -> list:
+        return list(self.modules.keys())
+
+    def getModuleAt(self, index: int) -> Module:
+        # Especially useful for netlist with only one module
+        return self.getModule(self.getModules()[index])
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Netlist):
+            return False
+        if len(self.getModules()) != len(other.getModules()):
+            print(
+                f"Number of modules are not equal: {len(self.getModules())} != {len(other.getModules())}"
+            )
+            return False
+        # compare modules
+        for module in self.getModules():
+            if module not in other.getModules():
+                print(f"Module {module} not found in other")
+                print(f"Modules in self: {self.getModules()}")
+                print(f"Modules in other: {other.getModules()}")
+                return False
+            if self.getModule(module) != other.getModule(module):
+                print(
+                    f"Module {module} in self is not equal to module {module} in other"
+                )
+                return False
+
+        # compare parameters
+        return super().__eq__(other)
+
+    def __ne__(self, value: object) -> bool:
+        return not self.__eq__(value)
---
+++ packages/leap-frontend/leap_frontend-0.1.2-py3-none-any.whl/frontend/verilog/modules/statement.py
@@ -0,0 +1,164 @@
+from frontend.verilog.modules.dfg import Any
+from frontend.verilog.modules.module import Any, Module
+from .dfg import *
+from .module import *
+from .moduleInst import *
+from .netlist import *
+from .definitions import *
+
+
+class Statement:
+    """
+    Statement is the base class for all statements in the module.
+    It is a callable object that can be called with a module object to add the statement to the module.
+    """
+
+    def __init__(self) -> None:
+        self.condition: DFGNode = None
+        self.event: DFGNode = None
+
+    def setCondition(self, condition):
+        self.condition = condition
+
+    def addCondition(self, condition: DFGNode):
+        if condition is None:
+            return
+        if self.condition is None:
+            self.condition = condition
+        else:
+            self.condition = OPNode("&&", OPType.BINARY_AND, self.condition, condition)
+        return self
+
+    def setEvent(self, event: DFGNode):
+        self.event = event
+        return self
+
+    def __call__(self, module: Module) -> Any:
+        raise NotImplementedError
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Statement):
+            print(f"Type mismatch: {type(value)}")
+            return False
+        if self.condition is None and value.condition is not None:
+            print(f"Condition mismatch: {self.condition} != {value.condition}")
+            return False
+        if self.condition != value.condition:
+            print(f"Condition mismatch: {self.condition} != {value.condition}")
+            return False
+        if self.event is None and value.event is not None:
+            print(f"Event mismatch: {self.event} != {value.event}")
+            return False
+        if self.event != value.event:
+            print(f"Event mismatch: {self.event} != {value.event}")
+            return False
+        return True
+
+
+class ModuleInstantiation(Statement):
+    def __init__(self, inst: ModuleInst) -> None:
+        super().__init__()
+        self.inst = inst
+
+    def __call__(self, module: Module) -> Any:
+        module.addInstance(self.inst)
+
+    def __repr__(self) -> str:
+        return f"ModuleInstantiation({self.inst})"
+
+
+class PortDeclaration(Statement):
+    def __init__(self, ports: list) -> None:
+        super().__init__()
+        self.ports = ports
+
+    def __repr__(self) -> str:
+        return f"PortDeclaration({self.ports})"
+
+    def __call__(self, module: Module) -> Any:
+        module.addPorts(self.ports)
+
+
+class MacroDefinition(Statement):
+    def __init__(self, name: str, value: DFGNode) -> None:
+        super().__init__()
+        self.name = name
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f"MacroDefinition({self.name} = {self.value})"
+
+    def __call__(self, module: Module) -> Any:
+        module.addMacro(self.name, self.value)
+
+
+class TimeScaleDefinition(Statement):
+    def __init__(self, val1: TimeValue, val2: TimeValue) -> None:
+        super().__init__()
+        self.val1 = val1
+        self.val2 = val2
+
+    def __repr__(self) -> str:
+        return f"TimeScaleDefinition({self.val1} {self.val2})"
+
+    def __call__(self, netlist: Netlist) -> Any:
+        netlist.setTimescale(self.val1, self.val2)
+
+
+class Assignment(Statement):
+    def __init__(
+        self, target: DFGNode, expression: DFGNode, condition: DFGNode = None
+    ) -> None:
+        super().__init__()
+        self.target: DFGNode = target
+        self.expression: DFGNode = expression
+        self.type = None
+        self.isBlocking = True
+
+    def __repr__(self) -> str:
+        retString = ""
+        if self.isBlocking:
+            retString += f"{self.target} = {self.expression}"
+        else:
+            retString += f"{self.target} <= {self.expression} ({self.condition})"
+        return retString
+
+    def replaceVariable(self, old: str, new: str):
+        self.target.replaceVariable(old, new)
+        self.expression.replaceVariable(old, new)
+        if self.condition is not None:
+            self.condition.replaceVariable(old, new)
+
+    def __call__(self, module: Module) -> Any:
+        return module.addAssignment(self)
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Assignment):
+            print(f"Type mismatch: {type(value)}")
+            return False
+        if self.target != value.target:
+            print(f"Target mismatch: {self.target} != {value.target}")
+            return False
+        if self.expression != value.expression:
+            print(f"Expression mismatch: {self.expression} != {value.expression}")
+            return False
+        if self.isBlocking != value.isBlocking:
+            print(f"Blocking mismatch: {self.isBlocking} != {value.isBlocking}")
+            return False
+        return super().__eq__(value)
+
+
+class BlockingAssignment(Assignment):
+    def __init__(
+        self, target: DFGNode, expression: DFGNode, condition: DFGNode = None
+    ) -> None:
+        super().__init__(target, expression, condition)
+        self.isBlocking = True
+
+
+class NonBlockingAssignment(Assignment):
+    def __init__(
+        self, target: DFGNode, expression: DFGNode, condition: DFGNode = None
+    ) -> None:
+        super().__init__(target, expression, condition)
+        self.isBlocking = False
