d2483edf7966d8bdaf1826c6dc254be93cea6267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 16:05:02 +0000

    Add package hashcons/0.1.0/hashcons-0.1.0-py3-none-any.whl

---
+++ packages/hashcons/hashcons-0.1.0-py3-none-any.whl/hashcons/__init__.py
@@ -0,0 +1,27 @@
+"""
+Simple implementation of flyweight instance management using hash consing.
+"""
+
+# HashCons: hash consing for flyweight classes
+# Copyright (C) 2024 Hashberg Ltd
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+# USA
+
+from .instance_store import InstanceStore
+
+__all__ = (
+    "InstanceStore",
+)
