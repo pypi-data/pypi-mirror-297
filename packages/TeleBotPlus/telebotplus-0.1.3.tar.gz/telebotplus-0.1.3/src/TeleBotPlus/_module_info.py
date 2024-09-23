0b1d5ab521e7c45d95cb6a9e3c12a448659883b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:47 +0000

    Add package TeleBotPlus/0.1.3/telebotplus-0.1.3.tar.gz

---
+++ packages/TeleBotPlus/telebotplus-0.1.3.tar.gz/telebotplus-0.1.3/src/TeleBotPlus/_module_info.py
@@ -0,0 +1,2 @@
+name = "TeleBotPlus"
+version = "0.1.3"
---
+++ packages/TeleBotPlus/telebotplus-0.1.3.tar.gz/telebotplus-0.1.3/src/TeleBotPlus/assets_converter.py
@@ -0,0 +1,5 @@
+def f0to1(index: dict) -> dict:
+  new_index = {}
+  new_index["format"] = 1
+  new_index["objects"] = index
+  return new_index
