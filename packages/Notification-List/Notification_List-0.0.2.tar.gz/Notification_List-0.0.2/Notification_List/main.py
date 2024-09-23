5c888d5f1d4ed27acd8c4433bfb202eb1e5ee211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:53 +0000

    Add package Notification-List/0.0.2/Notification_List-0.0.2.tar.gz

---
+++ packages/Notification-List/Notification_List-0.0.2.tar.gz/Notification_List-0.0.2/Notification_List/main.py
@@ -0,0 +1,11 @@
+def hello():
+    print("Hello, Ranjeet!")
+
+def unqid():
+    print("Hello, Ranjeet! Unique IDs are generated.")
+
+def merging():
+    print("Hello, Ranjeet! merged files are created.")
+
+def genratenl():
+    print("Hello, Ranjeet! Generated notification list.")
---
+++ packages/Notification-List/Notification_List-0.0.2.tar.gz/Notification_List-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from typing import List
+
+trigger_mode = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [r.replace("\n", "") for r in requirements]
+        if trigger_mode in requirements:
+            requirements.remove(trigger_mode)
+
+    return requirements
+
+
+setup(
+
+    name = 'Notification_List',
+    version= '0.0.2',
+    author= 'Ranjeet Aloriya',
+    author_email= 'ranjeet.aloriya@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+    
+)
