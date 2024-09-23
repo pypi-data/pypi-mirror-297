bdff9b9e7db099772e899f2568d60e77ec6b3055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:36 +0000

    Add package discord-advert-framework/4.1.0/discord_advert_framework-4.1.0-py3-none-any.whl

---
+++ packages/discord-advert-framework/discord_advert_framework-4.1.0-py3-none-any.whl/daf/__init__.py
@@ -0,0 +1,33 @@
+"""
+Discord Advertisement Framework
+"""
+import _discord as discord
+
+from . import misc
+from . import events
+
+from .client import *
+from .core import *
+from .dtypes import *
+from .guild import *
+from .message import *
+from .logging import *
+from .web import *
+from .convert import *
+from .remote import *
+from .responder import *
+from .messagedata import *
+
+import sys
+import warnings
+
+
+VERSION = "4.1.0"
+
+
+if sys.version_info.minor == 12 and sys.version_info.major == 3:
+    warnings.warn(
+        "DAF's support on Python 3.12 is limited. Web browser features and"
+        " SQL logging are not supported in Python 3.12. Please install Python 3.11 instead."
+        " Additional GUI may be unstable on Python 3.12"
+    )
