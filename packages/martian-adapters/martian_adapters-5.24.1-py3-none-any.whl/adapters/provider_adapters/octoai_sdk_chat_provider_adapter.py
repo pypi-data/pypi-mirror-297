4e583f796d4c4bf86f23403d260a9760ae4872a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:49 +0000

    Add package martian-adapters/5.24.1/martian_adapters-5.24.1-py3-none-any.whl

---
+++ packages/martian-adapters/martian_adapters-5.24.1-py3-none-any.whl/adapters/provider_adapters/octoai_sdk_chat_provider_adapter.py
@@ -0,0 +1,67 @@
+import re
+from typing import Pattern
+
+from adapters.abstract_adapters.openai_sdk_chat_adapter import OpenAISDKChatAdapter
+from adapters.abstract_adapters.provider_adapter_mixin import ProviderAdapterMixin
+from adapters.types import Cost, Model, ModelPredicates
+
+PROVIDER_NAME = "octoai"
+BASE_URL = "https://text.octoai.run/v1"
+API_KEY_NAME = "OCTOAI_API_KEY"
+API_KEY_PATTERN = re.compile(r".*")
+BASE_PREDICATES = ModelPredicates(open_source=True)
+
+
+class OctoaiModel(Model):
+    supports_streaming: bool = True
+    provider_name: str = PROVIDER_NAME
+    predicates: ModelPredicates = BASE_PREDICATES
+
+
+MODELS = [
+    OctoaiModel(
+        name="meta-llama-3-8b-instruct",
+        cost=Cost(prompt=0.1e-6, completion=0.25e-6),
+        context_length=8192,
+        vendor_name="meta-llama",
+    ),
+    OctoaiModel(
+        name="meta-llama-3.1-405b-instruct",
+        cost=Cost(prompt=3.0e-6, completion=9.0e-6),
+        context_length=128000,
+        vendor_name="meta-llama",
+    ),
+    OctoaiModel(
+        name="meta-llama-3.1-70b-instruct",
+        cost=Cost(prompt=0.9e-6, completion=0.9e-6),
+        context_length=131072,
+        vendor_name="meta-llama",
+    ),
+    OctoaiModel(
+        name="meta-llama-3.1-8b-instruct",
+        cost=Cost(prompt=0.15e-6, completion=0.15e-6),
+        context_length=131072,
+        vendor_name="meta-llama",
+    ),
+]
+
+
+class OctoaiSDKChatProviderAdapter(ProviderAdapterMixin, OpenAISDKChatAdapter):
+    @staticmethod
+    def get_supported_models():
+        return MODELS
+
+    @staticmethod
+    def get_provider_name() -> str:
+        return PROVIDER_NAME
+
+    def get_base_sdk_url(self) -> str:
+        return BASE_URL
+
+    @staticmethod
+    def get_api_key_name() -> str:
+        return API_KEY_NAME
+
+    @staticmethod
+    def get_api_key_pattern() -> Pattern:
+        return API_KEY_PATTERN
