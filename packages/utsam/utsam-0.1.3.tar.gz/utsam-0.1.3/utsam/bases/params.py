8edb57599dc63753a1e67d4f1ed28bebfab0f6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 17:04:38 +0000

    Add package utsam/0.1.3/utsam-0.1.3.tar.gz

---
+++ packages/utsam/utsam-0.1.3.tar.gz/utsam-0.1.3/utsam/bases/params.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class SemesterParams():
+    spring: str = "SPR" 
+    autumn: str = "AUT"
+
+_SELECT_ = 'Select'
+
+_CACHE_ = 'cache'
+_LOGS_ = 'logs'
+_METADATA_ = 'metadata'
