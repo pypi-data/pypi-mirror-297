3d69d8d25b3ed07d9a81fd981ff0e2566071d3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:44 +0000

    Add package plytix-pim-client/0.0.24/plytix_pim_client-0.0.24-py3-none-any.whl

---
+++ packages/plytix-pim-client/plytix_pim_client-0.0.24-py3-none-any.whl/plytix_pim_client/client.py
@@ -0,0 +1,27 @@
+from plytix_pim_client.http.async_ import AsyncClient
+from plytix_pim_client.http.sync import SyncClient
+from plytix_pim_client.mixins import _ProductsAPISync, _ProductsAPIAsync
+
+
+class PlytixSync:
+    def __init__(self, api_key: str | None = None, api_password: str | None = None):
+        self._client = SyncClient(api_key, api_password)
+
+    def close(self):
+        self._client.close()
+
+    @property
+    def products(self) -> _ProductsAPISync:
+        return _ProductsAPISync(self._client)
+
+
+class PlytixAsync:
+    def __init__(self, api_key: str | None = None, api_password: str | None = None):
+        self._client = AsyncClient(api_key, api_password)
+
+    async def close(self):
+        await self._client.close()
+
+    @property
+    def products(self) -> _ProductsAPIAsync:
+        return _ProductsAPIAsync(self._client)
---
+++ packages/plytix-pim-client/plytix_pim_client-0.0.24-py3-none-any.whl/plytix_pim_client/dtos/filters.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from enum import StrEnum
+from typing import List
+
+from plytix_pim_client.dtos.base import BaseDTO
+
+
+class OperatorEnum(StrEnum):
+    EXISTS = "exists"
+    NOT_EXISTS = "!exists"
+    EQUALS = "eq"
+    NOT_EQUALS = "!eq"
+    CONTAINS = "like"
+    MATCHES = "in"
+    NOT_MATCHES = "!in"
+    GREATER_THAN = "gt"
+    GREATER_THAN_OR_EQUAL = "gte"
+    LESS_THAN = "lt"
+    LESS_THAN_OR_EQUAL = "lte"
+    TEXT_SEARCH = "text_search"
+    BETWEEN = "bte"
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class SearchFilter(BaseDTO):
+    field: str
+    operator: OperatorEnum
+    value: str | int | float | bool | None
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class ProductsRelationshipFilter(BaseDTO):
+    id: str
+    qty_operator: OperatorEnum
+    value: List[str | int | float | bool]
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
+class RelationshipSearchFilter(BaseDTO):
+    relationship_id: str
+    operator: OperatorEnum
+    product_ids: List[ProductsRelationshipFilter]
