dc12fc9e0008775ee79e11488615865212b703d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 03:08:54 +0000

    Add package knowledge-verificator/0.1.1/knowledge_verificator-0.1.1.tar.gz

---
+++ packages/knowledge-verificator/knowledge_verificator-0.1.1.tar.gz/knowledge_verificator-0.1.1/knowledge_verificator/io_handler.py
@@ -0,0 +1,52 @@
+"""
+Module handling Input/Output, including parsing CLI arguments, provding
+an instance of `rich` console, and an instance of a preconfigured `Logger`.
+"""
+
+import logging
+
+from argparse import ArgumentParser
+from logging import Logger
+from rich.console import Console
+from rich.logging import RichHandler
+
+
+def get_argument_parser() -> ArgumentParser:
+    """
+    Provide an instance of the CLI arguments paraser.
+
+    Returns:
+        ArgumentParser: Configured instance of argument parser.
+    """
+    arg_parser = ArgumentParser(
+        prog='knowledge_verificator',
+    )
+
+    arg_parser.add_argument(
+        '-d',
+        '--debug',
+        action='store_true',
+        default=False,
+        help=(
+            'Turn on debug mode, which shows all logs including those from '
+            '`debug` and `info` levels.'
+        ),
+    )
+
+    return arg_parser
+
+
+console = Console()
+logger = Logger('main_logger')
+
+logging_handler = RichHandler(rich_tracebacks=True)
+
+parser = get_argument_parser()
+args = parser.parse_args()
+
+LOGGING_LEVEL = logging.WARNING
+if args.debug:
+    LOGGING_LEVEL = logging.DEBUG
+
+logging_handler.setLevel(LOGGING_LEVEL)
+logger.addHandler(logging_handler)
---
+++ packages/knowledge-verificator/knowledge_verificator-0.1.1.tar.gz/knowledge_verificator-0.1.1/knowledge_verificator/main.py
@@ -0,0 +1,61 @@
+"""Main module with CLI definition."""
+
+from rich.text import Text
+
+from knowledge_verificator.io_handler import logger, console
+from knowledge_verificator.answer_chooser import AnswerChooser
+from knowledge_verificator.nli import NaturalLanguageInference, Relation
+from knowledge_verificator.qg import QuestionGeneration
+
+
+if __name__ == '__main__':
+    chooser = AnswerChooser()
+    qg_module = QuestionGeneration()
+
+    console.print('Enter a paragraph you would like to learn: ')
+    paragraph = input().strip()
+    logger.debug('Loaded the following paragraph:\n %s', paragraph)
+
+    chosen_answer = chooser.choose_answer(paragraph=paragraph)
+    if not chosen_answer:
+        raise ValueError(
+            'The supplied paragaph is either too short or too general. '
+            'Please, try providing a longer or more specific paragraph.'
+        )
+
+    logger.debug(
+        'The `%s` has been chosen as the answer, based on which the question will be generated.',
+        chosen_answer,
+    )
+
+    question_with_context = qg_module.generate(
+        answer=chosen_answer, context=paragraph
+    )
+    question = question_with_context['question']
+    logger.debug(
+        'Question Generation module has supplied the question: %s', question
+    )
+
+    console.print(
+        f'\nAnswer the question with full sentence. {question} \nYour answer: '
+    )
+    user_answer = input().strip()
+
+    nli_module = NaturalLanguageInference()
+    relation = nli_module.infer_relation(
+        premise=paragraph, hypothesis=user_answer
+    )
+
+    match relation:
+        case Relation.ENTAILMENT:
+            FEEDBACK = 'correct'
+            STYLE = 'green'
+        case Relation.CONTRADICTION:
+            FEEDBACK = f'wrong. Correct answer is {chosen_answer}'
+            STYLE = 'red'
+        case Relation.NEUTRAL:
+            FEEDBACK = 'not directly associated with the posed question'
+            STYLE = 'yellow'
+
+    feedback_text = Text(f'Your answer is {FEEDBACK}.', style=STYLE)
+    console.print(feedback_text)
