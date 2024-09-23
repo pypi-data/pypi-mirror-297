0b2aac9efe6dba35e03e290618306242fb36c5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 14:05:24 +0000

    Add package huaweicloudsdkrgc/3.1.115/huaweicloudsdkrgc-3.1.115-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkrgc/huaweicloudsdkrgc-3.1.115-py2.py3-none-any.whl/huaweicloudsdkrgc/v1/__init__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from huaweicloudsdkrgc.v1.rgc_client import RgcClient
+from huaweicloudsdkrgc.v1.rgc_async_client import RgcAsyncClient
+
+from huaweicloudsdkrgc.v1.model.control_operate_req_body import ControlOperateReqBody
+from huaweicloudsdkrgc.v1.model.control_operation import ControlOperation
+from huaweicloudsdkrgc.v1.model.disable_control_request import DisableControlRequest
+from huaweicloudsdkrgc.v1.model.disable_control_response import DisableControlResponse
+from huaweicloudsdkrgc.v1.model.enable_control_request import EnableControlRequest
+from huaweicloudsdkrgc.v1.model.enable_control_response import EnableControlResponse
+from huaweicloudsdkrgc.v1.model.list_controls_for_organizational_unit_request import ListControlsForOrganizationalUnitRequest
+from huaweicloudsdkrgc.v1.model.list_controls_for_organizational_unit_response import ListControlsForOrganizationalUnitResponse
+from huaweicloudsdkrgc.v1.model.page_info_dto import PageInfoDto
+from huaweicloudsdkrgc.v1.model.show_control_operate_request import ShowControlOperateRequest
+from huaweicloudsdkrgc.v1.model.show_control_operate_response import ShowControlOperateResponse
+from huaweicloudsdkrgc.v1.model.target_control import TargetControl
+
