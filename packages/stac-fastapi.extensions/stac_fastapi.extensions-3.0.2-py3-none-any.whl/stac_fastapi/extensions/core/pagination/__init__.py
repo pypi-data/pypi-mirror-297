91ca59ec81f2abad3079a26194a70e71c3ec12c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-23 15:05:44 +0000

    Add package stac-fastapi.extensions/3.0.2/stac_fastapi.extensions-3.0.2-py3-none-any.whl

---
+++ packages/stac-fastapi.extensions/stac_fastapi.extensions-3.0.2-py3-none-any.whl/stac_fastapi/extensions/core/pagination/__init__.py
@@ -0,0 +1,6 @@
+"""Pagination classes as extensions."""
+
+from .pagination import PaginationExtension
+from .token_pagination import TokenPaginationExtension
+
+__all__ = ["PaginationExtension", "TokenPaginationExtension"]
