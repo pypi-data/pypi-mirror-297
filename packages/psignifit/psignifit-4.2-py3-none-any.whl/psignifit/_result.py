55cb5c50c8f0e2204062f005b9df02079040a315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 05:05:34 +0000

    Add package psignifit/4.2/psignifit-4.2-py3-none-any.whl

---
+++ packages/psignifit/psignifit-4.2-py3-none-any.whl/psignifit/_result.py
@@ -0,0 +1,142 @@
+import dataclasses
+from typing import Any, Dict, Tuple, List, TextIO, Union, Optional
+import json
+from pathlib import Path
+
+import numpy as np
+from numpy.typing import NDArray
+
+from ._configuration import Configuration
+from ._typing import ParameterGrid
+
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
+@dataclasses.dataclass
+class Result:
+    parameter_estimate: Dict[str, float]
+    configuration: Configuration
+    confidence_intervals: Dict[str, List[Tuple[float, float]]]
+    data: NDArray[float]
+    parameter_values: Dict[str, NDArray[float]]
+    prior_values: Dict[str, NDArray[float]]
+    marginal_posterior_values: Dict[str, NDArray[float]]
+    debug: Dict[Any, Any]
+
+    @classmethod
+    def from_dict(cls, result_dict: Dict[str, Any]):
+        result_dict = result_dict.copy()
+        config_dict = result_dict.pop('configuration')
+        return cls(configuration=Configuration.from_dict(config_dict),
+                   **result_dict)
+
+    def as_dict(self) -> Dict[str, Any]:
+        return dataclasses.asdict(self)
+
+    def save_json(self, file: Union[TextIO, str, Path], **kwargs: Any):
+        if 'cls' not in kwargs:
+            # converts data automatically to lists of lists
+            kwargs['cls'] = NumpyEncoder
+
+        result_dict = self.as_dict()
+        if hasattr(file, 'write'):
+            json.dump(result_dict, file, **kwargs)
+        else:
+            with open(file, 'w') as f:
+                json.dump(result_dict, f, **kwargs)
+
+    @classmethod
+    def load_json(cls, file: Union[TextIO, str, Path], **kwargs: Any):
+        if hasattr(file, 'read'):
+            result_dict = json.load(file, **kwargs)
+        else:
+            with open(file, 'r') as f:
+                result_dict = json.load(f, **kwargs)
+
+        result_dict['parameter_values'] = {
+            k: np.asarray(v) for k, v in result_dict['parameter_values'].items()}
+        result_dict['prior_values'] = {
+            k: np.asarray(v) for k, v in result_dict['prior_values'].items()}
+        result_dict['marginal_posterior_values'] = {
+            k: np.asarray(v) for k, v in result_dict['marginal_posterior_values'].items()}
+        result_dict['data'] = np.asarray(result_dict['data'])
+        return cls.from_dict(result_dict)
+
+    def threshold(self, proportion_correct: np.ndarray, unscaled: bool = False, return_ci: bool = True
+                  ) -> Union[np.ndarray, List[Tuple[np.ndarray, np.ndarray]]]:
+        """ Threshold stimulus value and confidence interval for a different proportion correct cutoff.
+
+        The CIs you may obtain from this are calculated based on the confidence
+        intervals only, e.g. with the shallowest and the steepest psychometric
+        function and may thus broaden if you move away from the standard
+        threshold of unscaled sigmoid. These CI are only upper bounds.
+
+        For a more accurate inference, refit psignifit using the proportion correct in the configuration.
+
+        Args:
+            proportion_correct: proportion correct at the threshold you want to calculate
+            unscaled: If the proportion correct you provide are for the unscaled
+                      sigmoid or for the one scaled by lambda and gamma.
+                      By default this function returns the one for the scaled sigmoid.
+            return_ci: If the confidence bounds should be returned along with the stimulus value
+        Returns:
+            thresholds: stimulus values for all provided proportion_correct (if return_ci=False)
+            (thresholds, ci): stimulus values along with confidence intervals
+
+        """
+        proportion_correct = np.asarray(proportion_correct)
+        sigmoid = self.configuration.make_sigmoid()
+
+        if unscaled:  # set asymptotes to 0 for everything.
+            lambd, gamma = 0, 0
+        else:
+            lambd, gamma = self.parameter_estimate['lambda'], self.parameter_estimate['gamma']
+        new_threshold = sigmoid.inverse(proportion_correct, self.parameter_estimate['threshold'],
+                                        self.parameter_estimate['width'], lambd, gamma)
+        if not return_ci:
+            return new_threshold
+
+        param_cis = [self.confidence_intervals[param] for param in ('threshold', 'width', 'lambda', 'gamma')]
+        if unscaled:  # set asymptotes to 0
+            param_cis[2] = np.zeros_like(param_cis[2])
+            param_cis[3] = np.zeros_like(param_cis[3])
+
+        new_ci = []
+        for (thres_ci, width_ci, lambd_ci, gamma_ci) in zip(*param_cis):
+            ci_min = sigmoid.inverse(proportion_correct, thres_ci[0], width_ci[0], lambd_ci[0], gamma_ci[0])
+            ci_max = sigmoid.inverse(proportion_correct, thres_ci[1], width_ci[1], lambd_ci[1], gamma_ci[1])
+            new_ci.append([ci_min, ci_max])
+
+        return new_threshold, new_ci
+
+    def slope(self, stimulus_level: np.ndarray) -> np.ndarray:
+        """ Slope of the psychometric function at a given stimulus levels.
+
+        Args:
+            stimulus_level: stimulus levels at where to evaluate the slope.
+        Returns:
+            Slopes of the psychometric function at the stimulus levels.
+        """
+        stimulus_level, param = np.asarray(stimulus_level), self.parameter_estimate
+        sigmoid = self.configuration.make_sigmoid()
+        return sigmoid.slope(stimulus_level, param['threshold'], param['width'], param['gamma'], param['lambda'])
+
+    def slope_at_proportion_correct(self, proportion_correct: np.ndarray, unscaled: bool = False):
+        """ Slope of the psychometric function at a given performance level in proportion correct.
+
+        Args:
+            proportion_correct: proportion correct at the threshold you want to calculate
+            unscaled: If the proportion correct you provide are for the unscaled
+                      sigmoid or for the one scaled by lambda and gamma.
+                      By default this function returns the one for the scaled sigmoid.
+        Returns:
+            Slopes of the psychometric function at the stimulus levels which
+            correspond to the given proportion correct.
+        """
+        stimulus_levels = self.threshold(proportion_correct, unscaled, return_ci=False)
+        return self.slope(stimulus_levels)
---
+++ packages/psignifit/psignifit-4.2-py3-none-any.whl/psignifit/tests/test_result.py
@@ -0,0 +1,98 @@
+import dataclasses
+
+import pytest
+import numpy as np
+
+from psignifit import Result
+from psignifit import Configuration
+
+
+@pytest.fixture
+def result():
+    return Result(configuration=Configuration(),
+                  parameter_estimate={
+                      'threshold': 0.005,
+                      'width': 0.005,
+                      'lambda': 1.-7,
+                      'gamma': 0.5,
+                      'eta': 0.0001
+                  },
+                  confidence_intervals={
+                      'threshold': [[0.001, 0.005], [0.005, 0.01], [0.1, 0.2]],
+                      'width': [[0.001, 0.005], [0.005, 0.01], [0.1, 0.2]],
+                      'lambda': [[0.001, 0.005], [0.005, 0.01], [0.1, 0.2]],
+                      'gamma': [[0.001, 0.005], [0.005, 0.01], [0.1, 0.2]],
+                      'eta': [[0.001, 0.005], [0.005, 0.01], [0.1, 0.2]]
+                  },
+                  data=np.random.rand(5, 3).tolist(),
+                  parameter_values={
+                      'threshold': np.random.rand(10,).tolist(),
+                      'width': np.random.rand(3,).tolist(),
+                      'lambda': np.random.rand(5,).tolist(),
+                      'gamma': np.random.rand(5,).tolist(),
+                      'eta': np.random.rand(16,).tolist()
+                  },
+                  prior_values={
+                      'threshold': np.random.rand(10,).tolist(),
+                      'width': np.random.rand(3,).tolist(),
+                      'lambda': np.random.rand(5,).tolist(),
+                      'gamma': np.random.rand(5,).tolist(),
+                      'eta': np.random.rand(16,).tolist()
+                  },
+                  marginal_posterior_values={
+                      'threshold': np.random.rand(10,).tolist(),
+                      'width': np.random.rand(3,).tolist(),
+                      'lambda': np.random.rand(5,).tolist(),
+                      'gamma': np.random.rand(5,).tolist(),
+                      'eta': np.random.rand(16,).tolist()
+                  },
+                  debug={'posteriors': np.random.rand(5, ).tolist()})
+
+
+def test_from_to_result_dict(result):
+    result_dict = result.as_dict()
+
+    assert isinstance(result_dict, dict)
+    assert isinstance(result_dict['configuration'], dict)
+    for field in dataclasses.fields(Result):
+        assert field.name in result_dict
+
+    assert result == Result.from_dict(result_dict)
+    assert result.configuration == Configuration.from_dict(result_dict['configuration'])
+
+
+def test_threshold_slope(result):
+    with pytest.raises(ValueError):
+        #  PC outside of sigmoid
+        proportion_correct = np.linspace(1e-12, 1 - 1e-12, num=1000)
+        result.threshold(proportion_correct)
+    proportion_correct = np.linspace(0.2, 0.5, num=1000)
+    stimulus_levels, confidence_intervals = result.threshold(proportion_correct)
+    np.testing.assert_allclose(result.slope(stimulus_levels),
+                               result.slope_at_proportion_correct(proportion_correct))
+
+
+def _close_numpy_dict(first, second):
+    """ Test if two dicts of numpy arrays are equal"""
+    if first.keys() != second.keys():
+        return False
+    return np.all(np.isclose(first[key], second[key]) for key in first)
+
+
+def test_save_load_result_json(result, tmp_path):
+    result_file = tmp_path / 'result.json'
+
+    assert not result_file.exists()
+    result.save_json(result_file)
+    assert result_file.exists()
+    other = Result.load_json(result_file)
+
+    assert result.parameter_estimate == other.parameter_estimate
+    assert result.configuration == other.configuration
+    assert result.confidence_intervals == other.confidence_intervals
+    assert np.all(np.isclose(result.data, other.data))
+    assert _close_numpy_dict(
+        result.parameter_values, other.parameter_values)
+    assert _close_numpy_dict(result.prior_values, other.prior_values)
+    assert _close_numpy_dict(
+        result.marginal_posterior_values, other.marginal_posterior_values)
