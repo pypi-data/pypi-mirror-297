8c6384f6b80805b3cdda72ee133e771816b08d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:06:15 +0000

    Add package scale-gp/0.1.0a45/scale_gp-0.1.0a45-py3-none-any.whl

---
+++ packages/scale-gp/scale_gp-0.1.0a45-py3-none-any.whl/scale_gp/types/shared_params/__init__.py
@@ -0,0 +1,5 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .chunk_extra_info_schema import ChunkExtraInfoSchema as ChunkExtraInfoSchema
+from .result_schema_generation import ResultSchemaGeneration as ResultSchemaGeneration
+from .string_extra_info_schema import StringExtraInfoSchema as StringExtraInfoSchema
