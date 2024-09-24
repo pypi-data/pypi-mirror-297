4502cba95a4fd4766cbe9deb8f5ac3a632dcb541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 05:04:46 +0000

    Add package raspsim/0.1.0.3/raspsim-0.1.0.3.tar.gz

---
+++ packages/raspsim/raspsim-0.1.0.3.tar.gz/raspsim-0.1.0.3/raspsim/__init__.py
@@ -0,0 +1,5 @@
+try:
+    from .core import *
+except ImportError as e:
+    raise ImportError("""Failed to import core module. This is most likely due to an unsupoorted python version, plattform or architecture.
+    Supported versions are 3.7 to 3.12 on linux x86_64""") from e
