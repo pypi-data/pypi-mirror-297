44a49140b8d4217ff8837701a0744e2763867603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 05:04:56 +0000

    Add package tomtom-apis/0.0.1a1/tomtom_apis-0.0.1a1-py3-none-any.whl

---
+++ packages/tomtom-apis/tomtom_apis-0.0.1a1-py3-none-any.whl/tomtom_apis/tracking_logistics/location_history.py
@@ -0,0 +1,21 @@
+"""Location History API."""
+
+from ..api import BaseApi
+
+
+class LocationHistoryApi(BaseApi):
+    """Location History API.
+
+    TomTom's Location History API is intended to keep track and manage the locations of multiple objects. It can share data with TomTom's Geofencing
+    service to enhance it with the history of object transitions through fence borders.
+
+    For more information, see: https://developer.tomtom.com/location-history-api/documentation/product-information/introduction
+    """
+
+    def __init__(self):  # pylint: disable=super-init-not-called
+        """Not implemented.
+
+        Raises:
+            NotImplementedError: Always.
+        """
+        raise NotImplementedError
