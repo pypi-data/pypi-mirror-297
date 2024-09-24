cc821c2bc5c1f8e64b2483da82790644219dcf6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:10:00 +0000

    Add package opik/0.1.15/opik-0.1.15-py3-none-any.whl

---
+++ packages/opik/opik-0.1.15-py3-none-any.whl/opik/exceptions.py
@@ -0,0 +1,14 @@
+class OpikException(Exception):
+    pass
+
+
+class DatasetItemUpdateOperationRequiresItemId(OpikException):
+    pass
+
+
+class ContextExtractorNotSet(OpikException):
+    pass
+
+
+class ConfigurationError(OpikException):
+    pass
