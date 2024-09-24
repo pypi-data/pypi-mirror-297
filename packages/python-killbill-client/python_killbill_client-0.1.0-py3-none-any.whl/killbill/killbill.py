86f0a9ceb47a747b21ad6fd92a6fcc5ac99a505a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:10:38 +0000

    Add package python-killbill-client/0.1.0/python_killbill_client-0.1.0-py3-none-any.whl

---
+++ packages/python-killbill-client/python_killbill_client-0.1.0-py3-none-any.whl/killbill/killbill.py
@@ -0,0 +1,23 @@
+from killbill.clients.tenant import TenantClient
+from killbill.clients.catalog import CatalogClient
+from killbill.clients.account import AccountClient
+from killbill.clients.subscription import SubscriptionClient
+from killbill.clients.bundle import BundleClient
+from killbill.clients.overdue import OverdueClient
+
+
+class KillBillClient:
+    """Kill Bill Client"""
+
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        api_url: str = "http://localhost:8080",
+    ):
+        self.tenant = TenantClient(username, password, api_url)
+        self.catalog = CatalogClient(username, password, api_url)
+        self.account = AccountClient(username, password, api_url)
+        self.subscription = SubscriptionClient(username, password, api_url)
+        self.bundle = BundleClient(username, password, api_url)
+        self.overdue = OverdueClient(username, password, api_url)
