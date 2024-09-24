5350b42a9aa86633431a68ad59f25bd6fa64c4ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:09:03 +0000

    Add package talisman-ie-datamodel/0.5.2/talisman_ie_datamodel-0.5.2-py3-none-any.whl

---
+++ packages/talisman-ie-datamodel/talisman_ie_datamodel-0.5.2-py3-none-any.whl/tie_datamodel/readers/directory.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from typing import Callable
+
+from tp_interfaces.readers.abstract import AbstractReader
+
+
+class DirectoryReader(AbstractReader):
+    def __init__(self, base_reader: Callable[[Path], AbstractReader], path_to_dir: Path):
+        self._path = path_to_dir
+        self._base_reader = base_reader
+
+    def read(self):
+        for file in self._path.iterdir():
+            yield from self._base_reader(file).read()
