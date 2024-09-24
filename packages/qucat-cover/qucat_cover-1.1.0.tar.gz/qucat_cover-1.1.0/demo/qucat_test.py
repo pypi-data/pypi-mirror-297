3677d156a9a9c12574d3d363bfc20a15bc2f098c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:09:50 +0000

    Add package qucat-cover/1.1.0/qucat_cover-1.1.0.tar.gz

---
+++ packages/qucat-cover/qucat_cover-1.1.0.tar.gz/qucat_cover-1.1.0/demo/qucat_test.py
@@ -0,0 +1,8 @@
+from qucat_cover.qucat_run import qucat
+
+
+
+
+# qusbt("/home/xinyi/projects/qusbt_project/AS_M1.ini")
+
+qucat(1,"/Users/xinyi/Documents/qucat_cpu_project1.1.0/demo/qram_sample.ini","/Users/xinyi/Documents/qucat_cpu_project1.1.0/demo/QR_seed.txt")
