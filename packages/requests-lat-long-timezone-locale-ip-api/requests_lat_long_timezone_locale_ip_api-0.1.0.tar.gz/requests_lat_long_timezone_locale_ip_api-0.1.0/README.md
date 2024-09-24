ddb76faaa32ed87fc30718fb15f15540cf1bf677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:10:22 +0000

    Add package requests-lat-long-timezone-locale-ip-api/0.1.0/requests_lat_long_timezone_locale_ip_api-0.1.0.tar.gz

---
+++ packages/requests-lat-long-timezone-locale-ip-api/requests_lat_long_timezone_locale_ip_api-0.1.0.tar.gz/requests_lat_long_timezone_locale_ip_api-0.1.0/README.md
@@ -0,0 +1,5 @@
+# Your Package Name
+
+A short description of your package.
+
+## Installation
---
+++ packages/requests-lat-long-timezone-locale-ip-api/requests_lat_long_timezone_locale_ip_api-0.1.0.tar.gz/requests_lat_long_timezone_locale_ip_api-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='requests-lat-long-timezone-locale-ip-api',  # Replace with your package name
+    version='0.1.0',  # Replace with your package version
+    author='dormic97',
+    author_email='crozz-boy@hotmail.com',
+    description='Get timezone, locale, and location from IP address to use in your requests',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/pim97/requests-lat-long-timezone-locale-ip-api.git',  # Replace with your repo URL
+    packages=find_packages(),
+    keywords=['ip', 'ip-api', 'ip-geolocation', 'ip-location', 'ip-address', 'ip-locator', 'ip-tracker', 'ip-geocoding', 'ip-api-geolocation', 'ip-api-location', 'ip-api-address', 'ip-api-locator', 'ip-api-tracker', 'ip-api-geocoding', 'ip-api-geolocation', 'ip-api-location', 'ip-api-address', 'ip-api-locator', 'ip-api-tracker', 'ip-api-geocoding'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Replace with your license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Replace with your required Python version
+    install_requires=[            # I get to this in a second
+          'requests'
+    ],
+)
