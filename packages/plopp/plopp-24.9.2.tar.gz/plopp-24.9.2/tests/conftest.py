af88461f3d4c40f9283bd0926ed858ec817a13ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:08:46 +0000

    Add package plopp/24.9.2/plopp-24.9.2.tar.gz

---
+++ packages/plopp/plopp-24.9.2.tar.gz/plopp-24.9.2/tests/conftest.py
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+
+import matplotlib
+import matplotlib.pyplot as plt
+import pytest
+
+import plopp as pp
+
+
+@pytest.fixture(autouse=True)
+def _reset_mpl_defaults():
+    matplotlib.rcdefaults()
+    matplotlib.use('Agg')
+    pp.backends.reset()
+
+
+@pytest.fixture(autouse=True)
+def _close_figures():
+    """
+    Force closing all figures after each test case.
+    Otherwise, the figures consume a lot of memory and matplotlib complains.
+    """
+    yield
+    for fig in map(plt.figure, plt.get_fignums()):
+        plt.close(fig)
+
+
+@pytest.fixture
+def _use_ipympl():
+    matplotlib.use('module://ipympl.backend_nbagg')
+
+
+def pytest_sessionfinish(session, exitstatus):
+    """
+    When running no tests (e.g. in the noplotly tox env), pytest returns the exit code
+    5, which causes the tox to fail. We use this to catch the exit status and convert
+    it to 0.
+    See https://github.com/pytest-dev/pytest/issues/2393#issuecomment-452634365
+    """
+    if exitstatus == 5:
+        session.exitstatus = 0
