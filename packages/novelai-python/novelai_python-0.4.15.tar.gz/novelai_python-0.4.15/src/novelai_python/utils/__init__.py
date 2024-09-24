fa73c84f4409ee79d2eeefb4e2cdf4e53307524c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 04:06:18 +0000

    Add package novelai-python/0.4.15/novelai_python-0.4.15.tar.gz

---
+++ packages/novelai-python/novelai_python-0.4.15.tar.gz/novelai_python-0.4.15/src/novelai_python/utils/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/1/26 上午10:51
+# @Author  : sudoskys
+# @File    : __init__.py.py
+# @Software: PyCharm
+import json
+from typing import Union
+
+from loguru import logger
+
+from .encode import encode_access_key  # noqa 401
+
+
+def try_jsonfy(obj: Union[str, dict, list, tuple]):
+    """
+    try to jsonfy a object
+    :param obj:
+    :return:
+    """
+    try:
+        return json.loads(obj)
+    except Exception as e:
+        logger.error(f"Decode Error {obj}")
+        return f"Decode Error {type(obj)}"
