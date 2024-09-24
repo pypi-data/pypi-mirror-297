bcda92f61484c16f713b9291ebcaf3fe80c07d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 06:05:27 +0000

    Add package lookup-license/0.1.9/lookup_license-0.1.9-py2.py3-none-any.whl

---
+++ packages/lookup-license/lookup_license-0.1.9-py2.py3-none-any.whl/lookup_license/config.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+lookup_license_version = "0.1.9"
+DESCRIPTION = " descr "
+EPILOG = " ...finally..."
