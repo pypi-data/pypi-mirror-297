e1f39fa3392e9c26daadb9caa3e783864891657f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:08:56 +0000

    Add package seadex/0.1.3/seadex-0.1.3.tar.gz

---
+++ packages/seadex/seadex-0.1.3.tar.gz/seadex-0.1.3/docs/api-reference/backup.md
@@ -0,0 +1,3 @@
+::: seadex.SeaDexBackup
+::: seadex.BackupFile
+
---
+++ packages/seadex/seadex-0.1.3.tar.gz/seadex-0.1.3/src/seadex/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from seadex._backup import BackupFile, SeaDexBackup
+from seadex._version import __version__, __version_tuple__
+
+__all__ = ("SeaDexBackup", "BackupFile", "__version__", "__version_tuple__")
