e6b5f47600f069571c42fadf13b8a1c3c7ff54f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:10:34 +0000

    Add package crowdsec-service-api/1.30.1/crowdsec_service_api-1.30.1.tar.gz

---
+++ packages/crowdsec-service-api/crowdsec_service_api-1.30.1.tar.gz/crowdsec_service_api-1.30.1/crowdsec_service_api/__init__.py
@@ -0,0 +1,71 @@
+from enum import Enum
+from .base_model import Page
+from .models import *
+from .services.blocklists import Blocklists
+from .services.integrations import Integrations
+from .services.info import Info
+from .services.hub import Hub
+from .http_client import ApiKeyAuth
+
+class Server(Enum):
+    production_server = 'https://admin.api.crowdsec.net/v1/'
+
+__all__ = [
+    'Blocklists',
+    'Integrations',
+    'Info',
+    'Hub',
+    'ApiKeyCredentials',
+    'BasicAuthCredentials',
+    'BlocklistAddIPsRequest',
+    'BlocklistContentStats',
+    'BlocklistCreateRequest',
+    'BlocklistCreateResponse',
+    'BlocklistDeleteIPsRequest',
+    'BlocklistGetResponse',
+    'BlocklistIncludeFilters',
+    'BlocklistResponse',
+    'BlocklistSearchRequest',
+    'BlocklistShareRequest',
+    'BlocklistSources',
+    'BlocklistStats',
+    'BlocklistSubscriberEntity',
+    'BlocklistSubscribersResponse',
+    'BlocklistSubscription',
+    'BlocklistSubscriptionRequest',
+    'BlocklistSubscriptionResponse',
+    'BlocklistUpdateRequest',
+    'BlocklistUsageStats',
+    'Body_uploadBlocklistContent',
+    'CtiAs',
+    'CtiBehavior',
+    'CtiCategory',
+    'CtiCountry',
+    'CtiIp',
+    'CtiScenario',
+    'EntityType',
+    'HTTPValidationError',
+    'InfoResponse',
+    'IntegrationCreateRequest',
+    'IntegrationCreateResponse',
+    'IntegrationGetResponse',
+    'IntegrationType',
+    'IntegrationUpdateRequest',
+    'IntegrationUpdateResponse',
+    'Links',
+    'OutputFormat',
+    'Page_BlocklistResponse_',
+    'Page_IntegrationGetResponse_',
+    'PaginatedBlocklistResponse',
+    'Permission',
+    'PricingTiers',
+    'Share',
+    'Stats',
+    'ValidationError',
+    'HubItem',
+    'HubType',
+    'Index',
+    'ApiKeyAuth',
+    'Server',
+    'Page'
+]
