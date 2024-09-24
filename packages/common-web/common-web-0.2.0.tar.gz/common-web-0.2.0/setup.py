538502ee000fe9f73b4ddeeeb7f5165e694fe893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 15:05:14 +0000

    Add package common-web/0.2.0/common-web-0.2.0.tar.gz

---
+++ packages/common-web/common-web-0.2.0.tar.gz/common-web-0.2.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="common-web",
+    version="0.2.0",
+    author="Cloud Bian",
+    author_email="cloudbian@139.com",
+    description="this is common for base",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
