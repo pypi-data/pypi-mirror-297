2ee18060109a8b810456aa9d10f527f7e9a79593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:10:15 +0000

    Add package django-haul/0.0.12/django-haul-0.0.12.tar.gz

---
+++ packages/django-haul/django-haul-0.0.12.tar.gz/django-haul-0.0.12/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'django>=3',
+    'djangorestframework>=3',
+    'ruamel.yaml>=0.17',
+    'graphlib-backport; python_version<"3.9"',
+]
+
+test_requirements = ['pytest>=3', 'typing_extensions']
+
+setup(
+    author="Eugene Pankov`",
+    author_email='e.pankov@elements.tv',
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Structured object import/export for Django ORM",
+    install_requires=requirements,
+    license="MIT",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords=['django', 'orm', 'export', 'import'],
+    name='django-haul',
+    packages=find_packages(include=['haul', 'haul.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/elements-storage/haul',
+    version='0.0.12',
+    zip_safe=False,
+)
