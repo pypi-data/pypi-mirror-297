b96a06f90285f1051a5ed833b5a03e99589090c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 16:05:23 +0000

    Add package svc-order-zxw/0.0.1/svc_order_zxw-0.0.1-py3-none-any.whl

---
+++ packages/svc-order-zxw/svc_order_zxw-0.0.1-py3-none-any.whl/svc_order_zxw/__init__.py
@@ -0,0 +1,10 @@
+"""
+# File       : __init__.py
+# Time       ：2024/9/24 15:33
+# Author     ：xuewei zhang
+# Email      ：shuiheyangguang@gmail.com
+# version    ：python 3.12
+# Description：
+"""
+from .apis import schemas_微信支付宝支付, schemas_通用管理, schemas_payments
+from .db.models import get_db_sync, get_db
