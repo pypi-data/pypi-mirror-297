413f9ba21d106bc101236d52aca30b19747dc8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 04:05:54 +0000

    Add package pyfar/0.6.10/pyfar-0.6.10-py2.py3-none-any.whl

---
+++ packages/pyfar/pyfar-0.6.10-py2.py3-none-any.whl/pyfar/plot/_line.py
@@ -0,0 +1,293 @@
+import numpy as np
+from pyfar import Signal, TimeData, FrequencyData
+import pyfar.dsp as dsp
+from . import _utils
+from .ticker import (
+    LogFormatterITAToolbox,
+    LogLocatorITAToolbox,
+    MultipleFractionLocator,
+    MultipleFractionFormatter)
+from matplotlib.ticker import NullFormatter
+
+
+def _time(signal, dB=False, log_prefix=20, log_reference=1, unit="s",
+          ax=None, **kwargs):
+    """Plot the time data of a signal."""
+
+    # check input
+    if not isinstance(signal, (Signal, TimeData)):
+        raise TypeError('Input data has to be of type: Signal or TimeData.')
+    _utils._check_time_unit(unit)
+
+    # prepare input
+    kwargs = _utils._return_default_colors_rgb(**kwargs)
+    if dB:
+        # avoid any zero-values because they result in -inf in dB data
+        data = dsp.decibel(signal, 'time', log_prefix, log_reference).T
+        ymax = np.nanmax(data) + 10
+        ymin = ymax - 100
+    else:
+        data = signal.time.T
+    # auto detect the time unit
+    if unit in [None, "auto"]:
+        unit = _utils._time_auto_unit(signal.times[..., -1])
+    # set the unit
+    if unit == 'samples':
+        times = np.arange(signal.n_samples)
+    else:
+        factor, unit = _utils._deal_time_units(unit)
+        times = signal.times * factor
+
+    # prepare figure
+    _, ax = _utils._prepare_plot(ax)
+    ax.set_xlabel(f"Time in {unit}")
+    if dB:
+        ax.set_ylabel("Amplitude in dB")
+        _utils._set_axlim(ax, ax.set_ylim, ymin, ymax, ax.get_ylim())
+    else:
+        ax.set_ylabel("Amplitude")
+    _utils._set_axlim(ax, ax.set_xlim, times[0], times[-1],
+                      ax.get_xlim())
+
+    # plot data
+    ax.plot(times, data, **kwargs)
+
+    return ax
+
+
+def _freq(signal, dB=True, log_prefix=None, log_reference=1, freq_scale='log',
+          ax=None, **kwargs):
+    """
+    Plot the logarithmic absolute spectrum on the positive frequency axis.
+    """
+
+    # check input
+    if not isinstance(signal, (Signal, FrequencyData)):
+        raise TypeError(
+            'Input data has to be of type: Signal or FrequencyData.')
+    _utils._check_axis_scale(freq_scale)
+
+    # prepare input
+    kwargs = _utils._return_default_colors_rgb(**kwargs)
+    if dB:
+        data = dsp.decibel(signal, 'freq', log_prefix, log_reference)
+        ymax = np.nanmax(data)
+        ymin = ymax - 90
+        ymax = ymax + 10
+    else:
+        data = np.abs(signal.freq)
+
+    # prepare figure
+    _, ax = _utils._prepare_plot(ax)
+    ax.grid(True, 'both')
+    if dB:
+        ax.set_ylabel("Magnitude in dB")
+        _utils._set_axlim(ax, ax.set_ylim, ymin, ymax, ax.get_ylim())
+    else:
+        ax.set_ylabel("Magnitude")
+    ax.set_xlabel("Frequency in Hz")
+    _utils._set_axlim(ax, ax.set_xlim, _utils._lower_frequency_limit(signal),
+                      signal.frequencies[-1], ax.get_xlim())
+
+    # plot data
+    if freq_scale == 'log':
+        ax.semilogx(signal.frequencies, data.T, **kwargs)
+    else:
+        ax.plot(signal.frequencies, data.T, **kwargs)
+
+    # set and format ticks
+    if freq_scale == 'log':
+        ax.xaxis.set_major_locator(LogLocatorITAToolbox())
+        ax.xaxis.set_minor_formatter(NullFormatter())
+    ax.xaxis.set_major_formatter(LogFormatterITAToolbox())
+
+    return ax
+
+
+def _phase(signal, deg=False, unwrap=False, freq_scale='log', ax=None,
+           **kwargs):
+    """Plot the phase of the spectrum on the positive frequency axis."""
+
+    # check input
+    if not isinstance(signal, (Signal, FrequencyData)):
+        raise TypeError(
+            'Input data has to be of type: Signal or FrequencyData.')
+    _utils._check_axis_scale(freq_scale)
+
+    # prepare figure
+    _, ax = _utils._prepare_plot(ax)
+
+    # prepare input
+    kwargs = _utils._return_default_colors_rgb(**kwargs)
+    phase_data = dsp.phase(signal, deg=deg, unwrap=unwrap)
+
+    # Construct the correct label string
+    ylabel_string = _utils._phase_label(unwrap, deg)
+
+    # y-axis formatting
+    y_margin = 5 if deg else np.radians(5)
+    if not deg and (not unwrap or unwrap == "360"):
+        # nice tick formatting is not done for unwrap=True. In this case
+        # it can create 1000 or more ticks.
+        ax.yaxis.set_major_locator(MultipleFractionLocator(np.pi, 2))
+        ax.yaxis.set_minor_locator(MultipleFractionLocator(np.pi, 6))
+        ax.yaxis.set_major_formatter(MultipleFractionFormatter(
+            nominator=1, denominator=2, base=np.pi, base_str=r'\pi'))
+
+    ymin = np.nanmin(phase_data) - y_margin  # more elegant solution possible?
+    ymax = np.nanmax(phase_data) + y_margin
+
+    # prepare figure
+    ax.set_xlabel("Frequency in Hz")
+    ax.set_ylabel(ylabel_string)
+    ax.grid(True, 'both')
+    _utils._set_axlim(ax, ax.set_xlim, _utils._lower_frequency_limit(signal),
+                      signal.frequencies[-1], ax.get_xlim())
+    _utils._set_axlim(ax, ax.set_ylim, ymin, ymax, ax.get_ylim())
+
+    # plot data
+    if freq_scale == 'log':
+        ax.semilogx(signal.frequencies, phase_data.T, **kwargs)
+    else:
+        ax.plot(signal.frequencies, phase_data.T, **kwargs)
+
+    # set and format ticks
+    if freq_scale == 'log':
+        ax.xaxis.set_major_locator(LogLocatorITAToolbox())
+        ax.xaxis.set_minor_formatter(NullFormatter())
+    ax.xaxis.set_major_formatter(LogFormatterITAToolbox())
+
+    return ax
+
+
+def _group_delay(signal, unit="s", freq_scale='log', ax=None, **kwargs):
+    """Plot the group delay on the positive frequency axis."""
+
+    # check input
+    if not isinstance(signal, Signal):
+        raise TypeError('Input data has to be of type: Signal.')
+    _utils._check_time_unit(unit)
+    _utils._check_axis_scale(freq_scale)
+
+    # prepare input
+    kwargs = _utils._return_default_colors_rgb(**kwargs)
+    data = dsp.group_delay(signal)
+    # auto detect the unit
+    if unit in [None, "auto"]:
+        unit = _utils._time_auto_unit(
+            np.nanmax(np.abs(data) / signal.sampling_rate))
+    # set the unit
+    if unit != "samples":
+        factor, unit = _utils._deal_time_units(unit)
+        data = data / signal.sampling_rate * factor
+
+    # prepare figure
+    _, ax = _utils._prepare_plot(ax)
+    ax.set_xlabel("Frequency in Hz")
+    ax.set_ylabel(f"Group delay in {unit}")
+    ax.grid(True, 'both')
+    _utils._set_axlim(ax, ax.set_xlim, _utils._lower_frequency_limit(signal),
+                      signal.frequencies[-1], ax.get_xlim())
+    _utils._set_axlim(ax, ax.set_ylim, .5 * np.nanmin(data),
+                      1.5 * np.nanmax(data), ax.get_ylim())
+
+    # plot data
+    if freq_scale == 'log':
+        ax.semilogx(signal.frequencies, data.T, **kwargs)
+    else:
+        ax.plot(signal.frequencies, data.T, **kwargs)
+
+    # set and format ticks
+    if freq_scale == 'log':
+        ax.xaxis.set_major_locator(LogLocatorITAToolbox())
+        ax.xaxis.set_minor_formatter(NullFormatter())
+    ax.xaxis.set_major_formatter(LogFormatterITAToolbox())
+
+    return ax
+
+
+def _time_freq(signal, dB_time=False, dB_freq=True, log_prefix_time=20,
+               log_prefix_freq=None, log_reference=1, freq_scale='log',
+               unit="s", ax=None, **kwargs):
+    """
+    Plot the time signal and magnitude spectrum in a 2 by 1 subplot layout.
+    """
+
+    fig, ax = _utils._prepare_plot(ax, (2, 1))
+    kwargs = _utils._return_default_colors_rgb(**kwargs)
+
+    _time(signal, dB_time, log_prefix_time, log_reference, unit, ax[0],
+          **kwargs)
+    _freq(signal, dB_freq, log_prefix_freq, log_reference, freq_scale, ax[1],
+          **kwargs)
+    fig.align_ylabels()
+
+    return ax
+
+
+def _freq_phase(signal, dB=True, log_prefix=None, log_reference=1,
+                freq_scale='log', deg=False, unwrap=False, ax=None, **kwargs):
+    """Plot the magnitude and phase spectrum in a 2 by 1 subplot layout."""
+
+    fig, ax = _utils._prepare_plot(ax, (2, 1))
+    kwargs = _utils._return_default_colors_rgb(**kwargs)
+
+    _freq(signal, dB, log_prefix, log_reference, freq_scale, ax[0], **kwargs)
+    _phase(signal, deg, unwrap, freq_scale, ax[1], **kwargs)
+    ax[0].set_xlabel(None)
+    fig.align_ylabels()
+
+    return ax
+
+
+def _freq_group_delay(signal, dB=True, log_prefix=None, log_reference=1,
+                      unit="s", freq_scale='log', ax=None, **kwargs):
+    """
+    Plot the magnitude and group delay spectrum in a 2 by 1 subplot layout.
+    """
+
+    fig, ax = _utils._prepare_plot(ax, (2, 1))
+    kwargs = _utils._return_default_colors_rgb(**kwargs)
+
+    _freq(signal, dB, log_prefix, log_reference, freq_scale, ax[0], **kwargs)
+    _group_delay(signal, unit, freq_scale, ax[1], **kwargs)
+    ax[0].set_xlabel(None)
+    fig.align_ylabels()
+
+    return ax
+
+
+def _custom_subplots(signal, plots, ax, **kwargs):
+    """
+    Generate subplot with a custom layout based on a list of plot function
+    handles. The subplot layout is taken from the shape of the plot function
+    handle list.
+
+    See pyfar.plot.line._custom_subplots for more information.
+    """
+    if not isinstance(signal, Signal):
+        raise TypeError('Input data has to be of type: Signal.')
+
+    plots = np.atleast_2d(np.asarray(plots))
+    subplots = plots.shape
+    fig, ax = _utils._prepare_plot(ax, subplots)
+
+    rows = subplots[0]
+    cols = subplots[1]
+
+    for row in range(rows):
+        for col in range(cols):
+            # current_axis
+            # this is a bit tricky: if a new multi plot is created ax will can
+            # be a nested list because it is created by fig.subplots() but ax
+            # will be a flat list if multi plots into an existing plot, because
+            # ax is obtained from ax = plt.gca()
+            try:
+                ca = ax[row][col] if rows > 1 and cols > 1 else \
+                     ax[max(row, col)]
+            except TypeError:
+                ca = ax[row * cols + col]
+            # plot
+            plots[row][col](signal, ax=ca, **kwargs)
+
+    return ax
---
+++ packages/pyfar/pyfar-0.6.10-py2.py3-none-any.whl/pyfar/signals/deterministic.py
@@ -0,0 +1,365 @@
+import numpy as np
+import pyfar
+
+
+def sine(frequency, n_samples, amplitude=1, phase=0, sampling_rate=44100,
+         full_period=False):
+    """Generate a single or multi channel sine signal.
+
+    Parameters
+    ----------
+    frequency : double, array like
+        Frequency of the sine in Hz (0 <= `frequency` <= `sampling_rate`/2).
+    n_samples : int
+        Length of the signal in samples.
+    amplitude : double, array like, optional
+        The amplitude. The default is ``1``.
+    phase : double, array like, optional
+        The phase in radians. The default is ``0``.
+    sampling_rate : int, optional
+        The sampling rate in Hz. The default is ``44100``.
+    full_period : boolean, optional
+        Make sure that the returned signal contains an integer number of
+        periods resulting in a periodic signal. This is done by adjusting the
+        frequency of the sine. The default is ``False``.
+
+    Returns
+    -------
+    signal : Signal
+        The sine signal. The Signal is in the time domain and has the ``rms``
+        FFT normalization (see :py:func:`~pyfar.dsp.fft.normalization`).
+        The exact frequency, amplitude and phase are written to `comment`.
+
+    Notes
+    -----
+    The parameters `frequency`, `amplitude`, and `phase` are broadcasted using
+    the :doc:`numpy rules<numpy:user/basics.broadcasting>`. For example
+    `frequency` could be of shape ``(2, 4)``, `amplitude` of shape ``(2, 1)``,
+    and `phase` could be a scalar. In this case all parameters would be
+    broadcasted to a shape of ``(2, 4)``.
+    """
+
+    # check and match the cshape
+    try:
+        cshape, (frequency, amplitude, phase) = _match_shape(
+            frequency, amplitude, phase)
+    except ValueError:
+        raise ValueError(("The parameters frequency, amplitude, and phase can "
+                          "not be broadcasted to the same shape"))
+
+    if np.any(frequency < 0) or np.any(frequency > sampling_rate/2):
+        raise ValueError(
+            f"The frequency must be between 0 and {sampling_rate/2} Hz")
+
+    # generate the sine signal
+    n_samples = int(n_samples)
+    times = np.arange(n_samples) / sampling_rate
+    sine = np.zeros(cshape + (n_samples, ))
+    for idx in np.ndindex(cshape):
+        if full_period:
+            # nearest number of full periods
+            num_periods = np.round(
+                n_samples / sampling_rate * frequency[idx])
+            # corresponding frequency
+            frequency[idx] = num_periods * sampling_rate / n_samples
+
+        sine[idx] = amplitude[idx] * \
+            np.sin(2 * np.pi * frequency[idx] * times + phase[idx])
+
+    # save to Signal
+    nl = "\n"  # required as variable because f-strings cannot contain "\"
+    comment = (f"Sine signal (f = {str(frequency).replace(nl, ',')} Hz, "
+               f"amplitude = {str(amplitude).replace(nl, ',')}, "
+               f"phase = {str(phase).replace(nl, ',')} rad)")
+
+    signal = pyfar.Signal(
+        sine, sampling_rate, fft_norm="rms", comment=comment)
+
+    return signal
+
+
+def impulse(n_samples, delay=0, amplitude=1, sampling_rate=44100):
+    """
+    Generate a single or multi channel impulse signal, also known as the
+    Dirac delta function.
+
+    .. math::
+        s(n) =
+        \\begin{cases}
+        \\text{amplitude},  & \\text{if $n$ = delay}\\\\
+        0, & \\text{else}
+        \\end{cases}
+
+
+    Parameters
+    ----------
+    n_samples : int
+        Length of the impulse in samples
+    delay : double, array like, optional
+        Delay in samples. The default is ``0``.
+    amplitude : double, array like, optional
+        The peak amplitude of the impulse. The default is ``1``.
+    sampling_rate : int, optional
+        The sampling rate in Hz. The default is ``44100``.
+
+    Returns
+    -------
+    signal : Signal
+        The impulse signal. The Signal is in the time domain and has the
+        ``none`` FFT normalization (see
+        :py:func:`~pyfar.dsp.fft.normalization`). The delay and amplitude
+        are written to `comment`.
+
+    Notes
+    -----
+    The parameters `delay` and `amplitude` are broadcasted using the
+    :doc:`numpy rules<numpy:user/basics.broadcasting>`. For example `delay`
+    could be of shape ``(2, 4)``, `amplitude` of shape ``(2, 1)`` or a scalar.
+    In this case all parameters would be broadcasted to a shape of ``(2, 4)``.
+    """
+    # check and match the cshape
+    try:
+        cshape, (delay, amplitude) = _match_shape(delay, amplitude)
+    except ValueError:
+        raise ValueError(("The parameters delay and amplitude can not be "
+                          "broadcasted to the same shape"))
+
+    # generate the impulse
+    n_samples = int(n_samples)
+    impulse = np.zeros(cshape + (n_samples, ), dtype=np.double)
+    for idx in np.ndindex(cshape):
+        impulse[idx + (delay[idx], )] = amplitude[idx]
+
+    # save to Signal
+    nl = "\n"  # required as variable because f-strings cannot contain "\"
+    comment = (f"Impulse signal (delay = {str(delay).replace(nl, ',')} "
+               f"samples, amplitude = {str(amplitude).replace(nl, ',')})")
+
+    signal = pyfar.Signal(impulse, sampling_rate, comment=comment)
+
+    return signal
+
+
+def linear_sweep_time(n_samples, frequency_range, n_fade_out=90, amplitude=1,
+                      sampling_rate=44100):
+    """Generate single channel sine sweep with linearly increasing frequency.
+
+    Time domain sweep generation according to [#]_:
+
+    .. math::
+        s(t) = \\sin(2\\pi f_\\mathrm{low} t + 2\\pi (f_\\mathrm{high}-
+        f_\\mathrm{low}) / T \\cdot t^2 / 2),
+
+    with :math:`T` the duration in seconds, :math:`t` the sampling points in
+    seconds, and the frequency limits :math:`f_\\mathrm{low}` and
+    :math:`f_\\mathrm{high}`.
+
+    Parameters
+    ----------
+    n_samples : int
+        The length of the sweep in samples
+    frequency_range : array like
+        Frequency range of the sweep given by the lower and upper cut-off
+        frequency in Hz.
+    n_fade_out : int, optional
+        The length of the squared cosine fade-out in samples. This is done to
+        avoid discontinuities at the end of the sweep. The default is ``90``,
+        which equals approximately 2 ms at sampling rates of 44.1 and 48 kHz.
+    amplitude : double, optional
+        The amplitude of the signal. The default is ``1``.
+    sampling_rate : int, optional
+        The sampling rate in Hz. The default is ``44100``.
+
+    Returns
+    -------
+    sweep : Signal
+        The sweep signal. The Signal is in the time domain and has the ``none``
+        FFT normalization (see :py:func:`~pyfar.dsp.fft.normalization`). The
+        sweep type, frequency range, and length of the fade our are written to
+        `comment`.
+
+    References
+    ----------
+    .. [#]  Farina, Angelo (2000): "Simultaneous measurement of impulse
+            response and distortion with a swept-sine technique." 108th AES
+            Convention, Paris: France.
+    """
+
+    signal = _time_domain_sweep(
+        n_samples, frequency_range, n_fade_out, amplitude,
+        sampling_rate, "linear")
+
+    return signal
+
+
+def exponential_sweep_time(n_samples, frequency_range, n_fade_out=90,
+                           amplitude=1, sweep_rate=None, sampling_rate=44100):
+    """
+    Generate single channel sine sweep with exponentially increasing frequency.
+
+    Time domain sweep generation according to [#]_:
+
+    .. math::
+        s(t) = \\sin(2\\pi f_\\mathrm{low} L \\left( e^{t/L} - 1 \\right))
+
+    with
+
+    .. math::
+        L = T / \\log(f_\\mathrm{high}/f_\\mathrm{low}),
+
+    :math:`T` the duration in seconds, :math:`t` the sampling points in
+    seconds, and the frequency limits :math:`f_\\mathrm{low}` and
+    :math:`f_\\mathrm{high}`.
+
+    Parameters
+    ----------
+    n_samples : int
+        The length of the sweep in samples
+    frequency_range : array like
+        Frequency range of the sweep given by the lower and upper cut-off
+        frequency in Hz.
+    n_fade_out : int, optional
+        The length of the squared cosine fade-out in samples. This is done to
+        avoid discontinuities at the end of the sweep. The default is ``90``,
+        which equals approximately 2 ms at sampling rates of 44.1 and 48 kHz.
+    amplitude : double, optional
+        The amplitude of the signal. The default is ``1``.
+    sweep_rate : double, optional
+        Rate at which the sine frequency increases over time. If this is given,
+        `n_samples` is calculated according to the sweep rate. The default is
+        ``None``, which uses `n_samples` without modifications.
+    sampling_rate : int, optional
+        The sampling rate in Hz. The default is ``44100``.
+
+    Returns
+    -------
+    sweep : Signal
+        The sweep signal. The Signal is in the time domain and has the ``none``
+        FFT normalization (see :py:func:`~pyfar.dsp.fft.normalization`). The
+        sweep type, frequency range, and length of the fade our are written to
+        `comment`.
+
+    References
+    ----------
+    .. [#]  Farina, Angelo (2000): "Simultaneous measurement of impulse
+            response and distortion with a swept-sine technique." 108th AES
+            Convention, Paris: France.
+    """
+
+    signal = _time_domain_sweep(
+        n_samples, frequency_range, n_fade_out, amplitude, sampling_rate,
+        "exponential", sweep_rate)
+
+    return signal
+
+
+def _time_domain_sweep(n_samples, frequency_range, n_fade_out, amplitude,
+                       sampling_rate, sweep_type, sweep_rate=None):
+
+    # check input
+    if np.atleast_1d(frequency_range).size != 2:
+        raise ValueError(
+            "frequency_range must be an array like with to elements.")
+    if frequency_range[1] > sampling_rate/2:
+        raise ValueError(
+            "Upper frequency limit is larger than half the sampling rate.")
+    if frequency_range[0] == 0 and sweep_type == "exponential":
+        raise ValueError("The exponential sweep can not start at 0 Hz.")
+
+    # generate sweep
+    if sweep_type == "linear":
+        sweep = _linear_sweep(
+            n_samples, frequency_range, amplitude, sampling_rate)
+    elif sweep_type == 'exponential':
+        sweep = _exponential_sweep(
+            n_samples, frequency_range, amplitude, sweep_rate, sampling_rate)
+
+    # fade out
+    n_fade_out = int(n_fade_out)
+    if n_fade_out > 0:
+        # check must be done here because n_samples might not be defined if
+        # using the sweep_rate for exponential sweeps
+        if sweep.size < n_fade_out:
+            raise ValueError("The sweep must be longer than n_fade_out.")
+
+        sweep[-n_fade_out:] *= np.cos(np.linspace(0, np.pi/2, n_fade_out))**2
+
+    # save to signal
+    comment = (f"{sweep_type} sweep between {frequency_range[0]} "
+               f"and {frequency_range[1]} Hz "
+               f"with {n_fade_out} samples squared cosine fade-out.")
+    signal = pyfar.Signal(
+        sweep, sampling_rate, fft_norm="none", comment=comment)
+
+    return signal
+
+
+def _linear_sweep(n_samples, frequency_range, amplitude, sampling_rate):
+
+    # generate sweep
+    n_samples = int(n_samples)
+    t = np.arange(n_samples) / sampling_rate
+    T = n_samples / sampling_rate
+
+    # [1, page 5]
+    sweep = amplitude * np.sin(
+        2 * np.pi * frequency_range[0] * t +
+        2 * np.pi * (frequency_range[1]-frequency_range[0]) / T * t**2 / 2)
+
+    return sweep
+
+
+def _exponential_sweep(n_samples, frequency_range, amplitude, sweep_rate,
+                       sampling_rate):
+
+    c = np.log(frequency_range[1] / frequency_range[0])
+
+    # get n_samples
+    if sweep_rate is not None:
+        L = 1 / sweep_rate / np.log(2)
+        T = L * c
+        n_samples = np.round(T * sampling_rate)
+    else:
+        n_samples = int(n_samples)
+
+    # L for actual n_samples
+    L = n_samples / sampling_rate / c
+
+    # make the sweep
+    times = np.arange(n_samples) / sampling_rate
+    sweep = amplitude * np.sin(
+        2 * np.pi * frequency_range[0] * L * (np.exp(times / L) - 1))
+
+    return sweep
+
+
+def _match_shape(*args):
+    """
+    Match the shape of *args to the shape of the arg with the largest size
+    using np.broadcast_shapes and np.broadcast_to()
+
+    Parameters
+    ----------
+    *args :
+        data for matching shape
+
+    Returns
+    -------
+    shape : tuple
+        new common shape of the args
+    args : list
+        args with new common shape
+        (*arg_1, *arg_2, ..., *arg_N)
+    """
+
+    # broadcast shapes
+    shape = np.broadcast_shapes(*[np.atleast_1d(arg).shape for arg in args])
+
+    # match the shape
+    result = []
+    for arg in args:
+        arg = np.broadcast_to(arg, shape)
+        arg.setflags(write=1)
+        result.append(arg)
+
+    return shape, result
