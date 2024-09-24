e6c534fc3e9f3ecc4ab80758dd555e31755c1b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 04:06:04 +0000

    Add package recomenda/0.1.1/recomenda-0.1.1.tar.gz

---
+++ packages/recomenda/recomenda-0.1.1.tar.gz/recomenda-0.1.1/recomenda/core/config.py
@@ -0,0 +1,29 @@
+# ./src/core/config.py
+
+import os
+from typing import Optional
+# from dotenv import load_dotenv
+from core.logger import logger
+
+# load_dotenv()  # Load environment variables from a .env file if present
+
+
+class EmbedderConfig:
+    OPENAI_API_KEY: str = os.getenv('OPENAI_API_KEY')
+    if not OPENAI_API_KEY:
+        logger.error("OPENAI_API_KEY environment variable is not set.")
+        raise EnvironmentError("OPENAI_API_KEY environment variable is not set.")
+
+    OPENAI_EMBEDDING_MODEL: str = os.getenv('OPENAI_EMBEDDING_MODEL', "text-embedding-3-small")
+
+
+class DatabaseConfig:
+    DATABASE_URL: str = os.getenv('DATABASE_URL', 'sqlite:///./recommender.db')
+
+
+class Config:
+    EMBEDDER = EmbedderConfig()
+    DATABASE = DatabaseConfig()
+
+
+config = Config()
