b020057c5f489f661ab60692915f6645b48d0ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:06:21 +0000

    Add package Toisto/0.25.0/Toisto-0.25.0-py3-none-any.whl

---
+++ packages/Toisto/Toisto-0.25.0-py3-none-any.whl/toisto/app.py
@@ -0,0 +1,50 @@
+"""Main module for the application."""
+
+from argparse import Namespace
+from configparser import ConfigParser
+from contextlib import suppress
+
+with suppress(ImportError):
+    import readline  # noqa: F401 `readline` imported but unused
+
+from .command.practice import practice
+from .command.show_progress import show_progress
+from .metadata import BUILT_IN_CONCEPT_JSON_FILES, latest_version
+from .model.filter import filter_concepts
+from .model.language import LanguagePair
+from .model.quiz.progress import Progress
+from .model.quiz.quiz_factory import create_quizzes
+from .persistence.concept_loader import ConceptLoader
+from .persistence.config import default_config, read_config
+from .persistence.progress import load_progress
+from .persistence.spelling_alternatives import load_spelling_alternatives
+from .ui.cli import create_argument_parser, parse_arguments
+from .ui.text import console, show_welcome
+
+
+def init() -> tuple[LanguagePair, ConfigParser, Namespace, Progress]:
+    """Initialize the main program."""
+    argument_parser = create_argument_parser(default_config())
+    config = read_config(argument_parser)
+    loader = ConceptLoader(argument_parser)
+    concepts = loader.load_concepts(*BUILT_IN_CONCEPT_JSON_FILES)
+    argument_parser = create_argument_parser(config, concepts)
+    args = parse_arguments(argument_parser)
+    language_pair = LanguagePair(args.target_language, args.source_language)
+    load_spelling_alternatives(language_pair)
+    concepts |= loader.load_concepts(*args.file)
+    concepts = filter_concepts(concepts, args.concepts, args.target_language, argument_parser)
+    quizzes = create_quizzes(language_pair, *concepts)
+    progress = load_progress(args.target_language, quizzes, argument_parser)
+    return language_pair, config, args, progress
+
+
+def main() -> None:
+    """Run the main program."""
+    language_pair, config, args, progress = init()
+    match args.command:
+        case "progress":
+            show_progress(progress, args.sort)
+        case _:  # Default command is "practice"
+            show_welcome(console.print, latest_version(), config)
+            practice(console.print, language_pair, progress, config, args.progress_update)
---
+++ packages/Toisto/Toisto-0.25.0-py3-none-any.whl/toisto/persistence/config.py
@@ -0,0 +1,125 @@
+"""Config file parser."""
+
+from argparse import ArgumentParser
+from collections.abc import Callable, Iterable
+from configparser import ConfigParser, Error
+from dataclasses import dataclass, field
+from enum import Enum
+from pathlib import Path
+from typing import Final, NoReturn
+
+from toisto.model.language.iana_language_subtag_registry import ALL_LANGUAGES
+from toisto.tools import platform
+
+from .folder import home
+
+# The schema for the config file. Top-level keys are sections, values are a dict per option with the key being the
+# option name and the value being an option consisting of the option quantifier, the allowed option values, and a
+# function evaluating the validity of the option value.
+
+
+class Quantifier(Enum):
+    """Quantifier enumeration."""
+
+    ANY = "any"
+    INTEGER = "whole numbers"
+    ONE_OF = "one of"
+
+
+@dataclass
+class Option:
+    """Configuration option."""
+
+    quantifier: Quantifier
+    allowed_values: Iterable[str] = field(default_factory=list)
+    is_valid: Callable[[str], bool] = field(default=lambda _value: True)
+    default_value: Callable[[], str] | str = ""
+
+
+DEFAULT_MP3PLAYERS = dict(darwin="afplay", linux="mpg123 --quiet")
+CONFIG_SCHEMA: Final[dict[str, dict[str, Option]]] = dict(
+    languages=dict(
+        target=Option(Quantifier.ONE_OF, ALL_LANGUAGES.keys(), ALL_LANGUAGES.__contains__),
+        source=Option(Quantifier.ONE_OF, ALL_LANGUAGES.keys(), ALL_LANGUAGES.__contains__),
+    ),
+    commands=dict(
+        mp3player=Option(Quantifier.ANY, default_value=lambda: DEFAULT_MP3PLAYERS.get(platform(), "builtin")),
+    ),
+    practice=dict(
+        progress_update=Option(Quantifier.INTEGER, ["0", "1", "2", "3", "..."], lambda value: value.isdigit(), "0"),
+    ),
+)
+CONFIG_FILENAME = home() / ".toisto.cfg"
+
+
+class ConfigSchemaValidator:
+    """Class to validate a config against the schema."""
+
+    def __init__(self, config_parser: ConfigParser, argument_parser: ArgumentParser, config_filename: Path) -> None:
+        self._argument_parser = argument_parser
+        self._config_parser = config_parser
+        self._config_filename = config_filename
+
+    def _error(self, message: str) -> NoReturn:
+        """Report the error and exit."""
+        self._argument_parser.error(f"While reading from '{self._config_filename}': {message}")
+
+    def validate(self) -> None:
+        """Validate the config file against the schema."""
+        for section in self._config_parser.sections():
+            self._validate_section(section)
+
+    def _validate_section(self, section: str) -> None:
+        """Validate the section, including its options."""
+        if section not in (allowed_sections := CONFIG_SCHEMA.keys()):
+            self._error(f"unknown section '{section}'. Allowed sections are: {', '.join(allowed_sections)}.")
+        for option in self._config_parser[section]:
+            self._validate_option(section, option)
+
+    def _validate_option(self, section: str, option_name: str) -> None:
+        """Validate the option, including its value(s)."""
+        if option_name not in (allowed_options := CONFIG_SCHEMA[section].keys()):
+            self._error(
+                f"unknown option '{option_name}' in section '{section}'. "
+                f"Allowed options are: {', '.join(allowed_options)}.",
+            )
+        option = CONFIG_SCHEMA[section][option_name]
+        value = self._config_parser.get(section, option_name)
+        if not option.is_valid(value):
+            self._error(
+                f"incorrect value '{value}' for option '{option_name}' in section '{section}'. "
+                f"Allowed values are {option.quantifier.value}: {', '.join(option.allowed_values)}.",
+            )
+
+
+def read_config(argument_parser: ArgumentParser, config_filename: Path = CONFIG_FILENAME) -> ConfigParser:
+    """Read the config file, validate it, and exit with an error message if it doesn't pass."""
+    parser = ConfigParser()
+    try:
+        with config_filename.open("r", encoding="utf-8") as config_file:
+            parser.read_file(config_file)
+    except FileNotFoundError:
+        pass
+    except (OSError, Error) as reason:
+        argument_parser.error(str(reason))
+    ConfigSchemaValidator(parser, argument_parser, config_filename).validate()
+    _add_defaults(parser)
+    return parser
+
+
+def default_config() -> ConfigParser:
+    """Return the default configuration."""
+    parser = ConfigParser()
+    _add_defaults(parser)
+    return parser
+
+
+def _add_defaults(parser: ConfigParser) -> None:
+    """Add the default configuration to the parser."""
+    for section in CONFIG_SCHEMA:
+        if section not in parser.sections():
+            parser.add_section(section)
+        for option_name, option in CONFIG_SCHEMA[section].items():
+            default = option.default_value
+            if default and option_name not in parser[section]:
+                parser[section][option_name] = default if isinstance(default, str) else default()
