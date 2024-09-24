065bdf9a1deab13ea946c0ac7f8e46a6caa00e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:10:22 +0000

    Add package asyncdb/2.9.2/asyncdb-2.9.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/asyncdb/asyncdb-2.9.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/asyncdb/drivers/outputs/generator.py
@@ -0,0 +1,18 @@
+"""
+Generator.
+
+Output format returning a list of dictionaries as a generator
+"""
+from .base import OutputFormat
+
+
+class genFormat(OutputFormat):
+    """
+    Most Basic Definition of Format.
+    """
+
+    async def serialize(self, result, error, *args, **kwargs):
+        if error:
+            return (None, error)
+        lsgen = (dict(row) for row in result)
+        return (lsgen, error)
