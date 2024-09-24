51f0b1afc02d98025d97bdfb934ca14dcd7ff48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:10:38 +0000

    Add package physeng/0.5.0/physeng-0.5.0.tar.gz

---
+++ packages/physeng/physeng-0.5.0.tar.gz/physeng-0.5.0/physeng/materials/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Andreas Mussgiller
+"""
+
+from .materialdb import MaterialDB
+from .material import *
+from .materialproperty import *
