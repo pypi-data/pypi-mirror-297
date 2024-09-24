21715458ffce176c2af77e1cc63dda2695899a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:05:51 +0000

    Add package optymus/0.1.8/optymus-0.1.8.tar.gz

---
+++ packages/optymus/optymus-0.1.8.tar.gz/optymus-0.1.8/src/optymus/benchmark/__init__.py
@@ -0,0 +1,36 @@
+
+from optymus.benchmark._comparison import methods_comparison
+from optymus.benchmark._obj_functions import (
+    AckleyFunction,
+    BealeFunction,
+    BoothFunction,
+    CrossintrayFunction,
+    CustomFunction,
+    EggholderFunction,
+    GoldsteinPriceFunction,
+    HimmeblauFunction,
+    MccormickFunction,
+    RastriginFunction,
+    RosenbrockFunction,
+    SphereFunction,
+    StyblinskiTangFunction,
+    SemionescuFunction,
+)
+
+__all__ = [
+    'MccormickFunction',
+    'RastriginFunction',
+    'AckleyFunction',
+    'EggholderFunction',
+    'CrossintrayFunction',
+    'SphereFunction',
+    'RosenbrockFunction',
+    'BealeFunction',
+    'GoldsteinPriceFunction',
+    'BoothFunction',
+    'StyblinskiTangFunction',
+    'SemionescuFunction',
+    'CustomFunction',
+    'HimmeblauFunction',
+    'methods_comparison',
+]
---
+++ packages/optymus/optymus-0.1.8.tar.gz/optymus-0.1.8/src/optymus/plots/__init__.py
@@ -0,0 +1,7 @@
+
+from optymus.plots._plots import plot_function, plot_optim
+
+__all__ = [
+    "plot_function",
+    "plot_optim",
+]
