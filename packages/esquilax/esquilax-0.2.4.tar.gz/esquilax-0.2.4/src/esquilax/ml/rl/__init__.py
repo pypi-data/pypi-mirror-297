abfa980f7ee13d0cbb317da4f781eed11439c2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:09:36 +0000

    Add package esquilax/0.2.4/esquilax-0.2.4.tar.gz

---
+++ packages/esquilax/esquilax-0.2.4.tar.gz/esquilax-0.2.4/src/esquilax/ml/rl/__init__.py
@@ -0,0 +1,8 @@
+"""
+Reinforcement learning utilities and training functionality
+"""
+from .agent import Agent
+from .agent_state import AgentState, BatchAgentState
+from .environment import Environment
+from .training import test, train
+from .types import Trajectory
