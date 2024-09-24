698a6b4ed257de4b3175dafc16418a8d721dddf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 15:05:45 +0000

    Add package common-web/0.1.9/common-web-0.1.9.tar.gz

---
+++ packages/common-web/common-web-0.1.9.tar.gz/common-web-0.1.9/dto/res_dto.py
@@ -0,0 +1,14 @@
+
+
+class ResObj:
+    __res = None
+
+    def __init__(self, data=None, message='', code=200):
+        self.__res = {
+            "code": code,
+            "msg": message,
+            "data": data
+        }
+
+    def get_res(self):
+        return self.__res
