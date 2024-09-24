6bb5d832e38c964960974e232ce8f5270befbddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:07:35 +0000

    Add package tracking-py/1.0.0/tracking_py-1.0.0.tar.gz

---
+++ packages/tracking-py/tracking_py-1.0.0.tar.gz/tracking_py-1.0.0/tracking/hand_tracking/partials/__init__.py
@@ -0,0 +1,6 @@
+from .drawing_hand import DrawingHand
+from .infos_hand import InfosMethods
+from .measures_hand import MeasuresMethods
+
+
+__all__ = ['DrawingHand', 'InfosMethods', 'MeasuresMethods']
