ca68a3609c223d3b32cb05400b2aedf35917f734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 05:05:34 +0000

    Add package sensei/0.1.0b3.post1/sensei-0.1.0b3.post1.tar.gz

---
+++ packages/sensei/sensei-0.1.0b3.post1.tar.gz/sensei-0.1.0b3.post1/sensei/_base_client.py
@@ -0,0 +1,40 @@
+from abc import abstractmethod, ABC
+from ._utils import get_path_params, fill_path_params
+from .types import IRateLimit, IResponse
+from sensei._descriptors import RateLimitAttr, PortAttr
+
+
+class BaseClient(ABC):
+    rate_limit = RateLimitAttr()
+    port = PortAttr()
+
+    def __init__(
+            self,
+            host: str,
+            port: int | None = None,
+            rate_limit: IRateLimit | None = None,
+    ):
+        self.rate_limit = rate_limit
+        self.port = port
+
+        if host.endswith('/'):
+            host = host[:-1]
+
+        self._host = host
+
+        if 'port' in get_path_params(host):
+            api_url = fill_path_params(host, {'port': port})
+        elif port is not None:
+            api_url = f'{host}:{port}'
+        else:
+            api_url = host
+
+        self._api_url = api_url
+
+    @property
+    def host(self) -> str:
+        return self._host
+
+    @abstractmethod
+    def request(self, method: str, *args, **kwargs) -> IResponse:
+        pass
