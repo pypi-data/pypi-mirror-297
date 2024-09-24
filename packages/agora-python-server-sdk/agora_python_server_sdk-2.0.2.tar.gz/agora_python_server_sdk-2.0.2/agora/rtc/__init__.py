13414398cc819e0e4d40dd83fca80dc0f567eced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 16:05:13 +0000

    Add package agora-python-server-sdk/2.0.2/agora_python_server_sdk-2.0.2.tar.gz

---
+++ packages/agora-python-server-sdk/agora_python_server_sdk-2.0.2.tar.gz/agora_python_server_sdk-2.0.2/agora/rtc/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import ctypes
+
+sdk_dir = os.path.dirname(os.path.abspath(__file__))
+lib_path = os.path.join(sdk_dir, 'agora_sdk')
+
+try:
+    if sys.platform == 'darwin':
+        lib_agora_rtc_path =os.path.join(lib_path, 'libAgoraRtcKit.dylib')
+        agora_lib = ctypes.CDLL(lib_agora_rtc_path)
+
+    elif sys.platform == 'linux':
+        lib_agora_rtc_path =os.path.join(lib_path, 'libagora_rtc_sdk.so')
+        ctypes.CDLL(os.path.join(lib_path, 'libagora-fdkaac.so'))
+        ctypes.CDLL(os.path.join(lib_path, 'libaosl.so'))
+        agora_lib = ctypes.CDLL(lib_agora_rtc_path)    
+except OSError as e:
+    print(f"Error loading the library: {e}")
+    print(f"Attempted to load from: {lib_agora_rtc_path}")
+    sys.exit(1)
