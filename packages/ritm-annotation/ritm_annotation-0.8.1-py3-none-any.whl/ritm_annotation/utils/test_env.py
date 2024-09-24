b87ceb0e4acf120b33eb419adb9ba88acc5eef43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 05:05:32 +0000

    Add package ritm-annotation/0.8.1/ritm_annotation-0.8.1-py3-none-any.whl

---
+++ packages/ritm-annotation/ritm_annotation-0.8.1-py3-none-any.whl/ritm_annotation/utils/test_env.py
@@ -0,0 +1,12 @@
+from easydict import EasyDict as edict
+
+import ritm_annotation.utils.i18n  # noqa:F401
+
+from .env import load_cfg_from_env
+
+
+def test_load_cfg_from_env():
+    input_dict = {"RITM_a": 2, "RITM_eoq__trabson": 3}
+    loaded = load_cfg_from_env(edict(), input_dict)
+    assert loaded.a == 2
+    assert loaded.eoq.trabson == 3
