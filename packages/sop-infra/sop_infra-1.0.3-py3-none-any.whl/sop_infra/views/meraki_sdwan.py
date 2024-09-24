5c2b90aaa25860590cf8ccf1b3cc4aed43c0d44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 16:05:24 +0000

    Add package sop-infra/1.0.3/sop_infra-1.0.3-py3-none-any.whl

---
+++ packages/sop-infra/sop_infra-1.0.3-py3-none-any.whl/sop_infra/views/meraki_sdwan.py
@@ -0,0 +1,34 @@
+from django.utils.translation import gettext_lazy as _
+
+from netbox.views import generic
+
+from sop_utils.utils import *
+
+from ..forms.meraki_sdwan import *
+from ..models import *
+
+
+__all__ = (
+    'MerakiSDWANDetailView',
+    'MerakiSDWANEditView',
+)
+
+
+class MerakiSDWANDetailView(generic.ObjectView):
+    '''
+    returns the MerakiSDWAN detail page with context
+    '''
+    queryset = InfraMerakiSDWAN.objects.all()
+
+
+class MerakiSDWANEditView(generic.ObjectEditView):
+    '''
+    edits a MerakiSDWAN instance
+    '''
+    queryset = InfraMerakiSDWAN.objects.all()
+    form = MerakiSDWANForm
+
+    def get_return_url(self, request, obj=None) -> str:
+        try:
+            return f'/dcim/sites/{obj.site.id}/infra'
+        except:return f'/dcim/sites/'
