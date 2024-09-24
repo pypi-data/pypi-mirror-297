c056bccbf6f3eab60f417a06df4b58487f4338d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 05:05:02 +0000

    Add package pypeec/4.18.3/pypeec-4.18.3.tar.gz

---
+++ packages/pypeec/pypeec-4.18.3.tar.gz/pypeec-4.18.3/pypeec/lib_check/check_data_geometry.py
@@ -0,0 +1,155 @@
+"""
+Module for checking the geometry data.
+"""
+
+__author__ = "Thomas Guillod"
+__copyright__ = "Thomas Guillod - Dartmouth College"
+__license__ = "Mozilla Public License Version 2.0"
+
+from pypeec.lib_check import datachecker
+from pypeec.lib_check import check_data_geometry_png
+from pypeec.lib_check import check_data_geometry_stl
+from pypeec.lib_check import check_data_geometry_shape
+from pypeec.lib_check import check_data_geometry_voxel
+
+
+def _check_resampling(resampling):
+    """
+    Check the validity of resampling parameters.
+    """
+
+    # check type
+    key_list = [
+        "use_reduce",
+        "use_resample",
+        "resampling_factor",
+    ]
+    datachecker.check_dict("resampling", resampling, key_list=key_list)
+
+    # check data
+    datachecker.check_boolean("use_reduce", resampling["use_reduce"])
+    datachecker.check_boolean("use_resample", resampling["use_resample"])
+    datachecker.check_integer_array("resampling_factor", resampling["resampling_factor"], size=3, is_positive=True, can_be_zero=False)
+
+
+def _check_domain_conflict(domain_list, domain_conflict):
+    """
+    Check the validity of the rules to solve conflict between domains (STL mesher).
+    """
+
+    # check type
+    datachecker.check_list("domain_conflict", domain_conflict, can_be_empty=True)
+
+    # check value
+    for domain_conflict_tmp in domain_conflict:
+        # check type
+        key_list = ["domain_keep", "domain_resolve"]
+        datachecker.check_dict("domain_conflict", domain_conflict_tmp, key_list=key_list)
+
+        # extract the data
+        domain_keep = domain_conflict_tmp["domain_keep"]
+        domain_resolve = domain_conflict_tmp["domain_resolve"]
+
+        # check type
+        datachecker.check_list("domain_keep", domain_keep, sub_type=str, can_be_empty=False)
+        datachecker.check_list("domain_resolve", domain_resolve, sub_type=str, can_be_empty=False)
+
+        # check data
+        for tag in domain_keep:
+            datachecker.check_choice("domain_keep", tag, domain_list)
+        for tag in domain_resolve:
+            datachecker.check_choice("domain_resolve", tag, domain_list)
+
+
+def _check_domain_connection(domain_list, domain_connection):
+    """
+    Check the domain connection data.
+    This list is defining the required connection between the domain
+    """
+
+    # check type
+    datachecker.check_dict("domain_connection", domain_connection)
+
+    # check value
+    for domain_connection_tmp in domain_connection.values():
+        # check type
+        key_list = ["connected", "domain_group"]
+        datachecker.check_dict("domain_connection", domain_connection_tmp, key_list=key_list)
+
+        # extract field
+        domain_group = domain_connection_tmp["domain_group"]
+        connected = domain_connection_tmp["connected"]
+
+        # check type
+        datachecker.check_boolean("connected", connected)
+        datachecker.check_list("domain_group", domain_group, can_be_empty=False)
+
+        # check value
+        for domain_group_tmp in domain_group:
+            datachecker.check_list("domain_group", domain_group_tmp, sub_type=str, can_be_empty=False)
+            for tag in domain_group_tmp:
+                datachecker.check_choice("domain_group", tag, domain_list)
+
+
+def check_data_geometry(data_geometry):
+    """
+    Check the mesher data type and extract the data.
+    """
+
+    # check type
+    key_list = [
+        "mesh_type",
+        "data_voxelize",
+        "resampling",
+        "check_cloud",
+        "check_conflict",
+        "check_connection",
+        "random_resolution",
+        "domain_conflict",
+        "domain_connection",
+        "pts_cloud",
+    ]
+    datachecker.check_dict("data_geometry", data_geometry, key_list=key_list)
+
+    # extract field
+    mesh_type = data_geometry["mesh_type"]
+    data_voxelize = data_geometry["data_voxelize"]
+    resampling = data_geometry["resampling"]
+    check_cloud = data_geometry["check_cloud"]
+    check_conflict = data_geometry["check_conflict"]
+    check_connection = data_geometry["check_connection"]
+    random_resolution = data_geometry["random_resolution"]
+    domain_conflict = data_geometry["domain_conflict"]
+    domain_connection = data_geometry["domain_connection"]
+    pts_cloud = data_geometry["pts_cloud"]
+
+    # check type
+    datachecker.check_choice("mesh_type", mesh_type, ["stl", "png", "shape", "voxel"])
+    datachecker.check_boolean("check_cloud", check_cloud)
+    datachecker.check_boolean("check_conflict", check_conflict)
+    datachecker.check_boolean("check_connection", check_connection)
+    datachecker.check_boolean("random_resolution", random_resolution)
+
+    # check the mesher
+    if mesh_type == "png":
+        domain_list = check_data_geometry_png.check_data_voxelize(data_voxelize)
+    elif mesh_type == "stl":
+        domain_list = check_data_geometry_stl.check_data_voxelize(data_voxelize)
+    elif mesh_type == "shape":
+        domain_list = check_data_geometry_shape.check_data_voxelize(data_voxelize)
+    elif mesh_type == "voxel":
+        domain_list = check_data_geometry_voxel.check_data_voxelize(data_voxelize)
+    else:
+        raise ValueError("invalid mesh type")
+
+    # check the resampling data
+    _check_resampling(resampling)
+
+    # check the conflict data
+    _check_domain_conflict(domain_list, domain_conflict)
+
+    # check the connection data
+    _check_domain_connection(domain_list, domain_connection)
+
+    # check the point cloud
+    datachecker.check_float_pts("pts_cloud", pts_cloud, size=3, can_be_empty=True)
---
+++ packages/pypeec/pypeec-4.18.3.tar.gz/pypeec-4.18.3/pypeec/lib_mesher/voxel_connection.py
@@ -0,0 +1,163 @@
+"""
+Different functions for finding the connected components of a voxel structure.
+First, a sparse undirected graph is constructed from the voxel structure.
+Afterward, the connected components of the graph are extracted.
+"""
+
+__author__ = "Thomas Guillod"
+__copyright__ = "Thomas Guillod - Dartmouth College"
+__license__ = "Mozilla Public License Version 2.0"
+
+import numpy as np
+import scipy.sparse as sps
+import scipy.sparse.csgraph as csg
+
+
+def _get_all_indices(domain_def):
+    """
+    Get the indices of the non-empty voxels for all the domains.
+    """
+
+    # init
+    idx = np.empty(0, dtype=np.int_)
+
+    # get the indices and colors
+    for tag in domain_def:
+        idx_tmp = domain_def[tag]
+        idx = np.append(idx, idx_tmp)
+
+    return idx
+
+
+def _get_tag_indices(domain_def, domain_list):
+    """
+    Get the indices of the non-empty voxels for the given domains.
+    """
+
+    # init
+    idx = np.empty(0, dtype=np.int_)
+
+    # get the indices and colors
+    for tag in domain_list:
+        idx_tmp = domain_def[tag]
+        idx = np.append(idx, idx_tmp)
+
+    return idx
+
+
+def _get_connection_matrix(n):
+    """
+    Get a sparse matrix describing the connection between the voxels.
+    This matrix is extracted for the complete voxel structure.
+    """
+
+    # extract the voxel data
+    (nx, ny, nz) = n
+    nv = nx*ny*nz
+
+    # voxel index array
+    x = np.arange(nx, dtype=np.int_)
+    y = np.arange(ny, dtype=np.int_)
+    z = np.arange(nz, dtype=np.int_)
+    (idx_x, idx_y, idx_z) = np.meshgrid(x, y, z, indexing="ij")
+
+    # voxel index number
+    idx = idx_x+idx_y*nx+idx_z*nx*ny
+
+    # create the sparse matrix
+    A_connection = sps.csc_matrix((nv, nv), dtype=np.int_)
+
+    # connections along x direction
+    idx_col = idx[0:-1, :, :].flatten()
+    idx_row = idx[1:, :, :].flatten()
+    data = np.ones((nx-1)*ny*nz, dtype=np.int_)
+    A_connection += sps.csc_matrix((data, (idx_row, idx_col)), shape=(nv, nv), dtype=np.int_)
+
+    # connections along y direction
+    idx_col = idx[:, 0:-1, :].flatten()
+    idx_row = idx[:, 1:, :].flatten()
+    data = np.ones(nx*(ny-1)*nz, dtype=np.int_)
+    A_connection += sps.csc_matrix((data, (idx_row, idx_col)), shape=(nv, nv), dtype=np.int_)
+
+    # connections along z direction
+    idx_col = idx[:, :, 0:-1].flatten()
+    idx_row = idx[:, :, 1:].flatten()
+    data = np.ones(nx*ny*(nz-1), dtype=np.int_)
+    A_connection += sps.csc_matrix((data, (idx_row, idx_col)), shape=(nv, nv), dtype=np.int_)
+
+    return A_connection
+
+
+def _check_domain_connection(domain_def, connection_def, tag_connection, domain_connection):
+    """
+    Check that the given connections between the domain exists.
+    """
+
+    # extract the data
+    domain_group = domain_connection["domain_group"]
+    connected = domain_connection["connected"]
+
+    # remove empty domains
+    idx_group = []
+    for domain_group_tmp in domain_group:
+        idx_tmp = _get_tag_indices(domain_def, domain_group_tmp)
+        idx_group.append(idx_tmp)
+
+    # init the connection matrix
+    matrix = np.full((len(connection_def), len(idx_group)), True, dtype=bool)
+
+    # fill the connection matrix
+    for i, idx_graph in enumerate(connection_def):
+        for j, idx_domain in enumerate(idx_group):
+            idx_shared = np.intersect1d(idx_graph, idx_domain)
+            matrix[i, j] = len(idx_shared) > 0
+
+    # count connection
+    vector = np.count_nonzero(matrix, axis=1)
+
+    # check validity
+    if connected:
+        idx_ok = vector == len(idx_group)
+        if not np.any(idx_ok):
+            raise RuntimeError("domain connection: domain connection is missing: %s" % tag_connection)
+    else:
+        idx_ok = np.logical_or(vector == 0, vector == 1)
+        if not np.all(idx_ok):
+            raise RuntimeError("domain connection: domain connection is illegal: %s" % tag_connection)
+
+
+def get_connection(n, domain_def, domain_connection):
+    """
+    Find the connected components of a voxel structure.
+    """
+
+    # get the indices of the non-empty voxels
+    idx = _get_all_indices(domain_def)
+
+    # get the connection matrix between the voxels
+    A_connection = _get_connection_matrix(n)
+
+    # get the graph matrix
+    A_graph = A_connection
+    A_graph = A_graph[idx, :]
+    A_graph = A_graph[:, idx]
+
+    # find the connected components in the graph
+    (n_comp, labels) = csg.connected_components(
+        csgraph=A_graph,
+        directed=False,
+        return_labels=True,
+    )
+
+    # get the indices of the connected components
+    connection_def = []
+    for i in range(n_comp):
+        idx_local = labels == i
+        idx_graph = idx[idx_local]
+        connection_def.append(idx_graph)
+
+    # check the connections between the domains
+    for tag, domain_connection_tmp in domain_connection.items():
+        _check_domain_connection(domain_def, connection_def, tag, domain_connection_tmp)
+
+    return connection_def
