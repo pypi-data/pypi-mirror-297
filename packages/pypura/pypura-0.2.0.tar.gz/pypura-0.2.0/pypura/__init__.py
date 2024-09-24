1debdfb750736877a0973c4c98a5b954876efc74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:07:04 +0000

    Add package pypura/0.2.0/pypura-0.2.0.tar.gz

---
+++ packages/pypura/pypura-0.2.0.tar.gz/pypura-0.2.0/pypura/__init__.py
@@ -0,0 +1,8 @@
+"""pypura module."""
+
+from .exceptions import PuraApiException, PuraAuthenticationError
+from .pura import Pura
+from .utils import fragrance_name
+
+__all__ = ["Pura", "PuraApiException", "PuraAuthenticationError", "fragrance_name"]
+__version__ = "0.2.0"
