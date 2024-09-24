216d5a57675a53fef501806a4d3b0a05fdc75eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 04:05:34 +0000

    Add package Taweret/1.1.0/Taweret-1.1.0-py3-none-any.whl

---
+++ packages/Taweret/Taweret-1.1.0-py3-none-any.whl/Taweret/core/base_model.py
@@ -0,0 +1,77 @@
+from abc import ABC, abstractmethod
+
+
+class BaseModel(ABC):
+    @abstractmethod
+    def evaluate(self):
+        '''
+        Calculate a point estimate for mixing model
+
+        Returns:
+        --------
+        evaluation : float
+            point estimate for model
+
+        Example
+        -------
+
+        .. code-block:: python
+
+                class MyModel(BaseModel):
+                    @property
+                    def model(self):
+                        return self._model
+
+                    @models.setter
+                    def model(self, the_model):
+                        self._model = the_model
+
+                    def evaluate(self, model_parameters):
+                        return self._model(model_parameters)
+                    # . . .
+        '''
+
+    @abstractmethod
+    def log_likelihood_elementwise(self):
+        r'''
+        Calculate log_likelihood for array of points given
+
+        Returns:
+        --------
+        log_likelis : np.ndarray
+            an array of length as shape[0] of the input evaluation points
+
+        Example:
+        --------
+
+        .. code-block:: python
+
+                class MyModel(BaseModel):
+                    def log_likelihood_elementwise(
+                        self, y_exp, y_err, model_params
+                    ):
+                        # Assuming a normal distribution for error
+                        y = self.evaluate(model_params)
+                        # If y_exp, y_err, y are numpy arrays of same length
+                        return np.exp(-(y - y_exp) **2 / (2 * y_err ** 2)) \
+                            / np.sqrt(2 * np.pi * y_err ** 2))
+        '''
+
+    @abstractmethod
+    def set_prior(self):
+        '''
+        User must provide function that sets a member variable called _prior.
+        Dictionary of prior distributions. Format should be compatible with
+        sampler.
+
+        Example:
+        --------
+
+        .. code-block:: python
+
+                class MyModel(BaseMixer):
+                    # . . .
+                    def set_prior(self, prior_dict):
+                        self._prior = prior_dict
+                    # . . .
+        '''
---
+++ packages/Taweret/Taweret-1.1.0-py3-none-any.whl/Taweret/sampler/likelihood_wrappers.py
@@ -0,0 +1,74 @@
+# Add any new likelihood wrappers here to be used with other calibration
+# packages
+import bilby
+import numpy as np
+
+
+class likelihood_wrapper_for_bilby(bilby.Likelihood):
+
+    def __init__(self, mixed_model, x_exp, y_exp, y_err):
+
+        """
+        Likelihood wrapper for Bilby calibrator
+        ...
+
+        Attributes:
+        -----------
+        mixed_model : object
+            mixed model object from Taweret model mixing class
+        x_exp : array
+            The independent variables for the experimental data.
+        y_exp : array
+            The dependent variables for the data.
+        y_err : array
+            The error bands on the data.
+
+        Methods:
+        --------
+        log_likelihood(self)
+            calculates the log likelihood for the parameter values specified in
+            the wrapper object.
+        """
+
+        param_dic = {i: None
+                     for i in mixed_model.prior.keys()
+                     }
+
+        super().__init__(parameters=param_dic)
+        self.mixed_model = mixed_model
+        self.x_exp = x_exp
+        self.y_exp = y_exp
+        self.y_err = y_err
+
+    def log_likelihood(self):
+        """
+        The log likelihood function that can be used with Bilby.
+
+        Parameters:
+        -----------
+        None.
+
+        Returns:
+        --------
+            The scalar log likelihood value.
+
+        """
+        params = list(self.parameters.values())
+
+        # Because when putting consteaints dummy variables enter which are None
+        # in parameters
+        params = [i for i in params if i is not None]
+        params = np.array(params).flatten()
+
+        mix_param = params[0:self.mixed_model.n_mix]
+        models_params = []
+        tot_sum = self.mixed_model.n_mix
+        n_args_list = list(self.mixed_model.nargs_model_dic.values())
+        for i in range(0, len(n_args_list)):
+            models_params.append(params[tot_sum: tot_sum + n_args_list[i]])
+            tot_sum += n_args_list[i]
+            if self.mixed_model.same_parameters:
+                break
+
+        return self.mixed_model.mix_loglikelihood(
+            mix_param, models_params, self.x_exp, self.y_exp, self.y_err)
