8e020011ee788a46d6220b1c52c1e50bc5a82065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:08:47 +0000

    Add package py-mod-test/1.0.11/py_mod_test-1.0.11.tar.gz

---
+++ packages/py-mod-test/py_mod_test-1.0.11.tar.gz/py_mod_test-1.0.11/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py_mod_test',
+    version='1.0.11',  # Versão inicial, será atualizada automaticamente
+    description='Ferramenta para deploy de pacotes Python',
+    long_description=open('README.md').read(),  # Adicione um README.md para descrição longa
+    long_description_content_type='text/markdown',
+    author='Fernando Pessoa',
+    author_email='fefe@example.com',  # Adicione seu email aqui
+    url='https://github.com/fpessoa64/py_mod_test',
+    packages=find_packages(),
+    install_requires=[],  # Adicione dependências aqui se houver
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Mude se usar outra licença
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Especifica a versão mínima do Python
+
+)
