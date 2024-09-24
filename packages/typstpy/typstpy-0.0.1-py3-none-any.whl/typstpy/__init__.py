87dce40c8a9521dbd72a700ca512288a4fb465c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 15:05:15 +0000

    Add package typstpy/0.0.1/typstpy-0.0.1-py3-none-any.whl

---
+++ packages/typstpy/typstpy-0.0.1-py3-none-any.whl/typstpy/__init__.py
@@ -0,0 +1,18 @@
+from .functions import emph, figure, heading, image, par, strong, text
+from .param_types import Block, Content, Label, Length, Ratio, Relative
+
+__all__ = [
+    "emph",
+    "figure",
+    "heading",
+    "image",
+    "par",
+    "strong",
+    "text",
+    "Block",
+    "Content",
+    "Label",
+    "Length",
+    "Ratio",
+    "Relative",
+]
