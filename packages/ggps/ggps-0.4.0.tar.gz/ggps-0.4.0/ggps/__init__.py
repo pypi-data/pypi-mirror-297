15ba53c1a7773dc98408f47183a4facbdcd98dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 06:05:39 +0000

    Add package ggps/0.4.0/ggps-0.4.0.tar.gz

---
+++ packages/ggps/ggps-0.4.0.tar.gz/ggps-0.4.0/ggps/__init__.py
@@ -0,0 +1,15 @@
+"""
+ggps library
+"""
+
+__author__ = "Chris Joakim <christopher.joakim@gmail.com>"
+__version__ = "0.4.0"
+
+AUTHOR = __author__
+VERSION = __version__
+
+from ggps.trackpoint import Trackpoint
+from ggps.base_handler import BaseHandler
+from ggps.gpx_handler import GpxHandler
+from ggps.path_handler import PathHandler
+from ggps.tcx_handler import TcxHandler
---
+++ packages/ggps/ggps-0.4.0.tar.gz/ggps-0.4.0/tests/test_library_info.py
@@ -0,0 +1,9 @@
+import ggps
+
+
+def test_author():
+    assert ggps.AUTHOR == "Chris Joakim <christopher.joakim@gmail.com>"
+
+
+def test_version():
+    assert ggps.VERSION == "0.4.0"
