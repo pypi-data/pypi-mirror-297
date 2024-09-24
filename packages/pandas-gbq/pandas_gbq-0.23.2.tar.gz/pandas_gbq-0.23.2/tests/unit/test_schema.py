293f4660ac53f20cfbf2e06db8c8a32ed5adce4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 05:05:12 +0000

    Add package pandas-gbq/0.23.2/pandas_gbq-0.23.2.tar.gz

---
+++ packages/pandas-gbq/pandas_gbq-0.23.2.tar.gz/pandas_gbq-0.23.2/tests/unit/test_schema.py
@@ -0,0 +1,196 @@
+# Copyright (c) 2017 pandas-gbq Authors All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+import datetime
+from typing import Any, Dict, List
+
+import google.cloud.bigquery
+import pandas
+import pytest
+
+
+@pytest.fixture
+def module_under_test():
+    import pandas_gbq.schema
+
+    return pandas_gbq.schema
+
+
+@pytest.mark.parametrize(
+    "original_fields,dataframe_fields",
+    [
+        (
+            [
+                {"name": "A", "type": "FLOAT"},
+                {"name": "B", "type": "FLOAT64"},
+                {"name": "C", "type": "STRING"},
+            ],
+            [{"name": "A", "type": "FLOAT64"}, {"name": "B", "type": "FLOAT"}],
+        ),
+        # Original schema from API may contain legacy SQL datatype names.
+        # https://github.com/pydata/pandas-gbq/issues/322
+        (
+            [{"name": "A", "type": "INTEGER"}],
+            [{"name": "A", "type": "INT64"}],
+        ),
+        (
+            [{"name": "A", "type": "BOOL"}],
+            [{"name": "A", "type": "BOOLEAN"}],
+        ),
+        (
+            # TODO: include sub-fields when struct uploads are supported.
+            [{"name": "A", "type": "STRUCT"}],
+            [{"name": "A", "type": "RECORD"}],
+        ),
+    ],
+)
+def test_schema_is_subset_passes_if_subset(
+    module_under_test, original_fields, dataframe_fields
+):
+    # Issue #24 schema_is_subset indicates whether the schema of the
+    # dataframe is a subset of the schema of the bigquery table
+    table_schema = {"fields": original_fields}
+    tested_schema = {"fields": dataframe_fields}
+    assert module_under_test.schema_is_subset(table_schema, tested_schema)
+
+
+def test_schema_is_subset_fails_if_not_subset(module_under_test):
+    table_schema = {
+        "fields": [
+            {"name": "A", "type": "FLOAT"},
+            {"name": "B", "type": "FLOAT"},
+            {"name": "C", "type": "STRING"},
+        ]
+    }
+    tested_schema = {
+        "fields": [{"name": "A", "type": "FLOAT"}, {"name": "C", "type": "FLOAT"}]
+    }
+    assert not module_under_test.schema_is_subset(table_schema, tested_schema)
+
+
+@pytest.mark.parametrize(
+    "dataframe,expected_schema",
+    [
+        (
+            pandas.DataFrame(data={"col1": [1, 2, 3]}),
+            {"fields": [{"name": "col1", "type": "INTEGER"}]},
+        ),
+        (
+            pandas.DataFrame(data={"col1": [True, False]}),
+            {"fields": [{"name": "col1", "type": "BOOLEAN"}]},
+        ),
+        (
+            pandas.DataFrame(data={"col1": [1.0, 3.14]}),
+            {"fields": [{"name": "col1", "type": "FLOAT"}]},
+        ),
+        (
+            pandas.DataFrame(data={"col1": ["hello", "world"]}),
+            {"fields": [{"name": "col1", "type": "STRING"}]},
+        ),
+        (
+            pandas.DataFrame(data={"col1": [datetime.datetime.now()]}),
+            {"fields": [{"name": "col1", "type": "TIMESTAMP"}]},
+        ),
+        (
+            pandas.DataFrame(
+                data={
+                    "col1": [datetime.datetime.now()],
+                    "col2": ["hello"],
+                    "col3": [3.14],
+                    "col4": [True],
+                    "col5": [4],
+                }
+            ),
+            {
+                "fields": [
+                    {"name": "col1", "type": "TIMESTAMP"},
+                    {"name": "col2", "type": "STRING"},
+                    {"name": "col3", "type": "FLOAT"},
+                    {"name": "col4", "type": "BOOLEAN"},
+                    {"name": "col5", "type": "INTEGER"},
+                ]
+            },
+        ),
+    ],
+)
+def test_generate_bq_schema(module_under_test, dataframe, expected_schema):
+    schema = module_under_test.generate_bq_schema(dataframe)
+    assert schema == expected_schema
+
+
+@pytest.mark.parametrize(
+    "schema_old,schema_new,expected_output",
+    [
+        (
+            {"fields": [{"name": "col1", "type": "INTEGER"}]},
+            {"fields": [{"name": "col2", "type": "TIMESTAMP"}]},
+            # Ignore fields that aren't in the DataFrame.
+            {"fields": [{"name": "col1", "type": "INTEGER"}]},
+        ),
+        (
+            {"fields": [{"name": "col1", "type": "INTEGER"}]},
+            {"fields": [{"name": "col1", "type": "BOOLEAN"}]},
+            # Update type for fields that are in the DataFrame.
+            {"fields": [{"name": "col1", "type": "BOOLEAN"}]},
+        ),
+        (
+            {
+                "fields": [
+                    {"name": "col1", "type": "INTEGER"},
+                    {"name": "col2", "type": "INTEGER"},
+                ]
+            },
+            {
+                "fields": [
+                    {"name": "col2", "type": "BOOLEAN"},
+                    {"name": "col3", "type": "FLOAT"},
+                ]
+            },
+            {
+                "fields": [
+                    {"name": "col1", "type": "INTEGER"},
+                    {"name": "col2", "type": "BOOLEAN"},
+                ]
+            },
+        ),
+    ],
+)
+def test_update_schema(module_under_test, schema_old, schema_new, expected_output):
+    output = module_under_test.update_schema(schema_old, schema_new)
+    assert output == expected_output
+
+
+@pytest.mark.parametrize(
+    ["bq_schema", "expected"],
+    [
+        ([], {"fields": []}),
+        (
+            [google.cloud.bigquery.SchemaField("test_col", "STRING")],
+            {"fields": [{"name": "test_col", "type": "STRING", "mode": "NULLABLE"}]},
+        ),
+        (
+            [google.cloud.bigquery.SchemaField("test_col", "STRING", mode="REQUIRED")],
+            {"fields": [{"name": "test_col", "type": "STRING", "mode": "REQUIRED"}]},
+        ),
+        (
+            [
+                google.cloud.bigquery.SchemaField("test1", "STRING"),
+                google.cloud.bigquery.SchemaField("test2", "INTEGER"),
+            ],
+            {
+                "fields": [
+                    {"name": "test1", "type": "STRING", "mode": "NULLABLE"},
+                    {"name": "test2", "type": "INTEGER", "mode": "NULLABLE"},
+                ]
+            },
+        ),
+    ],
+)
+def test_to_pandas_gbq(
+    bq_schema: List[google.cloud.bigquery.SchemaField], expected: Dict[str, Any]
+):
+    import pandas_gbq.schema
+
+    result = pandas_gbq.schema.to_pandas_gbq(bq_schema)
+    assert result == expected
---
+++ packages/pandas-gbq/pandas_gbq-0.23.2.tar.gz/pandas_gbq-0.23.2/tests/unit/test_to_gbq.py
@@ -0,0 +1,160 @@
+# Copyright (c) 2021 pandas-gbq Authors All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+import google.api_core.exceptions
+import google.cloud.bigquery
+from pandas import DataFrame
+import pytest
+
+from pandas_gbq import gbq
+
+
+@pytest.fixture
+def expected_load_method(mock_bigquery_client):
+    return mock_bigquery_client.load_table_from_dataframe
+
+
+def test_to_gbq_create_dataset_with_location(mock_bigquery_client):
+    mock_bigquery_client.get_table.side_effect = google.api_core.exceptions.NotFound(
+        "my_table"
+    )
+    mock_bigquery_client.get_dataset.side_effect = google.api_core.exceptions.NotFound(
+        "my_dataset"
+    )
+    gbq.to_gbq(
+        DataFrame([[1]]), "my_dataset.my_table", project_id="1234", location="us-west1"
+    )
+    assert mock_bigquery_client.create_dataset.called
+    args, _ = mock_bigquery_client.create_dataset.call_args
+    sent_dataset = args[0]
+    assert sent_dataset.location == "us-west1"
+
+
+def test_to_gbq_create_dataset_translates_exception(mock_bigquery_client):
+    mock_bigquery_client.get_table.side_effect = google.api_core.exceptions.NotFound(
+        "my_table"
+    )
+    mock_bigquery_client.get_dataset.side_effect = google.api_core.exceptions.NotFound(
+        "my_dataset"
+    )
+    mock_bigquery_client.create_dataset.side_effect = (
+        google.api_core.exceptions.InternalServerError("something went wrong")
+    )
+
+    with pytest.raises(gbq.GenericGBQException):
+        gbq.to_gbq(DataFrame([[1]]), "my_dataset.my_table", project_id="1234")
+
+
+def test_to_gbq_load_method_translates_exception(
+    mock_bigquery_client, expected_load_method
+):
+    mock_bigquery_client.get_table.side_effect = google.api_core.exceptions.NotFound(
+        "my_table"
+    )
+    expected_load_method.side_effect = google.api_core.exceptions.InternalServerError(
+        "error loading data"
+    )
+
+    with pytest.raises(gbq.GenericGBQException):
+        gbq.to_gbq(
+            DataFrame({"int_cole": [1, 2, 3]}),
+            "my_dataset.my_table",
+            project_id="myproj",
+        )
+    expected_load_method.assert_called_once()
+
+
+def test_to_gbq_with_if_exists_append(mock_bigquery_client, expected_load_method):
+    from google.cloud.bigquery import SchemaField
+
+    mock_bigquery_client.get_table.return_value = google.cloud.bigquery.Table(
+        "myproj.my_dataset.my_table",
+        schema=(
+            SchemaField("col_a", "FLOAT", mode="REQUIRED"),
+            SchemaField("col_b", "STRING", mode="REQUIRED"),
+        ),
+    )
+    gbq.to_gbq(
+        DataFrame({"col_a": [0.25, 1.5, -1.0], "col_b": ["a", "b", "c"]}),
+        "my_dataset.my_table",
+        project_id="myproj",
+        if_exists="append",
+    )
+    expected_load_method.assert_called_once()
+
+
+def test_to_gbq_with_if_exists_append_mismatch(mock_bigquery_client):
+    from google.cloud.bigquery import SchemaField
+
+    mock_bigquery_client.get_table.return_value = google.cloud.bigquery.Table(
+        "myproj.my_dataset.my_table",
+        schema=(SchemaField("col_a", "INTEGER"), SchemaField("col_b", "STRING")),
+    )
+    mock_bigquery_client.side_effect = gbq.InvalidSchema(
+        message=r"Provided Schema does not match Table *"
+    )
+
+    with pytest.raises((gbq.InvalidSchema)) as exception_block:
+        gbq.to_gbq(
+            DataFrame({"col_a": [0.25, 1.5, -1.0]}),
+            "my_dataset.my_table",
+            project_id="myproj",
+            if_exists="append",
+        )
+
+    exc = exception_block.value
+    assert exc.message == r"Provided Schema does not match Table *"
+
+
+def test_to_gbq_with_if_exists_replace(mock_bigquery_client, expected_load_method):
+    mock_bigquery_client.get_table.side_effect = (
+        # Initial check
+        google.cloud.bigquery.Table("myproj.my_dataset.my_table"),
+        # Recreate check
+        google.api_core.exceptions.NotFound("my_table"),
+    )
+    gbq.to_gbq(
+        DataFrame([[1]]),
+        "my_dataset.my_table",
+        project_id="myproj",
+        if_exists="replace",
+    )
+    expected_load_method.assert_called_once()
+
+
+def test_to_gbq_with_if_exists_replace_cross_project(
+    mock_bigquery_client, expected_load_method
+):
+    mock_bigquery_client.get_table.side_effect = (
+        # Initial check
+        google.cloud.bigquery.Table("data-project.my_dataset.my_table"),
+        # Recreate check
+        google.api_core.exceptions.NotFound("my_table"),
+    )
+    gbq.to_gbq(
+        DataFrame([[1]]),
+        "data-project.my_dataset.my_table",
+        project_id="billing-project",
+        if_exists="replace",
+    )
+    expected_load_method.assert_called_once()
+
+    # Check that billing project and destination table is set correctly.
+    expected_load_method.assert_called_once()
+    load_args, load_kwargs = expected_load_method.call_args
+    table_destination = load_args[1]
+    assert table_destination.project == "data-project"
+    assert table_destination.dataset_id == "my_dataset"
+    assert table_destination.table_id == "my_table"
+    assert load_kwargs["project"] == "billing-project"
+
+
+def test_to_gbq_with_if_exists_unknown():
+    with pytest.raises(ValueError):
+        gbq.to_gbq(
+            DataFrame([[1]]),
+            "my_dataset.my_table",
+            project_id="myproj",
+            if_exists="unknown",
+        )
