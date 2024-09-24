e91d7daee48e16a80e734b9bddfe93c67ed6ee61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 16:05:26 +0000

    Add package pytest_robotframework/4.2.6/pytest_robotframework-4.2.6.tar.gz

---
+++ packages/pytest_robotframework/pytest_robotframework-4.2.6.tar.gz/pytest_robotframework-4.2.6/tests/fixtures/test_python/test_pytest_runtest_protocol_item_hook/foo/conftest.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from typing import Iterator
+
+from pytest import Item, Session, StashKey, hookimpl
+
+run_key = StashKey[int]()
+
+
+@hookimpl(wrapper=True)
+def pytest_runtest_protocol(item: Item) -> Iterator[None]:
+    item.session.stash[run_key] = 1
+    yield
+    item.session.stash[run_key] += 1
+
+
+def pytest_sessionfinish(session: Session):
+    assert session.stash[run_key] == 2
---
+++ packages/pytest_robotframework/pytest_robotframework-4.2.6.tar.gz/pytest_robotframework-4.2.6/tests/fixtures/test_python/test_pytest_runtest_protocol_session_hook/conftest.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from typing import Iterator
+
+from pytest import Item, Session, StashKey, hookimpl
+
+run_key = StashKey[int]()
+
+
+@hookimpl(wrapper=True)
+def pytest_runtest_protocol(item: Item) -> Iterator[None]:
+    item.session.stash[run_key] = 1
+    yield
+    item.session.stash[run_key] += 1
+
+
+def pytest_sessionfinish(session: Session):
+    if run_key in session.stash:  # may not be if running with xdist
+        assert session.stash[run_key] == 2
