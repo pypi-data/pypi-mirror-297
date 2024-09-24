3c747f113fb06d2300b7361f6a1e7720d454a50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 15:05:36 +0000

    Add package coj-judger/0.17/coj_judger-0.17.tar.gz

---
+++ packages/coj-judger/coj_judger-0.17.tar.gz/coj_judger-0.17/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='coj_judger',
+    version='0.17',
+    description='A Framework for developing a judger of COJ.',
+    author='Caiwen',
+    author_email='3102733279@qq.com',
+    url='https://github.com/CZOI-Project/JudgerFramework',
+    packages=['coj_judger'],
+)
