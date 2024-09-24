f2fd4da225b3e6e8bf9d1a71eb28f1ef08fb02da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 06:04:54 +0000

    Add package HugeP2G/1.0.4/hugep2g-1.0.4.tar.gz

---
+++ packages/HugeP2G/hugep2g-1.0.4.tar.gz/hugep2g-1.0.4/hugep2g/versions.py
@@ -0,0 +1,80 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "1.0.4",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "1.0.3",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "1.0.2",
+        "features": [
+            "1. remove dependency of toolbiox",
+        ],
+    },
+    {
+        "number": "1.0.1",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "1.0.0",
+        "features": [
+            "1. release",
+            "2. debug",
+        ],
+    },
+    {
+        "number": "0.1.7",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "0.1.5",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "0.1.4",
+        "features": [
+            "1. Remove the time limit of genewise",
+        ],
+    },
+    {
+        "number": "0.1.3",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "0.1.2",
+        "features": [
+            "1. debug",
+            "2. modify log file",
+        ],
+    },
+    {
+        "number": "0.1.1",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "0.1.0",
+        "features": [
+            "1. Separate WPGmapper from ToolBiox and rename as HugeP2G",
+        ],
+    },
+]
