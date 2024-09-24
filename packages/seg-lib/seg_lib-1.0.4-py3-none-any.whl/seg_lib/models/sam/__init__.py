949c5a9e450b91e42d69411f1befa71b2d90ca80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:09:36 +0000

    Add package seg-lib/1.0.4/seg_lib-1.0.4-py3-none-any.whl

---
+++ packages/seg-lib/seg_lib-1.0.4-py3-none-any.whl/seg_lib/models/sam/__init__.py
@@ -0,0 +1,3 @@
+from seg_lib.models.sam.transforms import ResizeLongestSide
+from seg_lib.models.sam.predictor import SamPredictor
+from seg_lib.models.sam.build_sam import sam_model_registry
---
+++ packages/seg-lib/seg_lib-1.0.4-py3-none-any.whl/seg_lib/prompt/__init__.py
@@ -0,0 +1,6 @@
+from seg_lib.prompt.sampler import Sampler
+from seg_lib.prompt.train_sampler import TrainPromptSampler
+from seg_lib.prompt.inference_after_sample import MaskInferenceAfterSample
+from seg_lib.prompt.samus_sampler import (
+    fixed_bbox, random_bbox
+)
