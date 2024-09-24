f3904a874f9d2132f908014c821dfa8264639a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 15:05:14 +0000

    Add package blar-graph/1.1.0/blar_graph-1.1.0.tar.gz

---
+++ packages/blar-graph/blar_graph-1.1.0.tar.gz/blar_graph-1.1.0/src/blar_graph/run.py
@@ -0,0 +1,23 @@
+import traceback
+
+from blar_graph.db_managers import Neo4jManager
+from blar_graph.graph_construction.core.graph_builder import GraphConstructor
+
+repoId = "test"
+entity_id = "test"
+graph_manager = Neo4jManager(repoId, entity_id)
+
+
+try:
+    graph_constructor = GraphConstructor(
+        root="src",
+        graph_manager=graph_manager,
+        entity_id=entity_id,
+    )
+    nodes, relationships = graph_constructor.build_graph()
+    graph_manager.save_graph(nodes, relationships)
+    graph_manager.close()
+except Exception as e:
+    print(e)
+    print(traceback.format_exc())
+    graph_manager.close()
