2576b9162797f44461d0d64419e7d484dcb359c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 05:05:34 +0000

    Add package serto/0.0.0.dev1/serto-0.0.0.dev1-py3-none-any.whl

---
+++ packages/serto/serto-0.0.0.dev1-py3-none-any.whl/serto/__init__.py
@@ -0,0 +1,11 @@
+DEFAULT_THREADS_PER_SIMULATION = 4
+
+VERSION_INFO = (0, 0, 0, "dev1")
+__version__ = ".".join(map(str, VERSION_INFO))
+__author__ = "Caleb Buahin and Anne Mikelonis"
+__copyright__ = "Copyright (c) 2024"
+__credits__ = ["Caleb Buahin and Anne Mikelonis"]
+__license__ = "MIT"
+__maintainer__ = "Caleb Buahin and Anne Mikelonis"
+__email__ = "calebggh@gmail.com"
+__status__ = "Development"
