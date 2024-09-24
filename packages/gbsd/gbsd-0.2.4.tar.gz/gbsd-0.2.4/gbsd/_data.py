99eac6c021b245173637524d62a1a7db495342bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 06:04:55 +0000

    Add package gbsd/0.2.4/gbsd-0.2.4.tar.gz

---
+++ packages/gbsd/gbsd-0.2.4.tar.gz/gbsd-0.2.4/gbsd/_data.py
@@ -0,0 +1,7 @@
+"""Holds useful data such as scattering lengths."""
+
+# Bound coherent scattering lengths, in fm, from https://www.ncnr.nist.gov/resources/n-lengths/
+NEUTRON_SCATTERING_LENGTHS = {
+    "Si": 4.1491,
+    "O": 5.803,
+}
