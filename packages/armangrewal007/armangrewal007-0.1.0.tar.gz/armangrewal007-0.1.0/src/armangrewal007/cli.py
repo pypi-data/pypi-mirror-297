c6969c2165acf807c9c8f6712d67aecd85ab8e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 05:05:33 +0000

    Add package armangrewal007/0.1.0/armangrewal007-0.1.0.tar.gz

---
+++ packages/armangrewal007/armangrewal007-0.1.0.tar.gz/armangrewal007-0.1.0/src/armangrewal007/cli.py
@@ -0,0 +1,35 @@
+import os
+
+def display_resume():
+    resume = """
+    =======================================
+    Arman Singh Grewal's Resume
+    =======================================
+    
+    Name: Arman Singh Grewal
+    Email: your.email@example.com
+    LinkedIn: https://www.linkedin.com/in/armangrewal007
+    Github: https://github.com/armangrewal007
+    
+    Education:
+    - BSc in Machine Learning/AI
+    - Experience in Software Engineering, Redis, Neo4j, FastAPI, etc.
+
+    Projects:
+    - Redis Optimization
+    - Neo4j Node Management
+    - And more...
+    
+    Work Experience:
+    - Software Engineer Intern at Xperi (TiVo)
+    - Various contributions to open source
+
+    Skills:
+    - Python, FastAPI, Django, Redis, Neo4j, Kafka, Linux
+
+    =======================================
+    """
+    print(resume)
+
+def main():
+    display_resume()
