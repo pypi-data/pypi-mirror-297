d35a5bc65f13f665ae91f2c822f90dc4c6366e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:06:30 +0000

    Add package enhomie/0.11.0/enhomie-0.11.0-py3-none-any.whl

---
+++ packages/enhomie/enhomie-0.11.0-py3-none-any.whl/enhomie/builtins/params/store.py
@@ -0,0 +1,42 @@
+"""
+Functions and routines associated with Enasis Network Homie Automate.
+
+This file is part of Enasis Network software eco-system. Distribution
+is permitted, for more information consult the project license file.
+"""
+
+
+
+from typing import Annotated
+from typing import Literal
+from typing import Optional
+
+from encommon.types import BaseModel
+
+from pydantic import Field
+
+from ..store import BltnStoreOperas
+from ...homie.addons import HomiePersistValue
+
+
+
+class DriverBltnStoreParams(BaseModel, extra='forbid'):
+    """
+    Process and validate the Homie configuration parameters.
+    """
+
+    unique: Annotated[
+        str,
+        Field(...,
+              description='Unique key for the value',
+              min_length=1)]
+
+    operator: Annotated[
+        Literal[BltnStoreOperas],
+        Field(...,
+              description='Operator for the condition')]
+
+    value: Annotated[
+        Optional[HomiePersistValue],
+        Field(None,
+              description='Value for the condition')]
