1604cbcbc6da19f9599ad02a0b7e7a464d5228a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 05:04:53 +0000

    Add package wake-arena-cli/0.0.6/wake_arena_cli-0.0.6.tar.gz

---
+++ packages/wake-arena-cli/wake_arena_cli-0.0.6.tar.gz/wake_arena_cli-0.0.6/api/project_api.py
@@ -0,0 +1,81 @@
+import urllib
+from logging import Logger
+from typing import Optional
+
+import requests
+
+
+class ProjectApiError(Exception):
+    def __init__(self, message: str, code: str):
+        super().__init__(message)
+        self.code = code
+
+
+class ProjectApi:
+    __SERVER_URL = "https://wake-arena-project-api-1076910080992.europe-west3.run.app"
+
+    def __init__(self, logger: Logger, client_id: str, token: str):
+        self.logger = logger
+        self.client_id = client_id
+        self.token = token
+        self.headers = {
+            "Authorization": f"Bearer {token}",
+            "X-Client-Id": str(client_id),
+        }
+
+    def __check_response(self, res: requests.Response):
+        isJson = res.headers.get("content-type").startswith("application/json")
+        body = res.json() if isJson else res.text
+
+        if res.status_code != 200:
+            code = body.get("code") if isJson else res.status_code
+
+            self.logger.error(f"Wake Arena Project API error: {res.status_code}")
+            if code:
+                self.logger.error(f"{code}")
+
+            raise ProjectApiError(
+                f"Wake Arena Project API error: {res.status_code}", code
+            )
+
+        return body
+
+    def create_project(self, project_name: str):
+        body = {"name": project_name}
+        res = requests.post(
+            self.__SERVER_URL + "/api/v0/projects", json=body, headers=self.headers
+        )
+        return self.__check_response(res)
+
+    def get_project(self, project_id: str):
+        res = requests.get(
+            self.__SERVER_URL + f"/api/v0/projects/{project_id}", headers=self.headers
+        )
+        return self.__check_response(res)
+
+    def get_upload_link(self, project_id: str):
+        res = requests.post(
+            self.__SERVER_URL + f"/api/v0/projects/{project_id}/code-upload",
+            headers=self.headers,
+        )
+        return self.__check_response(res)
+
+    def get_vulnerability_check(self, project_id: str, check_id: str):
+        return self.__check_response(
+            requests.get(
+                self.__SERVER_URL + f"/api/v0/projects/{project_id}/checks/{check_id}",
+                headers=self.headers,
+            )
+        )
+
+    def get_vulnerability_check_state_logs(
+        self, project_id: str, check_id: str, last_seen_time: Optional[str] = None
+    ):
+        url = (
+            self.__SERVER_URL + f"/api/v0/projects/{project_id}/checks/{check_id}/logs"
+        )
+
+        if last_seen_time:
+            url += "?" + urllib.parse.urlencode({"lastSeenTime": last_seen_time})
+
+        return self.__check_response(requests.get(url, headers=self.headers))
---
+++ packages/wake-arena-cli/wake_arena_cli-0.0.6.tar.gz/wake_arena_cli-0.0.6/cli/init.py
@@ -0,0 +1,73 @@
+import sys
+from logging import Logger
+
+import rich_click as click
+
+import api
+
+sys.path.append("../cli")
+
+import api.project_api
+import cli.ui as ui
+from config import Config
+
+DEMO_CLIENT_ID = 17
+DEMO_CLIENT_NAME = "demo"
+DEMO_CLIENT_USER = "demo@ackee.xyz"
+DEMO_PROJECT = "4NWbTFse"
+
+
+@click.command("init")
+@click.pass_context
+def init(ctx):
+    logger: Logger = ctx.obj.get("logger")
+    config: Config = ctx.obj.get("config")
+
+    api_key = ui.ask_with_help(
+        title="Wake Arena API key",
+        desc="The API key is needed to authorize the demo version of the CLI",
+        enter="Enter the API key",
+    )
+
+    config.add_client(
+        name=DEMO_CLIENT_NAME,
+        user=DEMO_CLIENT_USER,
+        client_id=DEMO_CLIENT_ID,
+        token=api_key,
+    )
+
+    project = {}
+
+    with ui.spinner("Checking the API key"):
+        try:
+            project_api = api.ProjectApi(
+                logger, client_id=DEMO_CLIENT_ID, token=api_key
+            )
+
+            project = project_api.get_project(DEMO_PROJECT)
+
+            if project.get("id") != DEMO_PROJECT:
+                ui.error("Invalid API key")
+                return
+
+            config.set_active_client(DEMO_CLIENT_NAME)
+            config.set_active_project(DEMO_PROJECT)
+            config.write()
+
+        except api.project_api.ProjectApiError:
+            ui.error(
+                "The API key is invalid, please repeat the init step and input valid API key"
+            )
+            return
+
+    ui.success(
+        title="Successfully initialized! 🎉",
+        lines=[
+            "Current project set to "
+            + ui.highlight(project.get("name"))
+            + f' ({project.get("id")})',
+            "Go to folder with your Contract code and use "
+            + ui.command("check")
+            + " command",
+        ],
+    )
