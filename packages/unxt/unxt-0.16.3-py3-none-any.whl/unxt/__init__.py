a4e51995e2cde4f37ac596d8b494d37bdc1d95aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 06:05:31 +0000

    Add package unxt/0.16.3/unxt-0.16.3-py3-none-any.whl

---
+++ packages/unxt/unxt-0.16.3-py3-none-any.whl/unxt/__init__.py
@@ -0,0 +1,37 @@
+"""unxt: Quantities in JAX.
+
+Copyright (c) 2023 Galactic Dynamics. All rights reserved.
+"""
+
+from . import unitsystems
+from ._src import (
+    experimental,  # noqa: F401
+    quantity,
+)
+from ._src.dimensions.core import dimensions, dimensions_of
+from ._src.quantity import *
+from ._src.units.core import units, units_of
+from ._version import version as __version__
+from .unitsystems import AbstractUnitSystem, unitsystem, unitsystem_of
+
+# isort: split
+from . import _interop  # noqa: F401  # register interop
+
+__all__ = [
+    "__version__",
+    # dimensions
+    "dimensions",
+    "dimensions_of",
+    # units
+    "units",
+    "units_of",
+    # units systems
+    "unitsystems",  # module
+    "AbstractUnitSystem",  # base class
+    "unitsystem",  # convenience constructor
+    "unitsystem_of",  # get the unit system
+]
+__all__ += quantity.__all__
+
+# Clean up namespace
+del quantity
