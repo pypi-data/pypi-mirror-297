d52a6ff9861014d26ed66aeed543481877ffb854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:09:36 +0000

    Add package mrx0w0/1.0.5/mrx0w0-1.0.5.tar.gz

---
+++ packages/mrx0w0/mrx0w0-1.0.5.tar.gz/mrx0w0-1.0.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "funcbymrx/README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.5'
+DESCRIPTION = 'LEARN'
+LONG_DESCRIPTION = 'Learning'
+
+# Setting up
+setup(
+    name="mrx0w0",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
