6b19aea2955fa8b76c08143b34665bfc60a2fcf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 06:05:33 +0000

    Add package scaling-vin-pytorch/0.0.12/scaling_vin_pytorch-0.0.12-py3-none-any.whl

---
+++ packages/scaling-vin-pytorch/scaling_vin_pytorch-0.0.12-py3-none-any.whl/scaling_vin_pytorch/__init__.py
@@ -0,0 +1,8 @@
+from scaling_vin_pytorch.vin import (
+    ScalableVIN,
+    ActionSelector,
+    ValueIteration,
+    DynamicValueIteration,
+    AttentionValueIteration,
+    ValueIterationNetwork
+)
