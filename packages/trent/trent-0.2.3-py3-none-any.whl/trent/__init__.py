8ddc1b3ca84a475ff96303e1ab458ee118ca39de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 04:05:35 +0000

    Add package trent/0.2.3/trent-0.2.3-py3-none-any.whl

---
+++ packages/trent/trent-0.2.3-py3-none-any.whl/trent/__init__.py
@@ -0,0 +1,32 @@
+from .coll import icoll
+from .interface import (
+    cat,
+    catmap,
+    cfilter,
+    cmap,
+    coll,
+    groupcoll,
+    groupmap,
+    map_to_pair,
+    mapcat,
+    pairmap,
+    pmap,
+    pmap_,
+    rangify
+)
+
+__all__ = [
+    'coll',
+    'cmap', 
+    'cfilter',
+    'pmap',
+    'pmap_',
+    'cat',
+    'mapcat',
+    'catmap',
+    'pairmap',
+    'groupmap',
+    'groupcoll',
+    'map_to_pair',
+    'rangify'
+]
