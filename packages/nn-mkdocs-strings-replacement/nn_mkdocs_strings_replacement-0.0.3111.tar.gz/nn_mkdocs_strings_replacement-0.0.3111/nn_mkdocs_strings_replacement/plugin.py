1b5e8f71f3df44d4aae1e3af169b0b55cf66d4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 06:05:11 +0000

    Add package nn-mkdocs-strings-replacement/0.0.3111/nn_mkdocs_strings_replacement-0.0.3111.tar.gz

---
+++ packages/nn-mkdocs-strings-replacement/nn_mkdocs_strings_replacement-0.0.3111.tar.gz/nn_mkdocs_strings_replacement-0.0.3111/nn_mkdocs_strings_replacement/plugin.py
@@ -0,0 +1,14 @@
+from mkdocs.config import base, config_options as c
+from mkdocs.config.defaults import MkDocsConfig
+
+class _OldToNewStrings(base.Config):
+    old_value = c.Type(str)
+    new_value = c.Type(str)
+
+class StringsMarkdownReplacementPluginConfig(base.Config):
+    strings_replacements = c.ListOfItems(c.SubConfig(_OldToNewStrings))
+
+class StringsMarkdownReplacementPlugin(mkdocs.plugins.BasePlugin[StringsMarkdownReplacementPluginConfig]):
+
+    def on_page_markdown(self, markdown: str, config: MkDocsConfig, **kwargs):
+        return markdown.replace('[[_TOSP_]]', '')
