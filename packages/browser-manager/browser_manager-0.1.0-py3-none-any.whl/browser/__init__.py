e97c2862fbfe17af26db34feaabd66c24f3ffd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:10:36 +0000

    Add package browser-manager/0.1.0/browser_manager-0.1.0-py3-none-any.whl

---
+++ packages/browser-manager/browser_manager-0.1.0-py3-none-any.whl/browser/__init__.py
@@ -0,0 +1,7 @@
+from .chrome import Chrome
+from .chromium import Chromium
+from .firefox import Firefox
+from .iexplore import IExplore
+from .msedge import MSEdge
+from .opera import Opera
+from .safari import Safari
