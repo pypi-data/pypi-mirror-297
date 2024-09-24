78d3c2e027ebf327bbcab1e560e03fb7d37d1e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 16:04:54 +0000

    Add package spinsys/0.0.3/spinsys-0.0.3.tar.gz

---
+++ packages/spinsys/spinsys-0.0.3.tar.gz/spinsys-0.0.3/spinsys/data/store.py
@@ -0,0 +1,43 @@
+# Copyright 2024 The Spin Authors. All rights reserved.
+# Use of this source code is governed by the Apache 2.0
+# license that can be found in the LICENSE file.
+
+import dataclasses
+
+import spinsys.spin as spin
+from .entry_blocks import BlockAddress, BlockReference
+
+
+@dataclasses.dataclass
+class StoreRef:
+    address: BlockAddress = ""
+    reference: BlockReference = ""
+    volatile: bool = False
+    expires_at: spin.Time = 0
+
+    @staticmethod
+    def from_json(j: dict):
+        r = StoreRef()
+        r.unmarshal_json(j)
+        return r
+
+    def unmarshal_json(self, j: dict):
+        if "Address" in j:
+            self.address = j["Address"]
+        if "Reference" in j:
+            self.reference = j["Reference"]
+        if "Volatile" in j:
+            self.volatile = j["Volatile"]
+        if "ExpiresAt" in j:
+            self.expires_at = j["ExpiresAt"]
+
+    def to_json(self):
+        j = {}
+        self.marshal_json(j)
+        return j
+
+    def marshal_json(self, j: dict):
+        j["Address"] = self.address
+        j["Reference"] = self.reference
+        j["Volatile"] = self.volatile
+        j["ExpiresAt"] = self.expires_at
