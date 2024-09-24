078abcb2878b4f2cc35254044ba8f5da97c4f7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:06:32 +0000

    Add package ommlx/0.0.0.dev31/ommlx-0.0.0.dev31-py3-none-any.whl

---
+++ packages/ommlx/ommlx-0.0.0.dev31-py3-none-any.whl/ommlx/__about__.py
@@ -0,0 +1,60 @@
+from omlish.__about__ import ProjectBase
+from omlish.__about__ import SetuptoolsBase
+from omlish.__about__ import __version__
+
+
+class Project(ProjectBase):
+    name = 'ommlx'
+    description = 'ommlx'
+
+    dependencies = [
+        f'omdev == {__version__}',
+        f'omlish == {__version__}',
+    ]
+
+    optional_dependencies = {
+        'backends': [
+            'llama-cpp-python ~= 0.2',
+
+            'openai ~= 1.47',
+
+            'sentence_transformers ~= 3.1',
+
+            'transformers ~= 4.44',
+        ],
+
+        'huggingface': [
+            'huggingface-hub ~= 0.25',
+            'datasets ~= 3.0',
+        ],
+
+        'pillow': [
+            'pillow ~= 10.4',
+        ],
+
+        'tinygrad': [
+            # 'tinygrad @ git+https://github.com/tinygrad/tinygrad',
+            'tinygrad ~= 0.9',
+        ],
+
+        'torch': [
+            'torch ~= 2.4',
+        ],
+
+        'wiki': [
+            'mwparserfromhell ~= 0.6',
+
+            'wikitextparser ~= 0.56',  # !! GPL
+        ],
+
+        'xml': [
+            'lxml ~= 5.3; python_version < "3.13"',
+        ],
+    }
+
+
+class Setuptools(SetuptoolsBase):
+    find_packages = {
+        'include': [Project.name, f'{Project.name}.*'],
+        'exclude': [*SetuptoolsBase.find_packages['exclude']],
+    }
