58e958419a2f1f45eaf2a4be54f19b942d185a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 16:04:57 +0000

    Add package libtokit/0.1.3/libtokit-0.1.3.tar.gz

---
+++ packages/libtokit/libtokit-0.1.3.tar.gz/libtokit-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['libtokit']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'libtokit',
+    'version': '0.1.3',
+    'description': 'A simple utility for creating directories.',
+    'long_description': 'None',
+    'author': 'pytools',
+    'author_email': 'hyhlinux@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
