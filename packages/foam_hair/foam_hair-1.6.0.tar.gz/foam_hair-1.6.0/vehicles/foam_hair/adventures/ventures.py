308a312bea7cc4b4a97eaf29af8021fbbbb2385f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:07:15 +0000

    Add package foam_hair/1.6.0/foam_hair-1.6.0.tar.gz

---
+++ packages/foam_hair/foam_hair-1.6.0.tar.gz/foam_hair-1.6.0/vehicles/foam_hair/adventures/ventures.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+'''
+	from foam_hair.adventures.ventures import retrieve_ventures
+	the_ventures = retrieve_ventures ()
+'''
+
+#/
+#
+from .sanique.venture import sanique_venture
+from .demux_hap.venture import demux_hap_venture
+#
+#
+from foam_hair._essence import retrieve_essence
+#
+#
+from ventures import ventures_map
+#
+#\
+
+def retrieve_ventures ():
+	essence = retrieve_essence ()
+
+	return ventures_map ({
+		"map": essence ["ventures"] ["path"],
+		"ventures": [
+			demux_hap_venture (),
+			sanique_venture ()
+		]
+	})
