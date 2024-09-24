a1b51f8173bc0024f3309eb63f2255866e97e18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 15:05:12 +0000

    Add package japper/0.0.21/japper-0.0.21-py3-none-any.whl

---
+++ packages/japper/japper-0.0.21-py3-none-any.whl/japper_devtools/forge/app/views/__init__.py
@@ -0,0 +1,6 @@
+from .home import HomeView
+from .new_project import NewProjectView
+from .run_project import RunProjectView
+from .customize_project import CustomizeProjectView
+from .generate_doc import GenerateDocView
+from .build_project import BuildProjectView
