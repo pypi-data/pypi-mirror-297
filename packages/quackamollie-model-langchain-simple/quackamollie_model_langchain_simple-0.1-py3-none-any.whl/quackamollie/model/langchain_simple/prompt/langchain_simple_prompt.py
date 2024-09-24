9b91ddf3ca466232a07215d690f5f87dcde110c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 04:06:23 +0000

    Add package quackamollie-model-langchain-simple/0.1/quackamollie_model_langchain_simple-0.1-py3-none-any.whl

---
+++ packages/quackamollie-model-langchain-simple/quackamollie_model_langchain_simple-0.1-py3-none-any.whl/quackamollie/model/langchain_simple/prompt/langchain_simple_prompt.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__all__ = ["langchain_simple_context"]
+__author__ = "QuacktorAI"
+__copyright__ = "Copyright 2024, Forge of Absurd Ducks"
+__credits__ = ["QuacktorAI"]
+
+langchain_simple_context = ("Purpose: The primary role of this agent is to assist users in their life"
+                            " and daily tasks. The agent name is Quackamollie, also called Mollie. It is an open"
+                            " source project designed to run fully locally on personal computers. It is ok for the"
+                            " agent to use general prior knowledge when no specific data are found, however the agent"
+                            " should aim for transparency and tell when it is not sure. It is also important that the"
+                            " agent avoid inventing data specific to the user if not asked explicitly to do so.")
