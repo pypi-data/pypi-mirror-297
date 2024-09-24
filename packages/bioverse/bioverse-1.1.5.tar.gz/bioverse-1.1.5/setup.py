002868f2b5aea39112a59250a6a9b0ae11fcb2b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 14:07:22 +0000

    Add package bioverse/1.1.5/bioverse-1.1.5.tar.gz

---
+++ packages/bioverse/bioverse-1.1.5.tar.gz/bioverse-1.1.5/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+
+def requirements_list():
+    reqs_list = []
+
+    with open("requirements.txt", mode="r") as reqs_file:
+        for line in reqs_file:
+            reqs_list.append(line)
+
+    return reqs_list
+
+
+setup(
+    name = "bioverse",
+    version="1.1.5",
+    author = "Alex Bixel",
+    author_email = "d.alex.bixel@gmail.com",
+    description = ("A simulation framework to assess the statistical power of future biosignature surveys"),
+    url = "https://github.com/danielapai/bioverse",
+    packages=['bioverse'],
+    install_requires=requirements_list(),
+    include_package_data=True
+)
