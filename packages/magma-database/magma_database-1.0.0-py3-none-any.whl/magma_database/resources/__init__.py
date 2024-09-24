a45fbbb2d0eaf19e0f3905b591c7dfaf66dd7dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 16:05:13 +0000

    Add package magma-database/1.0.0/magma_database-1.0.0-py3-none-any.whl

---
+++ packages/magma-database/magma_database-1.0.0-py3-none-any.whl/magma_database/resources/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+from importlib_resources import files
+
+volcanoes_df = pd.read_excel(files("magma_database.resources")
+        .joinpath('volcanoes.xlsx'))
