7180b2a48272ddc34ee17286999e4a4e374e1005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 03:10:34 +0000

    Add package crowdsec-service-api/1.30.1/crowdsec_service_api-1.30.1-py3-none-any.whl

---
+++ packages/crowdsec-service-api/crowdsec_service_api-1.30.1-py3-none-any.whl/crowdsec_service_api/services/info.py
@@ -0,0 +1,24 @@
+import json
+from httpx import Auth
+from ..models import *
+from ..base_model import Page, Service
+from pydantic import BaseModel
+from ..http_client import HttpClient
+
+class Info(Service):
+    
+    def get_info(
+        self,
+    )-> InfoResponse:
+        endpoint_url = "/info"
+        loc = locals()
+        headers = {}
+        params = {}
+        path_params = {}
+        
+        response = self.http_client.get(
+            url=endpoint_url, path_params=path_params, params=params, headers=headers
+        )
+        
+        return InfoResponse(**response.json())
+    
