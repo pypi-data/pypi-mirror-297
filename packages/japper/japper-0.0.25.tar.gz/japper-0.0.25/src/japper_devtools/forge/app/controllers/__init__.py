cf697de0121487cacd0bbe9eaadf051eeee57c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 16:05:22 +0000

    Add package japper/0.0.25/japper-0.0.25.tar.gz

---
+++ packages/japper/japper-0.0.25.tar.gz/japper-0.0.25/src/japper_devtools/forge/app/controllers/__init__.py
@@ -0,0 +1,6 @@
+from .home import HomeController
+from .new_project import NewProjectController
+from .run_project import RunProjectController
+from .customize_project import CustomizeProjectController
+from .generate_doc import GenerateDocController
+from .build_project import BuildProjectController
---
+++ packages/japper/japper-0.0.25.tar.gz/japper-0.0.25/src/japper_devtools/forge/app/models/__init__.py
@@ -0,0 +1,6 @@
+from .home import HomeModel
+from .new_project import NewProjectModel
+from .run_project import RunProjectModel
+from .customize_project import CustomizeProjectModel
+from .generate_doc import GenerateDocModel
+from .build_project import BuildProjectModel
