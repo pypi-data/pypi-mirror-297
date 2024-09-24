1356b85ed1f456b0b7708ff06283621481729f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 16:05:01 +0000

    Add package botrun-ask-folder/4.9.241/botrun_ask_folder-4.9.241-py2.py3-none-any.whl

---
+++ packages/botrun-ask-folder/botrun_ask_folder-4.9.241-py2.py3-none-any.whl/botrun_ask_folder/models/reference.py
@@ -0,0 +1,28 @@
+from typing import List
+from pydantic import BaseModel, Field
+
+
+class Reference(BaseModel):
+    file_name: str
+    file_link: str
+    page_numbers: List[int] = Field(default_factory=list)
+    sheet_names: List[str] = Field(default_factory=list)
+
+    def to_md(self) -> str:
+        ref_name = self.file_name
+        if self.page_numbers:
+            pages = ", ".join(map(str, self.page_numbers))
+            ref_name += f" - 第 {pages} 頁"
+        elif self.sheet_names:
+            sheets = ", ".join(self.sheet_names)
+            ref_name += f" - 頁籤：{sheets}"
+        return f"[{ref_name}]({self.file_link})"
+
+
+class References(BaseModel):
+    references: List[Reference] = Field(default_factory=list)
+
+    def to_md(self) -> str:
+        if len(self.references) == 0:
+            return ""
+        return f"參考來源：\n" + "\n".join(ref.to_md() for ref in self.references)
