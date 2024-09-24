b79acfc25afb0227d3d8dee0d0fd97de89a66378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 05:05:14 +0000

    Add package titiler.core/0.18.9/titiler.core-0.18.9-py3-none-any.whl

---
+++ packages/titiler.core/titiler.core-0.18.9-py3-none-any.whl/titiler/core/__init__.py
@@ -0,0 +1,11 @@
+"""titiler.core"""
+
+__version__ = "0.18.9"
+
+from . import dependencies, errors, factory, routing  # noqa
+from .factory import (  # noqa
+    BaseTilerFactory,
+    MultiBandTilerFactory,
+    MultiBaseTilerFactory,
+    TilerFactory,
+)
