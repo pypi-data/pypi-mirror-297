bf1c183bb10d0193c1f8bd4a8f3d81dd6d91f103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-24 06:05:37 +0000

    Add package devservices/0.0.3/devservices-0.0.3-py3-none-any.whl

---
+++ packages/devservices/devservices-0.0.3-py3-none-any.whl/devservices/constants.py
@@ -0,0 +1,4 @@
+from __future__ import annotations
+
+DEVSERVICES_DIR_NAME = "devservices"
+CONFIG_FILE_NAME = "config.yml"
---
+++ packages/devservices/devservices-0.0.3-py3-none-any.whl/devservices/utils/docker_compose.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import subprocess
+
+from devservices.exceptions import DockerComposeError
+
+
+def run_docker_compose_command(command: str) -> subprocess.CompletedProcess[str]:
+    cmd = ["docker", "compose"] + command.split()
+    try:
+        return subprocess.run(cmd, check=True, capture_output=True, text=True)
+    except subprocess.CalledProcessError as e:
+        raise DockerComposeError(
+            command=command,
+            returncode=e.returncode,
+            stdout=e.stdout,
+            stderr=e.stderr,
+        )
